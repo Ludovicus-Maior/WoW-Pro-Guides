190ead748aeef8f555d4ac6d1bb19e3afb4b7042
unknown <kristjin@.(none)>
2010-09-15 22:46:49 -0700

    You gotta push to pull...

--- WoWPro_Leveling/Horde/000_00_00_Kristjin_Working_File.lua
+++ WoWPro_Leveling/Horde/000_00_00_Kristjin_Working_File.lua
@@ -3,34 +3,40 @@ WoWPro_Leveling:RegisterGuide("KriWrkFil", "WORKING: CATA Gob 1-12", "Kristjin.W
 -->
 --> Last modified by [Pretzl] 6:52 AM 9/14/2010
 return [[
-A Taking Care of Business|QID|14138|N|From Sassy Hardwrench|M|56.4,76.9|Z|Kezan|
+A Taking Care of Business|QID|14138|N|From Sassy Hardwrench|M|56.4,76.9|
 
-T Taking Care of Business|QID|14138|Head outside. You should see Foreman Dampwick down the path just a few feet and to the left. Turn in your quest to him and pick up the follow-ups.|M|60.2,74.6|Z|Kezan|
-A Trouble In The Mines|QID|14075|N|From Foreman Dampwick.|M|60.2,74.6|Z|Kezan|
-A Good Help is Hard to Find|QID|14069|N|From Foreman Dampwick.|M|60.2,74.6|Z|Kezan|
+T Taking Care of Business|QID|14138|N|Head outside. You should see Foreman Dampwick down the path just a few feet and to the left. Turn in your quest to him and pick up the follow-ups.|M|60.2,74.6|
+A Trouble In The Mines|QID|14075|N|From Foreman Dampwick.|M|60.2,74.6|
+A Good Help is Hard to Find|QID|14069|N|From Foreman Dampwick.|M|60.2,74.6|
 
-C Trouble in the Mines|QID|14075|S|N|Head toward the mines, down the hill to the east. In this area, right-click red Defiant Trolls to "adjust their attitudes" and kill Tunnel Worms inside the caves.|
-C Good Help is Hard to Find|QID|14069|
+C Good Help is Hard to Find|QID|14069|N|Right-click the red Defiant Trolls to "adjust their attitudes."|S|
+C Trouble in the Mines|QID|14075|M|64.6,88.8|N|Kill Tunnel Worms inside the caves.|
+C Good Help is Hard to Find|QID|14069|M|69.82|N|Right-click the red Defiant Trolls to "adjust their attitudes."|US|
 
-T Trouble in the Mines|QID|14075|US|N|Foreman Dampwick|M|60.2,74.6|Z|Kezan|
-T Good Help is Hard to Find|QID|14069|N|Foreman Dampwick|M|60.2,74.6|Z|Kezan|
-A Kaja'Cola|N|Foreman Dampwick|M|60.2,74.6|Z|Kezan|
 
-C Kaja'Cola|N|Head back down by the mines.  The cola looks like litle soda cans.  Pick up 5.|
 
-T Kaja'Cola|N|Sassy Hardwrench|M|56.4,76.9|Z|Kezan|
-A Do It Yourself||N|Sassy Hardwrench|M|56.4,76.9|Z|Kezan|
-A Off to the Bank|N|Sassy Hardwrench|M|56.4,76.9|Z|Kezan|
-A Cruising|QID|14071|N|Sassy Hardwrench|M|56.4,76.9|Z|Kezan|
-A Report for Tryouts|N|Sassy Hardwrench|M|56.4,76.9|Z|Kezan|
+T Trouble in the Mines|QID|14075|N|Foreman Dampwick|M|60.2,74.6|
+T Good Help is Hard to Find|QID|14069|N|Foreman Dampwick|M|60.2,74.6|
+A Kaja'Cola|QID|25473|N|Foreman Dampwick|M|60.2,74.6|
+
+C Kaja'Cola|QID|25473|N|Head back down by the mines.  The cola looks like litle soda cans.  Pick up 5.|
+
+T Kaja'Cola|QID|25473|N|Sassy Hardwrench|M|56.4,76.9|
+A Do It Yourself|QID|14070|N|Sassy Hardwrench|M|56.4,76.9|
+A Off to the Bank|QID|26712|N|Sassy Hardwrench|M|56.4,76.9|
+A Cruising|QID|14071|N|Sassy Hardwrench|M|56.4,76.9|
+A Report for Tryouts|N|Sassy Hardwrench|M|56.4,76.9|
 
 R Kajaro Field|N|Put the keys to the Hot Rod on an easily accessible button.  You'll be using it to get around from now on.  Get in your car now by using the keys.  Drive through the gate on the north side of the courtyard outside KTC Headquarters.  Instead of taking the roads, you can immediately turn left, and drive over the cliffs down to the field.
 
-C Cruising|QID|14071|N|Pick up Gobber at the field.|QO|Gobber picked up: 1/1|M|48.8,57.6|Z|Kezan|
-T Report for Tryouts|QID|24567|N|Coach Crosscheck|M|48.8,57.6|Z|Kezan|
-A The Replacements|QID|24488|N|Coach Crosscheck|M|48.8,57.6|Z|Kezan|
+C Cruising|QID|14071|N|Pick up Gobber at the field.|QO|Gobber picked up: 1/1|M|48.8,57.6|
+T Report for Tryouts|QID|24567|N|Coach Crosscheck|M|48.8,57.6|
+A The Replacements|QID|24488|N|Coach Crosscheck|M|48.8,57.6|
 
-R Off to the Bank|QID|26712|Now we're headed to the bank.  Follow the road West from the field to get there, picking up replacement parts along the way. You don't even need to leave the vehicle to pick them up!|
+R Off to the Bank|QID|26712|N|Now we're headed to the bank.  Follow the road West from the field to get there, picking up replacement parts along the way. You don't even need to leave the vehicle to pick them up!|
+
+T Off to the Bank|QID|26712|N|Enter the bank from the north. Plowing the long line of gobs standing there is loads of fun.|
+A The New You|QID|14109|
 
 
 
