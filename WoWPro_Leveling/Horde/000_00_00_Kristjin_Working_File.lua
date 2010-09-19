18fff57e4f1176d84349d3de9298a721750a2938
unknown <kristjin@.(none)>
2010-09-18 21:53:30 -0700

    Just current updates...

--- WoWPro_Leveling/Horde/000_00_00_Kristjin_Working_File.lua
+++ WoWPro_Leveling/Horde/000_00_00_Kristjin_Working_File.lua
@@ -1,4 +1,4 @@
-WoWPro_Leveling:RegisterGuide("KriWrkFil", "WORKING: CATA Gob 1-12", "Kristjin.WORKING", "00", "00", "Nil", "Horde", function()
+WoWPro_Leveling:RegisterGuide("KriWrkFil", "WORKING: Archaeology", "WORKING", "00", "00", "Nil", "Horde", function()
 --> Original guides written/designed by [Jame]
 -->
 --> Last modified by [Pretzl] 6:52 AM 9/14/2010
---
+++ WoWPro_Leveling/Horde/000_1_05_Manovan_Troll_Starter.lua
@@ -0,0 +1,151 @@
+WoWPro_Leveling:RegisterGuide("ManDar0105", "Darkspear Shore", "Manovan", "01", "05", "PreDar0512", "Horde", function()
+--> Original guides written/designed by [Manovan]
+-->
+--> Last modified by [Pretzl] 4:20 PM 9/16/2010
+return [[
+
+A The Rise of the Darkspear |QID|24776|C|Hunter| |N|From Jin'thala.|
+A The Rise of the Darkspear |QID|24770|C|Rogue| |N|From Jin'thala.|
+A The Rise of the Darkspear |QID|24764|C|Druid| |N|From Jin'thala.|
+A The Rise of the Darkspear |QID|24758|C|Shaman| |N|From Jin'thala.|
+A The Rise of the Darkspear |QID|24750|C|Mage| |N|From Jin'thala.|
+A The Rise of the Darkspear |QID|24782|C|Priest| |N|From Jin'thala.|
+A The Rise of the Darkspear |QID|24607|C|Warrior| |N|From Jin'thala.|
+A The Rise of the Darkspear |QID|26272|C|Warlock| |N|From Jin'thala.|
+
+T The Rise of the Darkspear |QID|24776|C|Hunter| |N|Turn in your class quest in the training grounds at Ortezza.|
+T The Rise of the Darkspear |QID|24770|C|Rogue| |N|Turn in your class quest in the training grounds at Legati.|
+T The Rise of the Darkspear |QID|24764|C|Druid| |N|Turn in your class quest in the training grounds at Zen'Tabra.|
+T The Rise of the Darkspear |QID|24758|C|Shaman| |N|Turn in your class quest in the training grounds at Nekali.|
+T The Rise of the Darkspear |QID|24750|C|Mage| |N|Turn in your class quest in the training grounds at Soratha.|
+T The Rise of the Darkspear |QID|24782|C|Priest| |N|Turn in your class quest in the training groundsat Tunari.|
+T The Rise of the Darkspear |QID|24607|C|Warrior| |N|Turn in your class quest in the training groundsat Nortet.|
+T The Rise of the Darkspear |QID|26272|C|Warlock| |N|Turn in your class quest in the training grounds at Voldreka.|
+
+A The Basics: Hitting Things |QID|24777|C|Hunter|N|From Ortezza|M|67.09,83.30
+A The Basics: Hitting Things |QID|24771|C|Rogue|
+A The Basics: Hitting Things |QID|24765|C|Druid|
+A The Basics: Hitting Things |QID|24759|C|Shaman|
+A The Basics: Hitting Things |QID|24751|C|Mage|
+A The Basics: Hitting Things |QID|24783|C|Priest|
+A The Basics: Hitting Things |QID|24639|C|Warrior|
+A The Basics: Hitting Things |QID|26273|C|Warlock|
+
+C The Basics: Hitting Things |QID|24777|C|Hunter| |N|Destroy 6 Tiki Targets in the training grounds.|
+C The Basics: Hitting Things |QID|24771|C|Rogue| |N|Destroy 6 Tiki Targets in the training grounds.|
+C The Basics: Hitting Things |QID|24765|C|Druid| |N|Destroy 6 Tiki Targets in the training grounds.|
+C The Basics: Hitting Things |QID|24759|C|Shaman| |N|Destroy 6 Tiki Targets in the training grounds.|
+C The Basics: Hitting Things |QID|24751|C|Mage| |N|Destroy 6 Tiki Targets in the training grounds.|
+C The Basics: Hitting Things |QID|24783|C|Priest| |N|Destroy 6 Tiki Targets in the training grounds.|
+C The Basics: Hitting Things |QID|24639|C|Warrior| |N|Destroy 6 Tiki Targets in the training grounds.|
+C The Basics: Hitting Things |QID|26273|C|Warlock| |N|Destroy 6 Tiki Targets in the training grounds.|
+
+T The Basics: Hitting Things |QID|24777|C|Hunter| |N|Turn in your class quest in the training grounds at Ortezza.|M|67.09,83.30
+T The Basics: Hitting Things |QID|24771|C|Rogue| |N|Turn in your class quest in the training grounds at Legati.|
+T The Basics: Hitting Things |QID|24765|C|Druid| |N|Turn in your class quest in the training grounds at Zen'Tabra.|
+T The Basics: Hitting Things |QID|24759|C|Shaman| |N|Turn in your class quest in the training grounds at Nekali.|
+T The Basics: Hitting Things |QID|24751|C|Mage| |N|Turn in your class quest in the training grounds at Soratha.|
+T The Basics: Hitting Things |QID|24783|C|Priest| |N|Turn in your class quest in the training groundsat Tunari.|
+T The Basics: Hitting Things |QID|24639|C|Warrior| |N|Turn in your class quest in the training groundsat Nortet.|
+T The Basics: Hitting Things |QID|26273|C|Warlock| |N|Turn in your class quest in the training grounds at Voldreka.|
+
+A A Rough Start |QID|24779|C|Hunter|
+A A Rough Start |QID|24773|C|Rogue|
+A A Rough Start |QID|24767|C|Druid|
+A A Rough Start |QID|24761|C|Shaman|
+A A Rough Start |QID|24753|C|Mage|
+A A Rough Start |QID|24785|C|Priest|
+A A Rough Start |QID|24641|C|Warrior|
+A A Rough Start |QID|26275|C|Warlock|
+
+C A Rough Start |QID|24779|C|Hunter| |N|Kill Wildmane Cats for 6 Wildmane Cat Pelts.|
+C A Rough Start |QID|24773|C|Rogue| |N|Kill Wildmane Cats for 6 Wildmane Cat Pelts.|
+C A Rough Start |QID|24767|C|Druid| |N|Kill Wildmane Cats for 6 Wildmane Cat Pelts.|
+C A Rough Start |QID|24761|C|Shaman| |N|Kill Wildmane Cats for 6 Wildmane Cat Pelts.|
+C A Rough Start |QID|24753|C|Mage| |N|Kill Wildmane Cats for 6 Wildmane Cat Pelts.|
+C A Rough Start |QID|24785|C|Priest| |N|Kill Wildmane Cats for 6 Wildmane Cat Pelts.|
+C A Rough Start |QID|24641|C|Warrior| |N|Kill Wildmane Cats for 6 Wildmane Cat Pelts.|
+C A Rough Start |QID|26275|C|Warlock| |N|Kill Wildmane Cats for 6 Wildmane Cat Pelts.|
+
+T A Rough Start |QID|24779|C|Hunter| |N|Turn in your class quest in the training grounds at Ortezza.|
+T A Rough Start |QID|24773|C|Rogue| |N|Turn in your class quest in the training grounds at Legati.|
+T A Rough Start |QID|24767|C|Druid| |N|Turn in your class quest in the training grounds at Zen'Tabra.|
+T A Rough Start |QID|24761|C|Shaman| |N|Turn in your class quest in the training grounds at Nekali.|
+T A Rough Start |QID|24753|C|Mage| |N|Turn in your class quest in the training grounds at Soratha.|
+T A Rough Start |QID|24785|C|Priest| |N|Turn in your class quest in the training groundsat Tunari.|
+T A Rough Start |QID|24641|C|Warrior| |N|Turn in your class quest in the training groundsat Nortet.|
+T A Rough Start |QID|26275|C|Warlock| |N|Turn in your class quest in the training grounds at Voldreka.|
+
+A Proving Pit |QID|24780|C|Hunter|
+A Proving Pit |QID|.....|C|Rogue|
+A Proving Pit |QID|24768|C|Druid|
+A Proving Pit |QID|24762|C|Shaman|
+A Proving Pit |QID|.....|C|Mage|
+A Proving Pit |QID|.....|C|Priest|
+A Proving Pit |QID|.....|C|Warrior|
+A Proving Pit |QID|.....|C|Warlock|
+
+C Proving Pit |QID|24780|C|Hunter| |N|Fight a naga in the proving pit.|
+C Proving Pit |QID|.....|C|Rogue| |N|Fight a naga in the proving pit.|
+C Proving Pit |QID|24768|C|Druid| |N|Fight a naga in the proving pit.|
+C Proving Pit |QID|24762|C|Shaman| |N|Fight a naga in the proving pit.|
+C Proving Pit |QID|.....|C|Mage| |N|Fight a naga in the proving pit.|
+C Proving Pit |QID|.....|C|Priest| |N|Fight a naga in the proving pit.|
+C Proving Pit |QID|.....|C|Warrior| |N|Fight a naga in the proving pit.|
+C Proving Pit |QID|.....|C|Warlock| |N|Fight a naga in the proving pit.|
+
+T Proving Pit |QID|24780|C|Hunter| |N|Turn in your class quest in the training grounds at Ortezza.|
+T Proving Pit |QID|.....|C|Rogue| |N|Turn in your class quest in the training grounds at Legati.|
+T Proving Pit |QID|24768|C|Druid| |N|Turn in your class quest in the training grounds at Zen'Tabra.|
+T Proving Pit |QID|24762|C|Shaman| |N|Turn in your class quest in the training grounds at Nekali.|
+T Proving Pit |QID|.....|C|Mage| |N|Turn in your class quest in the training grounds at Soratha.|
+T Proving Pit |QID|.....|C|Priest| |N|Turn in your class quest in the training groundsat Tunari.|
+T Proving Pit |QID|.....|C|Warrior| |N|Turn in your class quest in the training groundsat Nortet.|
+T Proving Pit |QID|.....|C|Warlock| |N|Turn in your class quest in the training grounds at Voldreka.|
+
+A More Than Expected |QID|24781|C|Hunter|
+A More Than Expected |QID|.....|C|Rogue|
+A More Than Expected |QID|24769|C|Druid|
+A More Than Expected |QID|24763|C|Shaman|
+A More Than Expected |QID|.....|C|Mage|
+A More Than Expected |QID|.....|C|Priest|
+A More Than Expected |QID|.....|C|Warrior|
+A More Than Expected |QID|.....|C|Warlock|
+
+T More Than Expected |QID|24787||N|Meet up with Vol'jin inside the main building overlooking the sea in the village to the south-east.|
+A Moraya |QID|25064|
+N Watch the vision in the fire.|
+A Crab Fishin' |QID|25037|
+T Moraya |QID|25064|
+A A Troll's Truest Companion |QID|24622|
+
+T A Troll's Truest Companion |QID|24622|N|At Bloodtalon Shore.  Turn into Kijara.|
+A Saving The Young |QID|24623|N|Kijara|
+A Mercy For The Lost |QID|24624|N|Tegashi|
+A Consort of the Sea Witch |QID|24625|N|Tegashi|
+N Things to do |QID|25037| |N|Kill crabs for 6 Fresh Crawler Meat, Kill 8 Corrupted Bloodtalon Raptors and use your whistle when close to the raptor babies, you may stop once you have saved 12.|
+
+K Naj'tess |QID|24625|L|50018| |N|Up the small hill in the middle of the island where you're killing raptors. Loot the Naj'tess' Orb of Corruption|
+T Consort of the Sea Witch |QID|24625| |N|At Bloodtalon Shore.|
+A Young And Vicious |QID|24626|
+C Young And Vicious |QID|24626| |N|Use the lasso to capture a raptor called Swiftclaw who is running around the island. |
+T Young And Vicious |QID|24626|
+T Crab Fishin' |QID|25037|
+A Breaking The Line |QID|25035| |N|Outside Vol'jin's building.|
+N Speak to Jornun |QID|25035| |N|You will automatically mount on a raptor and take you will automatically ride to Spitescale Cove.|
+T Breaking The Line |QID|25035|
+A No More Mercy |QID|24812|
+A Territorial Fetish |QID|24813| |N|Inside the Cave.|
+C No More Mercy |QID|24812|
+C Territorial Fetish |QID|24813|
+
+T No More Mercy |QID|24812| |N|At the Entrance of the Cave.|
+T Territorial Fetish |QID|24813|
+A An Ancient Enemy |QID|24814|
+N Speak to Vol'jin |QID|25035| |N|You are going to kill an NPC named Zar'jira with the help of NPC's. Go to Next step once you have killed this boss.|
+N Speak to Vanira |QID|25035| |N|She will teleport you back to Darkspear Hold.|
+T An Ancient Enemy |QID|24814|
+A Sen'jin Village  |QID|25073|
+
+]]
+end)
