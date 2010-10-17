0ec3e06beb1c0739ed18e4d1c43ffc814f515735
Jiyambi <sarah.ann.herzog@gmail.com>
2010-10-16 22:03:46 -0700

    Somehow these got lost, adding them back in. Updates to Horde Vash'jir

---
+++ WoWPro_Leveling/Horde/002_80_82_Arixan_Vashjir.lua
@@ -0,0 +1,58 @@
+WoWPro_Leveling:RegisterGuide("AriVas8082", "Vash'jir", "Arixan", "80", "82", "AriHyj8082", "Horde", function() 
+return [[
+
+T Warchief's Command: Vashj'ir!|QID|27718|M|55.84,12.29|Z|Durotar|N|To Commander Thorak.|
+A Call of Duty|QID|25924|M|55.84,12.29|Z|Durotar|N|From Commander Thorak.|
+C Call of Duty|QID|25924|NC|M|45.79,33.82|Z|Kelp'thar Forest|
+T Call of Duty|QID|25924|M|38.83,31.62|Z|Kelp'thar Forest|N|To Erunak Stonespeaker.|
+A Sea Legs|QID|25929|M|38.83,31.62|Z|Kelp'thar Forest|N|From Erunak Stonespeaker.|
+h The Immortal Coil|QID|25929|M|38.83,31.62|Z|Kelp'thar Forest|N|At Erunak Stonespeaker.|
+C Sea Legs|QID|25929|M|42.23,31.61|Z|Kelp'thar Forest|
+T Sea Legs|QID|25929|M|38.81,31.72|Z|Kelp'thar Forest|N|To Erunak Stonespeaker.|
+A Pay It Forward|QID|25936|M|38.81,31.72|Z|Kelp'thar Forest|N|From Erunak Stonespeaker.|
+C Pay It Forward|QID|25936|U|56227|M|40.58,26.68|Z|Kelp'thar Forest|
+T Pay It Forward|QID|25936|M|38.79,31.78|Z|Kelp'thar Forest|N|To Erunak Stonespeaker.|
+A Rest For the Weary|QID|25941|M|38.79,31.78|Z|Kelp'thar Forest|N|From Erunak Stonespeaker.|
+T Rest For the Weary|QID|25941|M|38.97,32.10|Z|Kelp'thar Forest|N|To Legionnaire Nazgrim.|
+A Buy Us Some Time|QID|25942|M|38.97,32.10|Z|Kelp'thar Forest|N|From Legionnaire Nazgrim.|
+A Traveling on Our Stomachs|QID|25943|M|38.97,32.10|Z|Kelp'thar Forest|N|From Legionnaire Nazgrim.|
+C Traveling on Our Stomachs|QID|25943|S|M|40.68,28.84|Z|Kelp'thar Forest|
+C Buy Us Some Time|QID|25942|M|40.86,34.14|Z|Kelp'thar Forest|
+T Buy Us Some Time|QID|25942|M|38.99,32.05|Z|Kelp'thar Forest|N|To Legionnaire Nazgrim.|
+T Traveling on Our Stomachs|QID|25943|M|38.99,32.05|Z|Kelp'thar Forest|N|To Legionnaire Nazgrim.|
+A Girding Our Loins|QID|25944|M|38.99,32.05|Z|Kelp'thar Forest|N|From Legionnaire Nazgrim.|
+T Helm's Deep|QID|25946|M|38.97,32.03|Z|Kelp'thar Forest|N|To Legionnaire Nazgrim.|
+A Finders, Keepers|QID|25947|M|38.97,32.03|Z|Kelp'thar Forest|N|From Legionnaire Nazgrim.|
+C Girding Our Loins|QID|25944|M|36.15,28.07|Z|Kelp'thar Forest|
+C Once More, With Eeling|QID|27729|M|49.76,22.20|Z|Kelp'thar Forest|
+C Finders, Keepers|QID|25947|M|50.42,27.70|Z|Kelp'thar Forest|
+T Girding Our Loins|QID|25944|M|39.04,32.15|Z|Kelp'thar Forest|N|To Legionnaire Nazgrim.|
+T Finders, Keepers|QID|25947|M|39.04,32.15|Z|Kelp'thar Forest|N|To Legionnaire Nazgrim.|
+T Bring It On!|QID|25948|M|38.80,31.48|Z|Kelp'thar Forest|N|To Legionnaire Nazgrim.|
+A Blood and Thunder!|QID|25949|M|38.80,31.48|Z|Kelp'thar Forest|N|From Legionnaire Nazgrim.|
+C Blood and Thunder!|QID|25949|M|46.26,46.73|Z|Kelp'thar Forest|
+T Blood and Thunder!|QID|25949|M|46.34,46.81|Z|Kelp'thar Forest|N|To Erunak Stonespeaker.|
+A Better Late Than Dead|QID|25477|M|46.14,46.80|Z|Kelp'thar Forest|N|From Moanah Stormhoof.|
+C Better Late Than Dead|QID|25477|U|54462|M|48.94,42.35|Z|Kelp'thar Forest|
+T Better Late Than Dead|QID|25477|M|46.06,46.83|Z|Kelp'thar Forest|N|To Moanah Stormhoof.|
+A The Abyssal Ride|QID|25371|M|46.06,46.83|Z|Kelp'thar Forest|N|From Moanah Stormhoof.|
+C The Abyssal Ride|QID|25371|M|45.81,46.50|Z|Kelp'thar Forest|
+T The Abyssal Ride|QID|25371|M|46.09,46.78|Z|Kelp'thar Forest|N|To Moanah Stormhoof.|
+A Good Deed Left Undone|QID|27685|M|46.50,46.76|Z|Kelp'thar Forest|N|From Rendel Firetongue.|
+T Good Deed Left Undone|QID|27685|M|53.50,43.14|Z|Kelp'thar Forest|N|To Adarrah.|
+A Gimme Shelter!|QID|25587|M|53.50,43.14|Z|Kelp'thar Forest|N|From Adarrah.|
+f Smuggler's Scar|QID|25587|M|56.24,31.23|Z|Kelp'thar Forest|N|At Swift Seahorse.|
+C Gimme Shelter!|QID|25587|M|53.67,34.32|Z|Kelp'thar Forest|
+T Gimme Shelter!|QID|25587|M|57.09,28.87|Z|Kelp'thar Forest|N|To Adarrah.|
+A Ain't Too Proud to Beg|QID|25598|M|57.09,28.87|Z|Kelp'thar Forest|N|From Adarrah.|
+A A Case of Crabs|QID|25388|M|49.82,40.84|Z|Kelp'thar Forest|N|From Clacksnap Pincer.|
+C Ain't Too Proud to Beg|QID|25598|M|48.23,39.49|Z|Kelp'thar Forest|
+A The Horde's Hoard|QID|25377|
+T Ain't Too Proud to Beg|QID|25598|M|57.23,28.77|Z|Kelp'thar Forest|N|To Adarrah.|
+T A Case of Crabs|QID|25388|M|57.23,28.77|Z|Kelp'thar Forest|N|To Adarrah.|
+A A Girl's Best Friend|QID|25390|M|57.18,28.74|Z|Kelp'thar Forest|N|From Adarrah.|
+A A Taste For Tail|QID|25389|M|57.18,28.74|Z|Kelp'thar Forest|N|From Adarrah.|
+A Can't Start a Fire Without a Spark|QID|25602|M|57.25,28.86|Z|Kelp'thar Forest|N|From Captain Samir.|
+]]
+
+end)
