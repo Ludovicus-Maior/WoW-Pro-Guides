06d8477c8550b6072ccf31649b12658a10cc9c43
tarix <mikem@tarix.net>
2010-10-19 07:11:00 +0900

    Mount Hyjal - First Quest Circuit

---
+++ WoWPro_Leveling/Horde/002_80_82_Arixan_Mt_Hyjal.lua
@@ -0,0 +1,83 @@
+WoWPro_Leveling:RegisterGuide("AriMtH8082", "Mount Hyjal", "Arixan", "80", "82", "nil", "Horde", function()
+return [[
+
+N Mt. Hyjal |N|There are places in this guide you will need a flying mount so be sure you have trained flying before you leave Orgrimmar.|
+R Orgrimmar |QID|27721|Z|Orgrimmar|N|Make your way to Orgimmar.|
+
+A Warchief's Command: Mount Hyjal |QID|27721|M|49.7,76.7|Z|Orgirmmar|N|Get the quest from the Warchief's Command Board.|
+
+N Talk to Cenarion Emissary Blackhoof |QID|27721|M|40.0,50.5|Z|Orgrimmar|N|In the Valley of Wisdom.|
+T Warchief's Command: Mount Hyjal |QID|27721|M|45.5,44.7|Z|Moonglade|N|At Emissary Windsong.|
+A As Hyjal Burns |QID|25316|M|45.5,44.7|Z|Moonglade|
+
+N Ride Aronus |QID|25316|M|46.0,45.1|Z|Moonglade|N|Ride on Aronus to get to Mount Hyjal.|
+T As Hyjal Burns |QID|25316|M|62.1,24.8|N|Inside the building at Ysera.|
+A Protect the World Tree |QID|25317|M|62.1,24.8|
+
+h Nordrassil |QID|25317|M|63.0,24.1|N|At Sebelia next to the door.|
+
+A The Earth Rises |QID|25460|M|63.9,22.7|N|From Anren Shadowseeker.|
+A Inciting the Elements |QID|25370|M|64.0,22.5|N|From Tholo Whitehoof.|
+
+N Inciting the Elements |QID|25370|L|53009 4|N|Loot 4 Juniper Berries.|S|
+C The Earth Rises |QID|25460|M|64.8,22.5|S|
+C Inciting the Elements |QID|25370|M|64.8,22.5|N|Use the Berries on Faerie Dragons and then kill the Twilight Inciter.|
+C The Earth Rises |QID|25460|M|64.8,22.5|US|
+
+T The Earth Rises |QID|25460|M|63.9,22.7|N|Back at Anren Shadowseeker.|
+T Inciting the Elements |QID|25370|M|64.0,22.5|N|At Tholo Whitehoof.|
+A Flames from Above |QID|25574|M|64.0,22.5|
+
+f Nordrassil, Hyjal |QID|25317|M|62.2,21.6|Follow the road west to Flight Master Fayran Elthas.|
+
+C Flames from Above |QID|25574|M|55.8,15.3|N|Use Tholo's Horn in the middle of the Twilight encampment.|
+T Flames from Above |QID|25574|M|64.0,22.5|N|Back at Tholo Whitehoof.|
+
+T Protect the World Tree |QID|25317|M|47.7,35.4|N|Fly or follow the road to Malfurion Stormrage.|
+A War on the Twilight's Hammer |QID|25319|M|47.7,35.4|
+A The Flameseer's Staff |QID|25472|M|47.7,35.4|N|From Windseeker Tamila|
+
+C The Flameseer's Staff |QID|25472|M|48.2,30.6|S|
+C War on the Twilight's Hammer |QID|25319|M|48.2,30.6|
+C The Flameseer's Staff |QID|25472|M|48.2,30.6|US
+
+T The Flameseer's Staff |QID|25472|M|47.7,35.4|N|Back at Malfurion Stormrage.|
+T War on the Twilight's Hammer |QID|25319|M|47.7,35.4|
+A Flamebreaker |QID|25323|M|47.7,35.4|
+
+C Flamebreaker |QID|25323|M|48.2,29.5;47.5,21.5|N|Use the Flameseer's Staff on the Blazebound Elementals in the charred areas around The Verdant Thicket. This will break up the elementals into 8-10 smaller elementals. Kill these Unbound Flame Spirits.|
+
+T Flamebreaker |QID|25323|M|47.7,35.4|N|Back at Malfurion Stormrage.|
+A The Return of Baron Geddon |QID|25464|M|47.7,35.4|
+
+C The Return of Baron Geddon |QID|25464|M|44.1,27.0|N|Go The Circle of Cinders, where you will find Baron Geddon. You need to use the Flameseer's Staff  on him. Every 10 seconds or so, he will begin to channel an AOE fire effect that will hurt you badly, forcing you to move away. When the fire dies down, run back in and use the staff again.|
+N The Revenge of Baron Geddon |QID|25464|N|As soon as the quest is complete, Baron Geddon will cast Living Bomb on you. After about 10 seconds, you will blow up, causing you to fly into the air (you will take some falling damage) and dealing damage to anyone around you. So get away from everyone until it goes off.|
+
+T The Return of Baron Geddon |QID|25464|M|47.7,35.4|N|Back at Malfurion Stormrage.|
+A Emerald Allies |QID|25430|M|47.7,35.4|N|From Windspeaker Tamila.|
+
+T Emerald Allies |QID|25430|M|48.5,19.0|N|At Alysra an Emerald Flight dragon.|
+A The Captured Scout |QID|25320|M|48.5,19.0|
+
+T The Captured Scout |QID|25320|M|44.5,18.8|N|Scout Larandia is located in a cage in the back of the Twilight Command Post.|
+A Twilight Captivity |QID|25321|M|44.5,18.8|
+
+C Twilight Captivity |QID|25321|M|44.3,20.3|Kill the Overseer's for the key.|
+
+T Twilight Captivity |QID|25321|M|44.5,18.8|N|Back at Scout Larandia.|
+A Return to Alysra |QID|25424|M|44.5,18.8|
+
+T Return to Alysra |QID|25424|M|48.5,19.0|N|Back to Alysra.|
+A A Prisoner of Interest |QID|25324|M|48.5,19.0|
+
+T A Prisoner of Interest |QID|25324|M|52.6,17.3;56.7,18.8|N|At Captain Saynna Stormrunner in the Hyjal Barrow Dens.|
+A Through the Dream |QID|25325|M|56.7,18.8|
+
+C Through the Dream |QID|25325|M|52.6,17.3|N|Fight your way out of the barrow dens.|
+T Through the Dream |QID|25325|M|52.1,17.4|N|At Alysra in front of you.|
+A Return to Nordrassil |QID|25578|M|52.1,17.4|
+
+T Return to Nordrassil |QID|25578|M|62.1,24.8|N|At Ysera back in Nordrassil.|
+
+]]
+end)
