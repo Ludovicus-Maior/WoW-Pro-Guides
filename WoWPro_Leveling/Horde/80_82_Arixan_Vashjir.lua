d18849250efde979ef32a4dcbf2608222e594ebb
tarix <mikem@tarix.net>
2010-11-03 13:36:22 +0900

    Horde/Vashjir (80-82) - 33% Complete

--- WoWPro_Leveling/Horde/80_82_Arixan_Vashjir.lua
+++ WoWPro_Leveling/Horde/80_82_Arixan_Vashjir.lua
@@ -1,7 +1,8 @@
-WoWPro_Leveling:RegisterGuide("AriVas8082", "Vash'jir", "Arixan", "80", "82", "AriHyj8082", "Horde", function() 
+WoWPro_Leveling:RegisterGuide('AriVas8082', 'Vashjir', 'Arixan', '80', '82', 'AriHyj8082', 'Horde', function() 
 return [[
 
-T Warchief's Command: Vashj'ir!|QID|27718|M|55.84,12.29|Z|Durotar|N|To Commander Thorak.|
+A Warchief's Command: Vashj'ir!|QID|27718|M|49.67,76.45|Z|Orgrimmar|N|From Warchief's Command Board.|
+T Warchief's Command: Vashj'ir!|QID|27718|M|55.84,12.29|Z|Durotar|N|To Commander Thorak inside the tower.|
 A Call of Duty|QID|25924|M|55.84,12.29|Z|Durotar|N|From Commander Thorak.|
 C Call of Duty|QID|25924|NC|M|45.79,33.82|Z|Kelp'thar Forest|
 T Call of Duty|QID|25924|M|38.83,31.62|Z|Kelp'thar Forest|N|To Erunak Stonespeaker.|
@@ -16,43 +17,69 @@ A Rest For the Weary|QID|25941|M|38.79,31.78|Z|Kelp'thar Forest|N|From Erunak St
 T Rest For the Weary|QID|25941|M|38.97,32.10|Z|Kelp'thar Forest|N|To Legionnaire Nazgrim.|
 A Buy Us Some Time|QID|25942|M|38.97,32.10|Z|Kelp'thar Forest|N|From Legionnaire Nazgrim.|
 A Traveling on Our Stomachs|QID|25943|M|38.97,32.10|Z|Kelp'thar Forest|N|From Legionnaire Nazgrim.|
-C Traveling on Our Stomachs|QID|25943|S|M|40.68,28.84|Z|Kelp'thar Forest|
-C Buy Us Some Time|QID|25942|M|40.86,34.14|Z|Kelp'thar Forest|
+C Traveling on Our Stomachs|QID|25943|S|M|40.68,28.84|Z|Kelp'thar Forest|N|Kill Splitclaw Skitterers until you get 8 Succulent Crab Meat.|
+C Buy Us Some Time|QID|25942|M|40.86,34.14|Z|Kelp'thar Forest|N|Zin'Jatari Raiders in the area above and around the ship.|
+C Traveling on Our Stomachs|QID|25943|US|M|40.68,28.84|Z|Kelp'thar Forest|N|Kill Splitclaw Skitterers until you get 8 Succulent Crab Meat.|
 T Buy Us Some Time|QID|25942|M|38.99,32.05|Z|Kelp'thar Forest|N|To Legionnaire Nazgrim.|
 T Traveling on Our Stomachs|QID|25943|M|38.99,32.05|Z|Kelp'thar Forest|N|To Legionnaire Nazgrim.|
 A Girding Our Loins|QID|25944|M|38.99,32.05|Z|Kelp'thar Forest|N|From Legionnaire Nazgrim.|
+A Helm's Deep|QID|25946|M|38.85,26.74|Z|Kelp'thar Forest|N|Swim out of the ship, to the piece of Alliance S.E.A.L. Equipment. Click on it to get the quest Helm?s Deep.|
 T Helm's Deep|QID|25946|M|38.97,32.03|Z|Kelp'thar Forest|N|To Legionnaire Nazgrim.|
 A Finders, Keepers|QID|25947|M|38.97,32.03|Z|Kelp'thar Forest|N|From Legionnaire Nazgrim.|
-C Girding Our Loins|QID|25944|M|36.15,28.07|Z|Kelp'thar Forest|
-C Once More, With Eeling|QID|27729|M|49.76,22.20|Z|Kelp'thar Forest|
+C Girding Our Loins|QID|25944|M|36.15,28.07|Z|Kelp'thar Forest|N|The armor for Girding Our Loins can be found on the seafloor and can also be dropped by Gilblin Scavengers.|
+A Once More, With Eeling|QID|27729|M|49.76,22.20|Z|Kelp'thar Forest|N|Kill a Slitherfin Eel to automatically begin this quest.|
+C Once More, With Eeling|QID|27729|S|M|49.76,22.20|Z|Kelp'thar Forest|
 C Finders, Keepers|QID|25947|M|50.42,27.70|Z|Kelp'thar Forest|
+C Once More, With Eeling|QID|27729|US|M|49.76,22.20|Z|Kelp'thar Forest|
+T Once More, With Eeling|QID|27729|M|49.76,22.20|Z|Kelp'thar Forest|N|Once this quest is complete you can turn it in.|
 T Girding Our Loins|QID|25944|M|39.04,32.15|Z|Kelp'thar Forest|N|To Legionnaire Nazgrim.|
 T Finders, Keepers|QID|25947|M|39.04,32.15|Z|Kelp'thar Forest|N|To Legionnaire Nazgrim.|
+A Bring It On!|QID|25948|M|38.97,32.03|Z|Kelp'thar Forest|N|From Legionnaire Nazgrim.|
 T Bring It On!|QID|25948|M|38.80,31.48|Z|Kelp'thar Forest|N|To Legionnaire Nazgrim.|
 A Blood and Thunder!|QID|25949|M|38.80,31.48|Z|Kelp'thar Forest|N|From Legionnaire Nazgrim.|
-C Blood and Thunder!|QID|25949|M|46.26,46.73|Z|Kelp'thar Forest|
+C Blood and Thunder!|QID|25949|M|46.26,46.73|Z|Kelp'thar Forest|N|If you have trouble completing this event stay inside the ship and wait for it to finish.|
 T Blood and Thunder!|QID|25949|M|46.34,46.81|Z|Kelp'thar Forest|N|To Erunak Stonespeaker.|
 A Better Late Than Dead|QID|25477|M|46.14,46.80|Z|Kelp'thar Forest|N|From Moanah Stormhoof.|
 C Better Late Than Dead|QID|25477|U|54462|M|48.94,42.35|Z|Kelp'thar Forest|
 T Better Late Than Dead|QID|25477|M|46.06,46.83|Z|Kelp'thar Forest|N|To Moanah Stormhoof.|
 A The Abyssal Ride|QID|25371|M|46.06,46.83|Z|Kelp'thar Forest|N|From Moanah Stormhoof.|
-C The Abyssal Ride|QID|25371|M|45.81,46.50|Z|Kelp'thar Forest|
+C The Abyssal Ride|QID|25371|M|45.81,46.50|Z|Kelp'thar Forest|N|Go to the west of Moanah Stormhoof to find the coil of rope. Use the coil of rope to send the bait out into the chasm. A Seahorse will come out and grab the bait. Hop on the seahorse. Use the abilities you are given when you are prompted as the Seahorse swims around the zone. If you end up falling off, you will be transported back to the rope to try again. You will find yourself back at Shallow's End when the Seahorse finishes his run.|
 T The Abyssal Ride|QID|25371|M|46.09,46.78|Z|Kelp'thar Forest|N|To Moanah Stormhoof.|
 A Good Deed Left Undone|QID|27685|M|46.50,46.76|Z|Kelp'thar Forest|N|From Rendel Firetongue.|
-T Good Deed Left Undone|QID|27685|M|53.50,43.14|Z|Kelp'thar Forest|N|To Adarrah.|
+T Good Deed Left Undone|QID|27685|M|53.50,43.14|Z|Kelp'thar Forest|N|Find Adarrah on the east side of the Kelp Forest. Turn in Good Deed Left Undone.|
 A Gimme Shelter!|QID|25587|M|53.50,43.14|Z|Kelp'thar Forest|N|From Adarrah.|
-f Smuggler's Scar|QID|25587|M|56.24,31.23|Z|Kelp'thar Forest|N|At Swift Seahorse.|
-C Gimme Shelter!|QID|25587|M|53.67,34.32|Z|Kelp'thar Forest|
-T Gimme Shelter!|QID|25587|M|57.09,28.87|Z|Kelp'thar Forest|N|To Adarrah.|
+f Smuggler's Scar|QID|25587|M|56.24,31.23|Z|Kelp'thar Forest|N|Swim toward Smuggler's Hole, a small cave just north of where you are. Go inside, until you reach the surface of the water. Get the flightpoint at the Swift Seahorse.|
+C Gimme Shelter!|QID|25587|M|53.67,34.32|Z|Kelp'thar Forest|N|Swim back out of the cave to signal Adarrah.|
+T Gimme Shelter!|QID|25587|M|57.09,28.87|Z|Kelp'thar Forest|N|Swim back into the cave and talk to Adarrah.|
 A Ain't Too Proud to Beg|QID|25598|M|57.09,28.87|Z|Kelp'thar Forest|N|From Adarrah.|
-A A Case of Crabs|QID|25388|M|49.82,40.84|Z|Kelp'thar Forest|N|From Clacksnap Pincer.|
-C Ain't Too Proud to Beg|QID|25598|M|48.23,39.49|Z|Kelp'thar Forest|
-A The Horde's Hoard|QID|25377|
+A A Case of Crabs|QID|25388|M|49.82,40.84|Z|Kelp'thar Forest|N|Swim back out into the Kelp Forest and find a crate. Click on the crate to accept A Case of Crabs.|
+C Ain't Too Proud to Beg|QID|25598|M|48.23,39.49|Z|Kelp'thar Forest|N|Find Captain Samir and Mack Fearsen. Talk to Samir and tell him about Smuggler's Hole.|
 T Ain't Too Proud to Beg|QID|25598|M|57.23,28.77|Z|Kelp'thar Forest|N|To Adarrah.|
 T A Case of Crabs|QID|25388|M|57.23,28.77|Z|Kelp'thar Forest|N|To Adarrah.|
 A A Girl's Best Friend|QID|25390|M|57.18,28.74|Z|Kelp'thar Forest|N|From Adarrah.|
 A A Taste For Tail|QID|25389|M|57.18,28.74|Z|Kelp'thar Forest|N|From Adarrah.|
 A Can't Start a Fire Without a Spark|QID|25602|M|57.25,28.86|Z|Kelp'thar Forest|N|From Captain Samir.|
+C A Girl's Best Friend|QID|25390|S|M|50.82,31.95|Z|Kelp'thar Forest|N|While you complete the other quests, look for Adarrah's Jewelry Box on the sea floor. They are small and a little hard to spot.|
+C A Taste For Tail|QID|25389|M|48.77,36.36|Z|Kelp'thar Forest|N|In the area just east, kill Clacksnap Pincers for A Taste For Tail. You should get a Tattered Treasure Map off one of them.|
+A The Horde's Hoard|QID|25377|N|If you don't have a Tattered Treasure Map yet, keep killing Clacksnap Pincers in the area until you get it. Use the map to accept the quest The Horde's Hoard.|
+A Kliklak's Craw|QID|25467|N|In the same area, kill Sabreclaw Skitterers until you get a Crumpled Treasure Map. This item will start the quest Kliklak's Craw.|
+C Kliklak's Craw|QID|25467|M|43.89,39.85|Z|Kelp'thar Forest|N|Kliklak patrols around this area. Kill him and loot the Corroded key.|
+T Kliklak's Craw|QID|25467|M|46.60,47.53|Z|Kelp'thar Forest|N|Swim to Shallow's End and locate the chest behind the two large shells. Open it to turn in Kliklak's Craw.|
+C Can't Start a Fire Without a Spark|QID|25602|M|55.41,38.91|Z|Kelp'thar Forest|N|At Budd's Dig.|
+C The Horde's Hoard|QID|25377|M|58.95,36.73|Z|Kelp'thar Forest|N|Kill Akasha who patrols around here and loot the Horde Chest Key.|
+T The Horde's Hoard|QID|25377|M|57.90,35.03|Z|Kelp'thar Forest|
+C A Girl's Best Friend|QID|25390|US|M|50.82,31.95|Z|Kelp'thar Forest|N|Finish collecting Adarrah's Keepsake.|
+T A Girl's Best Friend|QID|25390|M|57.06,28.81|Z|Kelp'thar Forest|N|To Adarrah.|
+T A Taste For Tail|QID|25389|M|57.06,28.81|Z|Kelp'thar Forest|N|To Adarrah.|
+T Can't Start a Fire Without a Spark|QID|25602|M|57.27,28.96|Z|Kelp'thar Forest|N|To Captain Samir.|
+A Nerve Tonic|QID|25358|M|57.20,29.33|Z|Kelp'thar Forest|N|From Mack Fearsen.|
+A Ophidophobia|QID|25459|M|57.11,28.85|Z|Kelp'thar Forest|N|From Adarrah.|
+C Nerve Tonic|QID|25358|S|M|56.87,39.51|Z|Kelp'thar Forest|N|Collect the small crates floating all around the ship.|
+C Ophidophobia|QID|25459|M|56.42,38.46|Z|Kelp'thar Forest|
+C Nerve Tonic|QID|25358|US|M|56.87,39.51|Z|Kelp'thar Forest|N|Finish collecting the small crates.|
+T Ophidophobia|QID|25459|M|57.08,28.86|Z|Kelp'thar Forest|N|To Adarrah.|
+T Nerve Tonic|QID|25358|M|57.25,29.26|Z|Kelp'thar Forest|N|To Mack Fearsen.|
+A A Desperate Plea|QID|25638|M|56.48,29.99|Z|Kelp'thar Forest|N|From Mack Fearsen.|
 ]]
 
 end)
