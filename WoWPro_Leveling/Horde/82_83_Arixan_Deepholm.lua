b934a93e1af83fb6f02860d9780e439ad3fbf975
tarix <mikem@tarix.net>
2010-11-06 21:20:29 +0900

    Horde/Deepholm - 40% Complete

--- WoWPro_Leveling/Horde/82_83_Arixan_Deepholm.lua
+++ WoWPro_Leveling/Horde/82_83_Arixan_Deepholm.lua
@@ -48,6 +48,85 @@ A Return to the Temple of Earth|QID|26255|N|From Stormcaller Mylra.|
 T Return to the Temple of Earth|QID|26255|M|49.63,52.97|N|To Maruut Stonebinder.|
 A Deathwing's Fall|QID|26258|M|49.63,52.97|N|From Maruut Stonebinder.|
 A Blood of the Earthwarder|QID|26259|M|49.69,52.99|N|From Seer Kormo.|
+C Blood of the Earthwarder|QID|26259|M|52.75,58.67|N|Leave the temple and go to The Blood Trail. All along the chain of small lava pools you will find Living Blood. Kill them until you have collected 5 Blood of Neltharion.|
+C Deathwing's Fall|QID|26258|NC|M|59.01,58.69|N|When you have finished, mount up and follow The Blood Trail until you reach Deathwing?s Fall. You will get the quest complete message.|
+T Deathwing's Fall|QID|26258|M|59.17,58.70|
+A Bleed the Bloodshaper|QID|26256|M|59.17,58.70|
+C Bleed the Bloodshaper|QID|26256|M|61.21,59.44|N|Find and kill a Twilight Bloodshaper in the area around Deathwing?s Fall. He should drop the Twilight Orders.|
+T Bleed the Bloodshaper|QID|26256|M|61.32,58.76|
+A Question the Slaves|QID|26261|M|61.32,58.76|
+N Question the Slaves|QID|26261|L|60739|M|62.8,59.6|N|The key you need is in a small tent. Collect it from the chest here.|
+C Question the Slaves|QID|26261|M|65.47,62.41|N|Mount up and fly to quarry just to the southeast. Free 6 Enslaved Miners.|
+T Question the Slaves|QID|26261|M|65.47,62.41|
+A The Forgemaster's Log|QID|26260|M|65.47,62.41|
+T The Forgemaster's Log|QID|26260|M|63.75,55.36|N|Find the Forgemaster?s Log at the top of a tower.|
+A Silvermarsh Rendezvous|QID|27007|M|63.75,55.36|
+C Silvermarsh Rendezvous|QID|27007|M|69.96,60.17|N|Fly up and to the southwest to Upper Silvermarsh.|
+T Silvermarsh Rendezvous|QID|27007|M|70.34,61.04|
+A Quicksilver Submersion|QID|27010|M|70.34,61.04|
+A Twilight Research|QID|27100|M|70.50,61.48|N|Find and kill a Mercurial Ooze. It will drop Maziel's Research. Use this to begin the quest Twilight Research.|
+C Twilight Research|QID|27100|S|M|74.00,60.91|N|Kill any Mercurial Ooze you see until you get 4 Twilight Research Notes.|
+N Quicksilver Submersion|QID|27010|L|60809|M|71.7,64.2|N|Loot a Trogg Crate from the ground here. A trogg will come out and attack you but shouldn't pose a problem.|
+C Quicksilver Submersion|QID|27010|U|60809|M|74.96,64.77|N|Use the Trogg Crate to put it on your head. You should be in the mercury pond. You need to get close to the Forgemaster's camp, but not too close. Watch the conversation that happens.|
+T Quicksilver Submersion|QID|27010|M|75.04,64.37|
+A The Twilight Overlook|QID|27061|M|75.04,64.33|
+C Twilight Research|QID|27100|US|M|74.00,60.91|N|Finish collecting Twilight Research Notes.|
+T Twilight Research|QID|27100|M|74.05,60.90|
+A Maziel's Revelation|QID|27101|M|74.05,60.90|
+T Maziel's Revelation|QID|27101|M|67.18,70.17|N|Fly to Lower Silvermarsh. Find Maziel's Journal.|
+A Maziel's Ascendancy|QID|27102|M|67.18,70.17|
+C Maziel's Ascendancy|QID|27102|M|69.37,68.20;72.79,61.98|N|Maziel is inside the cave just east of where the journal was located. Enter the cave and take the left fork when the path spits. Maziel is located on an island in the middle of a small pond.|
+T Maziel's Ascendancy|QID|27102|M|72.26,62.22|
+T The Twilight Overlook|QID|27061|M|64.47,82.23|N|To Stormcaller Mylra.|
+A Big Game, Big Bait|QID|26766|M|64.47,82.23|N|From Stormcaller Mylra.|
+A To Catch a Dragon|QID|26768|M|64.47,82.23|N|From Stormcaller Mylra.|
+A Into the Stonecore|QID|28866|M|64.47,82.23|N|From Stormcaller Mylra.|
+C Big Game, Big Bait|QID|26766|S|U|60382|M|56.30,85.57|N|Kill 5 Jadecrest Basilisk and use Mylra's Knife on their corpses.|
+C To Catch a Dragon|QID|26768|M|55.77,88.58|N|Find and kill Twilight Dragonstalkers until you get a Twilight Snare.|
+C Big Game, Big Bait|QID|26766|US|U|60382|M|56.30,85.57|N|Kill 5 Jadecrest Basilisk and use Mylra's Knife on their corpses.|
+T Big Game, Big Bait|QID|26766|M|64.48,82.23|N|To Stormcaller Mylra.|
+T To Catch a Dragon|QID|26768|M|64.48,82.23|N|To Stormcaller Mylra.|
+A Testing the Trap|QID|26771|M|64.48,82.23|N|From Stormcaller Mylra.|
+C Testing the Trap|QID|26771|U|60773|M|51.11,85.47|N|Fly far west, to the Matriarch's Feeding Grounds. Use the Trapped basilisk Meat in the center of the feeding ground to bring the Stonescale Matriarch down. She is a level 82 elite, and can hit quite hard. The trick to beating her is to rotate around her while doing damage, only staying in one spot as long as is necessary. This is simple if you are playing a melee class. Casters should only cast 2 or 3 spells before moving to a different position.|
+T Testing the Trap|QID|26771|M|64.50,82.19|N|To Stormcaller Mylra.|
+A Abyssion's Minions|QID|26857|M|64.50,82.19|N|From Stormcaller Mylra.|
+A Block the Gates|QID|26861|M|64.50,82.19|N|From Seer Galekk.|
+C Abyssion's Minions|QID|26857|S|M|69.72,77.39|N|Kill any of the cultists or dragonspawn.|
+N Block the Gates|QID|26861|U|60501|M|68.7, 75.1|N|Fly to the Twilight Gate and use the Stormstone to seal it.|
+C Block the Gates|QID|26861|U|60501|M|71.14,75.03|N|Fly to the Elemental Gate and seal it with the Stormstone.|
+C Abyssion's Minions|QID|26857|US|M|69.72,77.39|N|Kill any of the cultists or dragonspawn.|
+T Block the Gates|QID|26861|M|64.49,82.25|N|To Seer Galekk.|
+T Abyssion's Minions|QID|26857|M|64.49,82.25|N|To Stormcaller Mylra.|
+A The World Pillar Fragment|QID|26876|M|64.49,82.25|N|From Stormcaller Mylra.|
+C The World Pillar Fragment|QID|26876|U|60810|M|69.73,76.72|N|Fly to the center of Abyssion's Lair. Use the Earthen Ring Banner there to summon Abyssion. You will fight him the same way that you fought the Stonescale Matriarch. You will also have some NPCs there to help you out. When you see the warning that Abyssion is casting a powerful shadow nova, run away, preferably behind the pillar. It hits for about 20k, which could kill you if you are low.|
+T Blood of the Earthwarder|QID|26259|M|49.66,53.02|N|To Seer Kormo.|
+T The World Pillar Fragment|QID|26876|M|49.66,53.02|N|To Maruut Stonebinder.|
+A Where's Goldmine?|QID|26409|M|49.52,53.40|N|From Earthcaller Yevaa.|
+T Where's Goldmine?|QID|26409|M|46.48,57.33|N|Leave the Temple and start going to the west around the outside. Find Initiate Goldmine.|
+A Explosive Bonding Compound|QID|26410|M|46.48,57.33|N|From Initiate Goldmine.|
+A Something that Burns|QID|27135|M|46.48,57.33|N|From Initiate Goldmine.|
+C Explosive Bonding Compound|QID|26410|M|44.25,55.24|N|Kill Rockslice Flayers in the area to the west until you have 5 White Stone Resin.|
+C Something that Burns|QID|27135|U|60834|M|52.12,59.01|N|Fly east where you will find The Blood Trail. Use Goldmine's Fire Totem in any of the lava pools here to summon Magmatooth. Kill him and loot The Burning Heart.|
+T Explosive Bonding Compound|QID|26410|M|46.51,57.23|N|To Initiate Goldmine.|
+T Something that Burns|QID|27135|M|46.51,57.23|N|To Initiate Goldmine.|
+A Apply and Flash Dry|QID|26411|M|46.51,57.23|N|From Initiate Goldmine.|
+C Apply and Flash Dry|QID|26411|U|58502|M|46.51,57.23|N|Use the Explosive Bonding Compound on Flint Oremantle, who is lying on the ground next to Goldmine.|
+T Apply and Flash Dry|QID|26411|M|46.51,57.23|N|To Initiate Goldmine.|
+A Take Him to the Earthcaller|QID|26413|M|46.51,57.23|N|From Initiate Goldmine.|
+C Take Him to the Earthcaller|QID|26413|M|49.65,53.32|N|Return to the Temple of Earth. Flint Oremantle will follow you.|
+T Take Him to the Earthcaller|QID|26413|M|49.54,53.44|N|To Earthcaller Yevaa.|
+A To Stonehearth's Aid|QID|26484|M|49.54,53.44|N|From Earthcaller Yevaa.|
+T To Stonehearth's Aid|QID|26484|M|27.89,68.71|N|Fly to Stonehearth. Find Crag Rockcrusher.|
+A The Quaking Fields|QID|27931|M|27.89,68.71|N|From Crag Rockcrusher.|
+T The Quaking Fields|QID|27931|M|30.71,77.75|N|To Slate Quicksand.|
+A The Axe of Earthly Sundering|QID|27932|M|30.71,77.75|N|From Slate Quicksand.|
+A Elemental Ore|QID|27933|M|30.71,77.75|N|From Slate Quicksand.|
+C Elemental Ore|QID|27933|M|35.41,79.06|N|Kill Jade Ragers in the area until you get 6 Elemental Ore.|S|
+C The Axe of Earthly Sundering|QID|27932|U|60490|M|38.96,85.86|N|Find an Emerald Colossus and use The Axe of Earthly Sundering. It will turn into a Sundered Emerald Colossus, which if far easier to kill.|
+C Elemental Ore|QID|27933|M|35.41,79.06|N|Kill Jade Ragers in the area until you get 6 Elemental Ore.|US|
+T The Axe of Earthly Sundering|QID|27932|M|30.69,77.77|N|To Slate Quicksand.|
+T Elemental Ore|QID|27933|M|30.69,77.77|N|To Slate Quicksand.|
+A One With the Ground|QID|27934|M|30.69,77.77|N|From Slate Quicksand.|
 ]]
 
 end)
