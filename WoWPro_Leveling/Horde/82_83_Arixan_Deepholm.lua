375c258d55e068d5e3786d78ea17bd6c4aa10540
tarix <mikem@tarix.net>
2010-11-06 19:09:14 +0900

    Horde/Deepholm - 20% Complete

--- WoWPro_Leveling/Horde/82_83_Arixan_Deepholm.lua
+++ WoWPro_Leveling/Horde/82_83_Arixan_Deepholm.lua
@@ -2,7 +2,52 @@ WoWPro.Leveling:RegisterGuide('AriDee8283', 'Deepholm', 'Arixan', '82', '83', 'n
 return [[
 
 A Warchief's Command: Deepholm!|QID|27722|M|49.67,76.45|Z|Orgrimmar|N|From Warchief's Command Board.|
-
+T Warchief's Command: Deepholm!|QID|27722|M|50.30,38.24|Z|Orgrimmar|N|To Farseer Krogar.|
+A The Maelstrom|QID|27203|M|50.30,38.24|Z|Orgrimmar|N|From Farseer Krogar.|
+T The Maelstrom|QID|27203|M|33.20,50.22|Z|The Maelstrom|N|Click on the portal opened by Farseer Korgar and talk to Thrall.|
+A Deepholm, Realm of Earth|QID|27123|M|33.20,50.22|Z|The Maelstrom|N|From Thrall.|
+T Deepholm, Realm of Earth|QID|27123|M|49.63,53.07|N|Turn around and hop on the Wyvern behind you. There will be a very cool event where you fly into the Maelstrom. You'll wind up in Deepholm, landing at the Temple of Earth. Directly in front of you, turn in Deepholm, Realm of Earth to Maruut Stonebinder.|
+A Gunship Down|QID|26245|M|49.63,53.07|N|From Maruut Stonebinder.|
+A Elemental Energy|QID|27136|M|49.68,53.04|N|From Seer Kormo.|
+A The Earth Claims All|QID|26244|M|49.68,53.04|N|From Seer Kormo.|
+C Elemental Energy|QID|27136|U|60835|M|50.96,62.19|N|Use the Depleted Totem and kill the Energized Geodes and Lodestone Elementals near it.|
+A Elemental Energy|QID|27136|M|0.00,0.00|Z|
+C The Earth Claims All|QID|26244|S|M|55.08,73.90|
+N Gunship Down|QID|26245|QO|First Clue: 1/1|M|53.53,73.76|N|The ship is split in two. Fly inside the back half and find the corpse of Captain Skullshatter. Click on him to get the First Clue.|
+A Captain's Log|QID|26246|M|53.53,73.76|N|Next to Captain Skullshatter, click on the Captain's Log to get the quest Captain's Log.|
+N Gunship Down|QID|26245|QO|Second Clule: 1/1|M|56.09,74.17|N|The Second Clue is the Slain Cannoneer.|
+C Gunship Down|QID|26245|M|56.70,76.46|N|The Third Clue is on the ground just outside the front half of the wreckage.|
+C The Earth Claims All|QID|26244|US|M|55.08,73.90|
+T Gunship Down|QID|26245|M|49.63,53.03|N|To Maruut Stonebinder.|
+T Captain's Log|QID|26246|M|49.63,53.03|N|To Maruut Stonebinder.|
+T Elemental Energy|QID|27136|M|49.68,53.00|N|To Seer Kormo.|
+T The Earth Claims All|QID|26244|M|49.68,53.00|N|To Seer Kormo.|
+A Diplomacy First|QID|26247|M|49.65,53.06|N|From Maruut Stonebinder.|
+T Diplomacy First|QID|26247|N|Fly up to the Alliance Gunship. It patrols in the air along the east side of the Temple. You will find Stormcaller Mylra on the platform at the back of the ship.|
+A All Our Friends Are Dead|QID|26248|N|From Stormcaller Mylra.|
+A The Admiral's Cabin|QID|26249|N|From Stormcaller Mylra.|
+C All Our Friends Are Dead|QID|26248|S|U|58167|N|Find Slain Crew Members all around the ship. Use the Spirit Totem on them. They will either attack or simply tell you the information you are after. Do this a total of six times.|
+T The Admiral's Cabin|QID|26249|N|To First Mate Moody.|
+A Without a Captain or Crew|QID|26427|N|From First Mate Moody.|
+C All Our Friends Are Dead|QID|26248|US|U|58167|N|Make sure you have completed All Our Friends Are Dead.|
+T All Our Friends Are Dead|QID|26248|N|To Stormcaller Mylra.|
+A Take No Prisoners|QID|26251|N|From Stormcaller Mylra.|
+A On Second Thought, Take One Prisoner|QID|26250|N|From Stormcaller Mylra.|
+N Without a Captain or Crew|QID|26427|QO|Spool of Rope: 1/1|N|The Spool of Rope is located near the front of the ship, above deck. Get it and then take the stairs in the center of the ship down.|
+C Without a Captain or Crew|QID|26427|N|The Bottle of Whiskey is on a table straight in front of you as you come down the stairs.|
+C Take No Prisoners|QID|26251|S|
+C On Second Thought, Take One Prisoner|QID|26250|N|Sill on the lower level, at the front end of the airship, you will find Mor'norokk the Hateful. Attack him until he "surrenders". As soon as you try to talk to him he grabs you threatens to drop you from the ship. Mylra will come rescue you.|
+C Take No Prisoners|QID|26251|US|
+T Without a Captain or Crew|QID|26427|N|To First Mate Moody.|
+T Take No Prisoners|QID|26251|N|To Stormcaller Mylra.|
+T On Second Thought, Take One Prisoner|QID|26250|N|To Stormcaller Mylra.|
+A Some Spraining to Do|QID|26254|N|From Stormcaller Mylra.|
+C Some Spraining to Do|QID|26254|N|Hop on Stormbeak, the gryphon next to Mylra. Watch the short event.|
+T Some Spraining to Do|QID|26254|N|To Stormcaller Mylra.|
+A Return to the Temple of Earth|QID|26255|N|From Stormcaller Mylra.|
+T Return to the Temple of Earth|QID|26255|M|49.63,52.97|N|To Maruut Stonebinder.|
+A Deathwing's Fall|QID|26258|M|49.63,52.97|N|From Maruut Stonebinder.|
+A Blood of the Earthwarder|QID|26259|M|49.69,52.99|N|From Seer Kormo.|
 ]]
 
 end)
