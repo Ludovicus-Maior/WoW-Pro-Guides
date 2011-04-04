3475f0e752a27c0eb017bdc3b5f6203e691ee22b
Ludovicus <LuisOrtiz@Verizon.NET>
2011-04-04 18:16:35 -0400

    Add AnnaSender's new Loch Modan guide.  Needs some work still.

---
+++ WoWPro_Leveling/Alliance/10_20_Anna_Loch_Modan.lua
@@ -0,0 +1,190 @@
+
+WoWPro.Leveling:RegisterGuide('AnnLoc1020', 'Loch Modan', 'AnnaSender', '10', '20', 'WkjWet2025', 'Alliance', function()
+return [[
+; Start right after getting the Dark Iron Ambassador in Jail
+A Reinforcements for Loch Modan|QID|26131|M|41.8,52.2|Z|Ironforge|N|From Mountaineer Barleybrew.|
+R Reinforcements for Loch Modan|QID|26131|CC|M|55.46,47.76|Z|Ironforge|N|Go to flight master, accept flight.|
+T Reinforcements for Loch Modan|QID|26131|M|14.0,56.4|N|To Pilot Hammerfoot.|
+A The Lost Pilot|QID|26854|M|14.0,56.4|N|From Pilot Hammerfoot.|
+T The Lost Pilot|QID|26854|M|87.65,50.18|Z|Dun Morogh|N|Through the tunnel to a Dwarven Corpse.|
+A A Pilot's Revenge|QID|26855|M|87.65,50.18|Z|Dun Morogh|N|From A Dwarven Corpse.|
+C A Pilot's Revenge|QID|26855|M|87.37,49.26|Z|Dun Morogh|
+T A Pilot's Revenge|QID|26855|M|14.0,56.4|Z|Loch Modan|N|To Pilot Hammerfoot.|
+A South Gate Status Report|QID|13635|M|14.0,56.4|N|From Pilot Hammerfoot.|
+T South Gate Status Report|QID|13635|M|23.4,75.0|N|To Captain Rugglefuss.|
+A In Defense of the King's Lands|QID|26146|M|23.48,75.03|N|From Captain Rugelfuss.|
+A The Trogg Threat|QID|26145|M|23.44,74.94|N|From Mountaineer Cobbleflint.|
+R Up the Trail|QID|26145|CC|M|23.21,76.52;22.58,77.34;26.66,77.34;23.48,79.83;24.85,78.01;27.51,76.46;30.31,78.37|N|Go up this mountain trail and kill Troggs around the valley.|
+C The Trogg Threat|QID|26145|M|31.63,69.62|S|QO|Trogg Stone Tooth: 8/8|
+C In Defense of the King's Lands|QID|26146|M|35.78,80.53|
+C The Trogg Threat|QID|26145|M|31.63,69.62|US|QO|Trogg Stone Tooth: 8/8|
+T The Trogg Threat|QID|26145|M|23.45,74.98|N|To Mountaineer Cobbleflint.|
+T In Defense of the King's Lands|QID|26146|M|23.41,75.00|N|To Captain Rugelfuss.|
+A A Decisive Strike|QID|26148|M|23.37,75.01|N|From Captain Rugelfuss.|
+A Bigger and Uglier|QID|26147|M|23.40,75.02|N|From Mountaineer Wallbang.|
+C Bigger and Uglier|QID|26147|M|35.04,59.37|S|N|Killing Stonesplinters
+C A Decisive Strike|QID|26148|M|33.08;70.65;35.36,65.18;35.64.62.25;33.86,62.19;35.60,61.10|N|Kill Grawmug in the rear of the cave.|
+C Bigger and Uglier|QID|26147|M|35.04,59.37|US|N|Killing Stonesplinters|
+T A Decisive Strike|QID|26148|M|23.40,75.01|N|To Captain Rugelfuss.|
+T Bigger and Uglier|QID|26147|M|23.34,75.01|N|To Mountaineer Wallbang.|
+A Onward to Thelsamar|QID|26176|M|23.40,72.01|N|From Captain Rugelfuss.|
+
+; Thelsamar Series
+T Onward to Thelsamar|QID|26176|M|35.0,46.6|N|To Mountaineer Kadrell.|
+A Out of Gnoll-where|QID|26842|M|35.09,46.41|N|From Mountaineer Kadrell.|
+A Looking for Lurkers|QID|25118|M|35.37,42.86|N|From Dakk Blunderblast.|
+A WANTED: The Dark Iron Spy|QID|13648|M|37.26,46.44|N|From "Wanted" sign.|
+A Thelsamar Blood Sausages|QID|26860|M|34.84,49.11|N|From Vidra Hearthstove.|
+h Stoutlager Inn|QID|26928|M|35.48,48.44|N|At Innkeeper Hearthstove.|
+C Looking for Lurkers|QID|25118|S|M|40.48,54.79|N|Kill Lurkers along the way.|
+C Thelsamar Blood Sausages|QID|26860|S|M|28.17,46.32|N|Kill Black Bears for Bear Rumps.|
+R WANTED: The Dark Iron Spy|QID|13648|CC|M|40.39,61.08;36.46,61.23|N|Make your way up the path up the hill.|
+C WANTED: The Dark Iron Spy|QID|13648|M|36.46,61.23|N|Watch out for the Dark Iron Courier!|
+A Explorers' League Document (1 of 6)|QID|13656|M|36.76,61.25|
+T WANTED: The Dark Iron Spy|QID|13648|M|34.63,44.51|N|To Magistrate Bluntnose.|
+T Explorers' League Document (1 of 6)|QID|13656|M|37.18,47.82|N|To Torren Squarejaw.|
+C Out of Gnoll-where|QID|26842|QO|Mosshide Ear: 12/12|M|28.17,46.32|
+C Thelsamar Blood Sausages|QID|26860|M|28.17,46.32|N|Kill Black Bears for Bear Rumps.|US|
+C Looking for Lurkers|QID|25118|M|30.45,48.62|N|Kill Lurkers along the way.|US|
+T Out of Gnoll-where|QID|26842|M|35.03,46.56|N|To Mountaineer Kadrell.|
+A Stormpike's Orders|QID|13636|M|35.03,46.56|N|From Mountaineer Kadrell.|
+T Looking for Lurkers|QID|25118|M|35.35,42.82|N|To Dakk Blunderblast.|
+T Thelsamar Blood Sausages|QID|26860|M|34.84,49.11|N|To Vidra Hearthstove.|
+T Stormpike's Orders|QID|13636|M|25.51,17.96|N|To Mountaineer Stormpike.|
+
+; Shift to Algaz Station
+A A Tiny, Clever Commander|QID|26843|M|25.50,17.69|N|From Mountaineer Stormpike.|
+C A Tiny, Clever Commander|QID|26843|M|25.81,31.24|
+T A Tiny, Clever Commander|QID|26843|M|25.47,18.02|N|To Mountaineer Stormpike.|
+A Kobold and Kobolder|QID|26844|M|25.47,18.02|N|From Mountaineer Stormpike.|
+C Kobold and Kobolder|QID|26844|M|31.20,26.70|
+T Kobold and Kobolder|QID|26844|M|25.41,17.90|N|To Mountaineer Stormpike.|
+A Who's In Charge Here?|QID|26845|M|25.41,17.90|N|From Mountaineer Stormpike.|
+A Filthy Paws|QID|26863|M|25.41,17.90|N|From Mountaineer Stormpike.|
+A A Nasty Exploit|QID|26846|M|25.41,17.90|N|From Scout Dorli.|
+R Filthy Paws|QID|26863|CC|M|35.29,17.93|N|Run here|
+C A Nasty Exploit|QID|26846|M|36.23,23.45|S|
+C Filthy Paws|QID|26863|M|35.09,24.63|S|
+C Who's In Charge Here?|QID|26845|M|34.75,26.85|
+C Filthy Paws|QID|26863|M|35.09,24.63|US|
+C A Nasty Exploit|QID|26846|M|36.23,23.45|US|
+T A Nasty Exploit|QID|26846|M|25.48,17.74|N|To Scout Dorli.|
+
+; Back to Thelsamar
+H Stoutlager Inn|QID|26863|N|Hearth or run back.|
+T Filthy Paws|QID|26863|M|25.48,17.74|N|To Mountaineer Stormpike.|
+T Who's In Charge Here?|QID|26845|M|25.48,17.74|N|To Mountaineer Stormpike.|
+A The Bearer of Gnoll-edge|QID|26864|M|25.48,17.74|N|From Mountaineer Stormpike.|
+A Explorer's League Document (2 of 6)|QID|13655|M|41.32,39.12|N|Accept quest from document under dock.|
+T Explorers' League Document (2 of 6)|QID|13655|M|37.18,47.82|N|To Torren Squarejaw.|
+T The Bearer of Gnoll-edge|QID|26864|M|35.09,46.41|N|To Mountaineer Kadrell.|
+A Suddenly, Murlocs!|QID|26927|M|35.09,46.41|N|From Mountaineer Kadrell.|
+T Suddenly, Murlocs!|QID|26927|M|34.81,49.06|N|To Cannary Caskshot.|
+A Smells Like A Plan|QID|26928|M|34.81,49.06|N|From Cannary Caskshot.|
+A A Load of Croc|QID|26929|M|34.91,49.02|N|From Cannary Caskshot.|
+A Buzz Off|QID|26932|M|35.01,46.42|N|From Mountaineer Kadrell.|
+
+C Smells Like A Plan|QID|26928|M|42.92,47.90|S|N|From any Bluegill Murloc in The Loch.|
+C Buzz Off|QID|26932|M|54.39,59.98|S|
+C A Load of Croc|QID|26929|M|60.21,54.19|
+C Buzz Off|QID|26932|M|54.39,59.98|US|
+C Smells Like A Plan|QID|26928|M|42.92,47.90|US|N|From any Bluegill Murloc in The Loch.|
+
+T Buzz Off|QID|26932|M|35.01,46.84|N|To Mountaineer Kadrell.|
+T A Load of Croc|QID|26929|M|34.86,49.03|N|To Cannary Caskshot.|
+T Smells Like A Plan|QID|26928|M|34.85,49.07|N|To Cannary Caskshot.|
+A Axis of Awful|QID|26868|M|34.85,49.07|N|From Cannary Caskshot.|
+C Axis of Awful|QID|26868|U|60502|M|50.99,55.70|N|Open your bag and disguise yourself, then throw the potion at the gnoll.|
+T Axis of Awful|QID|26868|M|34.86,49.03|N|To Cannary Caskshot.|
+
+; Time to go off to the Excavation Site
+A Resupplying the Excavation|QID|13639|M|37.26,47.60|N|From Jern Hornhelm.|
+A Explorer's League Document (3 of 6)|QID|13657|M|61.66,73.09|N|Accept quest near cave.|
+C Resupplying the Excavation|QID|13639|NC|M|54.98,66.79|
+T Resupplying the Excavation|QID|13639|M|56.39,65.97|N|To Huldar.|
+A Protecting the Shipment|QID|309|M|56.39,65.97|N|From Huldar.|
+C Protecting the Shipment|QID|309|M|56.37,65.90|N|Protect Huldar.
+T Protecting the Shipment|QID|309|M|65.19,66.02|N|To Prospector Ironband.|
+A Keep Your Hands Off The Goods!|QID|13650|M|65.19,66.02|N|From Prospector Ironband.|
+A Gathering Idols|QID|26961|M|64.80,66.54|N|From Magmar Fellhew.|
+C Gathering Idols|QID|26961|M|72.57,66.29|S|
+A Explorers' League Document (4 of 6)|QID|13658|M|68.20,65.97|N|Walk up a near by ramp located close to the last objective completed.|
+R Keep Your Hands Off The Goods!|QID|13650|CC|M|70.64,67.59|N|Run, Kill and Loot as you go.|
+R Keep Your Hands Off The Goods!|QID|13650|CC|M|72.65,65.56|N|Run, Kill and Loot as you go.|
+C Keep Your Hands Off The Goods!|QID|13650|M|70.15,60.08|
+C Gathering Idols|QID|26961|M|72.57,66.29|US|
+T Keep Your Hands Off The Goods!|QID|13650|M|65.19,66.07|N|To Prospector Ironband.|
+T Gathering Idols|QID|26961|M|64.94,66.56|N|To Magmar Fellhew.|
+
+; Time to go off to the Farstrider Lodge
+A Joining the Hunt|QID|13647|M|64.93,66.60|N|From Magmar Fellhew.|
+T Joining the Hunt|QID|13647|M|81.82,61.74|N|To Marek Ironheart.|
+A Hornet Hunting|QID|27028|M|81.82,61.74|N|From Marek Ironheart.|
+A Foxtails By The Handful|QID|27030|M|81.87,61.88|N|From Marek Ironheart.|
+A Thistle While You Work|QID|27025|M|82.79,63.42|N|From Safety Warden Pipsy.|
+f Farstrider Lodge|QID|27025|M|81.96,64.10|N|At Eeryven Grayer.|
+h Farstrider Lodge|QID|27025|M|81.93,64.56|N|At Vyrin Swiftwind.|
+A Wing Nut|QID|27031|M|81.70,64.59|Z|Farstrider Lodge|N|From Bingles Blastenheimer.|
+A The Joy of Boar Hunting|QID|27016|M|83.35,65.01|Z|Farstrider Lodge|N|From Daryl the Youngling.|
+C Foxtails By The Handful|QID|27030|M|68.77,42.89|S|
+C Thistle While You Work|QID|27025|M|68.39,39.97|S|
+A Explorers' League Document (6 of 6)|QID|13659|M|73.13,35.96|N|Next to carving on the ground.|
+C Thistle While You Work|QID|27025|M|68.39,39.97|US|
+C Foxtails By The Handful|QID|27030|M|68.77,42.89|US|
+T Thistle While You Work|QID|27025|M|82.86,63.50|Z|Farstrider Lodge|N|To Safety Warden Pipsy.|
+A Defcon: Bobcat|QID|27026|M|82.86,63.50|Z|Farstrider Lodge|N|From Safety Warden Pipsy.|
+T Foxtails By The Handful|QID|27030|M|81.87,61.81|Z|Farstrider Lodge|N|To Marek Ironheart.|
+C Wing Nut|QID|27031|M|75.15,73.70|N|Pick up pristine feathers along the way.|S|
+C Defcon: Bobcat|QID|27026|M|69.67,73.70|S|
+C Wing Nut|QID|27031|M|75.15,73.70|N|Gather any remaining pristine feathers you need.|US|
+T Wing Nut|QID|27031|M|81.68,64.66|Z|Farstrider Lodge|N|To Bingles Blastenheimer.|
+A Bird is the Word|QID|27032|M|81.70,64.63|Z|Farstrider Lodge|N|From Bingles Blastenheimer.|
+R Bird is the Word|QID|27032|CC|M|71.23,76.72|N|Cave Entrance.|
+T Bird is the Word|QID|27032|M|78.52,76.30|N|To Rusted Skystrider in the back of the cave.|
+A Skystrider's Heart|QID|27033|M|78.51,76.32|N|From Rusted Skystrider.|
+C Defcon: Bobcat|QID|27026|M|69.67,73.70|US|
+T Defcon: Bobcat|QID|27026|M|82.87,63.35|Z|Farstrider Lodge|N|To Safety Warden Pipsy.|
+T Skystrider's Heart|QID|27033|M|81.63,64.61|Z|Farstrider Lodge|N|To Bingles Blastenheimer.|
+A He's That Age|QID|27034|M|81.66,64.55|Z|Farstrider Lodge|N|From Bingles Blastenheimer.|
+C Hornet Hunting|QID|27028|M|54.64,32.21|S|
+C The Joy of Boar Hunting|QID|27016|M|57.54,34.71|S|
+A Explorers' League Document (5 of 6)|QID|13660|M|53.70,38.21|N|Kill bees and boars as you go|
+C The Joy of Boar Hunting|QID|27016|M|57.54,34.71|N|Finish off the needed Boars.|US|
+C Hornet Hunting|QID|27028|M|54.64,32.21|N|Finish off the needed Hornets.|US|
+T He's That Age|QID|27034|M|58.47,28.99|N|To Ando Blastenheimer.|
+A Standing Up|QID|27035|M|58.47,28.99|N|From Ando Blastenheimer.|
+C Standing Up|QID|27035|M|50.64,23.82|
+T Standing Up|QID|27035|M|58.59,28.96|N|To Ando Blastenheimer.|
+A Fight the Hammer|QID|27074|M|58.59,28.96|N|From Ando Blastenheimer.|
+T Fight the Hammer|QID|27074|M|64.05,26.80|N|To Ashlan Stonesmirk.|
+A Servants of Cho'gall|QID|27075|M|64.05,26.80|N|From Ashlan Stonesmirk.|
+A Clutching at Chaos|QID|27077|M|64.05,26.80|N|From Ashlan Stonesmirk.|
+C Servants of Cho'gall|QID|27075|M|70.91,23.24|S|
+C Clutching at Chaos|QID|27077|M|69.40,23.80|N|Sparkley black long things.|
+C Servants of Cho'gall|QID|27075|M|70.91,23.24|US|
+T Servants of Cho'gall|QID|27075|M|64.05,26.83|N|To Ashlan Stonesmirk.|
+T Clutching at Chaos|QID|27077|M|64.05,26.83|N|To Ashlan Stonesmirk.|
+A Gor'kresh|QID|27078|M|64.05,26.83|N|From Ashlan Stonesmirk.|
+C Gor'kresh|QID|27078|M|79.80,14.90|
+T Gor'kresh|QID|27078|M|64.17,26.83|N|To Ashlan Stonesmirk.|
+A Ando's Call|QID|27115|M|64.17,26.83|N|From Ashlan Stonesmirk.|
+T Ando's Call|QID|27115|M|58.57,29.07|N|To Ando Blastenheimer.|
+A The Winds of Loch Modan|QID|27116|M|58.57,29.07|N|From Ando Blastenheimer.|
+T The Winds of Loch Modan|QID|27116|M|25.51,17.87|N|To Mountaineer Stormpike.|
+H Farstrider Lodge|QID|27028|M|81.93,64.56|N|Hearth now.|
+T Hornet Hunting|QID|27028|M|81.87,61.81|N|To Marek Ironheart.|
+T The Joy of Boar Hunting|QID|27016|M|83.40,65.26|N|To Daryl the Youngling.|
+A Vyrin's Revenge|QID|27036|M|81.99,64.44|N|From Vyrin Swiftwind.|
+C Vyrin's Revenge|QID|27036|M|80.28,52.17|
+T Vyrin's Revenge|QID|27036|M|83.39,65.25|N|To Daryl the Youngling.|
+A Vyrin's Revenge|QID|27037|M|83.39,65.25|N|From Daryl the Youngling.|
+T Vyrin's Revenge|QID|27037|M|81.88,64.42|N|To Vyrin Swiftwind.|
+F Thelsamar|QID|26128|N|Fly back to Thelsamar.|
+T Explorer's League Document (3 of 6)|QID|13657|M|37.18,47.75|N|To Torren Squarejaw.|
+T Explorers' League Document (4 of 6)|QID|13658|M|37.18,47.75|N|To Torren Squarejaw.|
+T Explorers' League Document (5 of 6)|QID|13660|M|37.18,47.75|N|To Torren Squarejaw.|
+T Explorers' League Document (6 of 6)|QID|13659|M|37.18,47.75|N|To Torren Squarejaw.|
+A Heartfelt Appreciation|QID|13661|M|37.18,47.75|N|Accept and turning in same step.|
+]]
+
+end)
