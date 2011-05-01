272cee3618c65bcc72513f62ea2b079325527170
Ludovicus <LuisOrtiz@Verizon.NET>
2011-05-01 16:56:34 -0400

    First Cut, incomplete

---
+++ WoWPro_Dailies/Alliance/Ludovicus_Icecrown.lua
@@ -0,0 +1,20 @@
+WoWPro.Dailies:RegisterGuide("LudoIceDaily", "Icecrown", "Valiance Expedition", "Lich", "Ludovicus", "Alliance", function()
+return [[
+A King of the Mountain|QID|13280|M|57.00,62.57|N|From Frazzle Geargrinder|
+C King of the Mountain|QID|13280|M|54.87,60.10|N|Right at the TOP.|
+T King of the Mountain|QID|13280|M|57.00,62.57|N|To Frazzle Geargrinder|
+A Assault by Air|QID|13309|M|62.56,51.34|N|From Ground Commander Koup|
+C Assault by Air|QID|13309|M|62.55,50.65|N|Hop onto the turret and fire on the Vrykul spear guns.|QO|Skybreaker Infiltrators dropped: 4/4|
+T Assault by Air|QID|13309|M|62.56,51.34|N|To Ground Commander Koup|
+A Assault by Ground|QID|13284|M|62.78,51.62|N|Skybreaker Squad Leader|
+C Assault by Ground|QID|13284|M|59.88,53.60|N|Escort.  No stop.|
+T Assault by Ground|QID|13284|M|62.56,51.34|N|To Ground Commander Koup|
+A Blood of the Chosen|QID|13336|M|61.8,56.6|N|To Knight-Captain Drosche, on the Skybreaker top deck|
+A Capture More Dispatches|QID|13333|M|62.2,49.2|N|High Captain Justin Bartlett, on the Skybreaker "Throne Room"|
+C Capture More Dispatches|QID|13333|M|48, 54|N|Sit on the Hill|
+C Blood of the Chosen|QID|13336|M|56,53|
+T Blood of the Chosen|QID|13336|M|61.8,56.6|N|To Knight-Captain Drosche, on the Skybreaker|
+T Capture More Dispatches|QID|13333|M|62.2,49.2|N|High Captain Justin Bartlett, on the Skybreaker|
+
+]]
+end)
