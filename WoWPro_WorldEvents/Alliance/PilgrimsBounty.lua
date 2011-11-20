b5f4d4b70d0f8ced1943e2323d7668eac056d224
Ludovicus <LuisOrtiz@Verizon.NET>
2011-11-20 10:10:45 -0500

    Unraveling the Mystery is given by Rutgar Glyphshaper

---
+++ WoWPro_WorldEvents/Alliance/PilgrimsBounty.lua
@@ -0,0 +1,54 @@
+WoWPro.WorldEvents:RegisterGuide("LudoPilBounty","Pilgrim's Bounty (Alliance)","Pilgrim's Bounty (Alliance)","WE", "Ludovicus", "Alliance", function()
+return [[
+A Pilgrim's Bounty|QID|14022|M|61.62,74.21|Z|Stormwind City|N|From Human Commoner|
+L Bountiful Cookbook|QID|14023|M|34.15,51.52|Z|Elwynn Forest|L|46809|N|Sold by Wilmina Holbeck, , near Stormwind gates.|
+N Open the Cookbook|U|46809|L|44860|N|Open the cookbook to get at the recipes|
+N Recipe: Spice Bread Stuffing|QID|14023||U|44860|N|Learn to cook Stuffing, then click to complete.|
+N Recipe: Pumpkin Pie|QID|14024|U|44862|N|Learn to cook Pie, then click to complete.|
+N Recipe: Cranberry Chutney|QID|14028|U|44858|N|Lean to cook Chutney, then click to complete.|
+N Recipe: Candied Sweet Potato|QID|14033|U|44859|N|Learn to cook Yams, then click to complete.|
+N Recipe: Slow-Roasted Turkey|QID|14035|U|44861|N|Learn to cook Turkey, then click to complete.|
+B Buy Autumnal Herbs|L|44835 20|N|You need 20 Herbs from Wilmina Holbeck, then click to complete.|
+B Buy Simple Flour|L|30817 10|N|You need 10 Flour|
+B Buy Mild Spices|L|2678 10|N|You need 10 Simple Spices|
+B Buy Ripe Elwynn Pumpkin|L|46784 10|You need 10 Pumpkins|
+B Buy Honey|L|44853 30|You will need 30 Honeys|
+T Pilgrim's Bounty|QID|14022|M|34.10,51.44|Z|Elwynn Forest|N|To Gregory Tabor, near Stormwind gates.|
+A Spice Bread Stuffing|QID|14023|M|34.10,51.44|Z|Elwynn Forest|N|From Gregory Tabor, near Stormwind gates.|
+L Spice Bread|QID|14023|L|30816 5|N|Cook up some Spice Bread|
+L Spice Bread Stuffing|QID|14023|L|44837 5|N|Cook up some stuff'n|
+A Sharing a Bountiful Feast|QID|14064|M|34.17,51.08|Z|Elwynn Forest|N|From the Stormwind Bountiful Table Hostess.|
+N Sharing a Bountiful Feast|QID|14064|N|Eat 5 of the 5 foods either by moving from chair to chair or passing them along|QO|Spirit of Sharing: 1/1|
+T Sharing a Bountiful Feast|QID|14064|M|34.17,51.08|Z|Elwynn Forest|N|To the Stormwind Bountiful Table Hostess.|
+T Spice Bread Stuffing|QID|14023|M|33.88,50.80|Z|Elwynn Forest|N|To Jasper Moore|
+A Pumpkin Pie|QID|14024|M|33.8,50.8|Z|Elwynn Forest|N|From Jasper Moore|
+L Pumpkin Pie|QID|14024|L|44836 5|N|Cook up some pie|
+T Pumpkin Pie|QID|14024|M|59.81,34.34|Z|Dun Morogh|N|To Edward Winslow, near Ironforge gates. |
+A Cranberry Chutney|QID|14028|M|59.81,34.34|Z|Dun Morogh|N|From Edward Winslow, near Ironforge gates.|
+L Buy Tangy Wetland Cranberries|L|44854 5|Buy 5 Cranberries from the Pilgrim's Bounty vendor|
+L Cranberry Chutney|QID|14028|L|44840 5|N|Cook up some Chutney|
+N Sharing a Bountiful Feast|M|59.90,34.98|Z|Dun Morogh|N|At the Ironforge Bountiful Table, eat 5 of the 5 foods either by moving from chair to chair or passing them along.|
+T Cranberry Chutney|QID|14028|M|33.8,50.8|Z|Elwynn Forest|N|To Jasper Moore, near Stormwind gates.|
+A They're Ravenous In Darnassus|QID|14030|M|33.8,50.8|Z|Elwynn Forest|N|From Jasper Moore|
+L Spice Bread|QID|14030|L|30816 5|N|Cook up some Spice Bread.|
+L Spice Bread Stuffing|QID|14030|L|44837 5|N|Cook up some stuff'n.|
+L Pumpkin Pie|QID|14030|L|44836 5|N|Cook up some pie.|
+b Teldrassil|M|22.79,55.94|Z|Stormwind City|N|Take the boat to Darnassus.|
+T They're Ravenous In Darnassus|QID|14030|M|61.48,49.11|Z|Darnassus|N|To Isaac Allerton, Darnassus, Warrior's Terrace.|
+A Candied Sweet Potatoes|QID|14033|M|61.4,49.2|Z|Darnassus|N|From Isaac Allerton|
+L Buy Teldrassil Sweet Potato|QID|14033|L|44855 5|N|Buy 5 Yams from the Pilgrim's Bounty vendor|
+L Candied Sweet Potatoes|QID|14033|L|44839 5|N|Cook up some yams|
+N Sharing a Bountiful Feast|M|63.38,47.55|Z|Darnassus|N|Eat 5 of the 5 foods either by moving from chair to chair or passing them along, then click to complete.|
+b The Exodar|M|44.25,78.77|Z|Darnassus|N|Take the portal to the Exodar|
+N Sharing a Bountiful Feast|M|76.54,52.03|Z|The Exodar|N|Eat 5 of the 5 foods either by moving from chair to chair or passing them along, then click to complete.|
+b Darnassus|M|47.56,62.22|Z|The Exodar|N|Take the portal to Darnassus|
+b Stormwind City|M|55.05,93.43|Z|Teldrassil|N|Take the boat back to Stormwind City|
+T Candied Sweet Potatoes|QID|14033|M|34.10,51.44|Z|Elwynn Forest|N|To Gregory Tabor, |
+A Slow-roasted Turkey|QID|14035|M|34.10,51.44|Z|Elwynn Forest|N|From Gregory Tabor|
+L Wild Turkey|QID|14035|M|47.55,59.55|Z|Elwynn Forest|L|44834 5|N|Get the raw turkeys, anywhere in Elwynn Forest|
+L Slow-roasted Turkey|QID|14035|L|44838 5|N|Cook up the turkeys|
+b Teldrassil|M|22.79,55.94|Z|Stormwind City|N|Take the boat to Darnassus.|
+T Slow-roasted Turkey|QID|14035|M|61.4,49.2|Z|Darnassus|N|To Isaac Allerton, Darnassus, Warrior's Terrace. |
+N Fini|N|Time to take a nap!|
+]]
+end)
