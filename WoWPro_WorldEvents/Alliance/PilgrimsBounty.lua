f9a3f9694b5dcf88b93658f24f9caadc4cb7b13e
Ludovicus <LuisOrtiz@Verizon.NET>
2011-11-21 05:38:04 +0800

    Update PilgrimsBounty

--- WoWPro_WorldEvents/Alliance/PilgrimsBounty.lua
+++ WoWPro_WorldEvents/Alliance/PilgrimsBounty.lua
@@ -1,54 +1,65 @@
-WoWPro.WorldEvents:RegisterGuide("LudoPilBounty","Pilgrim's Bounty (Alliance)","Pilgrim's Bounty (Alliance)","WE", "Ludovicus", "Alliance", function()
+WoWPro.WorldEvents:RegisterGuide("LudoPilBountyA","Pilgrim's Bounty (Alliance)","Pilgrim's Bounty (Alliance)","WE", "Ludovicus", "Alliance", function()
 return [[
+N Pilgrim's Bounty|QID|14022|N|This guide starts in Stormwind City and is designed to get you the following achievements: FOOD FIGHT!, Now We're Cookin', Pilgrim's Paunch, and Pilgrim's Peril.|
+N FOOD FIGHT!|QID|14022|N|Sit down at a table with at least one other person at it and target them and "share" the food a couple of times.|
+N Now We're Cookin'|QID|14022|N|Cook up one of every Pilgrim's Bounty dishes.|
+N Pilgrim's Paunch|QID|14022|N|Acquire the Spirit of Sharing at every Alliance capital.|
+N Pilgrim's Peril|QID|14022|N|Sit down at a table at every Horde capital.|
+N Sharing is Caring|QID|14022|N|Pass one of every dish at a Bountiful Table.|
 A Pilgrim's Bounty|QID|14022|M|61.62,74.21|Z|Stormwind City|N|From Human Commoner|
-L Bountiful Cookbook|QID|14023|M|34.15,51.52|Z|Elwynn Forest|L|46809|N|Sold by Wilmina Holbeck, , near Stormwind gates.|
-N Open the Cookbook|U|46809|L|44860|N|Open the cookbook to get at the recipes|
-N Recipe: Spice Bread Stuffing|QID|14023||U|44860|N|Learn to cook Stuffing, then click to complete.|
-N Recipe: Pumpkin Pie|QID|14024|U|44862|N|Learn to cook Pie, then click to complete.|
-N Recipe: Cranberry Chutney|QID|14028|U|44858|N|Lean to cook Chutney, then click to complete.|
-N Recipe: Candied Sweet Potato|QID|14033|U|44859|N|Learn to cook Yams, then click to complete.|
-N Recipe: Slow-Roasted Turkey|QID|14035|U|44861|N|Learn to cook Turkey, then click to complete.|
-B Buy Autumnal Herbs|L|44835 20|N|You need 20 Herbs from Wilmina Holbeck, then click to complete.|
-B Buy Simple Flour|L|30817 10|N|You need 10 Flour|
-B Buy Mild Spices|L|2678 10|N|You need 10 Simple Spices|
-B Buy Ripe Elwynn Pumpkin|L|46784 10|You need 10 Pumpkins|
-B Buy Honey|L|44853 30|You will need 30 Honeys|
+l Bountiful Cookbook|QID|14022|M|34.15,51.52|Z|Elwynn Forest|L|46809|N|Sold by Wilmina Holbeck, near Stormwind gates.|
+U Open the Cookbook|QID|14022|U|46809|L|44860|N|Open the cookbook to get at the recipes.|
+U Recipe: Spice Bread Stuffing|QID|14022||U|44860|N|Learn to cook Stuffing, then click to complete.|
+U Recipe: Pumpkin Pie|QID|14022|U|44862|N|Learn to cook Pie, then click to complete.|
+U Recipe: Cranberry Chutney|QID|14022|U|44858|N|Lean to cook Chutney, then click to complete.|
+U Recipe: Candied Sweet Potato|QID|14022|U|44859|N|Learn to cook Yams, then click to complete.|
+U Recipe: Slow-Roasted Turkey|QID|14022|U|44861|N|Learn to cook Turkey, then click to complete.|
+B Buy Autumnal Herbs|QID|14022|L|44835 20|N|You need 20 Herbs from Wilmina Holbeck, then click to complete.|
+B Buy Simple Flour|QID|14022|L|30817 10|N|You need 10 Flour|
+B Buy Mild Spices|QID|14022|L|2678 10|N|You need 10 Simple Spices|
+B Buy Ripe Elwynn Pumpkin|QID|14022|L|46784 10|You need 10 Pumpkins|
+B Buy Honey|QID|14022|L|44853 30|You will need 30 Honeys|
 T Pilgrim's Bounty|QID|14022|M|34.10,51.44|Z|Elwynn Forest|N|To Gregory Tabor, near Stormwind gates.|
 A Spice Bread Stuffing|QID|14023|M|34.10,51.44|Z|Elwynn Forest|N|From Gregory Tabor, near Stormwind gates.|
-L Spice Bread|QID|14023|L|30816 5|N|Cook up some Spice Bread|
-L Spice Bread Stuffing|QID|14023|L|44837 5|N|Cook up some stuff'n|
+l Spice Bread|QID|14023|L|30816 5|N|Cook up some Spice Bread|
+l Spice Bread Stuffing|QID|14023|L|44837 5|N|Cook up some stuff'n|
 A Sharing a Bountiful Feast|QID|14064|M|34.17,51.08|Z|Elwynn Forest|N|From the Stormwind Bountiful Table Hostess.|
 N Sharing a Bountiful Feast|QID|14064|N|Eat 5 of the 5 foods either by moving from chair to chair or passing them along|QO|Spirit of Sharing: 1/1|
 T Sharing a Bountiful Feast|QID|14064|M|34.17,51.08|Z|Elwynn Forest|N|To the Stormwind Bountiful Table Hostess.|
-T Spice Bread Stuffing|QID|14023|M|33.88,50.80|Z|Elwynn Forest|N|To Jasper Moore|
-A Pumpkin Pie|QID|14024|M|33.8,50.8|Z|Elwynn Forest|N|From Jasper Moore|
-L Pumpkin Pie|QID|14024|L|44836 5|N|Cook up some pie|
+T Spice Bread Stuffing|QID|14023|M|33.88,50.80|Z|Elwynn Forest|N|To Jasper Moore, Elwynn Forest.|
+A Pumpkin Pie|QID|14024|M|33.8,50.8|Z|Elwynn Forest|N|From Jasper Moore, Elwynn Forest.|
+l Pumpkin Pie|QID|14024|L|44836 5|N|Cook up some pie|
 T Pumpkin Pie|QID|14024|M|59.81,34.34|Z|Dun Morogh|N|To Edward Winslow, near Ironforge gates. |
 A Cranberry Chutney|QID|14028|M|59.81,34.34|Z|Dun Morogh|N|From Edward Winslow, near Ironforge gates.|
-L Buy Tangy Wetland Cranberries|L|44854 5|Buy 5 Cranberries from the Pilgrim's Bounty vendor|
-L Cranberry Chutney|QID|14028|L|44840 5|N|Cook up some Chutney|
-N Sharing a Bountiful Feast|M|59.90,34.98|Z|Dun Morogh|N|At the Ironforge Bountiful Table, eat 5 of the 5 foods either by moving from chair to chair or passing them along.|
+l Buy Tangy Wetland Cranberries|QID|14028|L|44854 5|Buy 5 Cranberries from the Pilgrim's Bounty vendor|
+l Cranberry Chutney|QID|14028|L|44840 5|N|Cook up some Chutney|
+N Sharing a Bountiful Feast|QID|14028|M|59.90,34.98|Z|Dun Morogh|N|At the Ironforge Bountiful Table, eat 5 of the 5 foods either by moving from chair to chair or passing them along.|
 T Cranberry Chutney|QID|14028|M|33.8,50.8|Z|Elwynn Forest|N|To Jasper Moore, near Stormwind gates.|
 A They're Ravenous In Darnassus|QID|14030|M|33.8,50.8|Z|Elwynn Forest|N|From Jasper Moore|
-L Spice Bread|QID|14030|L|30816 5|N|Cook up some Spice Bread.|
-L Spice Bread Stuffing|QID|14030|L|44837 5|N|Cook up some stuff'n.|
-L Pumpkin Pie|QID|14030|L|44836 5|N|Cook up some pie.|
-b Teldrassil|M|22.79,55.94|Z|Stormwind City|N|Take the boat to Darnassus.|
+l Spice Bread|QID|14030|L|30816 5|N|Cook up some Spice Bread.|
+l Spice Bread Stuffing|QID|14030|L|44837 5|N|Cook up some stuff'n.|
+l Pumpkin Pie|QID|14030|L|44836 5|N|Cook up some pie.|
+b Teldrassil|QID|14030|M|22.79,55.94|Z|Stormwind City|N|Take the boat to Darnassus.|
 T They're Ravenous In Darnassus|QID|14030|M|61.48,49.11|Z|Darnassus|N|To Isaac Allerton, Darnassus, Warrior's Terrace.|
 A Candied Sweet Potatoes|QID|14033|M|61.4,49.2|Z|Darnassus|N|From Isaac Allerton|
-L Buy Teldrassil Sweet Potato|QID|14033|L|44855 5|N|Buy 5 Yams from the Pilgrim's Bounty vendor|
-L Candied Sweet Potatoes|QID|14033|L|44839 5|N|Cook up some yams|
-N Sharing a Bountiful Feast|M|63.38,47.55|Z|Darnassus|N|Eat 5 of the 5 foods either by moving from chair to chair or passing them along, then click to complete.|
-b The Exodar|M|44.25,78.77|Z|Darnassus|N|Take the portal to the Exodar|
-N Sharing a Bountiful Feast|M|76.54,52.03|Z|The Exodar|N|Eat 5 of the 5 foods either by moving from chair to chair or passing them along, then click to complete.|
-b Darnassus|M|47.56,62.22|Z|The Exodar|N|Take the portal to Darnassus|
-b Stormwind City|M|55.05,93.43|Z|Teldrassil|N|Take the boat back to Stormwind City|
+B Buy Teldrassil Sweet Potato|QID|14033|L|44855 5|N|Buy 5 Sweet Potatoes from the Pilgrim's Bounty vendor|
+l Candied Sweet Potatoes|QID|14033|L|44839 5|N|Cook up some Sweet Potatoes|
+N Sharing a Bountiful Feast|QID|14033|M|63.38,47.55|Z|Darnassus|N|Eat 5 of the 5 foods either by moving from chair to chair or passing them along, then click to complete.|
+b The Exodar|QID|14033|M|44.25,78.77|Z|Darnassus|N|Take the portal to the Exodar|
+N Sharing a Bountiful Feast|QID|14033|M|76.54,52.03|Z|The Exodar|N|Eat 5 of the 5 foods either by moving from chair to chair or passing them along, then click to complete.|
+b Darnassus|QID|14033|M|47.56,62.22|Z|The Exodar|N|Take the portal to Darnassus|
+b Stormwind City|QID|14033|M|55.05,93.43|Z|Teldrassil|N|Take the boat back to Stormwind City|
 T Candied Sweet Potatoes|QID|14033|M|34.10,51.44|Z|Elwynn Forest|N|To Gregory Tabor, |
 A Slow-roasted Turkey|QID|14035|M|34.10,51.44|Z|Elwynn Forest|N|From Gregory Tabor|
-L Wild Turkey|QID|14035|M|47.55,59.55|Z|Elwynn Forest|L|44834 5|N|Get the raw turkeys, anywhere in Elwynn Forest|
-L Slow-roasted Turkey|QID|14035|L|44838 5|N|Cook up the turkeys|
-b Teldrassil|M|22.79,55.94|Z|Stormwind City|N|Take the boat to Darnassus.|
-T Slow-roasted Turkey|QID|14035|M|61.4,49.2|Z|Darnassus|N|To Isaac Allerton, Darnassus, Warrior's Terrace. |
+K Wild Turkey|QID|14035|M|47.55,59.55|Z|Elwynn Forest|L|44834 5|N|Get the raw turkeys, anywhere in Elwynn Forest|
+l Slow-roasted Turkey|QID|14035|L|44838 5|N|Cook up the turkeys|
+b Teldrassil|QID|14035|M|22.79,55.94|Z|Stormwind City|N|Take the boat to Darnassus.|
+T Slow-Roasted Turkey|QID|14035|M|61.4,49.2|Z|Darnassus|N|To Isaac Allerton, Darnassus, Warrior's Terrace. |
+N Get Clothes for Pilgrim's Peril|N|Do the dailies and get Pilgrim's Dress, Robe, or Attire for Pilgrim's Peril and put one on. Then ride or fly to your target.|
+N Pilgrim's Peril: Orgrimmar|M|46.2,14.2|Z|Durotar|N|Sit down at the table in Orgrimmar.|
+N Pilgrim's Peril: Thunder Bluff|M|46.8,59.0|Z|Mulgore|N|Sit down at the table in Thunder Bluff.|
+N Pilgrim's Peril: Undercity|M|64.46,7.89|Z|Undercity|N|Sit down at the table in Undercity.|
+N Pilgrim's Peril: Silvermoon City|M|55.4,53.2|Z|Eversong Woods|N|Sit down at the table in Silvermoon City.|
 N Fini|N|Time to take a nap!|
 ]]
 end)
--- WoWPro_WorldEvents/Horde/PilgrimsBounty.lua
+++ WoWPro_WorldEvents/Horde/PilgrimsBounty.lua
@@ -1,5 +1,11 @@
-WoWPro.WorldEvents:RegisterGuide("LudoPilBounty","Pilgrim's Bounty (Horde)","Pilgrim's Bounty (Horde)","WE", "Ludovicus", "Horde", function()
+WoWPro.WorldEvents:RegisterGuide("LudoPilBountyH","Pilgrim's Bounty (Horde)","Pilgrim's Bounty (Horde)","WE", "Ludovicus", "Horde", function()
 return [[
+N Pilgrim's Bounty|QID|14036|N|This guide starts in Orgrimmar and is designed to get you the following achievements: FOOD FIGHT!, Now We're Cookin', Pilgrim's Paunch, and Pilgrim's Peril|
+N FOOD FIGHT!|QID|14036|N|Sit down at a table with at least one other person at it and target them and "share" the food a couple of times.|
+N Now We're Cookin'|QID|14036|N|Cook up one of every Pilgrim's Bounty dishes.|
+N Pilgrim's Paunch|QID|14036|N|Acquire the Spirit of Sharing at every Horde capital.|
+N Pilgrim's Peril|QID|14036|N|Sit down at a table at every Alliance capital.|
+N Sharing is Caring|QID|14036|N|Pass one of every dish at a Bountiful Table.|
 A Pilgrim's Bounty|QID|14036|M|51.6,75.2|Z|Orgrimmar|N|From Orc Commoner|
 A Sharing a Bountiful Feast|QID|14065|M|46.2,14.2|Z|Durotar|N|From the Durotar Bountiful Feast Hostess|
 N Sharing a Bountiful Feast|QID|14065|N|Try each of the 5 foods either by moving from chair to chair or passing them along.|QO|Spirit of Sharing: 1/1|
@@ -7,51 +13,56 @@ T Sharing a Bountiful Feast|QID|14065|M|46.2,14.2|Z|Durotar|N|To the Durotar Bou
 b Tirisfal Glades|QID|14036|M|50.77,55.92|Z|Orgrimmar|N|Take the zepplin to Undercity|
 T Pilgrim's Bounty|QID|14036|M|64.2,11.4|Z|Undercity|N|From Miles Standish|
 A Spice Bread Stuffing|QID|14037|M|63.86,11.51|Z|Undercity|N|From Miles Standish, Undercity.|
-L Bountiful Cookbook|QID|14037|M|63.86,11.18|Z|Undercity|L|46810|N|Sold by Rose Standish, Undercity.|
-N Open the Cookbook|QID|14037|U|46810|N|Open the cookbook to get at the recipes, close off when done.|
-N Recipe: Spice Bread Stuffing|QID|14037||U|46803|N|Learn to cook Stuffing, close off when done.|
-N Recipe: Pumpkin Pie|QID|14024|U|46804|N|Learn to cook Pie, close off when done.|
-N Recipe: Cranberry Chutney|QID|14028|U|46805|N|Lean to cook Chutney, close off when done.|
-N Recipe: Candied Sweet Potato|QID|14033|U|46806|N|Learn to cook Yams, close off when done.|
-N Recipe: Slow-Roasted Turkey|QID|14035|U|46807|N|Learn to cook Turkey, close off when done.|
-B Buy Autumnal Herbs|L|44835 20|N|You need 20 Herbs from Rose Standish, Undercity.|
-B Buy Simple Flour|L|30817 10|N|You need 10 Flour|
-B Buy Mild Spices|L|2678 10|N|You need 10 Simple Spices|
-B Buy Honey|L|44853 30|You will need 30 Honeys|
+l Bountiful Cookbook|QID|14037|M|63.86,11.18|Z|Undercity|L|46810|N|Sold by Rose Standish, Undercity.|
+U Open the Cookbook|QID|14037|U|46810|L|46807|N|Open the cookbook to get at the recipes.|
+U Recipe: Spice Bread Stuffing|QID|14037||U|46803|N|Learn to cook Stuffing, close off when done.|
+U Recipe: Pumpkin Pie|QID|14037|U|46804|N|Learn to cook Pie, close off when done.|
+U Recipe: Cranberry Chutney|QID|14037|U|46805|N|Lean to cook Chutney, close off when done.|
+U Recipe: Candied Sweet Potato|QID|14037|U|46806|N|Learn to cook Yams, close off when done.|
+U Recipe: Slow-Roasted Turkey|QID|14037|U|46807|N|Learn to cook Turkey, close off when done.|
+B Buy Autumnal Herbs|QID|14037|L|44835 20|N|You need 20 Herbs from Rose Standish, Undercity.|
+B Buy Simple Flour|QID|14037|L|30817 10|N|You need 10 Flour|
+B Buy Mild Spices|QID|14037|L|2678 10|N|You need 10 Simple Spices|
+B Buy Honey|QID|14037|L|44853 30|You will need 30 Honeys|
 N Sharing a Bountiful Feast|QID|14037|M|64.46,7.89|Z|Undercity|N|Eat 5 of the 5 foods either by moving from chair to chair or passing them along.|
-L Spice Bread|QID|14037|L|30816 5|N|Cook up some Spice Bread|
+l Spice Bread|QID|14037|L|30816 5|N|Cook up some Spice Bread|
 N Spice Bread Stuffing|QID|14037|N|Cook up some stuff'n|QO|Spice Bread Stuffing: 5/5|
 T Spice Bread Stuffing|QID|14037|M|65.24,14.22|Z|Undercity|N|To William Mullins, Undercity.|
-B Buy Ripe Tirisfal Pumpkin|L|46796 10|You need 10 Pumpkins|
+B Buy Ripe Tirisfal Pumpkin|QID|14037|L|46796 10|You need 10 Pumpkins|
 A Pumpkin Pie|QID|14040|M|65.24,14.22|Z|Undercity|N|From William Mullins, Undercity.|
-L Pumpkin Pie|QID|14040|N|Cook up some pie|QO|Pumpkin Pie: 5/5|
+l Pumpkin Pie|QID|14040|N|Cook up some pie|QO|Pumpkin Pie: 5/5|
 b Orgrimmar|QID|14040|M|60.71,58.74|Z|Tirisfal Glades|N|Take the zepplin to Orgrimmar|
 T Pumpkin Pie|QID|14040|M|46.58,13.79|Z|Durotar|N|To Francis Eaton, Durotar|
 A Cranberry Chutney|QID|14041|M|6.58,13.79|Z|Durotar|N|From Francis Eaton, Durotar|
-B Buy Tangy Southfury Cranberries|M|46.62,13.79|Z|Durotar|L|46793 5|Buy 10 Cranberries from the Dalni Tallgrass.|
-L Cranberry Chutney|QID|14041|N|Cook up some Chutney|QO|Cranberry Chutney: 5/5|
+B Buy Tangy Southfury Cranberries|QID|14041|M|46.62,13.79|Z|Durotar|L|46793 10|Buy 10 Cranberries from the Dalni Tallgrass.|
+l Cranberry Chutney|QID|14041|N|Cook up some Chutney|QO|Cranberry Chutney: 5/5|
 b Thunder Bluff|QID|14041|M|43.04,65.00|Z|Orgrimmar|N|Take the zepplin to Thunder Bluff|
 T Cranberry Chutney|QID|14041|M|30.86,63.69|Z|Thunder Bluff|N|To Dokin Farplain, Thunder Bluff|
 A Candied Sweet Potatoes|QID|14043|M|30.86,63.69|Z|Thunder Bluff|N|From Dokin Farplain, Thunder Bluff|
-B Buy Mulgore Sweet Potato|QID|14043|M|31.02,63.33|Z|Thunder Bluff|L|46797 5|N|Buy 5 Yams from the Laha Farplain|
+B Buy Mulgore Sweet Potato|QID|14043|M|31.02,63.33|Z|Thunder Bluff|L|46797 5|N|Buy 5 Sweet Potatoes from the Laha Farplain|
 N Sharing a Bountiful Feast|QID|14043|M|46.8,59.0|Z|Mulgore|N|Eat 5 of the 5 foods either by moving from chair to chair or passing them along.|
-L Candied Sweet Potatoes|QID|14043|N|Cook up some yams||QO|Candied Sweet Potatoes: 5/5|
+l Candied Sweet Potatoes|QID|14043|N|Cook up some yams|QO|Candied Sweet Potatoes: 5/5|
 b Orgrimmar|QID|14043|M|15.35,25.65|Z|Thunder Bluff|N|Take the zepplin to Orgrimmar|
-T Candied Sweet Potatoes|QID|14043|M|6.58,13.79|Z|Durotar|N|To Francis Eaton, Durotar|
+T Candied Sweet Potatoes|QID|14043|M|46.58,13.79|Z|Durotar|N|To Francis Eaton, Durotar|
 A Undersupplied in the Undercity|QID|14044|M|6.58,13.79|Z|Durotar|N|From Francis Eaton, Durotar|
-L Spice Bread|QID|14044|L|30816 5|N|Cook up some Spice Bread|
-L Spice Bread Stuffing|QID|14044|N|Cook up some stuff'n|QO|Spice Bread Stuffing: 5/5|
-L Cranberry Chutney|QID|14044|N|Cook up some Chutney|QO|Cranberry Chutney: 5/5|
+l Spice Bread|QID|14044|L|30816 5|N|Cook up some Spice Bread|
+l Spice Bread Stuffing|QID|14044|N|Cook up some stuff'n|QO|Spice Bread Stuffing: 5/5|
+l Cranberry Chutney|QID|14044|N|Cook up some Chutney|QO|Cranberry Chutney: 5/5|
 b Tirisfal Glades|QID|14044|M|50.77,55.92|Z|Orgrimmar|N|Take the zepplin to Undercity|
 T Undersupplied in the Undercity|QID|14044|M|63.86,11.51|Z|Undercity|N|To Miles Standish, Undercity|
 A Slow-roasted Turkey|QID|14047|M|63.86,11.51|Z|Undercity|N|From Miles Standish, Undercity|
-L Wild Turkey|QID|14047|M|67.2,57.1|Z|Tirisfal Glades|L|44834 5|N|Get the raw turkeys|
-L Slow-Roasted Turkey|QID|14047|N|Cook up the turkeys|QO|Slow-Roasted Turkey: 5/5|
+K Wild Turkey|QID|14047|M|67.2,57.1|Z|Tirisfal Glades|L|44834 5|N|Get the raw turkeys, they are everywhere!|
+l Slow-Roasted Turkey|QID|14047|N|Cook up the turkeys|QO|Slow-Roasted Turkey: 5/5|
 b Silvermoon City|QID|14047|M|54.85,11.25|Z|Undercity|N|Zap yourself to Silvermoon City and then go to the gates.|
 N Sharing a Bountiful Feast|QID|14047|M|55.4,53.2|Z|Eversong Woods|N|Eat 5 of the 5 foods either by moving from chair to chair or passing them along.|
 b Undercity|QID|14047|M|49.53,14.81|Z|Silvermoon City|N|Zap yourself to Undercity|
-b Orgrimmar|QID|14040|M|60.71,58.74|Z|Tirisfal Glades|N|Take the zepplin to Orgrimmar|
+b Orgrimmar|QID|14047|M|60.71,58.74|Z|Tirisfal Glades|N|Take the zepplin to Orgrimmar|
 T Slow-roasted Turkey|QID|14047|M|46.6,13.8|Z|Durotar|N|To Francis Eaton, Durotar|
+N Get Clothes for Pilgrim's Peril|N|Do the dailies and get Pilgrim's Dress, Robe, or Attire for Pilgrim's Peril and put one on. Then ride or fly to your target.|
+A Pilgrim's Peril: Stormwind City|M|34.17,51.08|Z|Elwynn Forest|N|Sit down at the table in Stormwind City.|
+N Pilgrim's Peril: Ironforge|M|59.90,34.98|Z|Dun Morogh|N|Sit down at the table in Ironforge.|
+N Pilgrim's Peril: Darnassus|M|63.38,47.55|Z|Darnassus|N|Sit down at the table in Darnassus.|
+N Pilgrim's Peril: Exodar|M|76.54,52.03|Z|The Exodar|N|Sit down at the table in Exodar.|
 N Fini|N|Time to take a nap!|
 ]]
 end)
