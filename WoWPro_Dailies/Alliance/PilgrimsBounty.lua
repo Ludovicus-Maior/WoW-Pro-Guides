a9f8d7b8b9e6d6e7c8a14aff77ee9c2b6a041427
Ludovicus <LuisOrtiz@Verizon.NET>
2011-11-23 02:03:19 +0800

    Correct Loot counts and quest ordering

--- WoWPro_Dailies/Alliance/PilgrimsBounty.lua
+++ WoWPro_Dailies/Alliance/PilgrimsBounty.lua
@@ -13,12 +13,12 @@ K Wild Turkey|QID|14053|M|47.55,59.55|Z|Elwynn Forest|L|44834 20|N|Get the raw t
 b Ironforge|QID|14053|M|66.60,34.71|Z|Stormwind City|N|Take the Tram to Ironforge, or fly.|
 A Can't Get Enough Turkey|QID|14048|M|60.04,34.31|Z|Dun Morogh|N|From Caitrin Ironkettle, Ironforge Gates|
 A Don't Forget The Stuffing!|QID|14051|M|60.04,34.31|Z|Dun Morogh|N|From Caitrin Ironkettle, Ironforge Gates|
-l Slow-roasted Turkey|QID|14048|L|44838 5|N|Cook up the turkeys|
+l Slow-roasted Turkey|QID|14048|L|44838 20|N|Cook up the turkeys|
 T Can't Get Enough Turkey|QID|14048|M|60.04,34.31|Z|Dun Morogh|N|From Caitrin Ironkettle, Ironforge Gates|
 l Spice Bread|QID|14051|L|30816 20|N|Cook up some Spice Bread.|
 l Spice Bread Stuffing|QID|14051|L|44837 20|N|Cook up some stuff'n.|
-T Don't Forget The Stuffing!|QID|14051|M|60.04,34.31|Z|Dun Morogh|N|From Caitrin Ironkettle, Ironforge Gates|
-B Buy Tangy Wetland Cranberries|QID|14053|L|44854 20|Buy 20 Cranberries from the Pilgrim's Bounty vendor|
+T Don't Forget The Stuffing!|QID|14051|M|59.77,34.31|Z|Dun Morogh|N|From Caitrin Ironkettle, Ironforge Gates|
+B Buy Tangy Wetland Cranberries|QID|14053|M|59.77,34.34|Z|Dun Morogh|L|44854 20|Buy 20 Cranberries from the Pilgrim's Bounty vendor|
 
 b Stormwind City|QID|14053|M|73.02,50.22|Z|Ironforge|N|Take the Tram to Stormwind City to catch the boat to Darnassus.|
 b Teldrassil|QID|14053|M|22.79,55.94|Z|Stormwind City|N|Take the boat to Darnassus.|
@@ -26,7 +26,7 @@ b Teldrassil|QID|14053|M|22.79,55.94|Z|Stormwind City|N|Take the boat to Darnass
 A Easy As Pie|QID|14054|M|61.89,46.14|Z|Darnassus|N|From Mary Allerton, Warrior's Terrace, Darnassus|
 l Pumpkin Pie|QID|14054|L|44836 20|N|Cook up some pie.|
 T Easy As Pie|QID|14054|M|61.89,46.14|Z|Darnassus|N|From Mary Allerton, Warrior's Terrace, Darnassus|
-B Buy Teldrassil Sweet Potato|QID|14055|L|44855 20|N|Buy 20 Sweet Potatoes from the Pilgrim's Bounty vendor|
+B Buy Teldrassil Sweet Potato|QID|14055|M|61.65,49.12|Z|Darnassus|L|44855 20|N|Buy 20 Sweet Potatoes from the Pilgrim's Bounty vendor|
 
 b Stormwind City|QID|14053|M|55.05,93.43|Z|Teldrassil|N|Take the boat back to Stormwind City.|
 l Candied Sweet Potatoes|QID|14055|M|33.88,50.80|Z|Elwynn Forest|L|44839 20|N|Cook up some Sweet Potatoes|
