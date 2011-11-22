2b0aa371f7c86129d2f63027e088fa90bd78c554
Ludovicus <LuisOrtiz@Verizon.NET>
2011-11-23 01:59:54 +0800

    Unraveling the Mystery is from Rutgar Glyphshaper

--- WoWPro_Leveling/Neutral/55_60_Jiyambi_Silithus.lua
+++ WoWPro_Leveling/Neutral/55_60_Jiyambi_Silithus.lua
@@ -138,7 +138,7 @@ T Glyph Chasing|QID|8309|M|46.37,79.08|N|To Rutgar Glyphshaper.|
 T Brann Bronzebeard's Lost Letter|QID|8308|O|M|46.37,79.08|N|To Rutgar Glyphshaper.|
 T Breaking the Code|QID|8310|M|45.96,79.39|N|To Frankal Stonebridge.|
 
-A Unraveling the Mystery|QID|8314|M|45.96,79.39|N|To Frankal Stonebridge.|
+A Unraveling the Mystery|QID|8314|M|46.37,79.08|N|To Rutgar Glyphshaper.|
 
 C Wanted - Deathclasp, Terror of the Sands|QID|8283|M|49.61,82.45|N|Path is behind the camp. Watch his kickback. You might want to position him so that your back is to the wall.|
 C Noggle's Lost Satchel|QID|8282|M|49.32,81.46|N|Up the hill on the right.|
