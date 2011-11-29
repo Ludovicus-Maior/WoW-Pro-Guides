644e2b2f8c5b25cf4c09ce9a77387f9fefd9d2f8
Ludovicus <LuisOrtiz@Verizon.NET>
2011-11-29 16:19:55 -0500

    First draft of Winter Veil Event

---
+++ WoWPro_WorldEvents/Horde/WinterVeil.lua
@@ -0,0 +1,33 @@
+WoWPro.WorldEvents:RegisterGuide("LudoWinterVeilH","Winter Veil (Horde)","Winter Veil (Horde)","WE", "Ludovicus", "Horde", function()
+return [[
+N Eggs, Babs, Eggs!|N|You are going to need 5 Small Eggs.  You can either pay outrageous prices on the AH, or farm them and sell the extra ones for outrageous prices on the AH.  I'll give you 4 places to farm them.|
+K Small Eggs: Eversong Woods|M|40.4,50.8;45.8,64.8;48.0,53.6|Z|Eversong Woods|L|6889 20|N|Kill Feral Dragonhawk Hatchlings within the markers.|
+K Small Eggs: Eversong Woods|M|54.8,61.6;62.2,59.4;70.2,59.6|Z|Eversong Woods|L|6889 20|N|Kill Crazed Dragonhawks between the markers.|
+K Small Eggs: Mulgore|M|48.2,50.2;52.2,35.2;57.2,48.0|Z|Mulgore|L|6889 20|N|Kill Wiry Swoops|
+K Small Eggs: Mulgore|M|43.4,21.8;48.8,9.6;59.0,21.4|Z|Mulgore|L|6889 20|N|Kill Taloned Swoops; Use the back lift from Thunder Bluffs.|
+
+A Great-father Winter is Here!|QID|6961|LVL|10|M|52.4,77.2|Z|Orgrimmar|From Kaymard Copperpinch.|
+T Great-father Winter is Here!|QID|6961|LVL|10|M|49.8,77.8|Z|Orgrimmar|To Great-father Winter.|
+A Treats for Great-father Winter|QID|6962|LVL|10|M|9.8,77.8|Z|Orgrimmar|From Great-father Winter.|
+B Recipe: Gingerbread Cookie|QID|6962|M|52.6,77.6|Z|Orgrimmar|L|17200|From Penney Copperpinch.|
+B Holiday Spices|QID|6962|M|52.6,77.6|Z|Orgrimmar|L|17194 5|N|From Penney Copperpinch.|
+B Small Eggs|QID|6962|M|52.6,77.6|Z|Orgrimmar|L|6889 5|N|If you did not farm the eggs, buy them at the AH.|
+U Learn the Recipe|U|17200|N|Click off when done!|
+B Milk|QID|6962|L|1179|M|54.8,78.0|Z|Orgrimmar|N|Buy Milk from Barkeep Morag.|
+L Make 5 Gingerbread Cookies|L|17197 5|
+T Treats for Great-father Winter|QID|6962|LVL|10|M|9.8,77.8|Z|Orgrimmar|To Great-father Winter.|
+
+N Time to wait|N|Wait till Dec 25th!|
+b Orgrimmar|QID|8767|N|Go see Great-father Winter in Orgrimmar|
+A Winter's Presents|QID|8828|M|50.0,61.6|Z|Orgrimmar|N|From Wonderform Operator.|
+
+T Winter's Presents|QID|8828|M|49.8,77.8|Z|Orgrimmar|N|To Great-father Winter.|
+A A Gently Shaken Gift|QID|8767,8788|LVL|1|M|49.1,78.2|Z|Orgrimmar|N|Your first gift!|
+A A Carefully Wrapped Present|QID|8744|LVL|1|M|49.3,78.4|Z|Orgrimmar|N|Your second gift!|
+A A Festive Gift|QID|8803|LVL|10|M|49.6,77.7|Z|Orgrimmar|N|Your third gift!|
+A A Winter Veil Gift|QID|28878|LVL|10|M|49.4,77.5|Z|Orgrimmar|N|Your fourth gift!|
+A A Gaily Wrapped Present|QID|8768|LVL|20|M|49.6,78.2|Z|Orgrimmar|N|Your fifth gift!|
+
+]]
+end)
+
