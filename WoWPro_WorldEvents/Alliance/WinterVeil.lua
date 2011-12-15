e0f94e1421ef7f98399d203b0e85f827e875fc80
Ludovicus <LuisOrtiz@Verizon.NET>
2011-12-15 13:11:23 -0500

    Update WinterVeil to refect new truncated questline.

--- WoWPro_WorldEvents/Alliance/WinterVeil.lua
+++ WoWPro_WorldEvents/Alliance/WinterVeil.lua
@@ -5,50 +5,40 @@ K Small Eggs: Darkshore|M|41.4,47.2;42.4,52.4;46.6,46.4|Z|Darkshore|L|6889 20|T|
 K Small Eggs: Azuremyst Isle|M|46.2,16.4;48.8,15.4,49.4,10.8|Z|Azuremyst Isle|L|6889 20|T|Crazed Wildkin|N|Kill Crazed Wildkin between the markers, north of Stillpine Hold.|
 K Small Eggs: Loch Modan|M|48.2,60.6;50.6,58.4;55.0,61.4|Z|Loch Modan|L|6889 20|T|Loch Buzzard|N|Kill Loch Buzzards, between the markers, south of the lake.|
 K Small Eggs: Westfall|M|55.8,23.4;47.6,18.0;46.6,30.4|Z|Westfall|L|6889 20|T|[nodead] Young Fleshripper|N|Kill any kind of Fleshrippers. Markers are for the young ones.|
-K Deeprock Salt: Badlands|M|29.4,58.6;42.0,43.4|Z|Badlands|L|8150 11|N|Kill Rock Elementals between the markers, 1% drop rate.|
-K Deeprock Salt: Maraudon|M|31.4,78.2;31.6,78.2;31.8,78.8;35.8,59.6;36,59.4;40,53.4;40.2,53.6;40.6,53.4;40.8,64.2;40.8,64.8;41,54;41.8,74.2;42.4,75.2;54.4,92.6|Z|Maraudon|L|8150 11|N|Kill Rock Primordial Behemoths at the markers, 30% drop rate.|
-K Deeprock Salt: Uldaman1|M|36.4,51;36.8,50.6;37.4,49.8;37.8,49.8;38.2,49.4;39.2,49.2;39.4,9.4;39.4,9.6;39.6,9.4;39.6,10.2;39.6,49.2;40,11.4;40,11.6;40.4,8.4;40.8,49.2;41,8;41.2,38;41.2,38.6;41.4,39.6;41.6,40.2;41.6,40.6;41.8,49.2;42.4,6.8;42.4,19.6;42.8,6.6;43,6.4;43,21;43.2,41.2;43.4,22.4;43.4,22.6;43.6,22.4;43.6,22.8;43.6,49.2;43.8,23.6;44,41.2;44.2,5.8;44.6,41.4;44.8,6.4;45,7.2;45,41.6;45.4,8.2;45.4,46;45.4,46.6;45.4,48.2;45.4,49;45.6,41.4;45.6,43.4;45.6,44;45.6,44.8;45.6,46.2;45.6,46.6;45.6,47.6;45.6,49;45.8,9.2;46,41.6;46.2,10.4;46.4,11;46.6,11;46.6,43.6;47.8,50.4;48.4,51|Z|Uldaman|L|8150 11|N|Kill Rock Primordial Behemoths at the markers, 27% drop rate.|
-K Deeprock Salt: Uldaman2|M|60.8,63.6;61.4,63.2;61.6,62.8;62.4,62.4;62.6,62.6;63,62;63.8,61.4;63.8,61.6;64,40.4;64,40.6;64.4,41.8;64.4,42.6;64.6,41.4;64.6,42.4;64.6,61;64.8,43;65,43.6;65.4,44.8;65.6,45;65.6,45.6;66,47;66,59.8;66.2,47.6;66.8,49.4;67,50;67,59;67.2,50.6;67.4,51.6;67.6,52.4;67.6,58.6;67.8,53;67.8,57.8;68.6,55.8|Z|Uldaman|L|8150 11|N|Kill Rock Primordial Behemoths at the markers, 27% drop rate.|
 
-A Greatfather Winter is Here!|QID|7022|M|33.2,67.2|Z|Ironforge|From Wulmort Jinglepocket|
-T Greatfather Winter is Here!|QID|7022|M|33.2,67.2|Z|Ironforge|To Greatfather Winter.|
-A Treats for Greatfather Winter|QID|13444|M|33.2,67.2|Z|Ironforge|From Greatfather Winter.|
-B Recipe: Gingerbread Cookie|QID|6962|M|33.6,68.0|Z|Ironforge|From Macey Jinglepocket.|
-B Holiday Spices|QID|6962|M|33.6,68.0|Z|Ironforge|L|N|Buy 5 Holiday Spices from Macey Jinglepocket.|
-B Small Eggs|QID|6962|M|52.6,77.6|Z|Ironforge|L|6889 5|N|Buy 5 Small Eggs from the Auction House.|
-B Milk|QID|6962|L|1179|M|19.6,52.6|Z|Ironforge|N|Buy Milk from Gwenna Firebrew.|
-L Make 5 Gingerbread Cookies|L|17197 5|
-A Treats for Greatfather Winter|QID|13444|M|33.2,67.2|Z|Ironforge|To Greatfather Winter.|
+N On the first day of Winter Veil ...|N|Now wait till the first day ..|
 
-A The Reason for the Season|QID|7062|M|30.2,60.0|Z|Ironforge|N|From Goli Krumn.|
-T The Reason for the Season|QID|7062|M|77.0,11.8|Z|Ironforge|N|To Historian Karnik, in the Hall of Explorers.|
-A The Feast of Winter Veil|QID|7063|M|77.0,11.8|Z|Ironforge|N|From Historian Karnik, in the Hall of Explorers.|
-T The Feast of Winter Veil|QID|7063|M|39.4,55.8|Z|Ironforge|N|To Muradin Bronzebeard, in the room of thrones.|
+A Greatfather Winter is Here!|QID|7022|M|33.59,67.91|Z|Ironforge|N|From Wulmort Jinglepocket|or 7023|
+T Greatfather Winter is Here!|QID|7022|M|33.16,65.46|Z|Ironforge|N|To Greatfather Winter.|
+A Treats for Greatfather Winter|QID|7025|M|33.16,65.46|Z|Ironforge|N|From Greatfather Winter.|
+B Recipe: Gingerbread Cookie|QID|7025|M|33.59,67.91|Z|Ironforge|L|17200|N|From Wulmort Jinglepocket.|
+B Holiday Spices|QID|6962|M|33.59,67.91|Z|Ironforge|L|17194 5|N|Buy 5 Holiday Spices from Wulmort Jinglepocket.|
+U Learn the Recipe|U|17200|N|Click off when done!|
+B Small Eggs|QID|7025|M|24.83,73.72|Z|Ironforge|L|6889 5|N|Buy 5 Small Eggs from the Auction House.|
+B Milk|QID|7025|L|1179|M|18.60,51.75|Z|Ironforge|N|Buy Milk from Gwenna Firebrew.|
+L Make 5 Gingerbread Cookies|L|17197 5|N|Click off when done.|
+T Treats for Greatfather Winter|QID|7025|M|33.16,65.46|Z|Ironforge|To Greatfather Winter.|
 
+A The Reason for the Season|QID|7062|M|30.25,59.41|Z|Ironforge|N|From Goli Krumn.|
+T The Reason for the Season|QID|7062|M|77.52,11.83|Z|Ironforge|N|To Historian Karnik, in the Hall of Explorers.|
+A The Feast of Winter Veil|QID|7063|M|77.52,11.83|Z|Ironforge|N|From Historian Karnik, in the Hall of Explorers.|
+T The Feast of Winter Veil|QID|7063|M|39.18,56.10|Z|Ironforge|N|To Muradin Bronzebeard, in the room of thrones.|
 
-A Stolen Winter Veil Treats|QID|7042|M|33.6,68.0|Z|Ironforge|N|From Macey Jinglepocket.|
-T Stolen Winter Veil Treats|QID|7042|M|42.2,41.0|Z|Hillsbrad Foothills|N|To Strange Snowman.|
-A You're a Mean One...|QID|7043|M|42.2,41.0|Z|Hillsbrad Foothills|N|From Strange Snowman.|
-K The Abominable Greench|QID|7043|M|46.0,26.8;45.4,43.0;49.0,34.4|Z|Hillsbrad Foothills|L|17662|N|He wanders about quite a bit.  Keep going between the markers till you find him. Look out for "Holiday Snow" on the ground for snowballs to throw.|
-T You're a Mean One...|QID|7043|M|33.2,67.2|Z|Ironforge|N|To Wulmort Jinglepocket.|
-A A Smokywood Pastures' Thank You!|QID|7045|M|33.2,67.2|Z|Ironforge|N|From Wulmort Jinglepocket.|
-T A Smokywood Pastures' Thank You!|QID|7045|M|33.2,67.2|Z|Ironforge|To Greatfather Winter.|
+N Free Metzen|N|This new pair of daily quests are only for level 80 and above.|
+A You're a Mean One...|QID|7043|M|33.59,67.91|Z|Ironforge|N|From Wulmort Jinglepocket.|
+K The Abominable Greench|QID|7043|M|45.26,40.32|Z|Hillsbrad Foothills|N|Go to Growless Cave in the Foothills. Loot the Treats from the bags (fast) and talk to Metzen (fast) to complete if you were not around for the kill.  Yes, if you get there at the right time, you can complete the quest!|
+N Snowball|N|BTW, pick up some snowballs from the mounds around the cave.|
+T You're a Mean One...|QID|7043|M|33.59,67.91|Z|Ironforge|N|From Wulmort Jinglepocket.|
+A Smokywood Pastures' Thank You!|QID|7045|M|33.59,67.91|Z|Ironforge|N|From Wulmort Jinglepocket.|
+T Smokywood Pastures' Thank You!|QID|7045|M|33.16,65.46|Z|Ironforge|To Greatfather Winter.|
 
-A Metzen the Reindeer|QID|8762|M|33.2,67.2|Z|Ironforge|N|From Wulmort Jinglepocket.|
-U Smokywood Satchel|QID|8762|U|21315|L|21211|N|Open the Smokywood Satchel.|
-U Metzen the Reindeer|QID|8762|M|68.4,34.3|Z|Searing Gorge|N|U|21211|Talk to Metzen and then use the dust on him.|
-T Metzen the Reindeer|QID|8762|M|33.2,67.2|Z|Ironforge|N|To Wulmort Jinglepocket.|
-A The Hero of the Day|QID|8763|M|33.2,67.2|Z|Ironforge|N|From Wulmort Jinglepocket.|
-B Deeprock Salt|QID|8763|M|33.2,67.2|Z|Ironforge|L|8150|N|Buy some Deeprock Salt from the Auction House.|
-T The Hero of the Day|QID|8763|M|37.1,70.3|Z|Ironforge|N|To Holly Preserver.|
-
-N Time to wait|N|Wait till Dec 25th!|
+N Time to wait, again!|N|Wait till Dec 25th.|
 b Ironforge|QID|8767|N|Go see Greatfather Winter in Ironforge|
-A A Gently Shaken Gift|QID|8767,8788|LVL|1|M|33.4,66.0|Z|Ironforge|N|Your first gift!|
-A A Carefully Wrapped Present|QID|8744|LVL|1|M|33.5,60.0|Z|Ironforge|N|Your first gift!|
+A A Gently Shaken Gift|QID|8767,8788|M|33.4,66.0|Z|Ironforge|N|Your first gift!|
+A A Carefully Wrapped Present|QID|8744|M|33.5,60.0|Z|Ironforge|N|Your first gift!|
 A A Festive Gift|QID|8803|M|33.6,60.0|Z|Ironforge|N|Your third gift!|
 A A Winter Veil Gift|QID|28878|M|33.4,66.0|Z|Ironforge|N|Your fourth gift!|
-A A Gaily Wrapped Present|QID|8768|LVL|20|M|33.5,66.2|Z|Ironforge|N|Your fifth gift!|
+A A Gaily Wrapped Present|QID|8768|M|33.5,66.2|Z|Ironforge|N|Your fifth gift!|
 
 ]]
 end)
