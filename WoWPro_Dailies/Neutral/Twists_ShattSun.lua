54cfc50b4d859d4b4b848cdb5612cd93f1e22460
Ludovicus <LuisOrtiz@Verizon.NET>
2012-01-31 16:59:17 -0500

    Twists original Shattered Sun guide

---
+++ WoWPro_Dailies/Neutral/Twists_ShattSun.lua
@@ -0,0 +1,106 @@
+WoWPro.Dailies:RegisterGuide("TwiShattSun", "Isle of Quel'Danas", "Shattered Sun Offensive", "BC", "Twists", "Neutral", function()
+return [[
+
+R Isle of Quel'Danas|M|48.85,42.07|N|Take the portal in Shatt.|Z|Shattrath City|
+
+A The Battle Must Go On|QID|11537|M|47.68,34.98|N|From Harbinger Inuuro.|
+A The Air Strikes Must Continue|QID|11533|M|47.56,35.03|N|From Battlemage Arynna.|
+A Know Your Ley Lines|QID|11547|M|47.49,35.23|N|From Astromancer Darnarian.|
+
+A Further Conversions|QID|11525|M|47.13,30.65|N|From Vindicator Xayann.|
+A Arm the Wards!|QID|11523|M|47.38,30.55|N|From Captain Theris Dawnhearth.|
+
+A Rediscovering your Roots|QID|11521|M|51.49,32.47|N|From Mar'nah.|
+A Open for Business|QID|11546|M|51.49,32.47|N|From Mar'nah.|
+
+A Your Continued Support|QID|11548|M|48.27,37.71|N|From Anchorite Ayuri.|
+T Your Continued Support|QID|11548|M|48.27,37.71|N|From Anchorite Ayuri.|
+
+A Keeping the Enemy at Bay|QID|11543|M|50.54,39.03|N|From Vindicator Kaalan.|
+
+A Don't Stop Now....|QID|11536|M|50.56,40.69|N|From Smith Hauthaa.|
+A Ata'mal Armaments|QID|11544|M|50.56,40.69|N|From Smith Hauthaa.|
+
+A Disrupt the Greengill Coast|QID|11541|M|53.81,34.20|N|From Captain Valindria.|
+
+C Open for Business|QID|11546|N|Found on the ground.|S|
+K Darkspine Myrmidon|QID|11536|N|You need keys.|L|34477 3|S|
+K Darkspine Siren|QID|11541|M|63.68,41.93|N|You need orbs.|L|34483 4|
+
+C Disrupt the Greengill Coast|QID|11541|N|Release Murlocs.|U|34483|S|
+C Don't Stop Now....|QID|11536|N|Open Boxes.|S|
+K Darkspine Myrmidon|QID|11536|N|You need keys.|L|34477 3|US|
+C Know Your Ley Lines|QID|11547|M|61.16,62.36|QO|Shrine Reading Taken:1/1|U|34533|
+C Disrupt the Greengill Coast|QID|11541|N|Release Murlocs.|U|34483|US|
+C Don't Stop Now....|QID|11536|N|Open Boxes.|US|
+
+T Disrupt the Greengill Coast|QID|11541|M|53.81,34.20|N|To Captain Valindria.|
+T Don't Stop Now....|QID|11536|M|50.56,40.69|N|To Smith Hauthaa.|
+
+C The Battle Must Go On|QID|11537|N|Plant the banner after Killing the large one, Emissary of Hate.|U|34414|S|
+C Know Your Ley Lines|QID|11547|M|48.53,44.47|QO|Portal Reading Taken:1/1|U|34533|
+C The Battle Must Go On|QID|11537|N|Plant the banner after Killing the large one, Emissary of Hate.|U|34414|US|
+
+C Know Your Ley Lines|QID|11547|M|42.23,36.07|U|34533|
+
+C Further Conversions|QID|11525|N|Kill sentries and then use the crystal.|U|34368|S|
+K Wretched Fiend|QID|11523|M|39.44,37.53|N|Your looking for mana Remnants.|L|34338 4|
+C Further Conversions|QID|11525|M|41.96,28.40|N|Kill sentries and then use the crystal.|US|34368|
+C Open for Business|QID|11546|N|Found on the ground.|US|
+
+C The Air Strikes Must Continue|QID|11533|M|48.45,25.29|N|Speak to Ayren Cloudbreaker and select Speaking of Action.|U|34475|
+C Keeping the Enemy at Bay|QID|11543|M|48.45,25.29|N|Speak to Ayren Cloudbreaker and select I need to intercept. Aim for the sails.|U|34489|
+C Arm the Wards!|QID|11523|M|48.87,29.22|U|34338|
+
+T Further Conversions|QID|11525|M|47.13,30.65|N|To Vindicator Xayann.|
+T Arm the Wards!|QID|11523|M|47.38,30.55|N|To Captain Theris Dawnhearth.|
+
+T The Battle Must Go On|QID|11537|M|47.68,34.98|N|To Harbinger Inuuro.|
+T The Air Strikes Must Continue|QID|11533|M|47.56,35.03|N|To Battlemage Arynna.|
+T Know Your Ley Lines|QID|11547|M|47.49,35.23|N|To Astromancer Darnarian.|
+
+T Open for Business|QID|11546|M|51.49,32.47|N|To Mar'nah.|
+T Keeping the Enemy at Bay|QID|11543|M|50.54,39.03|N|To Vindicator Kaalan.|
+
+R Shattrath City|N|Use Darnarian's Scroll of Teleportation to return to Shattrath City.|Z|Shattrath City|U|35230|
+
+A Maintaining the Sunwell Portal|QID|11514|M|49.24,42.57|N|From Exarch Nasuun.|Z|Shattrath City|
+A Sunfury Attack Plans|QID|11877|M|62.70,35.97|N|From Lord Torvos.|Z|Shattrath City|
+A The Multiphase Survey|QID|11880|M|61.61,51.97|N|From Harbinger Haronem.|Z|Shattrath City|
+
+C The Multiphase Survey|QID|11880|M|44.66,70.40|N|Equip these once you arrive.\n\nLook for orange balls.|Z|Nagrand|
+
+K Bash'ir Raider, Spell-Thief, or Arcanist|QID|11514|M|49.50,20.57|N|Your looking for the Bash'ir Phasing Device.|L|34248|Z|Blade's Edge Mountains|
+C Maintaining the Sunwell Portal|QID|11514|N|Activate and then look for pink boxes.|U|34248|Z|Blade's Edge Mountains|
+C Sunfury Attack Plans|QID|11877|M|19.48,68.22|Z|Netherstorm|
+
+A Blood for Blood|QID|11515|M|58.11,17.69|N|From Magistrix Seyla.|Z|Hellfire Peninsula|
+A Blast the Gateway|QID|11516|M|58.11,17.69|N|From Magistrix Seyla.|Z|Hellfire Peninsula|
+
+C Blast the Gateway|QID|11516|N|Summon your Sizzling Ember and Kill Fel Sparks until it turns green.|Z|Hellfire Peninsula|U|34253|S|
+K Wrath Herald|QID|11515|N|For their Demonic Blood|L|34259 4|Z|Hellfire Peninsula|
+C Blood for Blood|QID|11515|N|Use the Fel Siphon on Felblood Initiates. Wait for them to change to Emaciated Felbloods before killing. Do not move once you start.|Z|Hellfire Peninsula|U|34257|
+C Blast the Gateway|QID|11516|M|58.56,18.63|N|Once it turns green, head to the portal.|Z|Hellfire Peninsula|US|
+
+T Blood for Blood|QID|11515|M|58.11,17.69|N|To Magistrix Seyla.|Z|Hellfire Peninsula|
+T Blast the Gateway|QID|11516|M|58.11,17.69|N|To Magistrix Seyla.|Z|Hellfire Peninsula|
+
+K Shadowmoon Valley Orcs|QID|11544|M|66.60,42.40|N|Watch for the elite.|L|34500 5|Z|Shadowmoon Valley|
+K Razorthorn Flayers|QID|11521|M|58.76,12.93|N|Looking for Razorthorn Flayer Gland.|Z|Terokkar Forest|L|34255|
+C Rediscovering your Roots|QID|11521|N|Use the Gland on a Ravager. Then use the pet ability expose Razorthorn root next to a dirt mount to recover the roots.|Z|Terokkar Forest|U|34255|
+
+T Sunfury Attack Plans|QID|11877|M|62.70,35.97|N|From Lord Torvos.|Z|Shattrath City|
+T The Multiphase Survey|QID|11880|M|61.61,51.97|N|From Harbinger Haronem.|Z|Shattrath City|
+T Maintaining the Sunwell Portal|QID|11514|M|49.24,42.57|N|To Exarch Nasuun.|Z|Shattrath City|
+
+R Isle of Quel'Danas|M|48.85,42.07|N|Take the portal in Shatt.|Z|Shattrath City|
+
+T Rediscovering your Roots|QID|11521|M|51.49,32.47|N|To Mar'nah.|
+C Ata'mal Armaments|QID|11544|M|50.56,40.69|N|Cleanse the ore at the forge.|U|34500|
+T Ata'mal Armaments|QID|11544|M|50.56,40.69|N|From Smith Hauthaa.|
+
+N End of Guide|N|You've reached the end of the guide! This guide will automatically reset when the dailies reset, or you can reset it manually by right-clicking this window's titlebar or frame.|
+]]
+
+end)
+
