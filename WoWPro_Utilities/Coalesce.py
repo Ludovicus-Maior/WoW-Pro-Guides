345ecb0b5a5fb1e418fd1ed4088fb05d1dddbdc8
Ludovicus <LuisOrtiz@Verizon.NET>
2012-02-11 11:01:11 -0500

    Helper script to merge a list of coordinates

---
+++ WoWPro_Utilities/Coalesce.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+
+import sys
+from math import sqrt
+import string
+
+def Delta(xy1,xy2):
+    return sqrt((xy1[0]-xy2[0])*(xy1[0]-xy2[0]) + (xy1[1]-xy2[1])*(xy1[1]-xy2[1]) )
+
+def Meld(xy1,xy2):
+    return ( (xy1[0]+xy2[0])/2 , (xy1[1]+xy2[1])/2 )
+
+def MeldPair(XYs, minDelta):
+    # Find the pair that is closest
+    delta = 1000
+    for IDX in range(0,len(XYs)):
+        for idx in range(IDX+1,len(XYs)):
+            if idx == IDX: continue
+            d = Delta(XYs[IDX],XYs[idx])
+#            print "# [%d,%d] %s -> %s delta %g" % (IDX,idx,XYs[IDX],XYs[idx],d)
+            if (d < delta):
+                print "Candidates to merge %s and %s with distance %g" % (XYs[IDX],XYs[idx],d)
+                lowIDX=IDX
+                highIDX=idx
+                delta = d
+    if delta > minDelta:
+        return XYs,1000
+    
+    print "Merging %d and %d" % (lowIDX,highIDX)
+    XY1=XYs[lowIDX]
+    XY2=XYs[highIDX]
+    XYs.remove(XY1)
+    XYs.remove(XY2)
+    XYs.append(Meld(XY1,XY2))
+    
+    return XYs,delta
+        
+
+def Coalesce(coords):
+    # First split along the semi-colons
+    CoordSet= coords.split(';')
+    print "Found %d Coordinates" % ( len(CoordSet))
+    if len(CoordSet) == 1:
+        print "Assuming WoWHead format."
+        coords = string.replace(coords,"],[",";")
+        coords = string.replace(coords,"[","")
+        coords = string.replace(coords,"]","")
+        CoordSet= coords.split(';')
+        print "Found %d Coordinates" % ( len(CoordSet))
+    
+    
+    # Now split along the commas and create the numeric form
+    XYs=[]
+    for coord in CoordSet:
+        C=coord.split(',')
+        if(len(C) != 2):
+            print "! Bad coord length [%s]" % (coord)
+            return None
+        xy=[]
+        for xx in C:
+            try:
+                x=float(xx)
+                xy.append(x)
+            except:
+                x = -1
+                if( x < 0 or x > 100):
+                    print "! Bad coord value %s: [%s]" % (coord)
+                    return None               
+        
+        XYs.append(xy)
+    
+    minDelta=2.0
+    actualDelta = 0
+    while actualDelta <= minDelta:
+        XYs,actualDelta = MeldPair(XYs,minDelta)
+        print "List now has %d elements" % len(XYs)
+    
+    firstItem=True
+    for xy in XYs:
+        if not firstItem:
+            sys.stdout.write(";")
+        firstItem = False
+        sys.stdout.write("%2.2f,%2.2f" %(xy[0],xy[1]))
+    print 
+    return None
+        
+            
+
+if __name__ == "__main__":
+    for arg in sys.argv[1:]:
+        Coalesce(arg)
+
+
+
