6941ea492982c14466b08fa1eb176e28a88e6d4c
Ludovicus <LuisOrtiz@Verizon.NET>
2012-06-30 16:29:28 -0400

    next version

--- WoWPro_Utilities/AutoUpdateGuides.sh
+++ WoWPro_Utilities/AutoUpdateGuides.sh
@@ -1,8 +1,9 @@
-#!/bin/sh -vx
+#!/bin/sh 
 
 # The start directory of this script is almost the top level dir to work on
 cd `dirname $0`
 cd ..
+echo "# Working directory: `pwd`"
 
 echo "# Cleaning local repository"
 if ! git checkout -- . ; then 
@@ -10,13 +11,17 @@ if ! git checkout -- . ; then
     exit 1
 fi
 
-echo "# Syncronizing to WoW-Pro.com"
+echo "# Syncronizing to GitHub"
 if ! git pull origin master ; then
     echo "! Error synchronzing local repo: $?"
     exit 2 
 fi
 
-# Insert Pyhton update here
+echo "# Syncronizing to WoW-Pro.com"
+if ! python WoWPro_Utilities/UpdateGuides.py --root=. ; then 
+    echo "! Error updating local repo: $?"
+    exit 2 
+fi
 
 echo "# Determining what has been updated"
 if ! git status --porcelain --untracked-files=no > /tmp/AU$$.tmp ; then
