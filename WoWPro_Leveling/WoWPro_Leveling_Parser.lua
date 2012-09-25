e05d1e10fadc1442fe81ffa8dc3881de1b4bacc2
Ludovicus <LuisOrtiz@Verizon.NET>
2012-09-25 04:19:37 -0400

    Get rid of WoWPro.Leveling.CheckAvailableSpells and WoWPro.Leveling.CheckAvailableTalents.

--- WoWPro_Leveling/WoWPro_Leveling_Parser.lua
+++ WoWPro_Leveling/WoWPro_Leveling_Parser.lua
@@ -717,8 +717,6 @@ function WoWPro.Leveling:EventHandler(self, event, ...)
 	end
 	if event == "PLAYER_LEVEL_UP" then
 		WoWPro.Leveling:AutoCompleteLevel(...)
-		WoWPro.Leveling.CheckAvailableSpells(...)
---		WoWPro.Leveling.CheckAvailableTalents()
 	end
 	if event == "TRAINER_UPDATE" then
 		WoWPro.Leveling.CheckAvailableSpells()
