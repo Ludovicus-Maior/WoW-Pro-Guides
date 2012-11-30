5335c7fc7816b5778cd5229f3657be2b2fec7d58
Ludovicus <LuisOrtiz@Verizon.NET>
2012-11-30 06:01:50 -0500

    Second cut of Alliance Operation Shieldwall

---
+++ WoWPro_Dailies/Alliance/Ludo_ShieldWall.lua
@@ -0,0 +1,82 @@
+
+
+
+WoWPro.Dailies:RegisterGuide("LudoShieldWall", "Krasarang Wilds", "Operation: Shieldwall", "MoP", "Ludovicus", "Alliance", function()
+return [[
+
+A Meet the Scout|QID|32246|M|64.16,58.75|Z|Vale of Eternal Blossoms|N|UI Popup when flying near Shrine of Seven Stars|
+T Meet the Scout|QID|32246|M|85.24,29.13|N|To Scout Lynna.|; 150
+
+A A King Among Men|QID|32247|PRE|32246|M|85.29,29.12|N|From King Varian Wynn.|
+C A King Among Men|QID|32247|M|79.64,25.04|QO|Marshal Troteman found: 1/1|N|Varian will help to fight. Talk to Troteman.|
+C A King Among Men|QID|32247|M|80.43,17.52|QO|Hilda Hornswaggle found: 1/1|
+C A King Among Men|QID|32247|M|78.70,19.85|QO|Horde slain: 25/25|N|Wander into the forest to kill any horde you need.|
+T A King Among Men|QID|32247|N|To King Varian Wynn, next to you.|; 300 Rep
+A Lion's Landing|QID|32109|PRE|32247|M|80.4,17.4|N|From 7th Legion Champion|
+T Lion's Landing|QID|32109|M|85.59,29.17|N|To Flare Launcher, and cutscene.|; 500 REp
+A A Little Patience|QID|32248||N|Automatically offered after cutscene.|
+f Lion's Landing|M|88.33,34.69|N|At Daggin Windbeard|
+
+; Now start the Dailies!
+A Get Mishka's Quests|QID|32149;32150|M|89.67,32.79|N|From Mishka.|
+A Get Toteman's Quests|QID|32116;32148;32151;32152;32153|M|89.69,33.08|N|From Marshall Troteman.|
+
+A Amber Kearnen's Quests|QID|32452|M||N|If Amber Kearnen offers this quest, off to the Skyfire or you go! Skip otherwise|
+; Bilgewater Operations
+T And Then There Were Goblins|QID|32452|M||N|To Sky Admiral Rogers aboard the Skyfire.|
+A The Only Good Goblin...|QID|32157|PRE|32452|M||From Sky Admiral Rogers|
+A It's Only Right|QID|32156|PRE|32452|M||From Mishka|
+A Seraphine of the Winter's Quest|QID||M||From Seraphine of the Winter|
+T Two Step Program|QID|32158|
+T Undermining the Under Miner|QID|32433|
+
+A Sully's Quests|QID||
+T Burn Out!|QID|32154|
+T Necessary Breaks|QID|32155|
+T Dis-Assembly Required|QID|32446|
+T Circle of Life|QID|32159|
+
+; Domination Point
+T Send A Message|QID|32451|M||N|To Sky Admiral Rogers aboard the Skyfire.|
+A We Will Rock You|QID|32142|PRE|32451|M||From Sky Admiral Rogers|
+A Hammer to Fall|QID|32146|PRE|32451|M||From Sky Admiral Rogers|
+A A Kind of Magic|QID|32143|PRE|32451|M||From Tinkmaster Overspark|
+A Under Pressure|QID|32144|PRE|32451|M||From Mishka|
+
+T A Kind of Magic|QID|32143|M||From Tinkmaster Overspark UI Popup????|
+A Don't Lose Your Head|QID|32145|PRE|32143|M||From Tinkmaster Overspark|
+T Don't Lose Your Head|QID|32145|PRE|32143|M||From Tinkmaster Overspark|
+
+; Ruins of Ogudei quests
+T Priorities, People!|QID|32116|M|80.43,17.52|To Hilda Hornswaggle|
+A Get Hilda's Quests|QID|32115;32119;32121;32122;32346;32347|M|80.43,17.52|From Hilda Hornswaggle|
+T The Spirit Trap|QID|32121|M|80.43,17.52|To XXX|
+T It Is A Mystery|QID|32119|M|80.43,17.52|To XXX|
+T Shackles of the Past|QID|32115|M|80.43,17.52|To XXX|
+T Eviction Notice|QID|32347|M|80.43,17.52|To XXX|
+T Oi Ain't Afraid o' No Ghosts!|QID|32346|M|80.43,17.52|To XXX|
+T Ogudei's Lieutenants|QID|32122|QID|32122|M|80.43,17.52|To XXX|
+
+; Quarry Quests
+C Siege Damage|QID|32152|M|78.06,17.87|L|92493|N|Kill Bilgewater Sappers till you get the Blasting Cap.|
+C Tower Defense|QID|32151|M|78,19|L|93022 20|N|Kill goblins and loot sparkly rocks.|
+
+; Lions Landing
+C Resource Gathering|QID|32149|M|76.68,22.28|N|Loot Lumber in the woods, kill raiders if ya see em.|
+C Attack! Move!|QID|32148|M|78.45,25.45|QO|Domination Point Shaman or Raider slain: 12/12|N|Raiders are patrollong the forest, Shamans are behind the barricade.|
+C Siege Damage|QID|32152|M|77.37,28.51|T|Domination Point Demolisher|N|Click on a Demolisher and blow it up!|
+C Hero Killer|QID|32153|M|78.75,27.33|N|Kill a Blademaster or Tauren Chieftan. Anything carrying a banner.|
+C Supply Block|QID|32150|M|77.60,32.03|
+
+T Resource Gathering|QID|32149|M|89.67,32.79|N|To Mishka.|
+T Supply Block|QID|32150|M|89.67,32.79|N|To Mishka.|
+
+T Attack! Move!|QID|32148|M|89.69,33.08|N|To Marshall Troteman.|
+T Siege Damage|QID|32152|M|89.69,33.08|N|To Marshall Troteman.|
+T Tower Defense|QID|32151|M|89.69,33.08|N|To Marshall Troteman.|
+T Hero Killer|QID|32153|M|89.69,33.08|N|To Marshall Troteman.|
+
+N Done for the Day|N|Come back tomorrow!|
+
+]]
+end)
