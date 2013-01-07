66e80d06939175d45c5339938e0c964374eb24eb
Ludovicus <LuisOrtiz@Verizon.NET>
2013-01-07 05:08:17 -0500

    AutoSync to WoW-Pro.COM on Mon Jan  7 05:08:17 EST 2013

--- WoWPro_WorldEvents/Neutral/Twists_Darkmoon_Faire.lua
+++ WoWPro_WorldEvents/Neutral/Twists_Darkmoon_Faire.lua
@@ -4,6 +4,11 @@
 -- Permissions beyond the scope of this license may be available at http://www.wow-pro.com/License.
 
 -- URL: http://wow-pro.com/wiki/darkmoon_faire
+-- Date: 2013-01-07 02:33
+-- Who: Emmaleah
+-- Log: changed the word flower to flour, changed all (I think) C steps to include NC, added statement about Fossil Fragments from Brann Brozebeard, added T Testing your strenth before A, since you never turn them in on the same trip to the fair, hopefully this helps)
+
+-- URL: http://wow-pro.com/node/3459/revisions/25249/view
 -- Date: 2012-12-16 00:37
 -- Who: Ludovicus Maior
 -- Log: You need 5X Fizzy Faire Drink.
@@ -62,8 +67,8 @@ R Transport to Entrance|QID|7926|M|36.49,35.11|N|Talk to her again to get ported
 B Moonberry Juice|QID|29506|M|43.76,65.88|L|1645 5|N|From Innkeeper Farley in the Lion's Pride Inn.|P|Alchemy;171;75|Z|Elwynn Forest|FACTION|Alliance|
 B Moonberry Juice|QID|29506|M|38.89,64.67|L|1645 5|N|From Kuruk of Kuruk's Goods in Thunder Bluff main elevator and to your right.|P|Alchemy;171;75|Z|Thunder Bluff|FACTION|Horde|
 
-B Simple Flower|QID|29509|M|41.87,67.04|L|30817 5|N|Purchase 5 from Tharynn Borden.|P|Cooking;185;75|Z|Elwynn Forest|FACTION|Alliance|
-B Simple Flower|QID|29509|M|40.6,63.6|L|30817 5|N|Purchase 5 from Shadi Mistrunner.|P|Cooking;185;75|Z|Thunder Bluff|FACTION|Horde|
+B Simple Flour|QID|29509|M|41.87,67.04|L|30817 5|N|Purchase 5 from Tharynn Borden.|P|Cooking;185;75|Z|Elwynn Forest|FACTION|Alliance|
+B Simple Flour|QID|29509|M|40.6,63.6|L|30817 5|N|Purchase 5 from Shadi Mistrunner.|P|Cooking;185;75|Z|Thunder Bluff|FACTION|Horde|
 
 B Light Parchment|QID|29515|M|41.87,67.04|L|30817 5|N|Purchase 5 from Tharynn Borden.|P|Inscription;773;75|Z|Elwynn Forest|FACTION|Alliance|
 B Light Parchment|QID|29515|M|40.28,63.41|L|30817 5|N|Purchase 5 from Shadi Mistrunner.|P|Inscription;773;75|Z|Thunder Bluff|FACTION|Horde|
@@ -86,17 +91,20 @@ B Red Dye|QID|29520|M|40.28,63.41|L|2604 1|N|Purchase 1 from Shadi Mistrunner.|P
 B Blue Dye|QID|29520|M|41.87,67.04|L|6260 1|N|Purchase 1 from Tharynn Borden.|P|Tailoring;197;75|Z|Elwynn Forest|FACTION|Alliance|
 B Blue Dye|QID|29520|M|40.28,63.41|L|6260 1|N|Purchase 1 from Shadi Mistrunner.|P|Tailoring;197;75|Z|Thunder Bluff|FACTION|Horde|
 
+B Fossil Archeology Fragments|M|83.6,61|Z|Vale of Eternal Blossoms|L|393 15|P|Archaeology;794;75|N|If you have some spare crated artifacts, You can use them to buy crates of fossil archeology fragments from Brann Bronzebeard at Mogu'shan palace, otherwise, you have to dig them up yourself.|
+
 B Sack o'Tokens|QID|29463|M|54.59,53.33|N|From Zina Sharpworth after you enter the portal. Open the bag in your inventory, we can't automate it yet!|L|71083|
 
 A Banners, Banners Everywhere!|QID|29520|M|55.50,54.63|N|From Selina Dourman.|P|Tailoring;197;75|
+N Create your Banner|QID|29520|N|Use your Darkmoon Banner Kit.|U|72048|L|72049|
+C Banners, Banners Everywhere!|QID|29520|S|M|50.99,86.23|U|72049|
 
 A It's Hammer Time|QID|29463|M|53.35,54.73|N|From Mola.|
-C It's Hammer Time|QID|29463|N|Talk to Mola again picking Ready to whack! Go in and wack. Avoid dolls and hitting Hoggar (big brown) grants 3 points.|
+C It's Hammer Time|QID|29463|NC|N|Talk to Mola again picking Ready to whack! Go in and wack. Avoid dolls and hitting Hoggar (big brown) grants 3 points.|
 T It's Hammer Time|QID|29463|N|To Mola.|
 
 A The Humanoid Cannonball|QID|29436|M|52.67,56.09|N|From Maxima Blastenheimer.|
-N Achievement|QID|29436|ACH|6021;0|N|Talk to Teleportologist Fozlebub for a return trip.|M|57.25,89.85|S|
-C The Humanoid Cannonball|QID|29436|N|Talk to Maxima again picking Launch me!. Change your view till you are looking down, then try to release (1) right when you get past the dock.
+N Achievement|QID|29436|ACH|6021;0|N|Talk to Teleportologist Fozlebub for a return trip.|M|57.25,89.85|S|The Humanoid Cannonball|QID|29436|N|Talk to Maxima again picking Launch me!. Change your view till you are looking down, then try to release (1) right when you get past the dock.
 T The Humanoid Cannonball|QID|29436|N|To Maxima Blastenheimer.|
 
 A Fun for the Little Ones|QID|29507|M|51.76,60.55|N|From Professor Thaddeus Paleo.|P|Archaeology;794;75|
@@ -107,30 +115,31 @@ A Rearm, Reuse, Recycle|QID|29518|M|49.41,60.83|N|From Rinling.|P|Mining;186;75|
 A Talkin' Tonks|QID|29511|M|49.41,60.83|N|From Rinling.|P|Engineering;202;75|
 
 A Eyes on the Prizes|QID|29517|M|49.41,60.83|N|From Rinling.|P|Leatherworking;165;75|
-C Eyes on the Prizes|QID|29517|N|Use your Darkmoon Craftsman's Kit.|U|71977|
+C Eyes on the Prizes|QID|29517|NC|N|Use your Darkmoon Craftsman's Kit.|U|71977|
 T Eyes on the Prizes|QID|29517|M|49.41,60.83|N|From Rinling.|
 
 N Achievement|QID|29438|ACH|6022;1|N|Pick one target and fire when it lights up.|S|
-C He Shoots, He Scores!|QID|29438|N|Talk to Rinling again picking Let's shoot! Aim at a target and press 1 to fire. You can get 2 shots in per target.|
+C He Shoots, He Scores!|QID|29438|NC|N|Talk to Rinling again picking Let's shoot! Aim at a target and press 1 to fire. You can get 2 shots in per target.|
 T He Shoots, He Scores!|QID|29438|N|To Rinling.|
 
-C Rearm, Reuse, Recycle|QID|29518|N|Look for Tonk Scrap. Look between the tents.|S|
-C Talkin' Tonks|QID|29511|N|Repair tonks.|S|
+C Rearm, Reuse, Recycle|QID|29518|NC|N|Look for Tonk Scrap. Look between the tents.|S|
+C Talkin' Tonks|QID|29511|NC|N|Repair tonks.|S|
 
 A Tonk Commander|QID|29434|M|50.51,64.77|N|From Finlay Coolshot.|
-C Tonk Commander|QID|29434|N|Talk to Finlay again and pick Ready to play! Drive through the targets and hit '1' when they're behind you. If you get targeted (ping sound) hit '2" for speed.|
+C Tonk Commander|QID|29434|NC|N|Talk to Finlay again and pick Ready to play! Drive through the targets and hit '1' when they're behind you. If you get targeted (ping sound) hit '2" for speed.|
 T Tonk Commander|QID|29434|M|50.51,64.77|N|To Finlay Coolshot.|
 
 T The Darkmoon Faire|QID|7905|M|48.10,64.88|N|To Gelvas Grimegate.|FACTION|Alliance|
 T The Darkmoon Faire|QID|7926|M|48.10,64.88|N|To Gelvas Grimegate.|FACTION|Horde|
 
+T Test Your Strength|QID|29433|M|48.06,67.05|N|To Kerri Hicks.|
 A Test Your Strength|QID|29433|M|48.06,67.05|N|From Kerri Hicks.|
 
 A Putting the Crunch in the Frog|QID|29509|M|52.88,67.92|N|From Stamp Thunderhorn.|P|Cooking;185;75|
 A Spoilin' for Salty Sea Dogs|QID|29513|M|52.88,67.92|N|From Stamp Thunderhorn.|P|Fishing;356;75|
 
-U Flower Base|QID|29509|L|72057|U|72056|
-C Putting the Crunch in the Frog|QID|29509|N|Throw the breaded frogs into the green flowing pot next to Stamp Thunderhorn.|U|72057|
+U Flour Base|QID|29509|L|72057|U|72056|
+C Putting the Crunch in the Frog|QID|29509|NC|N|Throw the breaded frogs into the green flowing pot next to Stamp Thunderhorn.|U|72057|
 T Putting the Crunch in the Frog|QID|29509|M|52.88,67.92|N|To Stamp Thunderhorn.|
 
 B Darkmoon Dog|QID|99602603|ACH|6026;3|M|52.88,67.92|N|From Stamp Thunderhorn.|L|19223|LVL|5|
@@ -142,7 +151,7 @@ B Spiced Beef Jerky|QID|99602610|ACH|6026;10|M|52.88,67.92|N|From Stamp Thunderh
 B Red Hot Wings|QID|99602608|ACH|6026;8|M|52.88,67.92|N|From Stamp Thunderhorn.|L|19224|LVL|25|
 B Deep Fried Candybar|QID|99602605|ACH|6026;5|M|52.88,67.92|N|From Stamp Thunderhorn.|L|19225|LVL|45|
 B Forest Strider Drumstick|QID|99602605|ACH|6026;5|M|52.88,67.92|N|From Stamp Thunderhorn.|L|33254|LVL|65|
-B Salty Sea Dog|QID|99602609|ACH|6026;9|M|52.88,67.92|N|From Stamp Thunderhorn.|L|73260|LVL|85|LVL|85|
+B Salty Sea Dog|QID|99602609|ACH|6026;9|M|52.88,67.92|N|From Stamp Thunderhorn.|L|73260|LVL|85|
 
 N Eat Darkmoon Dog|QID|99602603|ACH|6026;3|U|19223|LVL|5|
 N Eat Pickled Kodo Foot|QID|99602607|ACH|6026;7|U|19305|LVL|15|
@@ -161,11 +170,11 @@ A Herbs for Healing|QID|29514|M|55.00,70.76|N|From Chronos.|P|Herbalism;182;75|
 A Tan My Hide|QID|29519|M|55.00,70.7|N|From Chronos.|P|Skinning;393;75|
 
 N Collect 6 Darkblossom|QID|29514|L|72046 6|S|
-C Tan My Hide|QID|29519|N|Found throughout the Island.|S|
+C Tan My Hide|QID|29519|NC|N|Found throughout the Island.|S|
 N Collect 5 Bits of Glass|QID|29516|N|Green sparkling Gems.|L|72052 5|S|
 
-C Putting the Carnies Back Together Again|QID|29512|M|54.87,70.75;47.44,74.85|N|Use the bandage on Carnies.|U|71978|
-C Herbs for Healing|QID|29514|US|
+C Putting the Carnies Back Together Again|QID|29512|NC|M|54.87,70.75;47.44,74.85|N|Use the bandage on Carnies.|U|71978|
+C Herbs for Healing|QID|29514|US|NC|
 T Putting the Carnies Back Together Again|QID|29512|M|55.00,70.76|N|To Chronos.|
 T Herbs for Healing|QID|29514|M|55.00,70.76|N|To Chronos.|
 
@@ -189,7 +198,7 @@ N Drink Fizzy Faire Drink 'Classic'|QID|99602615|M|50.31,69.39|ACH|6026;15|U|332
 N Drink Sasparilla Sinker|QID|99602618|M|50.31,69.39|ACH|6026;18|U|74822|LVL|85|
 
 B Fizzy Faire Drink|QID|29506|N|From Sylannia.|L|19299 5|
-C A Fizzy Fusion|QID|29506|N|Use the Cocktail Shaker.|U|72043|
+C A Fizzy Fusion|QID|29506|NC|N|Use the Cocktail Shaker.|U|72043|
 T A Fizzy Fusion|QID|29506|M|50.31,69.39|N|To Sylannia.|
 
 B Darkmoon Fireworks (6)|QID|99603001|M|48.50,71.76|N|Buy 6 from Boomie Sparks.|ACH|6030;1|L|74142 6|FACTION|Alliance|
@@ -199,35 +208,33 @@ A Putting Trash to Good Use|QID|29510|M|52.94,75.94|N|From Sayge.|P|Enchanting;3
 A Writing the Future|QID|29515|M|52.94,75.94|N|From Sayge.|P|Inscription;773;75|
 
 N Create 5 Prophetic Ink|QID|29515|N|Use the Bundle of Exotic Herbs.|L|71972|U|71971|
-C Writing the Future|QID|29515|N|Use the Prophetic Ink. Repeat until completed.|U|71972|
+C Writing the Future|QID|29515|NC|N|Use the Prophetic Ink. Repeat until completed.|U|71972|
 T Writing the Future|QID|29515|M|52.94,75.94|N|To Sayge.|
 
 N Collect 6 Discarded Weapon|QID|29510|N|Around the faire|L|72018 6|S|
 
 A Target: Turtle|QID|29455|M|51.45,77.77|N|Jessica Rogers.|
-C Target: Turtle|QID|29455|N|Talk to Jessica Rogers and select Ready to play! Press 1, aim the middle ring of the big ring on the pole of the turtle. Once you're in position spam 1->click. If he moves wait a moment, aim and spam.|
+C Target: Turtle|QID|29455|NC|N|Talk to Jessica Rogers and select Ready to play! Press 1, aim the middle ring of the big ring on the pole of the turtle. Once you're in position spam 1->click. If he moves wait a moment, aim and spam.|
 T Target: Turtle|QID|29455|M|51.45,77.77|N|Jessica Rogers.|
 
 A Baby Needs Two Pair of Shoes|QID|29508|M|50.87,81.80|N|From Yebb Neblegear.|P|Blacksmithing;164;75|
 
 N Achievement|QID|99602501|M|50.75,81.63;56.67,81.68|N|Follow the maze and hop on a horse at the end.|ACH|6025;0|
 
-N Create your Banner|QID|29520|N|Use your Darkmoon Banner Kit.|U|72048|L|72049|
-C Banners, Banners Everywhere!|QID|29520|M|50.99,86.23|U|72049|
-
 C Spoilin' for Salty Sea Dogs|QID|29513|M|51.48,91.09|N|Fish off the dock.|
 
 N Make 4 horseshoes at the Anvil|QID|29508|M|47.54,66.31|L|71967 4|U|71964|
 
 N Collect 6 Discarded Weapon|QID|29510|N|Around the faire|L|72018 6|US|
-C Tan My Hide|QID|29519|N|Found throughout the Island.|US|
-C Putting Trash to Good Use|QID|29510|U|72018|
+C Tan My Hide|QID|29519|NC|N|Found throughout the Island.|US|
+C Banners, Banners Everywhere!|QID|29520|NC|S|M|50.99,86.23|U|72049|
+C Putting Trash to Good Use|QID|29510|NC|U|72018|
 
 N Collect 5 Bits of Glass|QID|29516|N|Green sparkling Gems.|L|72052 5|US|
-C Keeping the Faire Sparkling|QID|29516|N|Convert to Sparkling 'Gemstone'|U|72052|
+C Keeping the Faire Sparkling|QID|29516|NC|N|Convert to Sparkling 'Gemstone'|U|72052|
 
-C Rearm, Reuse, Recycle|QID|29518|N|Look for Tonk Scrap.|US|
-C Talkin' Tonks|QID|29511|US|
+C Rearm, Reuse, Recycle|QID|29518|NC|N|Look for Tonk Scrap.|US|
+C Talkin' Tonks|QID|29511|US|NC|
 
 T Rearm, Reuse, Recycle|QID|29518|M|49.41,60.83|N|From Rinling.|
 T Talkin' Tonks|QID|29511|M|49.41,60.83|N|From Rinling.|
@@ -235,7 +242,7 @@ T Keeping the Faire Sparkling|QID|29516|M|54.84,70.66|N|From Chronos.|
 T Spoilin' for Salty Sea Dogs|QID|29513|N|To Stamp Thunderhorn.|
 T Putting Trash to Good Use|QID|29510|M|52.94,75.94|N|To Sayge.|
 
-C Baby Needs Two Pair of Shoes|QID|29508|M|50.87,81.80|N|Shoe Baby.|U|71967|
+C Baby Needs Two Pair of Shoes|QID|29508|NC|M|50.87,81.80|N|Shoe Baby.|U|71967|
 T Baby Needs Two Pair of Shoes|QID|29508|M|50.87,81.80|N|From Yebb Neblegear.|
 
 T Tan My Hide|QID|29519|M|54.84,70.66|N|From Chronos.|
