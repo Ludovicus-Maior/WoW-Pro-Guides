e8f78f6e453321ea46b9e5f4922a3981ee562df6
Ludovicus <LuisOrtiz@Verizon.NET>
2013-05-14 05:15:23 -0400

    AutoSync to WoW-Pro.COM on Tue May 14 05:15:23 EDT 2013

--- WoWPro_Profession/Professions/Enchanting.lua
+++ WoWPro_Profession/Professions/Enchanting.lua
@@ -9,7 +9,12 @@ return [[
 N Learn Apprentice Enchanting|QID|903330000|N|Learn from a trainer in any major city. You must be level 5.|P|Enchanting;0;;75|
 
 N This guide has not yet been written. Should be available soon.|QID|903330001|
-
+N This guide has not yet been written. Should be available soon.|QID|903330001|
+N This guide has not yet been written. Should be available soon.|QID|903330001|
+N This guide has not yet been written. Should be available soon.|QID|903330001|
+N This guide has not yet been written. Should be available soon.|QID|903330001|
+N This guide has not yet been written. Should be available soon.|QID|903330001|
+N This guide has not yet been written. Should be available soon.|QID|903330001|
 N That completes Enchanting.|QID|903330002|
 ]]
 
--- WoWPro_Profession/Professions/Engineering.lua
+++ WoWPro_Profession/Professions/Engineering.lua
@@ -8,7 +8,12 @@ return [[
 
 N Learn Apprentice Engineering|QID|902020000|N|Learn from a trainer in any major city. You must be level 5.|P|Engineering;0;;75|
 N This guide has not yet been written. Should be available soon.|QID|902020001|
-
+N This guide has not yet been written. Should be available soon.|QID|902020001|
+N This guide has not yet been written. Should be available soon.|QID|902020001|
+N This guide has not yet been written. Should be available soon.|QID|902020001|
+N This guide has not yet been written. Should be available soon.|QID|902020001|
+N This guide has not yet been written. Should be available soon.|QID|902020001|
+N This guide has not yet been written. Should be available soon.|QID|902020001|
 N That completes Engineering.|QID|902020002|
 ]]
 
