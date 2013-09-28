7b5e6dfdd0ee00b2a0d60e7441e1bebbf167e22e
Ludovicus <LuisOrtiz@Verizon.NET>
2013-09-28 13:05:18 -0400

    Add Est_UnbornValkyr.lua and Est_Glorious.lua

---
+++ WoWPro_Achievements/General/Est_UnbornValkyr.lua
@@ -0,0 +1,35 @@
+WoWPro.Achievements:RegisterGuide('EstAchUnVa','Dalaran', 'Unborn Valkyr', 'Pet Battle', 'MoP', 'Estelyen','Neutral', function()
+
+return
+[[
+N Unborn Val'kyr|N|This pet has only 3 possible spawn points per zone, spread out across Northrend. This guide will lead you to all of them in a reasonable order. It would be a good idea to create a macro with "/tar Unborn Val'kyr" so that you can spam it whenever you reach one of the spawn points. Even at the Spawn Points, the pet spawns quite rarely, so you'll probably have to do this guide several time before finding one|
+R Crystalsong Forest 1|Z|Crystalsong Forest|M|17.60,57.00|CC|
+R Crystalsong Forest 2|N|Storm Peaks coming up, Crystalsong Forest 3 will be done at the very end to lead us back to Dalaran|Z|Crystalsong Forest|M|43.60,44.00|CC|
+R The Storm Peaks 1|Z|The Storm Peaks|M|41.80,78.80|CC|
+R The Storm Peaks 2|Z|The Storm Peaks|M|65.20,41.80|CC|
+R The Storm Peaks 3|Z|The Storm Peaks|M|29.00,51.40|CC|
+R Icecrown 1|Z|Icecrown|M|73.60,64.80|CC|
+R Icecrown 2|Z|Icecrown|M|48.20,87.00|CC|
+R Icecrown 3|Z|Icecrown|M|44.20,33.60|CC|
+R Sholazar Basin 1|Z|Sholazar Basin|M|58.40,22.20|CC|
+R Sholazar Basin 2|Z|Sholazar Basin|M|36.80,19.40|CC|
+R Sholazar Basin 3|Z|Sholazar Basin|M|44.40,69.20|CC|
+R Borean Tundra 1|Z|Borean Tundra|M|47.60,7.80|CC|
+R Borean Tundra 2|Z|Borean Tundra|M|32.70,60.10|CC|
+R Borean Tundra 3|Z|Borean Tundra|M|80.80,48.20|CC|
+R Dragonblight 1|Z|Dragonblight|M|26.80,54.00|CC|
+R Dragonblight 2|Z|Dragonblight|M|65.40,35.60|CC|
+R Dragonblight 3|Z|Dragonblight|M|82.20,65.80|CC|
+R Grizzly Hills 1|N|We'll do a detour to Howling Fjord before doing Grizzly Hills 2 & 3 on our way back north|Z|Grizzly Hills|M|25.80,56.80|CC|
+R Howling Fjord 1|Z|Howling Fjord|M|45.80,43.00|CC|
+R Howling Fjord 2|Z|Howling Fjord|M|68.20,67.60|CC|
+R Howling Fjord 3|Z|Howling Fjord|M|71.80,43.00|CC|
+R Grizzly Hills 2|Z|Grizzly Hills|M|79.50,51.60|CC|
+R Grizzly Hills 3|Z|Grizzly Hills|M|61.50,18.00|CC|
+R Zul'Drak 1|Z|Zul'Drak|M|75.20,23.00|CC|
+R Zul'Drak 2|Z|Zul'Drak|M|57.60,39.60|CC|
+R Zul'Drak 3|Z|Zul'Drak|M|24.50,63.90|CC|
+R Crystalsong Forest 3|Z|Crystalsong Forest|M|68.20,49.20|CC|
+N Finished|N|That was all of the spawn points. If you didn't find a pet, try again. Good luck!|
+]]
+end)
