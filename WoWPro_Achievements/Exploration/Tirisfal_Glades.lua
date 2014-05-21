a22f2602eb5849ca0e52976e5314c86d15295f4f
Ludovicus <LuisOrtiz@Verizon.NET>
2014-05-21 16:46:12 -0400

    AutoSync to WoW-Pro.COM on Wed May 21 16:46:12 EDT 2014

--- WoWPro_Achievements/Exploration/Tirisfal_Glades.lua
+++ WoWPro_Achievements/Exploration/Tirisfal_Glades.lua
@@ -4,6 +4,116 @@
 -- Permissions beyond the scope of this license may be available at http://www.wow-pro.com/License.
 
 -- URL: http://wow-pro.com/wiki/exploration_achievements_eastern_kingdoms
+-- Date: 2014-05-21 16:15
+-- Who: Ludovicus
+-- Log: Registration guide
+
+-- URL: http://wow-pro.com/node/3475/revisions/26191/view
+-- Date: 2014-05-21 16:14
+-- Who: Ludovicus
+-- Log: Registration guide
+
+-- URL: http://wow-pro.com/node/3475/revisions/26190/view
+-- Date: 2014-05-21 16:13
+-- Who: Ludovicus
+-- Log: Registration guide
+
+-- URL: http://wow-pro.com/node/3475/revisions/26189/view
+-- Date: 2014-05-21 16:12
+-- Who: Ludovicus
+-- Log: Registration guide
+
+-- URL: http://wow-pro.com/node/3475/revisions/26188/view
+-- Date: 2014-05-21 16:09
+-- Who: Ludovicus
+-- Log: Registration guide
+
+-- URL: http://wow-pro.com/node/3475/revisions/26187/view
+-- Date: 2014-05-21 16:07
+-- Who: Ludovicus
+-- Log: Registration guide
+
+-- URL: http://wow-pro.com/node/3475/revisions/26186/view
+-- Date: 2014-05-21 16:05
+-- Who: Ludovicus
+-- Log: Registration guide
+
+-- URL: http://wow-pro.com/node/3475/revisions/26185/view
+-- Date: 2014-05-21 16:03
+-- Who: Ludovicus
+-- Log: Registration guide
+
+-- URL: http://wow-pro.com/node/3475/revisions/26184/view
+-- Date: 2014-05-21 14:53
+-- Who: Ludovicus
+-- Log: Registration guide
+
+-- URL: http://wow-pro.com/node/3475/revisions/26183/view
+-- Date: 2014-05-21 14:52
+-- Who: Ludovicus
+-- Log: Registration guide
+
+-- URL: http://wow-pro.com/node/3475/revisions/26182/view
+-- Date: 2014-05-21 14:48
+-- Who: Ludovicus
+-- Log: Registration guide
+
+-- URL: http://wow-pro.com/node/3475/revisions/26181/view
+-- Date: 2014-05-21 14:47
+-- Who: Ludovicus
+-- Log: Registration guide
+
+-- URL: http://wow-pro.com/node/3475/revisions/26180/view
+-- Date: 2014-05-21 14:45
+-- Who: Ludovicus
+-- Log: Registration guide
+
+-- URL: http://wow-pro.com/node/3475/revisions/26179/view
+-- Date: 2014-05-21 14:40
+-- Who: Ludovicus
+-- Log: Registration guide
+
+-- URL: http://wow-pro.com/node/3475/revisions/26178/view
+-- Date: 2014-05-21 14:38
+-- Who: Ludovicus
+-- Log: Registration guide
+
+-- URL: http://wow-pro.com/node/3475/revisions/26177/view
+-- Date: 2014-05-21 14:37
+-- Who: Ludovicus
+-- Log: Registration guide
+
+-- URL: http://wow-pro.com/node/3475/revisions/26176/view
+-- Date: 2014-05-21 14:35
+-- Who: Ludovicus
+-- Log: Registration guide
+
+-- URL: http://wow-pro.com/node/3475/revisions/26175/view
+-- Date: 2014-05-21 14:33
+-- Who: Ludovicus
+-- Log: Registration guide
+
+-- URL: http://wow-pro.com/node/3475/revisions/26174/view
+-- Date: 2014-05-21 14:31
+-- Who: Ludovicus
+-- Log: Registration guide
+
+-- URL: http://wow-pro.com/node/3475/revisions/26173/view
+-- Date: 2014-05-21 14:12
+-- Who: Ludovicus
+-- Log: Registration guide
+
+-- URL: http://wow-pro.com/node/3475/revisions/26172/view
+-- Date: 2014-05-21 14:05
+-- Who: Ludovicus
+-- Log: Registration guide
+
+-- URL: http://wow-pro.com/node/3475/revisions/26171/view
+-- Date: 2014-05-21 14:05
+-- Who: Ludovicus
+-- Log: Registration guide
+
+-- URL: http://wow-pro.com/node/3475/revisions/25352/view
 -- Date: 2013-01-12 17:49
 -- Who: Ludovicus
 -- Log: Update exploration of Dun_Morogh: The Grizzled Den is gone.
@@ -17,7 +127,11 @@
 -- Date: 2012-05-27 17:50
 -- Who: Ludovicus
 
-WoWPro.Achievements:RegisterGuide("LudoExpTiG","Tirisfal Glades","Tirisfal Glades","Exploration", "Eastern Kingdom", "Ludovicus", "Neutral", function()
+local guide = WoWPro:RegisterGuide("LudoExpTiG","Tirisfal Glades","Tirisfal Glades", "Neutral")
+WoWPro:GuideLevels(guide,0,0)
+WoWPro:GuideIcon(guide,"ACH",768)
+WoWPro:GuideNextGuide(guide, "Ludovicus")
+WoWPro:GuideSteps(guide, function()
 return [[
 
 F Deathknell|QID|907680001|M|32.09,64.60|ACH|768;1|
