80b2f32e0f91f0b813969c15982da411428e635e
Ludovicus <LuisOrtiz@Verizon.NET>
2014-11-13 08:32:40 -0500

    Allow WoWPro.CheckAstrolabeData() to be forced

--- WoWPro/WoWPro_Mapping.lua
+++ WoWPro/WoWPro_Mapping.lua
@@ -687,7 +687,7 @@ function WoWPro:RemoveMapPoint()
 	end
 end
 
-function  WoWPro.CheckAstrolabeData()
+function  WoWPro.CheckAstrolabeData(force)
     if not WoWPro.Astrolabe['zeroData'] then
         WoWPro:dbp("CheckAstrolabeData(): No Astrolabe!")
         return
@@ -695,12 +695,13 @@ function  WoWPro.CheckAstrolabeData()
     local Astrolabe = WoWPro.Astrolabe
     local map, pizo = Astrolabe:GetCurrentPlayerPosition()
     if not (map and pizo) then
+        WoWPro:dbp("CheckAstrolabeData(): No player position yet!")
         -- We are not mapped yet.
         return
     end
     local AW = Astrolabe.WorldMapSize[map][pizo]
     local Az = Astrolabe.zeroData
-    if AW ~= Az then
+    if (not force) and AW ~= Az then
         -- We have data
         WoWPro:dbp("Map data present for %d/%d", map, pizo)
         return
