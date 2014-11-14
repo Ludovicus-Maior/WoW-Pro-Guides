3fc02275b9547c77c688f00e248392a1611239a4
Ludovicus <LuisOrtiz@Verizon.NET>
2014-11-13 21:09:07 -0500

    Added BloodthornCave and LunarfallExcavation.

--- WoWPro/Libraries/Astrolabe/Astrolabe.lua
+++ WoWPro/Libraries/Astrolabe/Astrolabe.lua
@@ -1654,6 +1654,9 @@ WorldMapSize[941][3] = { xOffset = -5645.000000, height = 240.000000, yOffset = 
 WorldMapSize[941][4] = { xOffset = -5516.000000, height = 410.000000, yOffset = -6540.000000, width = 615.000000 , __index = zeroDataFunc };
 WorldMapSize[941][5] = { xOffset = -3296.250000, height = 295.000000, yOffset = -5475.000000, width = 442.500000 , __index = zeroDataFunc };
 WorldMapSize[941][7] = { xOffset = -4147.500000, height = 550.000000, yOffset = -6650.000000, width = 825.000000 , __index = zeroDataFunc };
+WorldMapSize[947][15] = { xOffset = -583.750000, height = 231.000000, yOffset = -1030.750000, width = 346.500000 , __index = zeroDataFunc };
 WorldMapSize[949][17] = { xOffset = -1013.75000, height = 165.000000, yOffset = -4330.000000, width = 247.500000 , __index = zeroDataFunc };
 WorldMapSize[949][19] = { xOffset = -1375.75000, height = 265.000000, yOffset = -6950.000000, width = 397.500000 , __index = zeroDataFunc };
-WorldMapSize[970][1] = { xOffset =  2720.379883, height = 303.172852, yOffset = -4359.998535, width = 454.759766 , __index = zeroDataFunc };
+WorldMapSize[970][1]  = { xOffset =  2720.379883, height = 303.172852, yOffset = -4359.998535, width = 454.759766 , __index = zeroDataFunc };
+WorldMapSize[971][23] = { xOffset =  -31.0000000, height = 250.000000, yOffset = -1806.000000, width = 375.000000 , __index = zeroDataFunc };
+
