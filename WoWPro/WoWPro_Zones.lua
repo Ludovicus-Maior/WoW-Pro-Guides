c0d6f444975a6ae1ebb3fd0b13800553a40c814b
Ludovicus <LuisOrtiz@Verizon.NET>
2014-12-19 15:54:18 -0500

    Add WoWPro.Map2Zone map.

--- WoWPro/WoWPro_Zones.lua
+++ WoWPro/WoWPro_Zones.lua
@@ -17,6 +17,7 @@ WoWPro.SubZone = {
 	
 WoWPro.Zone2MapID = {}
 WoWPro.MapID2Zone = {}
+WoWPro.Map2Zone = {}
 
 local function DupCheck(zone,who)
     if WoWPro.MapID2Zone[zone] then
@@ -28,12 +29,14 @@ local function DefineDungeonArea(mapID, floor, zi, dungeon, mapName)
     DupCheck(zi,"DefineDungeonArea")
     WoWPro.Zone2MapID[zi] = {mapID=mapID, floor=floor, dungeon=dungeon, mapName=mapName}
     WoWPro.MapID2Zone[mapID] = zi
+    WoWPro.Map2Zone[mapName] = zi
 end
 
 local function DefineInstance(mapID, numFloors, zi, mapName)
     DupCheck(zi,"DefineInstance")
     if mapName then
         WoWPro.Zone2MapID[zi] = {mapID=mapID, numFloors=numFloors, mapName=mapName}
+        WoWPro.Map2Zone[mapName] = zi
     else
         WoWPro.Zone2MapID[zi] = {mapID=mapID, numFloors=numFloors, mapName=zi}
     end
@@ -44,6 +47,7 @@ local function DefineTerrain(cont, zonei, mapID, numFloors, zone, mapName)
     if mapName then
         DupCheck(zone,"DefineTerrain1")
         WoWPro.Zone2MapID[zone] = {cont=cont, zonei=zonei, mapID=mapID, numFloors=numFloors, zone=zone, mapName=mapName}
+        WoWPro.Map2Zone[mapName] = zone
     else
         DupCheck(zone,"DefineTerrain2")
         WoWPro.Zone2MapID[zone] = {cont=cont, zonei=zonei, mapID=mapID, numFloors=numFloors, zone=zone, mapName=zone}
