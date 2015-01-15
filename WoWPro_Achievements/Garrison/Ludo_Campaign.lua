5a39574c5c843504a24378c2843d1e53b77a4667
Ludovicus <LuisOrtiz@Verizon.NET>
2015-01-15 16:05:01 -0500

    Next update to Garrison/Ludo_Campaign

--- WoWPro_Achievements/Garrison/Ludo_Campaign.lua
+++ WoWPro_Achievements/Garrison/Ludo_Campaign.lua
@@ -100,49 +100,58 @@ T Rylak Rescue|QID|34450|M|67.97,90.12|Z|Shadowmoon Valley@Draenor|N|Click on Ni
 
 ; Garrison Campaign: The Sargerei|QID|34026|M|64.51,69.90|N|To Jr. Surveyor Dorn|
 ; A Garrison Campaign: The Sargerei|QID|35185|M|40.0,44.4|N|From Khadgar's Servant|
-T Garrison Campaign: The Sargerei|QID|35185|M|43.6,77.2|Z|Shadowmoon Valley@Draenor|N|To Archmage Modera|
-A Infiltrating the Sargerei|QID|35187|PRE|35185|M|43.6,77.2|Z|Shadowmoon Valley@Draenor|N|From Archmage Modera|
+T Garrison Campaign: The Sargerei|QID|35185|M|43.56,77.29|Z|Shadowmoon Valley@Draenor|N|To Archmage Modera|
+A Infiltrating the Sargerei|QID|35187|PRE|35185|M|43.56,77.29|Z|Shadowmoon Valley@Draenor|N|From Archmage Modera|
 C Sargerei Slippers|QID|35187|M|49.65,73.40;45.33,74.53;49.74,76.49|Z|Shadowmoon Valley@Draenor|CN|QO|1|N|Kill Sargerei Initiate and others for shoes|S|
 C Sargerei Robe|QID|35187|M|51.00,75.50;49.40,73.10;48.05,80.25|Z|Shadowmoon Valley@Draenor|CN|QO|2|N|Kill Sargerei Demonlords and others for a robe|S|
 C Sargerei Cowl|QID|35187|M|50.50,79.60;44.50,78.30;44.20,74.40;50.25,74.47;48.60,72.85;45.85,80.75;51.20,76.70|Z|Shadowmoon Valley@Draenor|CN|QO|3|N|Kill Sargerei Workers for a cowl|S|
 C Sargerei Slippers|QID|35187|M|49.65,73.40;45.33,74.53;49.74,76.49|Z|Shadowmoon Valley@Draenor|CN|QO|1|N|Kill Sargerei Initiate and others for shoes|US|
 C Sargerei Robe|QID|35187|M|51.00,75.50;49.40,73.10;48.05,80.25|Z|Shadowmoon Valley@Draenor|CN|QO|2|N|Kill Sargerei Demonlords and others for a robe|US|
 C Sargerei Cowl|QID|35187|M|50.50,79.60;44.50,78.30;44.20,74.40;50.25,74.47;48.60,72.85;45.85,80.75;51.20,76.70|Z|Shadowmoon Valley@Draenor|CN|QO|3|N|Kill Sargerei Workers for a cowl|US|
-l Sargerei Disguise|QID|35187|L|112904|U|112893|N|Click on the slippers to make the Disguise.  
-C Infiltrating the Sargerei|QID|35187|M|49.8,81.4|Z|Shadowmoon Valley@Draenor|N|Speak to Soulbinder Zamaya|
-C Infiltrating the Sargerei|QID|35187|M|50.0,81.2|Z|Shadowmoon Valley@Draenor|N|Speak to Anchorite Laanda|
-T Infiltrating the Sargerei|QID|35187|M|50.0,81.2|Z|Shadowmoon Valley@Draenor|N|To Anchorite Laanda|
+l Sargerei Disguise|QID|35187|L|112904|U|112893|N|Click on the slippers to make the Disguise.
+U Put on the Disguise|QID|35187|U|112904|QO|5|N|If you lose the disguise, you will need to get all 3 peices and make a new one.|  
+C Infiltrating the Sargerei|QID|35187|M|49.80,81.50|Z|Shadowmoon Valley@Draenor|QO|6|N|Speak to Soulbinder Zamaya|
+C Infiltrating the Sargerei|QID|35187|M|50.01,81.22|Z|Shadowmoon Valley@Draenor|CHAT|QO|7|N|Tell him 'the rain falls on Shadowmoon.'|
+T Infiltrating the Sargerei|QID|35187|M|50.01,81.22|Z|Shadowmoon Valley@Draenor|N|To Anchorite Laanda|
 
 ; These are parallel
-A Shattering the Enemy|QID|35191|PRE|35185|M|50.0,81.2|N|From Anchorite Laanda|; Neutral
-A The Word of Socrethar|QID|35189|PRE|35185|M|50.0,81.2|N|From Anchorite Laanda|; Neutral
-A Pesky Podlings|QID|35188|PRE|35185|M|49.8,81.4|N|From Soulbinder Zamaya|; Neutral
-A An Ancient Threat|QID|35190|PRE|35185|35190|M|49.8,81.4|N|From Soulbinder Zamaya|; Neutral
+A Shattering the Enemy|QID|35191|PRE|35185|M|50.01,81.22|Z|Shadowmoon Valley@Draenor|N|From Anchorite Laanda|; Neutral
+A The Word of Socrethar|QID|35189|PRE|35185|M|50.01,81.22|Z|Shadowmoon Valley@Draenor|N|From Anchorite Laanda|; Neutral
+A An Ancient Threat|QID|35190|PRE|35185|35190|M|49.80,81.50|Z|Shadowmoon Valley@Draenor|N|From Soulbinder Zamaya|; Neutral
+A Pesky Podlings|QID|35188|PRE|35185|M|49.80,81.50|Z|Shadowmoon Valley@Draenor|N|From Soulbinder Zamaya|; Neutral
 
-C Shattering the Enemy|QID|35191|M|50.4,75.6|N|Felstorm Crystal|
-C The Word of Socrethar|QID|35189|M|51.4,75.6|N|The Eredar Destiny|
-C The Word of Socrethar|QID|35189|M|50.0,72.4|N|The Purpose of Unity|
-C The Word of Socrethar|QID|35189|M|50.6,79.6|N|The Promise of Eternity|
-C Pesky Podlings|QID|35188|M|44.8,72.0|N|Shadowglen Thornshooter|
-C Pesky Podlings|QID|35188|M|45.0,73.4|N|Shadowglen Spitter|
-C Pesky Podlings|QID|35188|M|44.8,77.2|N|Brambleking Fili|
-C An Ancient Threat|QID|35190|M|45.4,73.8|N|Eldermoss|
+C Shattering the Enemy|QID|35191|M|50.4,75.6|Z|Shadowmoon Valley@Draenor|N|Felstorm Crystal|S|
+C The Word of Socrethar|QID|35189|M|51.4,75.6|Z|Shadowmoon Valley@Draenor|QO|1|N|The Eredar Destiny|
+C The Word of Socrethar|QID|35189|M|50.0,72.4|Z|Shadowmoon Valley@Draenor|QO|2|N|The Purpose of Unity|
+C The Word of Socrethar|QID|35189|M|50.6,79.6|Z|Shadowmoon Valley@Draenor|QO|3|N|The Promise of Eternity|
+C Shattering the Enemy|QID|35191|M|50.4,75.6|Z|Shadowmoon Valley@Draenor|N|Felstorm Crystal|US
+C Pesky Podlings|QID|35188|M|44.8,72.0|Z|Shadowmoon Valley@Draenor|N|Shadowglen Thornshooter|
+C Pesky Podlings|QID|35188|M|45.0,73.4|Z|Shadowmoon Valley@Draenor|N|Shadowglen Spitter|
+C Pesky Podlings|QID|35188|M|44.8,77.2|Z|Shadowmoon Valley@Draenor|N|Brambleking Fili|
 
-T Shattering the Enemy|QID|35191|M|50.0,81.2|N|To Anchorite Laanda|
-T The Word of Socrethar|QID|35189|M|50.0,81.2|N|To Anchorite Laanda|
-T Pesky Podlings|QID|35188|M|49.8,81.4|N|To Soulbinder Zamaya|
-T An Ancient Threat|QID|35190|M|49.8,81.4|N|To Soulbinder Zamaya|
+C An Ancient Threat|QID|35190|M|45.54,73.81|Z|Shadowmoon Valley@Draenor|N|Eldermoss|
 
-A Facing Demons|QID|35194|PRE|35189;35191|M|50.0,81.2|N|From Anchorite Laanda|; Neutral
-C Facing Demons|QID|35194|M|45.8,80.6|N|Invokaz|
-T Facing Demons|QID|35194|M|50.0,81.2|N|To Laanda's Scroll|
-A Socrethar's Fury|QID|35195|PRE|35194|M|50.0,81.2|N|From Laanda's Scroll|; Neutral
-C Socrethar's Fury|QID|35195|M|50.6,82.4|N|Soulbinder Zamaya|
-T Socrethar's Fury|QID|35195|M|50.0,81.2|N|To Anchorite Laanda|
+T Shattering the Enemy|QID|35191|M|50.01,81.22|Z|Shadowmoon Valley@Draenor|N|To Anchorite Laanda|
+T The Word of Socrethar|QID|35189|M|50.01,81.22|Z|Shadowmoon Valley@Draenor|N|To Anchorite Laanda|
+T An Ancient Threat|QID|35190|M|49.80,81.50|Z|Shadowmoon Valley@Draenor|N|To Soulbinder Zamaya|
+T Pesky Podlings|QID|35188|M|49.80,81.50|Z|Shadowmoon Valley@Draenor|N|To Soulbinder Zamaya|
+
+A Facing Demons|QID|35194|PRE|35189;35191|M|50.01,81.22|Z|Shadowmoon Valley@Draenor|N|From Anchorite Laanda|; Neutral
+K Invokaz|QID|35194|M|45.94,80.54|Z|Shadowmoon Valley@Draenor|QO|1|N|Ride up the rise to find Invokaz|
+C Shut the Daemon Gate|QID|35194|M|46.29,80.58|Z|Shadowmoon Valley@Draenor|QO|2|
+T Facing Demons|QID|35194|M|50.01,81.22|Z|Shadowmoon Valley@Draenor|N|To Laanda's Scroll|
+A Socrethar's Fury|QID|35195|PRE|35194|M|50.01,81.22|Z|Shadowmoon Valley@Draenor|N|From Laanda's Scroll|; Neutral
+C Socrethar's Fury|QID|35195|M|50.33,81.96|Z|Shadowmoon Valley@Draenor|QO|1|N|Click on the now closed door and wait in the vestible till you "confront".|
+C Socrethar's Fury|QID|35195|M|50.6,82.4|Z|Shadowmoon Valley@Draenor|N|Kill Soulbinder Zamaya|
+
+T Socrethar's Fury|QID|35195|M|50.58,82.30|Z|Shadowmoon Valley@Draenor|N|To Anchorite Laanda|
+A Forging Ahead|QID|35196|PRE|35195||M|50.58,82.30|N|From Anchorite Laanda|
+
+T Forging Ahead|QID|35196|M|40.0,44.4|N|To Khadgar's Servant, wandering in your Garrison.|
 
 
 ; Garrison Campaign: Deep Recon
-A Garrison Campaign: Deep Recon|QID|35837|M|0.0,0.0|N|From ?|
+; A Garrison Campaign: Deep Recon|QID|35837|M|0.0,0.0|N|From ?|
 T Garrison Campaign: Deep Recon|QID|35837|M|49.8,37.0|N|To Jeanine Amrell|
 A The Warlord's Council|QID|35838|PRE|35837|M|0.0,0.0|N|From ?|
 T The Warlord's Council|QID|35838|M|41.8,37.0|N|To Flint Shadowmore|
@@ -154,7 +163,7 @@ T Plans of War|QID|35839|M|37.8,36.8|N|To Bodrick Grey|
 
 
 ; Garrison Campaign: Crows In The Field
-A Garrison Campaign: Crows In The Field|QID|34409|M|0.0,0.0|N|From ?|
+; A Garrison Campaign: Crows In The Field|QID|34409|M|0.0,0.0|N|From ?|
 T Garrison Campaign: Crows In The Field|QID|34409|M|39.8,37.0|N|To Thisalee Crow|
 A Ambushing the Enemy|QID|34336|PRE|34409|M|0.0,0.0|N|From ?|; Neutral
 C Ambushing the Enemy|QID|34336|M|40.4,40.4|N|Lieutenant O'rok|
@@ -164,6 +173,44 @@ A Bladefury's Orders|QID|34347|PRE|34409|M|0.0,0.0|N|From ?|; Neutral
 T Bladefury's Orders|QID|34347|M|39.8,37.0|N|To Thisalee Crow|
 A The Giant Cauldron|QID|34337|PRE|34336;34347|M|0.0,0.0|N|From ?|; Neutral
 T The Giant Cauldron|QID|34337|M|54.2,56.0|N|To Thisalee Crow|
+
+; The Exarch's Call|
+A Garrison Campaign: The Exarch's Call|QID|36163|M|33.0,34.6|N|From Deedree|
+F Elodor||QID|36163|M|48.00,49.83|N|Fly to Embaari Villiage on Air Longdrink.|
+T Garrison Campaign: The Exarch's Call|QID|36163|M|65.23,26.18|Z|Shadowmoon Valley@Draenor|N|To Yrel, at the Altar of the Sha'tar|
+
+A The Trial of Faith|QID|36168|PRE|36163|M|65.33,26.38|Z|Shadowmoon Valley@Draenor|N|From Exarch Maladaar|
+A The Trial of Heart|QID|36167|PRE|36163|M|65.40,26.17|Z|Shadowmoon Valley@Draenor|N|From Exarch Naielle|
+A The Trial of Courage|QID|36164|PRE|36163|M|65.33,25.97|Z|Shadowmoon Valley@Draenor|N|From Exarch Akama|
+
+F Retribution Point|QID|36168|M|58.6,31.8|Z|Shadowmoon Valley@Draenor|QO|1|N|Fly to Auchindoun on Air Vamuun.|
+C The Trial of Faith|QID|36168|M|46.32,74.09|Z|Talador|CHAT|QO|1|N|Speak to Soulbinder Tuulani to start the solo scenario in the Auchindoun dungeon. Just go straight ahead and follow Yrel.|
+K Hataaru|QID|36168|M|46.2,74.0|Z|DraenorAuchindoun|QO|2|N|Go straight through the dungeon until you get to Hataaru.  Try to avoid the moving glowing X's.|
+K Maraad|QID|36168|M|46.2,74.0|Z|DraenorAuchindoun|QO|3|N|Clear all the trash mobs and get to the central platform and click on the urn to summon Maraad. At the end of the fight you need to click on the hammer Yrel throws.|
+
+F Rilzit's Holdfast|QID|36167|M|42.11,76.79|Z|Talador|QO|1|N|Air Beldos to Nagrand.|
+C The Trial of Heart|QID|36167|M|46.53,16.64|Z|Nagrand@Draenor|QO|1|CHAT|N|Speak with Yrel to start|
+A An Act of Kindness|QID|36193|M|49.42,16.59|Z|Nagrand@Draenor|N|From Rangari Saardar|RANK|2|
+C Revive Fallen Rangari|QID|36193|QO|1|S|U|114926|N|The goldcaps heal you as wellas the rangers|
+K Plaguebloom|QID|36167|M|46.4,10.4|Z|Nagrand@Draenor|QO|2|
+C Revive Fallen Rangari|QID|36193|QO|1|US|U|114926|N|The goldcaps heal you as wellas the rangers|
+T An Act of Kindness|QID|36193|M|49.42,16.59|Z|Nagrand@Draenor|N|From Rangari Saardar|
+
+F Bloodmaul Slag Mines|QID|36164|M|50.75,30.64|Z|Nagrand@Draenor|QO|1|N|Go to the Iron Siegeworks.|
+C The Trial of Courage|QID|36164|M|53.12,33.04|Z|Frostfire Ridge|CHAT|QO|1|N|Speak to Yrel to start.|
+K Everburn|QID|36164|M|52.6,32.2|Z|Frostfire Ridge|QO|2|N|Everburn|
+
+H Lunarfall|QID|36164|N|Hearth back or fly back to your garrison before moving on to the "Tranquil Court" in Shadowmoon Valley.|
+
+F Tranquil Court|QID|36168|M|48.00,49.83|N|Fly to Tranquil Court on Air Longdrink.|
+T The Trial of Heart|QID|36167|M|75.20,57.53|Z|Shadowmoon Valley@Draenor|N|To Exarch Naielle|
+T The Trial of Courage|QID|36164|M|74.90,57.99|Z|Shadowmoon Valley@Draenor|N|To Exarch Akama|
+T The Trial of Faith|QID|36168|M|75.14,57.89|Z|Shadowmoon Valley@Draenor|N|To Exarch Maladaar|
+
+A The Trial of Champions|QID|36169|PRE|36168;36164;36167|M|75.14,57.89|Z|Shadowmoon Valley@Draenor|N|From Exarch Maladaar|
+C The Trial of Champions|QID|36169|M|75.14,57.89|Z|Shadowmoon Valley@Draenor|CHAT|N|Tell Exarch Maladaar you are ready to bash the Exarch Council.  Do not MC Exarch Maladaar or it is auto-fail! Be sure to use Yrel's buff on the ground.|
+K Exarch Council|QID|36169|QO|1|N|They dont hit hard.  Use your "Call to Arms"|
+T The Trial of Champions|QID|36169|M|75.14,57.89|Z|Shadowmoon Valley@Draenor|N|To Exarch Maladaar|
 
 
 D Next!|QID|0|N|Click to return to whatever guide you were doing before.|
@@ -192,8 +239,6 @@ T The Beating Heart|QID|34410|M|47.0,88.4|N|To Morthis Whisperwing|
 
 
 
-A Forging Ahead|QID|35196|M|50.0,81.2|N|From Anchorite Laanda|
-T Forging Ahead|QID|35196|M|40.0,44.4|N|To Khadgar's Servant|
 
 A Intercepting the Orders|QID|35840|M|0.0,0.0|N|From ?|
 T Intercepting the Orders|QID|35840|M|37.8,36.8|N|To Bodrick Grey|
