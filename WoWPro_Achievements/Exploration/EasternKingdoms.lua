cca3a571249ddae64dd6b88a9c8df09413d84e1b
Ludovicus <LuisOrtiz@Verizon.NET>
2015-04-13 15:08:17 -0400

    AutoSync to WoW-Pro.COM on Mon Apr 13 15:08:17 EDT 2015

--- WoWPro_Achievements/Exploration/EasternKingdoms.lua
+++ WoWPro_Achievements/Exploration/EasternKingdoms.lua
@@ -1,4 +1,156 @@
 
+-- WoWPro Guides by "The WoW-Pro Community" are licensed under a Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License.
+-- Based on a work at github.com.
+-- Permissions beyond the scope of this license may be available at http://www.wow-pro.com/License.
+
+-- URL: http://wow-pro.com/wiki/exploration_achievements_eastern_kingdoms
+-- Date: 2015-04-13 17:22
+-- Who: Ludovicus
+-- Log: Converted to continent guide with proximity sorting.
+
+-- URL: http://wow-pro.com/node/3475/revisions/26375/view
+-- Date: 2014-05-28 19:48
+-- Who: Ludovicus
+-- Log: Correct exploration for EK.
+
+-- URL: http://wow-pro.com/node/3475/revisions/26324/view
+-- Date: 2014-05-27 00:59
+-- Who: Ludovicus
+-- Log: Guide Intro edits
+
+-- URL: http://wow-pro.com/node/3475/revisions/26309/view
+-- Date: 2014-05-26 17:28
+-- Who: Ludovicus
+-- Log: Added guide type.
+
+-- URL: http://wow-pro.com/node/3475/revisions/26308/view
+-- Date: 2014-05-26 17:18
+-- Who: Ludovicus
+-- Log: Added guide type.
+
+-- URL: http://wow-pro.com/node/3475/revisions/26192/view
+-- Date: 2014-05-21 16:15
+-- Who: Ludovicus
+-- Log: Registration guide
+
+-- URL: http://wow-pro.com/node/3475/revisions/26191/view
+-- Date: 2014-05-21 16:14
+-- Who: Ludovicus
+-- Log: Registration guide
+
+-- URL: http://wow-pro.com/node/3475/revisions/26190/view
+-- Date: 2014-05-21 16:13
+-- Who: Ludovicus
+-- Log: Registration guide
+
+-- URL: http://wow-pro.com/node/3475/revisions/26189/view
+-- Date: 2014-05-21 16:12
+-- Who: Ludovicus
+-- Log: Registration guide
+
+-- URL: http://wow-pro.com/node/3475/revisions/26188/view
+-- Date: 2014-05-21 16:09
+-- Who: Ludovicus
+-- Log: Registration guide
+
+-- URL: http://wow-pro.com/node/3475/revisions/26187/view
+-- Date: 2014-05-21 16:07
+-- Who: Ludovicus
+-- Log: Registration guide
+
+-- URL: http://wow-pro.com/node/3475/revisions/26186/view
+-- Date: 2014-05-21 16:05
+-- Who: Ludovicus
+-- Log: Registration guide
+
+-- URL: http://wow-pro.com/node/3475/revisions/26185/view
+-- Date: 2014-05-21 16:03
+-- Who: Ludovicus
+-- Log: Registration guide
+
+-- URL: http://wow-pro.com/node/3475/revisions/26184/view
+-- Date: 2014-05-21 14:53
+-- Who: Ludovicus
+-- Log: Registration guide
+
+-- URL: http://wow-pro.com/node/3475/revisions/26183/view
+-- Date: 2014-05-21 14:52
+-- Who: Ludovicus
+-- Log: Registration guide
+
+-- URL: http://wow-pro.com/node/3475/revisions/26182/view
+-- Date: 2014-05-21 14:48
+-- Who: Ludovicus
+-- Log: Registration guide
+
+-- URL: http://wow-pro.com/node/3475/revisions/26181/view
+-- Date: 2014-05-21 14:47
+-- Who: Ludovicus
+-- Log: Registration guide
+
+-- URL: http://wow-pro.com/node/3475/revisions/26180/view
+-- Date: 2014-05-21 14:45
+-- Who: Ludovicus
+-- Log: Registration guide
+
+-- URL: http://wow-pro.com/node/3475/revisions/26179/view
+-- Date: 2014-05-21 14:40
+-- Who: Ludovicus
+-- Log: Registration guide
+
+-- URL: http://wow-pro.com/node/3475/revisions/26178/view
+-- Date: 2014-05-21 14:38
+-- Who: Ludovicus
+-- Log: Registration guide
+
+-- URL: http://wow-pro.com/node/3475/revisions/26177/view
+-- Date: 2014-05-21 14:37
+-- Who: Ludovicus
+-- Log: Registration guide
+
+-- URL: http://wow-pro.com/node/3475/revisions/26176/view
+-- Date: 2014-05-21 14:35
+-- Who: Ludovicus
+-- Log: Registration guide
+
+-- URL: http://wow-pro.com/node/3475/revisions/26175/view
+-- Date: 2014-05-21 14:33
+-- Who: Ludovicus
+-- Log: Registration guide
+
+-- URL: http://wow-pro.com/node/3475/revisions/26174/view
+-- Date: 2014-05-21 14:31
+-- Who: Ludovicus
+-- Log: Registration guide
+
+-- URL: http://wow-pro.com/node/3475/revisions/26173/view
+-- Date: 2014-05-21 14:12
+-- Who: Ludovicus
+-- Log: Registration guide
+
+-- URL: http://wow-pro.com/node/3475/revisions/26172/view
+-- Date: 2014-05-21 14:05
+-- Who: Ludovicus
+-- Log: Registration guide
+
+-- URL: http://wow-pro.com/node/3475/revisions/26171/view
+-- Date: 2014-05-21 14:05
+-- Who: Ludovicus
+-- Log: Registration guide
+
+-- URL: http://wow-pro.com/node/3475/revisions/25352/view
+-- Date: 2013-01-12 17:49
+-- Who: Ludovicus
+-- Log: Update exploration of Dun_Morogh: The Grizzled Den is gone.
+
+-- URL: http://wow-pro.com/node/3475/revisions/25011/view
+-- Date: 2012-06-30 16:06
+-- Who: Ludovicus
+-- Log: Wrong zone name for The Cape of Stranglethorn
+
+-- URL: http://wow-pro.com/node/3475/revisions/24982/view
+-- Date: 2012-05-27 17:50
+-- Who: Ludovicus
 
 local guide = WoWPro:RegisterGuide("TwiExpEK","Achievements","Eastern Kingdoms","Twists+Ludovicus", "Neutral")
 WoWPro:GuideLevels(guide,20,90)
@@ -23,7 +175,6 @@ F Circle of Outer Binding|QID|907610013|M|46.39,52.16|ACH|761;11|Z|Arathi Highla
 F Dabyrie's Farmstead|QID|907610014|M|49.64,40.59|ACH|761;14|Z|Arathi Highlands|
 F Circle of East Binding|QID|907610015|M|61.62,30.33|ACH|761;15|Z|Arathi Highlands|
 F Hammerfall|QID|907610016|M|69.06,36.18|ACH|761;16|Z|Arathi Highlands|
-
 
 F Witherbark Village|QID|907610001|M|63.04,75.75|ACH|761;12|Z|Badlands|
 F Go'Shek Farm|QID|907610002|M|56.62,60.15|ACH|761;13|Z|Badlands|
@@ -364,8 +515,7 @@ F Dun Algaz|QID|908410014|M|48.06,74.43|ACH|841;14|Z|Wetlands|
 F Slabchisel's Survey|QID|908410015|M|57.46,71.70|ACH|841;15|Z|Wetlands|
 F Raptor Ridge|QID|908410016|M|69.89,38.34|ACH|841;16|Z|Wetlands|
 
-
 N Congratulations on exploring The Eastern Kingdoms!|
 
-]] 
+]]
 end)
