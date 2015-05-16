9b649bc2e95b6dd294b13d9a794580f8748a2fa8
Ludovicus <LuisOrtiz@Verizon.NET>
2015-05-16 09:05:01 -0400

    Tweak for Gnomergan zone for Gnone starter zone.

--- WoWPro/Libraries/Astrolabe/Astrolabe-TomTom.lua
+++ WoWPro/Libraries/Astrolabe/Astrolabe-TomTom.lua
@@ -1658,7 +1658,7 @@ Astrolabe.MicroDungeonSize = MicroDungeonSize
 Astrolabe.zeroData = zeroData
 Astrolabe.activate = activate
 
-WorldMapSize[27][10] = { xOffset =  -500.500000, height = 380.000000, yOffset =  5242.000000, width = 570.000000 , __index = zeroDataFunc };
+WorldMapSize[27][10] = { xOffset =  -500.000000, height = 380.000000, yOffset =  5242.500000, width = 570.000000 , __index = zeroDataFunc };
 WorldMapSize[811][3] = { xOffset =  -114.999512, height = 173.333984, yOffset =  -726.333008, width = 260.000977 , __index = zeroDataFunc };
 WorldMapSize[811][4] = { xOffset =   -87.500000, height = 210.000000, yOffset =  -693.750000, width = 315.000000 , __index = zeroDataFunc };
 WorldMapSize[857][1] = { xOffset =   994.375000, height = 175.000000, yOffset =   750.000000, width = 262.500000 , __index = zeroDataFunc };
