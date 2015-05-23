5a10f771bb34a66c643f09a0c1d0f840ed3df710
Ludovicus <LuisOrtiz@Verizon.NET>
2015-05-22 21:14:40 -0400

    AutoSync to WoW-Pro.COM on Fri May 22 21:14:40 EDT 2015

--- WoWPro_Leveling/Alliance/05_12_Snowflakes_Azuremyst.lua
+++ WoWPro_Leveling/Alliance/05_12_Snowflakes_Azuremyst.lua
@@ -4,6 +4,11 @@
 -- Permissions beyond the scope of this license may be available at http://www.wow-pro.com/License.
 
 -- URL: http://wow-pro.com/wiki/source_code_azuremyst_isle
+-- Date: 2015-05-22 20:59
+-- Who: Ludovicus
+-- Log: Missing N tag
+
+-- URL: http://wow-pro.com/node/3206/revisions/27257/view
 -- Date: 2015-05-16 21:21
 -- Who: Ludovicus
 -- Log: Adjusted Azuremyst guide levels
