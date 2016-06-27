6dc9b96653001c445311f9d4166920a76f900900
Luis Ortiz <LuisOrtiz@Verizon.NET>
2016-06-27 07:31:57 -0400

    Add WoWPro_Recorder/WoWPro_Recorder.toc to CutRelease.sh

--- WoWPro_Utilities/CutRelease.sh
+++ WoWPro_Utilities/CutRelease.sh
@@ -2,7 +2,7 @@
 
 
 if [ ! -d WowPro -o ! -d WoWPro_Leveling -o ! -d WoWPro_Dailies -o ! -d WowPro_Profession -o ! -d WoWPro_WorldEvents -o ! -d WoWPro_Achievements ] ; then
-    echo "# This program must be run from a directory containing WowPro, WoWPro_Leveling, WoWPro_Dailies, WowPro_Profession, WoWPro_WorldEvents and WoWPro_Achievements" 
+    echo "# This program must be run from a directory containing WowPro, WoWPro_Leveling, WoWPro_Dailies, WowPro_Profession, WoWPro_WorldEvents and WoWPro_Achievements"
     exit 1
 fi
 
@@ -11,11 +11,11 @@ crelease=`awk -F: '$1 == "## Version" {print $2}' < WoWPro/WoWPro.toc | tr -d ' 
 echo '#' The current release is "[${crelease}]"
 echo -n '#' Please enter the new release number:
 read nrelease
-echo '#' The new release number will be "[${nrelease}]".  
+echo '#' The new release number will be "[${nrelease}]".
 echo -n "# Please ^C or abort this command or hit enter to proceed:"
 read confirm
 
-for toc in WowPro/WowPro.toc WoWPro_Leveling/WoWPro_Leveling.toc WoWPro_Dailies/WoWPro_Dailies.toc WowPro_Profession/WowPro_Profession.toc WoWPro_WorldEvents/WoWPro_WorldEvents.toc WoWPro_Achievements/WoWPro_Achievements.toc ; do
+for toc in WowPro/WowPro.toc WoWPro_Leveling/WoWPro_Leveling.toc WoWPro_Dailies/WoWPro_Dailies.toc WowPro_Profession/WowPro_Profession.toc WoWPro_WorldEvents/WoWPro_WorldEvents.toc WoWPro_Achievements/WoWPro_Achievements.toc WoWPro_Recorder/WoWPro_Recorder.toc ; do
   echo '#' Moving $toc to ${toc}~
   mv ${toc} ${toc}~
   echo "#" Editing  ${toc}
