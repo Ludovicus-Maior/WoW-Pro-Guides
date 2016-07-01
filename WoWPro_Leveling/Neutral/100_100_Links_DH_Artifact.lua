e14bbf7f90bac59406a84dfc7a31d04536e37cb1
Ludovicus <LuisOrtiz@Verizon.NET>
2016-06-30 20:23:53 -0400

    Add Initial Demon Hunter Starters.

---
+++ WoWPro_Leveling/Neutral/100_100_Links_DH_Artifact.lua
@@ -0,0 +1,66 @@
+
+local guide = WoWPro:RegisterGuide('LinksDHArtCH100100', 'Leveling', 'Orgrimmar', 'Linkslegend5', 'Horde')
+WoWPro:GuideLevels(guide,100, 100)
+WoWPro:GuideNextGuide(guide, '')
+WoWPro:GuideSteps(guide, function()
+return [[
+A Audience with the Warchief|QID|40976|M|52.53,88.18|Z|Orgrimmar|N|From Archmage Khadgar.|
+C Audience with the Warchief|QID|40976|QO|1|M|50.03,75.97|Z|Orgrimmar|N|1/1 Report to Saurfang|
+C Audience with the Warchief|QID|40976|QO|2|M|0.00,0.00|Z|Orgrimmar|N|1/1 Learn the Fate of the Horde|
+T Audience with the Warchief|QID|40976|M|45.81,15.11|Z|Durotar|N|To Allari the Souleater.|
+A Second Sight|QID|40982|M|45.81,15.11|Z|Durotar|N|From Allari the Souleater.|
+C Second Sight|QID|40982|QO|1|M|45.81,15.11|Z|Durotar|N|1/1 Spectral Sight used|
+T Second Sight|QID|40982|M|45.81,15.11|Z|Durotar|N|To Allari the Souleater.|
+A Demons Among Them|QID|40983|M|45.81,15.11|Z|Durotar|N|From Allari the Souleater.|
+C Demons Among Them|QID|40983|QO|2|M|45.59,15.93|Z|Durotar|N|12/12 Demons slain|
+T Demons Among Them|QID|40983|M|45.68,15.94|Z|Durotar|N|To Lady Sylvanas Windrunner.|
+A A Weapon of the Horde|QID|41002|M|45.68,15.94|Z|Durotar|N|From Lady Sylvanas Windrunner.|
+T A Weapon of the Horde|QID|41002|M|53.26,58.00|Z|Orgrimmar|N|To Elthyn Da'rai.|
+A In the Blink of an Eye|QID|44663|M|53.06,57.84|Z|Orgrimmar|N|From Khadgar's Upgraded Servant.|
+C In the Blink of an Eye|QID|44663|QO|1|M|0.00,0.00|Z|Orgrimmar|N|1/1 Take the Portal to Dalaran|
+T In the Blink of an Eye|QID|44663|M|57.63,45.77|Z|Dalaran|N|To Emissary Auldbridge.|
+A Call of the Illidari|QID|39047|M|57.99,44.59|Z|Dalaran|N|From Kor'vas Bloodthorn.|
+T Call of the Illidari|QID|39047|M|74.98,49.02|Z|Dalaran|N|To Altruis the Sufferer.|
+A The Power to Survive|QID|40816|M|74.98,49.02|Z|Dalaran|N|From Altruis the Sufferer.|
+C The Power to Survive|QID|40816|QO|1|M|74.98,49.02|Z|Dalaran|N|1/1 Artifact weapon chosen|
+T The Power to Survive|QID|40816|M|74.98,49.02|Z|Dalaran|N|To Altruis the Sufferer.|
+A Making Arrangements|QID|41120|M|74.98,49.02|Z|Dalaran|N|From Altruis the Sufferer.|
+T Making Arrangements|QID|41120|M|65.64,67.26|Z|Dalaran|N|To Altruis the Sufferer.|
+A By Any Means|QID|41121|M|65.64,67.26|Z|Dalaran|N|From Altruis the Sufferer.|
+C By Any Means|QID|41121|QO|1|M|66.08,68.11|Z|Dalaran|N|1/1 Convince Warden Alturas|
+C By Any Means|QID|41121|QO|2|M|0.00,0.00|Z|Dalaran|N|1/1 Enter the Violet Hold|
+C By Any Means|QID|41121|QO|3|M|52.13,47.80|Z|The Violet Hold|N|1/1 Taldath interrogated|
+T By Any Means|QID|41121|M|50.29,70.94|Z|The Violet Hold|N|To Altruis the Sufferer.|
+A The Hunt|QID|41119|M|50.29,70.94|Z|The Violet Hold|N|From Altruis the Sufferer.|
+C The Hunt|QID|41119|QO|1|M|75.13,47.54|Z|Dalaran|N|1/1 Fly to Felsoul Hold|
+T The Hunt|QID|41119|M|73.84,46.03|Z|Dalaran|N|To Kor'vas Bloodthorn.|
+A Eternal Vigil|QID|42869|M|73.84,46.03|Z|Dalaran|N|From Kor'vas Bloodthorn.|
+C Eternal Vigil|QID|42869|QO|1|M|94.25,63.07|Z|Dalaran|N|Glide to Illidari Redoubt|
+T Eternal Vigil|QID|42869|M|95.15,66.03|Z|Dalaran|N|To Jace Darkweaver.|
+A Securing the Way|QID|42872|M|95.15,66.03|Z|Dalaran|N|From Jace Darkweaver.|
+C Securing the Way|QID|42872|QO|1|M|96.47,66.29|Z|Dalaran|N|1/1 Have the prisoners' souls removed|
+C Securing the Way|QID|42872|QO|2|M|97.72,68.79|Z|Dalaran|N|1/1 Rip open a gateway to the Fel Hammer|
+T Securing the Way|QID|42872|M|95.17,66.00|Z|Dalaran|N|To Jace Darkweaver.|
+A Return to Mardum|QID|41033|M|95.02,66.10|Z|Dalaran|N|From Matron Mother Malevolence.|
+T Return to Mardum|QID|41033|M|57.63,67.36|Z|Orgrimmar|N|To Altruis the Sufferer.|
+A Unbridled Power|QID|41060|M|57.63,67.36|Z|Orgrimmar|N|From Altruis the Sufferer.|
+C Unbridled Power|QID|41060|QO|1|M|58.09,56.36|Z|Orgrimmar|N|1/1 Activate the Fel Hammer|
+T Unbridled Power|QID|41060|M|58.64,57.75|Z|Orgrimmar|N|To Altruis the Sufferer.|
+A Spoils of Victory|QID|41070|M|58.64,57.75|Z|Orgrimmar|N|From Altruis the Sufferer.|
+C Spoils of Victory|QID|41070|QO|1|M|55.88,54.11|Z|Orgrimmar|N|1/1 Speak to Battlelord Gaardoun|
+C Spoils of Victory|QID|41070|QO|2|M|0.00,0.00|Z|Orgrimmar|N|1/1 Follow Battlelord Gaardoun|
+T Spoils of Victory|QID|41070|M|0.00,0.00|Z|Orgrimmar|N|To Battlelord Gaardoun.|
+A Cursed Forge of the Nathrezim|QID|41064|M|0.00,0.00|Z|Orgrimmar|N|From Battlelord Gaardoun.|
+C Cursed Forge of the Nathrezim|QID|41064|QO|1|M|0.00,0.00|Z|Orgrimmar|N|1/1 Artifact empowered|
+T Cursed Forge of the Nathrezim|QID|41064|M|0.00,0.00|Z|Orgrimmar|N|To Battlelord Gaardoun.|
+A The Hunter's Gaze|QID|41066|M|0.00,0.00|Z|Orgrimmar|N|From Allari the Souleater.|
+C The Hunter's Gaze|QID|41066|QO|1|M|0.00,0.00|N|1/1 Gaze upon the Burning Legion|
+T The Hunter's Gaze|QID|41066|M|59.39,51.34|Z|Mardum, the Shattered Abyss|N|To Allari the Souleater.|
+A Time is of the Essence|QID|41096|M|59.39,51.34|Z|Mardum, the Shattered Abyss|N|From Allari the Souleater.|
+T Time is of the Essence|QID|41096|M|0.00,0.00|Z|Mardum, the Shattered Abyss|N|To Altruis the Sufferer.|
+A Direct Our Wrath|QID|41099|M|0.00,0.00|Z|Mardum, the Shattered Abyss|N|From Altruis the Sufferer.|
+T Direct Our Wrath|QID|41099|M|0.00,0.00|Z|Mardum, the Shattered Abyss|N|To Altruis the Sufferer.|
+]]
+
+end)
+
