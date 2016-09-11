ab1cacbad1ca838a689d8eeff53d2873d550d2bc
Ludovicus <LuisOrtiz@Verizon.NET>
2016-09-11 15:19:22 -0400

    AutoSync to WoW-Pro.COM on Sun Sep 11 15:19:22 EDT 2016

--- WoWPro_Leveling/Alliance/098_110_Ludovicus_Stormheim.lua
+++ WoWPro_Leveling/Alliance/098_110_Ludovicus_Stormheim.lua
@@ -4,6 +4,11 @@
 -- Permissions beyond the scope of this license may be available at http://www.wow-pro.com/License.
 
 -- URL: http://wow-pro.com/wiki/stormheim_alliance
+-- Date: 2016-09-11 21:06
+-- Who: Ludovicus
+-- Log: Play through to [Felskorn Warcamp]
+
+-- URL: http://wow-pro.com/node/3683/revisions/27734/view
 -- Date: 2016-09-10 21:47
 -- Who: Ludovicus
 -- Log: Play through to[Knocking on Valor's Door]. Added prototype for faction specific Skold-Ashil section.
@@ -256,7 +261,6 @@ $ Glimmering Treasure Chest|QID|38763|M|49.70,49.20;49.69,47.28|CS|N|Dodge the e
 A Eating Into Our Business|QID|39789|M|51.46,56.95|N|From Ootasa Galehoof.|RANK|2|
 C Eating Into Our Business|QID|39789|M|48.54,56.93|S|N|Kill Duskpelt worg.|RANK|2|
 $ Small Treasure Chest|QID|43207|M|49.07,60.00|N|Loot for a bit of treasure and resources.|RANK|2|
-; K Kill Houndmaster Ely|QID|38712|M|47.10,57.20|QO|1|ITEM|129037|T|Houndmaster Ely|N|Silver - Kill and loot for a bit of loot and Treasure.|RANK|2| ; Emma Horde only
 C Eating Into Our Business|QID|39789|M|48.54,56.93|US|N|Finish killing Duskpelt worg.|RANK|2|
 T Eating Into Our Business|QID|39789|M|51.46,57.06|N|To Rax Sixtrigger.|RANK|2|
 A Only the Finest|QID|39793|M|51.46,57.06|N|From Rax Sixtrigger.|RANK|2|
@@ -421,49 +425,53 @@ T Paid in Lifeblood|QID|39855|M|60.14,50.74|N|To Havi.|
 A A Heavy Burden|QID|40078|M|60.14,50.74|N|From Havi.|
 C A Heavy Burden|QID|40078|M|59.97,50.62;60.53,51.22|CS|NC|N|Carry Havi's fish to the fire.|
 T A Heavy Burden|QID|40078|M|60.13,50.72|N|To Havi.|
-N Warning!|QID|40001|N|This guide has not been tested beyond this point. Use at your own risk.|
 A Knocking on Valor's Door|QID|40001|M|60.13,50.72|N|From Havi.|
 
-A To Catch a Banshee|QID|39059|M|59.97,51.2|N|From Ensign Ward.|
-C To Catch a Banshee|QID|39059|M|71.6,59.8|CHAT|N|Chat with Ensign Ward to get going.|
-T To Catch a Banshee|QID|39059|M|71.6,59.8|N|To Genn Greymane.|
-A Combustible Contagion|QID|39060|M|71.6,59.8|N|From Genn Greymane.|
-A Whispers from the Dark|QID|39060|M|71.6,59.8|N|From Genn Greymane.|
-A Cut Out the Heart|QID|39060|M|72.4,61.1|N|From Forsaken Battle Plans.|
-K Apothecary Curseheart|QID|39060|M|75.0,61.8|T|Apothecary Curseheart|
-T Combustible Contagion|QID|39060|M|71.6,59.8|N|To Genn Greymane.|
-T Whispers from the Dark|QID|39060|M|71.6,59.8|N|To Genn Greymane.|
-T Cut Out the Heart|QID|39060|M|71.6,59.8|N|To Genn Greymane.|
-A To Skold-Ashil|QID|39062|M|71.6,59.8|N|From Genn Greymane.|
-T To Skold-Ashil|QID|39062|M|63.6,61.8|N|To Commander Lorna Crowley|
-A Shielded Secrets|QID|39063|M|63.6,61.8|N|From Commander Lorna Crowley|
-A Assault on Skold-Ashil-Bonus Objective|QID|38939|M|56.76,66.30|N|Autoa-accepted when you enter the area.|
-C Assault on Skold-Ashil-Bonus Objective|QID|38939|M|56.76,66.30|S|N|Kill vrykal and destroy the weapon racks. There are also some shieldmaiden statues, you can click on the for a 10 minute buff Three differant buffs, so click all you see.|
+A To Catch a Banshee|QID|39059|M|59.97,51.36|N|From Ensign Ward.|
+C To Catch a Banshee|QID|39059|M|71.54,59.82|CHAT|N|Chat with Ensign Ward to get going.|
+f Greywatch|QID|39059|M|72.18,59.85|N|At Gryphonp-Master Clifford.|
+T To Catch a Banshee|QID|39059|M|71.54,59.82|N|To Genn Greymane.|
+A Combustible Contagion|QID|39060|M|71.54,59.82|N|From Genn Greymane.|
+A Whispers from the Dark|QID|39061|M|71.54,59.82|N|From Genn Greymane.|
+A Cut Out the Heart|QID|39472|M|72.4,61.1|N|From Forsaken Battle Plans. Make your way up the hill.|
+C Western Plague Cache|QID|39060|M|72.56, 60.81|QO|3|U|127295|N|Toss the toch on the banner.|
+C Eastern Plague Cache|QID|39060|M|73.49, 63.18|QO|2|U|127295|N|Toss the toch on the banner.|
+C Northern Plague Cache|QID|39060|M|74.46,58.33|QO|1|U|127295|N|Toss the toch on the banner.|
+K Apothecary Curseheart|QID|39472|M|75.0,61.8|QO|1|T|Apothecary Curseheart|N|Beware the Festering Inquisitor (level 110)|
+T Combustible Contagion|QID|39060|M|71.54,59.82|N|To Genn Greymane.|
+T Whispers from the Dark|QID|39061|M|71.54,59.82|N|To Genn Greymane.|
+T Cut Out the Heart|QID|39472|M|71.54,59.82|N|To Genn Greymane.|
+A To Skold-Ashil|QID|39062|M|71.54,59.82|N|From Genn Greymane.|
+T To Skold-Ashil|QID|39062|M|63.56,61.73|N|To Commander Lorna Crowley|
+A Shielded Secrets|QID|39063|M|63.56,61.73|N|From Commander Lorna Crowley|
+A Assault on Skold-Ashil-Bonus Objective|QID|39119|M|56.76,66.30|N|Auto-accepted when you enter the area.|
+C Assault on Skold-Ashil-Bonus Objective|QID|39119|M|56.76,66.30|S|N|Kill vrykal and destroy the weapon racks. There are also some shieldmaiden statues, you can click on the for a 10 minute buff Three differant buffs, so click all you see.|
 C Shielded Secrets|QID|39063|M|63.42,64.64|S|NC|N|Examine Engraved Shields, these are scattered all over the area.|
 A Stories of Battle|QID|39405|M|58.56,64.21|N|From Verse of Ashilvara lying on the table.|
 C Stories of Battle|QID|39405|M|61.93,61.46|S|N|Kill pristesses of Alir to loot Verses of Ashilvara.|
 $ Small Treasure Chest|QID|40090|M|57.95,63.35|N|Loot for a bit of treasure and resources.|
-$ Treasure Chest|QID|40089|M|61.83,62.89|
+$ Treasure Chest|QID|40089|M|61.83,62.89|N|Loot for a bit of treasure and resources.|
 K Kill Isel the Hammer|QID|39120|M|62.02,60.48|QO|1|ITEM|129133|T|Isel the Hammer|N|Silver - Kill and loot for a bit of resources and treasure.|
-$ Treasure Chest|QID|40088|M|59.25,58.59|N|Loot for a bit of treasure and resources.|
+$ Treasure Chest|QID|40088|M|59.31,58.45|N|Loot for a bit of treasure and resources.|
 C Shielded Secrets|QID|39063|M|63.42,64.64|US|NC|N|Finish examining the Engraved Shields.|
 C Stories of Battle|QID|39405|M|61.93,61.46|US|N|Finish collecting the Verses of Ashilvara.|
-C Assault on Skold-Ashil|QID|38939|M|61.93,61.46|US|N|Finish this up or click it off manually.|
-t Assault on Skold-Ashil|QID|38939|M|61.93,61.46|N|Auto turned in when completed.|
+C Assault on Skold-Ashil|QID|39119|M|61.93,61.46|US|N|Finish this up or click it off manually.|
+t Assault on Skold-Ashil|QID|39119|M|61.93,61.46|N|Auto turned in when completed.|
 T Stories of Battle|QID|39405|M|61.93,61.46|N|To UI Alert.|
-T Shielded Secrets|QID|39063|M|60.6,61.8|N|To Commander Lorna Crowley|
-A Becoming the Ascendant|QID|39092|M|60.4,65|From Commander Lorna Crowley|
+T Shielded Secrets|QID|39063|M|60.59,65.08|N|To Commander Lorna Crowley|
+A Becoming the Ascendant|QID|39092|M|60.59,65.08|N|From Commander Lorna Crowley|
 C Becoming the Ascendant|QID|39092|M|60.42,66.71|QO|3|NC|N|Recieve Blessing at Eyir's Shield.|
 K Kill Ivory Sentinel|QID|39031|M|59.86,68.07|QO|1|ITEM|132895|T|Ivory Sentinel|N|Silver - Kill and loot for a bit of treasure and resources.|
 C Becoming the Ascendant|QID|39092|M|60.49,69.52|QO|2|NC|N|Recieve Blessing at Eyir's Pauldron.|
 C Becoming the Ascendant|QID|39092|M|61.33,69.52|QO|1|NC|N|Recieve Blessing at Eyir's Helm.|
 C Becoming the Ascendant|QID|39092|M|61.36,66.75|QO|4|NC|N|Recieve Blessing at Eyir's Spear.|
 C Becoming the Ascendant|QID|39092|M|61.72,68.09|QO|5|CHAT|N|Commune with the statue.|
-T Becoming the Ascendant|QID|39092|M|62.2,68|To Genn Greymane|
-A Ending the New Beginning|QID|39122|M|62.2,68|From Genn Greymane|
+T Becoming the Ascendant|QID|39092|M|62.31,68.09|N|To Genn Greymane|
+A Ending the New Beginning|QID|39122|M|62.31,68.09|N|From Genn Greymane|
 C Banshee Queen|QID|39122|QO|1|N|Follow Greymane and see what the Banshee Queen is up to|
-T Ending the New Beginning|QID|39122|M|71.4,60.6|N|ToCommander Lorna Crowley|
+T Ending the New Beginning|QID|39122|M|71.46,60.58|N|ToCommander Lorna Crowley|
 
+N Warning!|QID|40001|N|This guide has not been tested beyond this point. Use at your own risk.|
 R Felskorn Warcamp|QID|40001|M|62.85,60.42|N|The best way I think is to use your grapple hook and go up the rooks at the back of Skol Ashil (near where Isel the Hammer was).  There is also a gate out the back of Skol Ashil then a path thru the Runewoods and finaly into the Felskorn Warcamp.|
 T Knocking on Valor's Door|QID|40001|M|65.91,59.53|N|To Vethir.|
 A A Familiar Fate|QID|40002|M|65.91,59.53|N|From Vethir.|
@@ -492,7 +500,7 @@ C Stormheim's Salvation|QID|40005|M|70.22,69.50|CHAT|N|Ask Vethir for a ride to 
 C Stormheim's Salvation|QID|40005|M|70.94,70.16|N|God-King Skovald defeated.|
 T Stormheim's Salvation|QID|40005|M|70.25,69.24|N|Ask Vethir for a ride back down so you can turn the quest in to Havi.|
 A Securing the Aegis|QID|40072|M|70.25,69.24|N|From Havi. This is a dungeon quest, so queue, call friends over or check it off and move on.|
-H Return to Dalaran|QID|40072|M|70.52,69.24|U|140192|N|If you are doing this guide on rank 1, you are done now, hearth ot Dalaran or your class hall and continue on your adventuring.|LEAD|39792|
+H Return to Dalaran|QID|40072|M|70.52,69.24|U|140192|N|If you are doing this guide on rank 1, you are done now, hearth to Dalaran or your class hall and continue on your adventuring.|LEAD|39792|
 
 R Valdisdall|QID|39984|M|63.00,48.61|U|6948|N|On to Valdisdall for some optional quests, silvers and treasure.|RANK|2|
 A Remnants of the Past|QID|39984|M|63.00,48.61|N|From Watcher's Journal.|RANK|2|
