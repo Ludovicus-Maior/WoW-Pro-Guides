01bf9544ad075b10d310612660123288e74aaf02
Ludovicus <LuisOrtiz@Verizon.NET>
2017-05-14 16:44:50 -0400

    Add placeholders for Forgotten Hippogryph and Wailing Caverns Pet Dungeon.

---
+++ WoWPro_Achievements/General/Emmaleah_Hippogryph.lua
@@ -0,0 +1,150 @@
+local guide = WoWPro:RegisterGuide('Emm_Hippogryph', 'Achievements', 'Azsuna', 'Emmaleah', 'Neutral')
+WoWPro:GuideIcon(guide,"ACH",11796)
+WoWPro.Achievements:GuideMisc(guide, "Reins of the Long Forgotten Hippogryph", "Mount", "Legion")
+WoWPro:GuideLevels(guide,100, 110)
+WoWPro:GuideSteps(guide, function()
+
+return [[
+
+N Ephemeral Crystals|N|You are searching for 5 ephemeral crystals somewhere in Azsuna, when you find the fifth one, you get the mount and a zonewide message tells the other treasure hunters it has been found. "you hear a faint caw in the distance". For more info google Forgotten Hippogryph or see comments accompanying this guide on wow-pro.com.|
+F subzone|QID|99999|M|54.75,33.85|N|In the water with the basilisks.#A1|
+F Olivian Veil_|QID|99999|M|60.15,49|N|Bottom edge of the hill in Olivian Veil.#A10|
+F subzone|QID|99999|M|49.67,55.35|N|In the cave next to some piles of gold.#A100|
+F subzone|QID|99999|M|36,36|N|On the cliff edge.#A101|
+F subzone|QID|99999|M|55.98,42.82|N|.#A102|
+F subzone|QID|99999|M|70,30|N|.#A103|
+F subzone|QID|99999|M|46.55,5.36|N|.#A104|
+F subzone|QID|99999|M|65.4,38.4|N|On the hill.#A105|
+F subzone|QID|99999|M|36.58,12.14|N|.#A106|
+F subzone|QID|99999|M|57,31|N|Next to tree in the middle of circle.#A107|
+F subzone|QID|99999|M|60,17|N|Beside tree with the owlcats.#A108|
+F subzone|QID|99999|M|42.96,28.75|N|Near Doomlord Kazrok.#A109|
+F subzone|QID|99999|M|29.85,35.87|N|Between some dead trees.#A11|
+F subzone|QID|99999|M|45.55,17.25|N|Next to the lake where lake turns to river .#A12|
+F subzone|QID|99999|M|56.45,40.5|N|Under the bridge.#A13|
+F subzone|QID|99999|M|67.1,46.3|N|Next to rock and log.#A14|
+F Lair of the Deposed_|QID|99999|M|49.15,8.15|N|On little rock inside Lair of the Deposed cave .#A15|
+F subzone|QID|99999|M|48.45,48.15|N|On a little grass patch.#A16|
+F subzone|QID|99999|M|59.75,27.85|N|Behind the tree.#A17|
+F subzone|QID|99999|M|54.33,26.03|N|Next to roots of tree- river side.#A18|
+F subzone|QID|99999|M|54.8,28.05|N|Between the large tree and the river.#A18|
+F subzone|QID|99999|M|54.16,27.6|N|Behind cement hut in bushes.#A19|
+F subzone|QID|99999|M|37.5,32.9|N|Nor'danil wellspring - behind the wall next to bush and tree.#A2|
+F Olivian Veil_|QID|99999|M|62.53,52.36|N|On hill next to tree - Olivian Veil.#A20|
+F subzone|QID|99999|M|52.41,13.44|N|By the tree.#A21|
+F The Ruined Sanctum_|QID|99999|M|67,51.95|N|Outside doorway to building -The Ruined Sanctum.#A22|
+F Llothien Highlands_|QID|99999|M|35.6,37.8|N|Next to tree - Llothien Highlands.#A23|
+F Oceanus Cove_|QID|99999|M|48.57,57.28|N|Inside broken ship inside Oceanus cave.#A24|
+F subzone|QID|99999|M|54.85,52.25|N|Inside cave where Cole neutral mob is.#A25|
+F subzone|QID|99999|M|50.48,57|N|Inside cave.#A26|
+F subzone|QID|99999|M|52.29,25.24|N|Off road next to tree a little hidden.#A29|
+F Leyhollow_|QID|99999|M|50.55,20.45|N|In the Leyhollow cave, near wall-entrance at 47.9,24.8.#A3|
+F Ruins of Nar'thalas_|QID|99999|M|58.52,45.48|N|In cave at Ruins of Nar’thalas on rock between spine and skull Commander Eksis mob in there.#A30|
+F subzone|QID|99999|M|59.28,38.4|N|At bottom of cliff, behind tent.#A31|
+F subzone|QID|99999|M|61.6,40.1|N|Next to tree near eye of Azshara portal .#A32|
+F subzone|QID|99999|M|60.54,46.73|N|To the right outside of the blue teleporter cave.#A32|
+F subzone|QID|99999|M|61.9,30.9|N|Behind the tree.#A33|
+F Azurewing Repose_|QID|99999|M|47.1,25.8|N|Between the tree and the statue, next to Senegos pool.#A34|
+F subzone|QID|99999|M|34.91,17.15|N|Behind the ruins.#A35|
+F subzone|QID|99999|M|48.88,45.61|N|On a rock.#A36|
+F subzone|QID|99999|M|53.7,28.05|N|In the cave by the river.#A37|
+F Oceanus Cove_|QID|99999|M|49.3,50.55|N|Behind the sleeping giant.#A38|
+F subzone|QID|99999|M|50.21,23.49|N|In cave - very hidden inside the crystal formation only the tip shows.#A4|
+F subzone|QID|99999|M|57.39,16.69|N|On the path.#A40|
+F subzone|QID|99999|M|48.05,52.7|N|In the cave next to some piles of gold.#A41|
+F subzone|QID|99999|M|46.58,53.6|N|Between some rocks by the sleeping bears.#A42|
+F Felblaze Ingress_|QID|99999|M|65.4,29.5|N|At felblaze inside stump of big tree.#A43|
+F subzone|QID|99999|M|46.95,48.93|N|On hill above the neutral giants - behind tree near the eternal bride and groom.#A44|
+F subzone|QID|99999|M|40.23,32.8|N|Behind the tree.#A45|
+F subzone|QID|99999|M|49.28,58.03|N|By the broken pillar right next to bride and groom.#A46|
+F subzone|QID|99999|M|41.5,31|N|Inside the Jilted Lover npc cave.#A47|
+F subzone|QID|99999|M|36,23|N|Between 2 trees on the road.#A48|
+F subzone|QID|99999|M|61.15,30.4|N|In a cave, on the rock to the right (near Kira Iresoul npc).#A5|
+F subzone|QID|99999|M|52.7,57.9|N|On the side of the cliff.#A50|
+F subzone|QID|99999|M|50,33.1|N|Next to shells and hut in murloc area .#A51|
+F subzone|QID|99999|M|51.31,65.15|N|Inbetween 2 large wooden logs.#A52|
+F subzone|QID|99999|M|38.69,9.31|N|By the berry bushes.#A53|
+F Felblaze Ingress_|QID|99999|M|67.75,32.9|N|Felblaze Ingress - down hill near water .#A54|
+F subzone|QID|99999|M|55.55,10.3|N|Up the side of the cliff.#A55|
+F subzone|QID|99999|M|53.61,63.36|N|By torch inside alcove.#A56|
+F subzone|QID|99999|M|45,53.4|N|Inside the broken ship.#A57|
+F subzone|QID|99999|M|64.46,33.19|N|Between the tree and tent.#A58|
+F subzone|QID|99999|M|63.38,46.14|N|Under tree.#A59|
+F subzone|QID|99999|M|47.3,61.9|N|Between 2 rocks.#A6|
+F subzone|QID|99999|M|65.15,50.82|N|Under tree.#A60|
+F subzone|QID|99999|M|45.38,45.49|N|Next to tree east of Illidari Stand flight path.#A61|
+F subzone|QID|99999|M|55.96,29.36|N|Inside the three pillars to the right of the building where Pridelord Meowl is.#A62|
+F subzone|QID|99999|M|58.22,24.65|N|By tree on the top of cliff.#A64|
+F subzone|QID|99999|M|62.25,35.9|N|By the trees.#A65|
+F subzone|QID|99999|M|46.9,17.75|N|At the top of the slope.#A66|
+F subzone|QID|99999|M|50.75,50|N|In the cave by the table.#A67|
+F subzone|QID|99999|M|37,21.75|N|By the tree.#A68|
+F subzone|QID|99999|M|56.5,12.45|N|By the pink flower.#A69|
+F subzone|QID|99999|M|34.55,35.68|N|Inside the cave.#A7|
+F Hatecoil Warcamp_|QID|99999|M|65.49,42.47|N|Next to water near bridge in the corner of Hatecoil Warcamp.#A70|
+F subzone|QID|99999|M|51.7,31.1|N|On the grass.#A71|
+F subzone|QID|99999|M|49.6,53.79|N|By the waterfall.#A72|
+F Azurewing Repose_|QID|99999|M|49.3,24.2|N|Inside bushes behind shrine up on a hill overlooking Azurewing Repose.#A73|
+F subzone|QID|99999|M|40.54,37.36|N|By the tree, south side.#A74|
+F subzone|QID|99999|M|49.32,31.53|N|A bit south from Azurewing Repose Corner of hill behind bush .#A75|
+F subzone|QID|99999|M|60.2,35.03|N|Next to broken pillar next to the middle of the road split.#A76|
+F subzone|QID|99999|M|60.2,54.4|N|Near the shadowfiends under little half tent.#A77|
+F subzone|QID|99999|M|51.4,37.8|N|Underwater closer to Mrrgrl rare elite.#A78|
+F subzone|QID|99999|M|42.25,8.35|N|Up the side of the cliff.#A8|
+F subzone|QID|99999|M|46,8.8|N|Bottom of slope.#A81|
+F subzone|QID|99999|M|29.9,26.55|N|On the island.#A82|
+F subzone|QID|99999|M|56.85,38.9|N|Near the water.#A83|
+F Gloombound Barrow_|QID|99999|M|63.5,54.1|N|Gloombound Barrow in the cave that you fight an eradari for the imps quest.#A84|
+F Isle of the Watchers_|QID|99999|M|51,76.35|N|Island of watchers cave by roots.#A85|
+F subzone|QID|99999|M|57.69,42.31|N|Inside cave with big giant.#A86|
+F subzone|QID|99999|M|55.56,32.72|N|Bottom of cliff, next to tree.#A87|
+F subzone|QID|99999|M|52,71|N|On a rock by the water? not confirmed where.#A88|
+F subzone|QID|99999|M|44.1,59.8|N|On a rock overlooking a ship.#A9|
+F Empyrean Society Enclave_|QID|99999|M|64,34|N|Near the crossroads near the table in The Empyrean Society Enclave, very visable.#A90|
+F Farondale_|QID|99999|M|68.2,24.3|N|Farondale- north of the demon camp; very much visible from a great distance.#A91|
+F subzone|QID|99999|M|43,18|N|Inside Cave.#A92|
+F subzone|QID|99999|M|62.17,54.76|N|Next to the tree.#A97|
+F subzone|QID|99999|M|67,33.7|N|Behind the ruins.#A98|
+F subzone|QID|99999|M|59.7,36.8|N|On edge of hill next to tree .#A99|
+F Hatecoil Warcamp_|QID|99999|M|62.3,40.5|N|Next to naga tent .#B100|
+F subzone|QID|99999|M|67,46|N|Next to the log.#B111|
+F subzone|QID|99999|M|41.4,31|N|Inside Llothien Grizzly Cave to the right .#B20|
+F subzone|QID|99999|M|42.66,18.06|N|Inside Runa’s Hovel Cave on rock between spine and skull.#B23|
+F subzone|QID|99999|M|42.2,62.3|N|On the hill near the Cove Skrog .#B24|
+F subzone|QID|99999|M|43,28|N|Behind tree next to Doomlord Kazrok.#B25|
+F subzone|QID|99999|M|45,53.6|N|At the coast inside the broken half of a ship hard to see from almost every angle.#B29|
+F Lost Orchard_|QID|99999|M|46.56,8.53|N|In the lost Orchard next to tree.#B30|
+F subzone|QID|99999|M|47.2,33|N|On grass next to the river.#B35|
+F subzone|QID|99999|M|48,48|N|Next to the two neutral giants at the bottom of the valley .#B38|
+F subzone|QID|99999|M|49.39,27.7|N|Next to tree - a little hidden .#B43|
+F subzone|QID|99999|M|49.18,53.5|N|4 On a cliff just above the bridge where Warbringer Mox'na patrols.#B46|
+F subzone|QID|99999|M|50.5,16.4|N|Between 3 trees on the left with one of those unicorn horses.#B48|
+F subzone|QID|99999|M|50.73,49.89|N|Inside the cave with some neutral NPCs at Shipwreck Arena.#B51|
+F subzone|QID|99999|M|50.48,56.99|N|In the cave to the left close to eternal bride and groom.#B52|
+F subzone|QID|99999|M|51,65|N|Next to the tied rope around the poles .#B54|
+F subzone|QID|99999|M|52.15,31.85|N|By the shrine.#B59|
+F subzone|QID|99999|M|34.8,35.3|N|On another cliff edge.#B6|
+F Nar'thalas Academy_|QID|99999|M|52.96,35.94|N|Under the water, under rock arch, next to Narthalas Academy.#B60|
+F subzone|QID|99999|M|53.36,26.08|N|Between the 3 trees to the left of the path .#B62|
+F subzone|QID|99999|M|34.6,35.7|N|In cave at the bottom of the hill .#B7|
+F subzone|QID|99999|M|55,55|N|Bottom of hill.#B75|
+F subzone|QID|99999|M|56.92,26|N|Inside the cave in Llothein entrance is at 55.74 25.46.#B78|
+F subzone|QID|99999|M|56,40|N|By the lake under the bridge .#B79|
+F subzone|QID|99999|M|35,22|N|Beachside near the water  .#B8|
+F Ley Ruins of Zar'khenar_|QID|99999|M|57.4,16.79|N|At Ley-Ruins of Zarkhenar on edge of hill .#B80|
+F subzone|QID|99999|M|57.5,26.6|N|Next to the road.#B81|
+F subzone|QID|99999|M|59.06,37.48|N|Near the female NE statue.#B88|
+F subzone|QID|99999|M|35.72,25.07|N|Beside the tree with the lantern and fence to the left of the path  .#B9|
+F subzone|QID|99999|M|29.9,36|N|In the corner crack between the hills on an island.#C1|
+F subzone|QID|99999|M|47.1,61.7|N|Left from Oceanus cove, on a hill.#C14|
+F subzone|QID|99999|M|51,61|N|.#C23|
+F subzone|QID|99999|M|59.08,44.88|N|.#C39|
+F subzone|QID|99999|M|48.7,48.5|N|.#C44|
+F subzone|QID|99999|M|50.4,7.8|N|.#C47|
+F subzone|QID|99999|M|45.7,9.2|N|.#C51|
+F subzone|QID|99999|M|40.55,36.29|N|Off road beside tree.#C63|
+N Reset|QID|99999|N|Hope you found your mount, if not reset and try another day.|
+
+]]
+
+end)
