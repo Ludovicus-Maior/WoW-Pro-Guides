f6e7a8b2861ce99013e45cbce32d9e38e47f6fdb
Luis Ortiz <ludovicus.maior@gmail.com>
2018-02-01 14:57:10 +0000

    Add /wp taint command to find tainted globals.

--- WoWPro/WoWPro_Command.lua
+++ WoWPro/WoWPro_Command.lua
@@ -19,8 +19,25 @@ local function handler(msg, editbox)
             local msg = string.format("Player at %.2f,%.2f@%s", X*100, Y*100, WoWPro.GetZoneText())
             ChatFrame1:AddMessage(msg)
         end
+    elseif ltoken == "taint" then
+        WoWProDB.global.Tainted = {}
+        local now = date("%Y%m%d%H%M")
+        for key in pairs(_G) do
+            local isSecure, taint = issecurevariable(key)
+            if not isSecure then
+                if taint == "WoWPro" then
+                    WoWPro:Warning("Variable %s tainted by WoWPro", key, taint)
+                end
+                WoWProDB.global.Tainted[taint] = WoWProDB.global.Tainted[taint] or {}
+                WoWProDB.global.Tainted[taint][key] =  now
+            end
+		end
+		local msg = string.format("WoWPro taint report logged to debug log.")
+        ChatFrame1:AddMessage(msg)
+        msg = string.format("Global taint log in: <World\ of\ Warcraft>/WTF/Account/<#>/SavedVariables/WoWPro.lua ")
+        ChatFrame1:AddMessage(msg)
     else
-        local msg = string.format("%s or %s", SLASH_WOWPRO1, SLASH_WOWPRO2)
+        local msg = string.format("%s or %s [where|taint]", SLASH_WOWPRO1, SLASH_WOWPRO2)
         ChatFrame1:AddMessage(msg)
     end
 end
