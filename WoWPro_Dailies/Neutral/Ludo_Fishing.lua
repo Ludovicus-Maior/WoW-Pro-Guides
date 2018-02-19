5003e876026b22e9f636f0c3278580feae1d3bb2
Ludovicus <ludovicus.maior@gmail.com>
2018-02-19 17:06:31 -0500

    Updated WoWPro_Dailies/Neutral/Ludo_Fishing.lua from WoW-Pro.COM

--- WoWPro_Dailies/Neutral/Ludo_Fishing.lua
+++ WoWPro_Dailies/Neutral/Ludo_Fishing.lua
@@ -4,6 +4,11 @@
 -- Permissions beyond the scope of this license may be available at http://www.wow-pro.com/License.
 
 -- URL: http://wow-pro.com/wiki/dailies_fishing_dailies_capitol_cities_shattrath_dalaran
+-- Date: 2018-02-19 22:57
+-- Who: Ludovicus_Maior
+-- Log: Upload Grail corrections to PRE/LEAD tags
+
+-- URL: http://wow-pro.com/node/3752/revisions/28965/view
 -- Date: 2018-02-02 20:53
 -- Who: Ludovicus
 -- Log:  added [Nat Pagle, Angler Extreme]
@@ -54,7 +59,7 @@ T Cold Water Fishing|QID|29342|M|48.4,8.2|Z|Ironforge|N|To Grimnur Stonebrand|FA
 
 A Fish fer Squrky|QID|29344|M|48.4,8.2|Z|Ironforge|N|From Grimnur Stonebrand|O|FACTION|Alliance|
 l Blind Minnows|ACTIVE|29344|M|47.81,14.26|Z|Ironforge|N|Turn around and fish the water in The Forlorn Cavern.|L|69933 3|FACTION|Alliance|
-C Feed Squrky||QID|29344|M|48.4,8.8|Z|Ironforge|N|Feed the minnows to Squrky|FACTION|Alliance|
+C Feed Squrky|QID|29344|M|48.4,8.8|Z|Ironforge|N|Feed the minnows to Squrky|FACTION|Alliance|
 T Fish fer Squrky|QID|29344|M|48.4,8.2|Z|Ironforge|N|To Grimnur Stonebrand|FACTION|Alliance|
 
 A Live Bait|QID|29347|M|48.4,8.2|Z|Ironforge|N|From Grimnur Stonebrand|O|FACTION|Alliance|
@@ -74,7 +79,7 @@ l Rat Traps|ACTIVE|29350|N|Head into the Ironforge Deeprun Tram and click on 3 R
 C Bait-o-Matic Blueprints|QID|29350|QO|1|U|69979|N|Use the blueprints to built it and then turn it in.|FACTION|Alliance|
 T The Gnomish Bait-o-Matic|QID|29350|M|48.4,8.2|Z|Ironforge|N|To Grimnur Stonebrand|FACTION|Alliance|
 
-A I Got Nothin' Left!|QID|6609|M|48.4,8.2|Z|Ironforge|N|From Grimnur Stonebrand|O|FACTION|Alliance|
+A I Got Nothin' Left!|QID|6609|LEAD|6607|M|48.4,8.2|Z|Ironforge|N|From Grimnur Stonebrand|O|FACTION|Alliance|
 T I Got Nothin' Left!|QID|6609|M|58.6,60.2|Z|Dustwallow Marsh|N|Head out to Theramore and head south to the island that Nat Pagle is on.|FACTION|Alliance|
 
 ; Stormwind Quests
@@ -99,7 +104,7 @@ A Thunder Falls|QID|26536|M|55.0,69.6|Z|Stormwind City|N|From Catherine Leland|O
 C Violet Perch|QID|26536|M|22.10,57.10;18.52,60.70;17.65,67.90;20.70,59.56;17.67,64.33;22.97,58.91;25.92,59.76|CN|Z|Elwynn Forest|N|Head out to Elwynn Forest to Thunder Falls, north of Mirror lake on top of the hill.|FACTION|Alliance|
 T Thunder Falls|QID|26536|M|55.0,69.6|Z|Stormwind City|N|To Catherine Leland|FACTION|Alliance|
 
-; Horde Fishing Guidehttps://www.wowhead.com/quests/max-req-level:80/side:2?filter=48;356;0
+; Horde FishingGuidehttps://www.wowhead.com/quests/max-req-level:80/side:2?filter=48;356;0
 ;Orgrimmarhttps://www.wowhead.com/npc=43239/razgar
 A A Furious Catch|QID|26588|M|65.8,40.8|Z|Orgrimmar@Orgrimmar|N|From Razgar|O|FACTION|Horde|
 C Giant Furious Pike|QID|26588|M|36.20,22.70;36.70,12.45;35.53,2.45;37.20,20.35;37.00,4.45;37.20,18.00;36.77,6.98;36.90,9.75;37.39,15.64|Z|Durotar|CN|N|Head out to the Southfury River on the Durotar or Northen Barrens banks to fish. Do not go to Ashenvale or Azshara!|FACTION|Horde|
@@ -123,7 +128,7 @@ A No Dumping Allowed|QID|26556|M|65.8,40.8|Z|Orgrimmar@Orgrimmar|N|From Razgar|O
 C Toxic Puddlefish|QID|26556|M|36.42,82.70;39.00,83.12;35.83,79.95;37.53,84.48;38.24,80.28;35.62,77.46|CN|Z|Orgrimmar@Orgrimmar|N|Go to the Goblin Slums in Orgrimmar and fish them up.|FACTION|Horde|
 T No Dumping Allowed|QID|26556|M|65.8,40.8|Z|Orgrimmar@Orgrimmar|N|To Razgar|FACTION|Horde|
 
-; ThunderBluffhttps://www.wowhead.com/npc=3028/kah-mistrunner
+;ThunderBluffhttps://www.wowhead.com/npc=3028/kah-mistrunner
 A Craving Crayfish|QID|29349|M|56,47|Z|Thunder Bluff|N|From Kah Mistrunner|O|FACTION|Horde|
 C Stonebull Crayfish|QID|29349|M|51.90,49.30;44.52,56.04;46.64,53.17;43.82,59.54;53.48,52.05;49.72,51.88;52.43,55.18;45.51,63.42|CN|Z|Mulgore|N|Head out to Stonebull Lake around Bloodhoof Villiage and click on the Crayfish.|T|Stonebull Crayfish|FACTION|Horde|
 T Craving Crayfish|QID|29349|M|56,47|Z|Thunder Bluff|N|To Kah Mistrunner|FACTION|Horde|
@@ -180,7 +185,7 @@ C Savage Coast Blue Sailfin|QID|6607|QO|4|M|32.80,39.40;31.15,38.15;35.85,55.70;
 C Misty Reed Mahi Mahi|QID|6607|QO|2|M|72.60,5.00;78.10,92.50;78.70,12.90;84.60,88.00;86.40,22.10;87.40,77.00;88.90,80.20;89.50,45.70;90.80,37.70;91.80,40.10;84.40,85.60;84.85,83.25;81.45,15.40;88.05,72.60;84.30,18.60;85.73,78.88;89.45,29.30;73.35,8.65;78.84,9.74|CN|Z|Swamp of Sorrows|N|Head on over to the coast of the Swamp of Sorrows.|
 T Nat Pagle, Angler Extreme|QID|6607|M|58.6,60.2|Z|Dustwallow Marsh|N|To Nat Pagle|
 
-; Shattrath Cityhttps://www.wowhead.com/npc=25580/old-man-barlo#starts, Level 70
+; ShattrathCityhttps://www.wowhead.com/npc=25580/old-man-barlo#starts, Level 70
 
 A Bait Bandits|QID|11666|M|38.6,12.8|Z|Terokkar Forest|N|From Old Man Barlo|O|
 C Blackfin Darter|QID|11666|M|55.60,51.30;59.80,34.90;59.80,54.20;51.80,35.50;51.65,33.40;55.35,29.40;52.33,27.57;59.90,38.25;52.51,37.68;58.95,29.32;52.61,30.94;55.45,44.55;51.67,40.65|Z|Terokkar Forest|CN|N|Fish rivers in the Terokkar Forest for your Blackfin Darter|
@@ -204,7 +209,7 @@ A The One That Got Away|QID|11667|M|38.6,12.8|Z|Terokkar Forest|N|From Old Man B
 C World's Largest Mudfish|QID|11667|M|37.40,43.80;60.50,31.60;61.00,26.80;62.90,32.10;34.45,44.40;33.25,54.80;57.50,24.08;34.25,46.92;35.90,50.80;31.07,55.08;58.09,33.06;59.30,25.25;37.45,46.33;54.53,30.07;61.06,34.49|CN|Z|Nagrand|N|Head out to Nagrand and fish the lakes for the Mudfish.|
 T The One That Got Away|QID|11667|M|38.6,12.8|Z|Terokkar Forest|N|To Old Man Barlo|
 
-; Dalaranhttps://www.wowhead.com/npc=28742, Level 70
+;Dalaranhttps://www.wowhead.com/npc=28742, Level 70
 A Jewel Of The Sewers|QID|13832|M|52.6,64.8|Z|Dalaran City@Dalaran|N|From Marcia Chase|O|
 C Jewel Of The Sewers|QID|13832|M|40.60,57.10;43.80,67.80;61.10,12.40;37.40,51.70;36.95,47.60;32.85,47.30;44.44,65.18|Z|The Underbelly@DalaranCity|CN|N|Go to the sewers and fish away.|L|45903|FACTION|Horde|
 T Jewel Of The Sewers|QID|13832|M|52.6,64.8|Z|Dalaran City@Dalaran|N|To Marcia Chase|
