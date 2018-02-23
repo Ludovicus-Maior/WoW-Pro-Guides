b2202b7c581d44bc01aeb1d1f14ee2595f2bf397
Ludovicus <ludovicus.maior@gmail.com>
2018-02-22 19:17:12 -0500

    Updated WoWPro_Leveling/Neutral/89_90_Emmaleah_Townlong_Steppes.lua from WoW-Pro.COM

--- WoWPro_Leveling/Neutral/89_90_Emmaleah_Townlong_Steppes.lua
+++ WoWPro_Leveling/Neutral/89_90_Emmaleah_Townlong_Steppes.lua
@@ -4,6 +4,11 @@
 -- Permissions beyond the scope of this license may be available at http://www.wow-pro.com/License.
 
 -- URL: http://wow-pro.com/wiki/source_code_townlong_steppes_neutral
+-- Date: 2018-02-23 01:37
+-- Who: Ludovicus_EditBot
+-- Log: Upload Grail corrections to PRE/LEAD/QO tags
+
+-- URL: http://wow-pro.com/node/3492/revisions/28938/view
 -- Date: 2018-01-17 00:34
 -- Who: Ludovicus
 -- Log: New Levels
@@ -145,13 +150,13 @@ A Ranger Rescue|QID|30774|PRE|30769;30770;30771|M|67.25,52.22|N|From Suna Silent
 A Pitching In|QID|30773|PRE|30769;30770;30771|M|67.21,52.27|N|From Ban Bearheart.|
 C Seeing Red|QID|30772|S|M|67.61,47.50|
 C Pitching In|QID|30773|S|NC|U|81193|M|66.61,45.86|N|Use the item, aim with your mouse from range.|
-C Rescue Longying Rangers|QID|30774|S|QO|Longying Ranger rescued: 4/4|M|68.06,49.62|N|Kill Osul Marauders to get the stone keys needed to unlock the cages.|
+C Rescue Longying Rangers|QID|30774|S|QO|1|M|68.06,49.62|N|Kill Osul Marauders to get the stone keys needed to unlock the cages.|; Longying Ranger rescued: 4/4
 A The Exile|QID|30775|PRE|30769;30770;30771|M|67.08,51.43|N|From Katak the Defeated.|
-K Kill Battat|QID|30775|T|Battat|QO|Battat slain: 1/1|M|65.7,51.7|
-K Kill Urang|QID|30775|T|Urang|QO|Urang slain: 1/1|M|64.8,50.2|
-C Ranger Rescue |QID|30774|NC|QO|Free Lin Silentstrike: 1/1|M|66.6,49.2|N|No key needed for Lin...|
-K Kill Ku-Tong|QID|30775|T|Ku-Tong|QO|Ku-Tong slain: 1/1|M|66.9,48.0|
-C Rescue Longying Rangers|QID|30774|US|QO|Longying Ranger rescued: 4/4|M|68.06,49.62|N|Kill Osul Marauders to get the stone keys needed to unlock the cages.|
+K Kill Battat|QID|30775|T|Battat|QO|1|M|65.7,51.7|; Battat slain: 1/1
+K Kill Urang|QID|30775|T|Urang|QO|2|M|64.8,50.2|; Urang slain: 1/1
+C Ranger Rescue |QID|30774|NC|QO|2|M|66.6,49.2|N|No key needed for Lin...|; Free Lin Silentstrike: 1/1
+K Kill Ku-Tong|QID|30775|T|Ku-Tong|QO|3|M|66.9,48.0|; Ku-Tong slain: 1/1
+C Rescue Longying Rangers|QID|30774|US|QO|1|M|68.06,49.62|N|Kill Osul Marauders to get the stone keys needed to unlock the cages.|; Longying Ranger rescued: 4/4
 C Pitching In|QID|30773|US|NC|U|81193|M|66.61,45.86|N|Use the item, aim with your mouse from range.|
 C Seeing Red|QID|30772|US|M|67.61,47.50|
 T The Exile|QID|30775|M|67.01,51.39|N|To Katak the Defeated.|
@@ -159,7 +164,7 @@ T Seeing Red|QID|30772|M|67.21,52.27|N|To Ban Bearheart.|
 T Ranger Rescue|QID|30774|M|67.21,52.27|N|To Ban Bearheart.|
 T Pitching In|QID|30773|M|67.21,52.27|N|To Ban Bearheart.|
 A Jung Duk|QID|30776|PRE|30772;30773;30774|M|67.25,52.22|N|From Suna Silentstrike.|
-K Jung Duk|QID|30776|QO|Jung Duk slain: 1/1|M|66.61,50.98|N|Plant the banner and fight him.|
+K Jung Duk|QID|30776|QO|1|M|66.61,50.98|N|Plant the banner and fight him.|; Jung Duk slain: 1/1
 T Jung Duk|QID|30776|M|70.98,56.56|N|To Ban Bearheart.|
 A In Search of Suna|QID|30777|PRE|30776|M|70.98,56.56|N|From Ban Bearheart. Hop on the caravan for a ride to Hatred's Vice.|
 
@@ -172,10 +177,10 @@ A Totemic Research|QID|30780|PRE|30777|M|82.59,73.02|N|From Yalia Sagewhisper.|
 C Slaying the Scavengers|QID|30779|S|M|84.07,72.45|
 C Totemic Research|QID|30780|S|NC|M|82.52,71.68|N|Pick up the totems scattered around the camp.|
 C Dust to Dust|QID|30778|S|NC|U|81356|M|82.63,70.03|N|Use the Shado-Pan Torch to burn the corpses.|
-C Examine the Western Smoke Trail|QID|30781|NC|QO|Western Smoke Trail examined: 1/1|M|80.95,72.09|
-C Examine the Northwestern Smoke Trail|QID|30781|NC|QO|Northwestern Smoke Trail examined: 1/1|M|83.89,70.72|
-C Examine the Eastern Smoke Trail|QID|30781|NC|QO|Eastern Smoke Trail examined: 1/1|M|84.79,71.52|
-C Examine the Southern Smoke Trail|QID|30781|NC|QO|Southern Smoke Trail examined: 1/1|M|83.64,78.5|
+C Examine the Western Smoke Trail|QID|30781|NC|QO|2|M|80.95,72.09|; Western Smoke Trail examined: 1/1
+C Examine the Northwestern Smoke Trail|QID|30781|NC|QO|4|M|83.89,70.72|; Northwestern Smoke Trail examined: 1/1
+C Examine the Eastern Smoke Trail|QID|30781|NC|QO|1|M|84.79,71.52|; Eastern Smoke Trail examined: 1/1
+C Examine the Southern Smoke Trail|QID|30781|NC|QO|3|M|83.64,78.5|; Southern Smoke Trail examined: 1/1
 C Dust to Dust|QID|30778|US|NC|U|81356|M|82.63,70.03|N|Use the Shado-Pan Torch to burn the corpses.|
 C Totemic Research|QID|30780|US|NC|M|82.52,71.68|N|Pick up the totems scattered around the camp.|
 C Slaying the Scavengers|QID|30779|US|M|84.07,72.45|
@@ -196,14 +201,14 @@ T Spiteful Spirits|QID|30782|M|82.68,73.02|N|To Ban Bearheart.|
 A The Point of No Return|QID|30784|PRE|30782;30783|M|82.68,73.02|N|From Ban Bearheart.|
 C The Point of No Return|QID|30784|U|81430|M|84.25,70.72|N|Use the totem on Suna.|
 T The Point of No Return|QID|30784|M|82.68,73.02|N|To Ban Bearheart.|
-A Gao-Ran Battlefront|QID|30785|PRE|30784|M|82.68,73.02|N|From Ban Bearheart.|
-A A Delicate Balance|QID|31894|PRE|30784|M|82.59,73.02|N|From Yalia Sagewhisper.|
+A Gao-Ran Battlefront|QID|30785|LEAD|30884|PRE|30784|M|82.68,73.02|N|From Ban Bearheart.|
+A A Delicate Balance|QID|31894|LEAD|30786|PRE|30784|M|82.59,73.02|N|From Yalia Sagewhisper.|
 
 ; Cannot easily get to Goa-Ran Battle without flying mount from Hatred's Vice so I'm moving the Goa-Ran quests to later.
 R Lower Sumprushes|QID|30786|M|68.02,67.58|
 T A Delicate Balance|QID|31894|M|68.02,67.58|N|To Orbiss.|
 A A Spear Through My Side, A Chain Through My Soul|QID|30786|M|68.02,67.58|N|From Orbiss.|
-K Kill Dmong Naruuk|QID|30786|T|Dmong Naruuk|QO|Dmong Naruuk slain: 1/1|M|68.51,71.48|
+K Kill Dmong Naruuk|QID|30786|T|Dmong Naruuk|QO|1|M|68.51,71.48|; Dmong Naruuk slain: 1/1
 T A Spear Through My Side, A Chain Through My Soul|QID|30786|M|67.86,67.64|N|To Orbiss.|
 A The Torches|QID|30787|PRE|30786|M|67.86,67.64|N|From Orbiss.|
 C The Torches|QID|30787|M|68.26,69.16|N|Kill Mist-Shamans, when they die, you can loot their torch. Make sure the shaman didn't just kill their victim, the torch disappears a few seconds after the victim dies (and you can't loot it).|
@@ -229,7 +234,7 @@ T Golgoss Hungers|QID|30790|M|67.90,67.76|N|To Orbiss.|
 T Arconiss Thirsts|QID|30791|M|67.90,67.76|N|To Orbiss.|
 T Orbiss Fades|QID|30792|M|67.90,67.76|N|To Orbiss.|
 A Mists' Opportunity|QID|30793|PRE|30790;30791;30792|M|67.90,67.76|N|From Orbiss.|
-K Kill Jahesh of Osul|QID|30793|T|Jahesh of Osul|QO|Jahesh of Osul slain: 1/1|M|76.47,72.71|
+K Kill Jahesh of Osul|QID|30793|T|Jahesh of Osul|QO|1|M|76.47,72.71|; Jahesh of Osul slain: 1/1
 T Mists' Opportunity|QID|30793|M|76.19,72.93|N|To Orbiss.|
 
 R Gao-Ran Battlefront|QID|30785|M|74.44,81.47|
@@ -250,10 +255,10 @@ A The Restless Watch|QID|30890|PRE|30887|M|79.47,84.46|N|From Taoshi.|
 A Trap Setting|QID|30889|PRE|30887|M|79.38,84.45|N|From Mao the Lookout.|
 C Trap Setting|QID|30889|S|NC|M|81.81,84.36|N|Click on the traps to reset them.|
 C Breach in the Defenses|QID|30888|S|U|81925|M|80.71,86.18|N|The mobs are stealthed at the front of the orange dustclouds moving along the ground, use the flare to make them visable.|
-C Deliver Orders to Scout Wei-chin|QID|30890|NC|QO|Deliver Orders to Scout Wei-chin: 1/1|M|82.24,84.07|
-C Deliver Orders to Scout Long|QID|30890|NC|QO|Deliver Orders to Scout Long: 1/1|M|81.34,86.18|
-C Deliver Orders to Scout Jai-gan|QID|30890|NC|QO|Deliver Orders to Scout Jai-gan: 1/1|M|78.97,90.24|
-C Deliver Orders to Scout Ying|QID|30890|NC|QO|Deliver Orders to Scout Ying: 1/1|M|82.53,89.5|
+C Deliver Orders to Scout Wei-chin|QID|30890|NC|QO|1|M|82.24,84.07|; Deliver Orders to Scout Wei-chin: 1/1
+C Deliver Orders to Scout Long|QID|30890|NC|QO|2|M|81.34,86.18|; Deliver Orders to Scout Long: 1/1
+C Deliver Orders to Scout Jai-gan|QID|30890|NC|QO|4|M|78.97,90.24|; Deliver Orders to Scout Jai-gan: 1/1
+C Deliver Orders to Scout Ying|QID|30890|NC|QO|3|M|82.53,89.5|; Deliver Orders to Scout Ying: 1/1
 C Breach in the Defenses|QID|30888|US|U|81925|M|80.71,86.18|N|The mobs are stealthed at the front of the orange dustclouds moving along the ground, use the flare to make them visable.|
 C Trap Setting|QID|30889|US|NC|M|81.81,84.36|N|Click on the traps to reset them.|
 T Breach in the Defenses|QID|30888|M|79.47,84.46|N|To Taoshi.|
@@ -291,7 +296,7 @@ A Cutting the Swarm|QID|30898|PRE|30895|M|76.38,82.40|N|From Taran Zhu.|
 C Cutting the Swarm|QID|30898|M|73.95,84.06|N|Use the fireworks/rocket launcher (vehicle UI) to rain destruction on your enemies.|
 T Cutting the Swarm|QID|30898|M|74.27,84.96|N|To Taoshi.|
 A Terror of the Dread Wastes|QID|30900|PRE|30898|M|74.27,84.96|N|From Taoshi.|
-K Norvakess|QID|30900|T|Norvakess|QO|Norvakess slain: 1/1|M|74.63,87.34|
+K Norvakess|QID|30900|T|Norvakess|QO|1|M|74.63,87.34|; Norvakess slain: 1/1
 T Terror of the Dread Wastes|QID|30900|M|76.45,82.41|N|To Taran Zhu.|
 A Along the Southern Front|QID|30901|PRE|30900|M|76.45,82.41|N|From Taran Zhu.|
 
@@ -330,7 +335,7 @@ A Hostile Skies|QID|30978|PRE|30899|M|51.58,87.21|N|From Hawkmaster Nurong.|
 C Hostile Skies|QID|30978|M|51.67,87.32|N|Use Nurong's rocket launcher(vehicle UI) to shoot the flying mantids.|
 T Hostile Skies|QID|30978|M|51.67,87.32|N|To Hawkmaster Nurong.|
 A Devastation Below|QID|30979|PRE|30978|M|51.67,87.32|N|From Hawkmaster Nurong.|
-K Kill Voress'thalik|QID|30979|T|Voress'thalik|QO|Voress'thalik slain: 1/1|M|54.13,86.94|N|Jump down to get to Voress'thalik. Don't worry, you have a slow fall debuff.|
+K Kill Voress'thalik|QID|30979|T|Voress'thalik|QO|1|M|54.13,86.94|N|Jump down to get to Voress'thalik. Don't worry, you have a slow fall debuff.|; Voress'thalik slain: 1/1
 T Devastation Below|QID|30979|M|53.94,86.92|N|To Hawkmaster Nurong.|
 A Heroes of the Shado-Pan|QID|30980|PRE|30979|M|53.94,86.92|N|From Hawkmaster Nurong.|
 C Choking the Skies|QID|31032|US|NC|U|83768|M|51.25,83.72|N|After you kill the bugs, burn them with the torch.|
@@ -349,9 +354,9 @@ A The Search for Restless Leng|QID|31688|PRE|31065|M|53.93,78.08|N|From Rensai O
 T Choking the Skies|QID|31032|M|51.31,83.00|N|To Marksman Ye.|
 C The Search for Restless Leng|QID|31688|S|NC|M|46.36,76.34|N|Don't forget to break open the cages that are near the Sik'thik,|
 C Thinning the Sik'thik|QID|31687|S|M|41.38,74.31|N|Don't forget to break open the cages that are near the Sik'thik,|
-K Kill Korvexxis|QID|30981|T|Korvexxis|QO|Korvexxis slain: 1/1|M|46.7,83.0|
-K Kill Serevex|QID|31063|T|Serevex|QO|Serevex slain: 1/1|M|45.2,77.5|
-K Kill Rothek|QID|31064|T|Rothek|QO|Rothek slain: 1/1|M|39.7,76.0|
+K Kill Korvexxis|QID|30981|T|Korvexxis|QO|1|M|46.7,83.0|; Korvexxis slain: 1/1
+K Kill Serevex|QID|31063|T|Serevex|QO|1|M|45.2,77.5|; Serevex slain: 1/1
+K Kill Rothek|QID|31064|T|Rothek|QO|1|M|39.7,76.0|; Rothek slain: 1/1
 C Thinning the Sik'thik|QID|31687|US|M|41.38,74.31|N|Don't forget to break open the cages that are near the Sik'thik,|
 C The Search for Restless Leng|QID|31688|US|NC|M|46.36,76.34|N|Don't forget to break open the cages that are near the Sik'thik,|
 T Taoshi and Korvexxis|QID|30981|M|47.55,78.86|N|To Taran Zhu.|
@@ -376,8 +381,8 @@ C The Field Armorer|QID|30963|S|M|59.21,57.52|N|Kill turtles, lots of turtles.|
 C Natural Antiseptic|QID|30922|S|M|59.20,54.06|N|Kill Mushan till you have 5 full bladders (too bad thier isn't more water around.)|
 C The Motives of the Mantid|QID|30921|S|M|57.37,51.66|N|Kill mantids untill you get the four clues.|
 l Hardened Sap of Kiri'vess|QID|31424|S|M|57.4,56.6|N|Another item to loot for gold and XP.  Other coordinates have also been reported.  All of them map to in the grass among the roots of the giant tree.|
-C Explode the Kri'thik Supplies|QID|30923|NC|U|81891|QO|Kri'thik Supplies Exploded: 1/1|M|56.7,53.6|
-C Explode the Kri'thik Weapons|QID|30923|NC|U|81891|QO|Kri'thik Weapons Exploded: 1/1|M|55.2,53.0|
+C Explode the Kri'thik Supplies|QID|30923|NC|U|81891|QO|2|M|56.7,53.6|; Kri'thik Supplies Exploded: 1/1
+C Explode the Kri'thik Weapons|QID|30923|NC|U|81891|QO|1|M|55.2,53.0|; Kri'thik Weapons Exploded: 1/1
 C The Motives of the Mantid|QID|30921|US|M|57.37,51.66|N|Kill mantids untill you get the four clues.|
 C Natural Antiseptic|QID|30922|US|M|59.20,54.06|N|Kill Mushan till you have 5 full bladders (too bad thier isn't more water around.)|
 C The Field Armorer|QID|30963|US|M|59.21,57.52|N|Kill turtles, lots of turtles.|
@@ -408,10 +413,10 @@ A The Terrible Truth|QID|30926|PRE|30925|M|39.37,62.18|N|From Ogo the Elder.|
 A A Trail of Fear|QID|30928|PRE|30925|M|39.19,62.05|N|From Yak-Keeper Kyana.|
 A Give Them Peace|QID|30927|PRE|30925|M|39.38,62.00|N|From Sentinel Commander Qipan.|
 A Father's Footsteps|QID|30932|PRE|30931|M|39.43,62.03|N|From Sentinel Commander Qipan.|
-C Father's Footsteps|QID|30932|NC|QO|Find Sentinel Yalo: 1/1|M|39.13, 61.39|
-C Father's Footsteps|QID|30932|NC|QO|Find Father's Shield: 1/1|M|37.54, 61.45|
-C Father's Footsteps|QID|30932|NC|QO|Find Father's Bedroll: 1/1|M|37.41,60.90|
-C Father's Footsteps|QID|30932|NC|QO|Find Ha-Cha: 1/1|M|37.62, 64.01|
+C Father's Footsteps|QID|30932|NC|QO|4|M|39.13, 61.39|; Find Sentinel Yalo: 1/1
+C Father's Footsteps|QID|30932|NC|QO|2|M|37.54, 61.45|; Find Father's Shield: 1/1
+C Father's Footsteps|QID|30932|NC|QO|1|M|37.41,60.90|; Find Father's Bedroll: 1/1
+C Father's Footsteps|QID|30932|NC|QO|3|M|37.62, 64.01|; Find Ha-Cha: 1/1
 R Niuzao Catacombs|QID|30927|M|32.71,61.17|N|The trail to the catacombs is behind the temple. There are flags on each side of the trail marking the start.|
 C A Trail of Fear|QID|30928|S|M|28.88,73.48|N|Fighting Dreadlings will cause various "special" effects, enjoy!|
 C Give Them Peace|QID|30927|S|NC|M|32.43, 61.12|N|The hand turns gold at a greater range that it actually works, you have to practically stand on the sentinals to give them peace.|
