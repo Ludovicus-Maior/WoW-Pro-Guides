aa859538dd853840581f7d3c954925a7e74c8fe9
Ludovicus <ludovicus.maior@gmail.com>
2018-02-22 19:15:54 -0500

    Updated WoWPro_Leveling/Horde/65_67_Jame_Terokkar_Forest.lua from WoW-Pro.COM

--- WoWPro_Leveling/Horde/65_67_Jame_Terokkar_Forest.lua
+++ WoWPro_Leveling/Horde/65_67_Jame_Terokkar_Forest.lua
@@ -4,6 +4,11 @@
 -- Permissions beyond the scope of this license may be available at http://www.wow-pro.com/License.
 
 -- URL: http://wow-pro.com/wiki/source_code_terokkar_forest_horde
+-- Date: 2018-02-22 22:57
+-- Who: Ludovicus_EditBot
+-- Log: Upload Grail corrections to PRE/LEAD/QO tags
+
+-- URL: http://wow-pro.com/node/3278/revisions/28908/view
 -- Date: 2018-01-16 23:56
 -- Who: Ludovicus
 -- Log: New Levels
@@ -131,7 +136,7 @@ l Vicious Teromoth Sample|QID|9968|US|N|Kill Vicious Teromoths to get 4 Vicious 
 T Clues in the Thicket|QID|9971|M|44.3,26.3|N|To Earthbinder Tavgren.|
 C Strange Energy|N|Kill teromoths until you get the items for this quest.|QID|9968|M|44.00,28.00|QO|1|
 T Strange Energy|QID|9968|M|44.3,26.3|N|To Earthbinder Tavgren.|
-A By Any Means Necessary|QID|9978|PRE|9968;9971|M|44.3,26.3|N|From Earthbinder Tavgren.|
+A By Any Means Necessary|QID|9978|PRE|9968+9971|M|44.3,26.3|N|From Earthbinder Tavgren.|
 T By Any Means Necessary|QID|9978|M|47.1,27.0|N|Find Empoor on the road between Shattrath City and Tuurem. Talk to him and then fight him to low health. Turn this quest in right after this.|
 A Wind Trader Lathrai|QID|9979|PRE|9978|M|47.1,27.0|N|From Empoor.|
 
