8aa255be4c07d46e4386036166f800a416d56b67
Ludovicus <ludovicus.maior@gmail.com>
2018-02-23 16:56:40 -0500

    Add HighTauren and Nightborn starters.

---
+++ WoWPro_Leveling/Horde/20_20_Ludovicus_HighTauren.lua
@@ -0,0 +1,24 @@
+local guide = WoWPro:RegisterGuide("LudoHighTauren", "Leveling", "ThunderTotem", "Ludovicus", "Horde")
+WoWPro:GuideLevels(guide,20,20)
+WoWPro:GuideRaceSpecific(guide,"HighmountainTauren")
+WoWPro:GuideNextGuide(guide, "JiySto2530")
+WoWPro:GuideIcon(guide,"ACH",12245)
+WoWPro:GuideSteps(guide, function()
+return
+[[
+A For the Horde|QID|49773|M|54.78,63.28|Z|ThunderTotem/6|N|From Mayla Highmountain.|
+P Orgrimmar|ACTIVE|49773|M|45.95, 63.79|Z|ThunderTotem/6|N|Click on the portal to go to Orgrimmar|
+T For the Horde|QID|49773|M|39.39,79.58|Z|Orgrimmar@Orgrimmar|N|To Ambassador Blackguard|
+A Stranger in a Strange Land|QID|50319|PRE|49773|M|39.39,79.58|Z|Orgrimmar@Orgrimmar|N|From Ambassador Blackguard|
+C Hero's Call board|QID|50319|M|49.66, 76.47|Z|Orgrimmar@Orgrimmar|N|Click on the Hero's Call board and select your next zone.\n By default, this guide will go to Stonetalon, if you don't choose anything.|
+T Stranger in a Strange Land|QID|50319|M|49.79,76.44|Z|Orgrimmar@Orgrimmar|N|To Halian Shlavahawk|
+
+D On to Silverpine Forest|QID|26964|N|The next guide will now load.|GUIDE|MalSil1220|
+D On to Hillsbrad Foothills|QID|28571|N|The next guide will now load.|GUIDE|AyaHil2025|
+D On to Ashenvale|QID|284493|N|The next guide will now load.|GUIDE|BitAsh2025|
+D On to Azshara|QID|28496|N|The next guide will now load.|GUIDE|BitAzs1220|
+D On to Northen Barrens|QID|28494|N|The next guide will now load.|GUIDE|BitNor1220|
+D On to Stonetalon Mountains|QID|28532|N|The next guide will now load.|GUIDE|JiySto2530|
+
+]]
+end)
---
+++ WoWPro_Leveling/Horde/20_20_Ludovicus_Nightborne.lua
@@ -0,0 +1,25 @@
+local guide = WoWPro:RegisterGuide("LudoNightborne", "Leveling", "Suramar", "Ludovicus", "Horde")
+WoWPro:GuideLevels(guide,20,20)
+WoWPro:GuideRaceSpecific(guide,"Nightborne")
+WoWPro:GuideNextGuide(guide, "JiySto2530")
+WoWPro:GuideIcon(guide,"ACH",12446)
+WoWPro:GuideSteps(guide, function()
+return
+[[
+A For the Horde|QID|49933|M|59.34, 85.53|Z|Suramar|N|From First Arcanist Thalyssra.|
+P Orgrimmar|ACTIVE|49933|M|58.18, 87.33|Z|Suramar|N|Click on the portal to go to Orgrimmar|
+T For the Horde|QID|49933|M|39.39,79.58|Z|Orgrimmar@Orgrimmar|N|To Ambassador Blackguard|
+A Stranger in a Strange Land|QID|50303|PRE|49933|M|39.39,79.58|Z|Orgrimmar@Orgrimmar|N|From Ambassador Blackguard|
+C Hero's Call board|QID|50303|M|49.66, 76.47|Z|Orgrimmar@Orgrimmar|N|Click on the Hero's Call board and select your next zone.\n By default, this guide will go to Stonetalon, if you don't choose anything.|
+T Stranger in a Strange Land|QID|50303|M|49.79,76.44|Z|Orgrimmar@Orgrimmar|N|To Melitier Vahlouran|
+
+D On to Silverpine Forest|QID|26964|N|The next guide will now load.|GUIDE|MalSil1220|
+D On to Hillsbrad Foothills|QID|28571|N|The next guide will now load.|GUIDE|AyaHil2025|
+D On to Ashenvale|QID|284493|N|The next guide will now load.|GUIDE|BitAsh2025|
+D On to Azshara|QID|28496|N|The next guide will now load.|GUIDE|BitAzs1220|
+D On to Northen Barrens|QID|28494|N|The next guide will now load.|GUIDE|BitNor1220|
+D On to Stonetalon Mountains|QID|28532|N|The next guide will now load.|GUIDE|JiySto2530|
+
+
+]]
+end)
