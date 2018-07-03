783fa38b775a7e9986b31ebcb8dacc3575cf3ee8
Ludovicus <ludovicus.maior@gmail.com>
2018-07-03 15:03:34 -0400

    Updated WoWPro_Leveling/Horde/110_110_Emmaleah_WarCampaign.lua from WoW-Pro.COM

--- WoWPro_Leveling/Horde/110_110_Emmaleah_WarCampaign.lua
+++ WoWPro_Leveling/Horde/110_110_Emmaleah_WarCampaign.lua
@@ -1,3 +1,33 @@
+
+-- WoWPro Guides by "The WoW-Pro Community" are licensed under a Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License.
+-- Based on a work at github.com.
+-- Permissions beyond the scope of this license may be available at http://www.wow-pro.com/License.
+
+-- URL: http://wow-pro.com/wiki/horde_war_campaign
+-- Date: 2018-07-03 02:49
+-- Who: Emmaleah
+-- Log: Oops, missed editting title
+
+-- URL: http://wow-pro.com/node/3761/revisions/29385/view
+-- Date: 2018-07-03 02:48
+-- Who: Emmaleah
+-- Log: changed guide registration to be war campaign
+
+-- URL: http://wow-pro.com/node/3761/revisions/29302/view
+-- Date: 2018-05-13 05:51
+-- Who: Emmaleah
+-- Log: Many note updates, add more guide registration icon, and autoswitch, fixed up J steps to reflect correct guide. Note that coords in Orgrimmar need updated as they changed with the last patch.
+
+-- URL: http://wow-pro.com/node/3761/revisions/29300/view
+-- Date: 2018-05-12 17:55
+-- Who: Ludovicus_Maior
+-- Log: Use a real nil in GuideNextGuide()
+
+-- URL: http://wow-pro.com/node/3761/revisions/29274/view
+-- Date: 2018-05-08 21:54
+-- Who: Emmaleah
+-- Log: First Draft.
+
 local guide = WoWPro:RegisterGuide('EmmHWarCampaign', 'Leveling', 'Zandalar', 'Emmaleah', 'Horde')
 WoWPro:GuideLevels(guide,110, 110)
 WoWPro:GuideNextGuide(guide, nil)
