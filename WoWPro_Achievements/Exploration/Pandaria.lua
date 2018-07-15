fa5a0a22774ec8c1f49764a394270f651840d760
Ludovicus <ludovicus.maior@gmail.com>
2018-07-15 10:26:17 -0400

    Updated WoWPro_Achievements/Exploration/Pandaria.lua from WoW-Pro.COM

--- WoWPro_Achievements/Exploration/Pandaria.lua
+++ WoWPro_Achievements/Exploration/Pandaria.lua
@@ -4,6 +4,11 @@
 -- Permissions beyond the scope of this license may be available at http://www.wow-pro.com/License.
 
 -- URL: http://wow-pro.com/wiki/exploration_achievements_pandaria
+-- Date: 2018-07-10 18:19
+-- Who: Ludovicus_Maior
+-- Log: Lots of missing coords.
+
+-- URL: http://wow-pro.com/node/3645/revisions/27414/view
 -- Date: 2016-01-08 23:28
 -- Who: Ludovicus_Maior
 -- Log: 3645
@@ -38,93 +43,93 @@ F Garrosh'ar Point|QID|963510015|M|46,90|Z|Jade Forest|ACH|6351;15|
 F Windward Isle|QID|963510016|M|65,30|Z|Jade Forest|ACH|6351;16|
 F Honeydew Village|QID|963510017|M|29,13|Z|Jade Forest|ACH|6351;17|
 
-F Anglers Wharf|QID|969750001|M|0.00,0.00|Z|Krasarang Wilds|ACH|6975;1|
-F Cradle of Chi-Ji|QID|969750002|M|0.00,0.00|Z|Krasarang Wilds|ACH|6975;2|
-F Dojani River|QID|969750003|M|0.00,0.00|Z|Krasarang Wilds|ACH|6975;3|
-F Fallsong Village|QID|969750004|M|0.00,0.00|Z|Krasarang Wilds|ACH|6975;4|
-F Krasarang Cove|QID|969750005|M|0.00,0.00|Z|Krasarang Wilds|ACH|6975;5|
-F The Krasari Ruins|QID|969750006|M|0.00,0.00|Z|Krasarang Wilds|ACH|6975;6|
-F Nayeli Lagoon|QID|969750007|M|0.00,0.00|Z|Krasarang Wilds|ACH|6975;7|
-F Crane Wing Refuge|QID|969750008|M|0.00,0.00|Z|Krasarang Wilds|ACH|6975;8|
-F Ruins of Dojan|QID|969750009|M|0.00,0.00|Z|Krasarang Wilds|ACH|6975;9|
-F Ruins of Korja|QID|969750010|M|0.00,0.00|Z|Krasarang Wilds|ACH|6975;10|
-F Temple of the Red Crane|QID|969750011|M|0.00,0.00|Z|Krasarang Wilds|ACH|6975;11|
-F The Deepwild|QID|969750012|M|0.00,0.00|Z|Krasarang Wilds|ACH|6975;12|
-F The Forbidden Jungle|QID|969750013|M|0.00,0.00|Z|Krasarang Wilds|ACH|6975;13|
-F The Southern Isles|QID|969750014|M|0.00,0.00|Z|Krasarang Wilds|ACH|6975;14|
-F Unga Ingoo|QID|969750015|M|0.00,0.00|Z|Krasarang Wilds|ACH|6975;15|
-F Zhu's Watch|QID|969750016|M|0.00,0.00|Z|Krasarang Wilds|ACH|6975;16|
+F Anglers Wharf|QID|969750001|M|67,44|Z|Krasarang Wilds|ACH|6975;1|
+F Cradle of Chi-Ji|QID|969750002|M|33,70|Z|Krasarang Wilds|ACH|6975;2|
+F Dojani River|QID|969750003|M|64,31|Z|Krasarang Wilds|ACH|6975;3|
+F Fallsong Village|QID|969750004|M|29,38|Z|Krasarang Wilds|ACH|6975;4|
+F Krasarang Cove|QID|969750005|M|82,26|Z|Krasarang Wilds|ACH|6975;5|
+F The Krasari Ruins|QID|969750006|M|70,22|Z|Krasarang Wilds|ACH|6975;6|
+F Nayeli Lagoon|QID|969750007|M|41,75|Z|Krasarang Wilds|ACH|6975;7|
+F Crane Wing Refuge|QID|969750008|M|40,33|Z|Krasarang Wilds|ACH|6975;8|
+F Ruins of Dojan|QID|969750009|M|55,34|Z|Krasarang Wilds|ACH|6975;9|
+F Ruins of Korja|QID|969750010|M|24,45|Z|Krasarang Wilds|ACH|6975;10|
+F Temple of the Red Crane|QID|969750011|M|40,55|Z|Krasarang Wilds|ACH|6975;11|
+F The Deepwild|QID|969750012|M|46,38|Z|Krasarang Wilds|ACH|6975;12|
+F The Forbidden Jungle|QID|969750013|M|21,35|Z|Krasarang Wilds|ACH|6975;13|
+F The Southern Isles|QID|969750014|M|16,56|Z|Krasarang Wilds|ACH|6975;14|
+F Unga Ingoo|QID|969750015|M|47,90|Z|Krasarang Wilds|ACH|6975;15|
+F Zhu's Watch|QID|969750016|M|77,10|Z|Krasarang Wilds|ACH|6975;16|
 
-F Gao-Ran Battlefront|QID|969770001|M|0.00,0.00|Z|Townlong Steppes|ACH|6977;1|
-F Kri'vess|QID|969770002|M|0.00,0.00|Z|Townlong Steppes|ACH|6977;2|
-F Rensai's Watchpost|QID|969770003|M|0.00,0.00|Z|Townlong Steppes|ACH|6977;3|
-F Niuzao Temple|QID|969770004|M|0.00,0.00|Z|Townlong Steppes|ACH|6977;4|
-F Fire Camp Osul|QID|969770005|M|0.00,0.00|Z|Townlong Steppes|ACH|6977;5|
-F Hatred's Vice|QID|969770006|M|0.00,0.00|Z|Townlong Steppes|ACH|6977;6|
-F Shado-Pan Garrison|QID|969770007|M|0.00,0.00|Z|Townlong Steppes|ACH|6977;7|
-F Shan'ze Dao|QID|969770008|M|0.00,0.00|Z|Townlong Steppes|ACH|6977;8|
-F Sik'vess|QID|969770009|M|0.00,0.00|Z|Townlong Steppes|ACH|6977;9|
-F Sra'vess|QID|969770010|M|0.00,0.00|Z|Townlong Steppes|ACH|6977;10|
-F The Sumprushes|QID|969770011|M|0.00,0.00|Z|Townlong Steppes|ACH|6977;11|
+F Gao-Ran Battlefront|QID|969770001|M|76,82|Z|Townlong Steppes|ACH|6977;1|
+F Kri'vess|QID|969770002|M|56,56|Z|Townlong Steppes|ACH|6977;2|
+F Rensai's Watchpost|QID|969770003|M|54,78|Z|Townlong Steppes|ACH|6977;3|
+F Niuzao Temple|QID|969770004|M|41,63|Z|Townlong Steppes|ACH|6977;4|
+F Fire Camp Osul|QID|969770005|M|67,47|Z|Townlong Steppes|ACH|6977;5|
+F Hatred's Vice|QID|969770006|M|84,72|Z|Townlong Steppes|ACH|6977;6|
+F Shado-Pan Garrison|QID|969770007|M|48,70|Z|Townlong Steppes|ACH|6977;7|
+F Shan'ze Dao|QID|969770008|M|29,27|Z|Townlong Steppes|ACH|6977;8|
+F Sik'vess|QID|969770009|M|45,86|Z|Townlong Steppes|ACH|6977;9|
+F Sra'vess|QID|969770010|M|22,52|Z|Townlong Steppes|ACH|6977;10|
+F The Sumprushes|QID|969770011|M|66,65|Z|Townlong Steppes|ACH|6977;11|
 
-F Ruins of Guo-Lai|QID|969790001|M|0.00,0.00|Z|Vale of Eternal Blossoms|ACH|6979;1|
-F Mistfall Village|QID|969790002|M|0.00,0.00|Z|Vale of Eternal Blossoms|ACH|6979;2|
-F Mogu'shan Palace|QID|969790003|M|0.00,0.00|Z|Vale of Eternal Blossoms|ACH|6979;3|
-F Setting Sun Garrison|QID|969790004|M|0.00,0.00|Z|Vale of Eternal Blossoms|ACH|6979;4|
-F The Golden Stair|QID|969790005|M|0.00,0.00|Z|Vale of Eternal Blossoms|ACH|6979;5|
-F Shrine of Seven Stars|QID|969790006|M|0.00,0.00|Z|Vale of Eternal Blossoms|ACH|6979;6|
-F The Golden Pagoda|QID|969790007|M|0.00,0.00|Z|Vale of Eternal Blossoms|ACH|6979;7|
-F Tu Shen Burial Ground|QID|969790008|M|0.00,0.00|Z|Vale of Eternal Blossoms|ACH|6979;8|
-F Shrine of Two Moons|QID|969790009|M|0.00,0.00|Z|Vale of Eternal Blossoms|ACH|6979;9|
-F Whitepetal Lake|QID|969790010|M|0.00,0.00|Z|Vale of Eternal Blossoms|ACH|6979;10|
-F The Five Sisters|QID|969790011|M|0.00,0.00|Z|Vale of Eternal Blossoms|ACH|6979;11|
+F Ruins of Guo-Lai|QID|969790001|M|29,37|Z|Vale of Eternal Blossoms|ACH|6979;1|
+F Mistfall Village|QID|969790002|M|38,73|Z|Vale of Eternal Blossoms|ACH|6979;2|
+F Mogu'shan Palace|QID|969790003|M|80,34|Z|Vale of Eternal Blossoms|ACH|6979;3|
+F Setting Sun Garrison|QID|969790004|M|21,71|Z|Vale of Eternal Blossoms|ACH|6979;4|
+F The Golden Stair|QID|969790005|M|46,16|Z|Vale of Eternal Blossoms|ACH|6979;5|
+F Shrine of Seven Stars|QID|969790006|M|86,65|Z|Vale of Eternal Blossoms|ACH|6979;6|
+F The Golden Pagoda|QID|969790007|M|56,43|Z|Vale of Eternal Blossoms|ACH|6979;7|
+F Tu Shen Burial Ground|QID|969790008|M|47,72|Z|Vale of Eternal Blossoms|ACH|6979;8|
+F Shrine of Two Moons|QID|969790009|M|62,20|Z|Vale of Eternal Blossoms|ACH|6979;9|
+F Whitepetal Lake|QID|969790010|M|41,46|Z|Vale of Eternal Blossoms|ACH|6979;10|
+F The Five Sisters|QID|969790011|M|17,45|Z|Vale of Eternal Blossoms|ACH|6979;11|
 
-F Winds' Edge|QID|969690001|M|0.00,0.00|Z|Valley of the Four Winds|ACH|6969;1|
-F Dustback Gorge|QID|969690002|M|0.00,0.00|Z|Valley of the Four Winds|ACH|6969;2|
-F Gilded Fan|QID|969690003|M|0.00,0.00|Z|Valley of the Four Winds|ACH|6969;3|
-F The Imperial Granary|QID|969690004|M|0.00,0.00|Z|Valley of the Four Winds|ACH|6969;4|
-F Halfhill|QID|969690005|M|0.00,0.00|Z|Valley of the Four Winds|ACH|6969;5|
-F Stoneplow|QID|969690006|M|0.00,0.00|Z|Valley of the Four Winds|ACH|6969;6|
-F Kunzen Village|QID|969690007|M|0.00,0.00|Z|Valley of the Four Winds|ACH|6969;7|
-F Mudmug's Place|QID|969690008|M|0.00,0.00|Z|Valley of the Four Winds|ACH|6969;8|
-F Nesingwary Safari|QID|969690009|M|0.00,0.00|Z|Valley of the Four Winds|ACH|6969;9|
-F Paoquan Hollow|QID|969690010|M|0.00,0.00|Z|Valley of the Four Winds|ACH|6969;10|
-F Pools of Purity|QID|969690011|M|0.00,0.00|Z|Valley of the Four Winds|ACH|6969;11|
-F Rumbling Terrace|QID|969690012|M|0.00,0.00|Z|Valley of the Four Winds|ACH|6969;12|
-F Silken Fields|QID|969690013|M|0.00,0.00|Z|Valley of the Four Winds|ACH|6969;13|
-F Singing Marshes|QID|969690014|M|0.00,0.00|Z|Valley of the Four Winds|ACH|6969;14|
-F Stormstout Brewery|QID|969690015|M|0.00,0.00|Z|Valley of the Four Winds|ACH|6969;15|
-F The Heartland|QID|969690016|M|0.00,0.00|Z|Valley of the Four Winds|ACH|6969;16|
-F Thunderfoot Ranch|QID|969690017|M|0.00,0.00|Z|Valley of the Four Winds|ACH|6969;17|
-F Zhu's Descent|QID|969690018|M|0.00,0.00|Z|Valley of the Four Winds|ACH|6969;18|
+F Winds' Edge|QID|969690001|M|51,77|Z|Valley of the Four Winds|ACH|6969;1|
+F Dustback Gorge|QID|969690002|M|13,76|Z|Valley of the Four Winds|ACH|6969;2|
+F Gilded Fan|QID|969690003|M|56,36|Z|Valley of the Four Winds|ACH|6969;3|
+F The Imperial Granary|QID|969690004|M|50.7,63.7|Z|Valley of the Four Winds|ACH|6969;4|
+F Halfhill|QID|969690005|M|54,46|Z|Valley of the Four Winds|ACH|6969;5|
+F Stoneplow|QID|969690006|M|18,55|Z|Valley of the Four Winds|ACH|6969;6|
+F Kunzen Village|QID|969690007|M|30.1,35.7|Z|Valley of the Four Winds|ACH|6969;7|
+F Mudmug's Place|QID|969690008|M|68,43|Z|Valley of the Four Winds|ACH|6969;8|
+F Nesingwary Safari|QID|969690009|M|18,81|Z|Valley of the Four Winds|ACH|6969;9|
+F Paoquan Hollow|QID|969690010|M|16,35|Z|Valley of the Four Winds|ACH|6969;10|
+F Pools of Purity|QID|969690011|M|60,27|Z|Valley of the Four Winds|ACH|6969;11|
+F Rumbling Terrace|QID|969690012|M|74.18,63.75|Z|Valley of the Four Winds|ACH|6969;12|
+F Silken Fields|QID|969690013|M|63.57|Z|Valley of the Four Winds|ACH|6969;13|
+F Singing Marshes|QID|969690014|M|25.19,43.64|Z|Valley of the Four Winds|ACH|6969;14|
+F Stormstout Brewery|QID|969690015|M|34,70|Z|Valley of the Four Winds|ACH|6969;15|
+F The Heartland|QID|969690016|M|39,39|Z|Valley of the Four Winds|ACH|6969;16|
+F Thunderfoot Ranch|QID|969690017|M|75,25|Z|Valley of the Four Winds|ACH|6969;17|
+F Zhu's Descent|QID|969690018|M|81.53,47.91|Z|Valley of the Four Winds|ACH|6969;18|
 
-F Binan Village|QID|969760001|M|0.00,0.00|Z|Kun-Lai Summit|ACH|6976;1|
-F Firebough Nook|QID|969760002|M|0.00,0.00|Z|Kun-Lai Summit|ACH|6976;2|
-F Gate of the August Celestials|QID|969760003|M|0.00,0.00|Z|Kun-Lai Summit|ACH|6976;3|
-F Isle of Reckoning|QID|969760004|M|0.00,0.00|Z|Kun-Lai Summit|ACH|6976;4|
-F Kota Peak|QID|969760005|M|0.00,0.00|Z|Kun-Lai Summit|ACH|6976;5|
-F Mogujia|QID|969760006|M|0.00,0.00|Z|Kun-Lai Summit|ACH|6976;6|
-F Mount Neverest|QID|969760007|M|0.00,0.00|Z|Kun-Lai Summit|ACH|6976;7|
-F Muskpaw Ranch|QID|969760008|M|0.00,0.00|Z|Kun-Lai Summit|ACH|6976;8|
-F Peak of Serenity|QID|969760009|M|0.00,0.00|Z|Kun-Lai Summit|ACH|6976;9|
-F Shado-Pan Monastery|QID|969760010|M|0.00,0.00|Z|Kun-Lai Summit|ACH|6976;10|
-F Temple of the White Tiger|QID|969760011|M|0.00,0.00|Z|Kun-Lai Summit|ACH|6976;11|
-F The Burlap Trail|QID|969760012|M|0.00,0.00|Z|Kun-Lai Summit|ACH|6976;12|
-F Valley of Emperors|QID|969760013|M|0.00,0.00|Z|Kun-Lai Summit|ACH|6976;13|
-F Zouchin Village|QID|969760014|M|0.00,0.00|Z|Kun-Lai Summit|ACH|6976;14|
+F Binan Village|QID|969760001|M|72,91|Z|Kun-Lai Summit|ACH|6976;1|
+F Firebough Nook|QID|969760002|M|44,86|Z|Kun-Lai Summit|ACH|6976;2|
+F Gate of the August Celestials|QID|969760003|M|55,91|Z|Kun-Lai Summit|ACH|6976;3|
+F Isle of Reckoning|QID|969760004|M|75,13|Z|Kun-Lai Summit|ACH|6976;4|
+F Kota Peak|QID|969760005|M|35,65|Z|Kun-Lai Summit|ACH|6976;5|
+F Mogujia|QID|969760006|M|59,72|Z|Kun-Lai Summit|ACH|6976;6|
+F Mount Neverest|QID|969760007|M|45,51|Z|Kun-Lai Summit|ACH|6976;7|
+F Muskpaw Ranch|QID|969760008|M|68,73|Z|Kun-Lai Summit|ACH|6976;8|
+F Peak of Serenity|QID|969760009|M|49,40|Z|Kun-Lai Summit|ACH|6976;9|
+F Shado-Pan Monastery|QID|969760010|M|34,47|Z|Kun-Lai Summit|ACH|6976;10|
+F Temple of the White Tiger|QID|969760011|M|68,48|Z|Kun-Lai Summit|ACH|6976;11|
+F The Burlap Trail|QID|969760012|M|47,67|Z|Kun-Lai Summit|ACH|6976;12|
+F Valley of Emperors|QID|969760013|M|57,47|Z|Kun-Lai Summit|ACH|6976;13|
+F Zouchin Village|QID|969760014|M|62,29|Z|Kun-Lai Summit|ACH|6976;14|
 
-F Heart of Fear|QID|969780001|M|0.00,0.00|Z|Dread Wastes|ACH|6978;1|
-F Klaxxi'vess|QID|969780002|M|0.00,0.00|Z|Dread Wastes|ACH|6978;2|
-F Kypari Vor|QID|969780003|M|0.00,0.00|Z|Dread Wastes|ACH|6978;3|
-F Kypari Zar|QID|969780004|M|0.00,0.00|Z|Dread Wastes|ACH|6978;4|
-F Rikkitun Village|QID|969780005|M|0.00,0.00|Z|Dread Wastes|ACH|6978;5|
-F Soggy's Gamble|QID|969780006|M|0.00,0.00|Z|Dread Wastes|ACH|6978;6|
-F Terrace of Gurthan|QID|969780007|M|0.00,0.00|Z|Dread Wastes|ACH|6978;7|
-F The Briny Muck|QID|969780008|M|0.00,0.00|Z|Dread Wastes|ACH|6978;8|
-F The Clutches of Shek'zeer|QID|969780009|M|0.00,0.00|Z|Dread Wastes|ACH|6978;9|
-F The Sunset Brewgarden|QID|969780010|M|0.00,0.00|Z|Dread Wastes|ACH|6978;10|
-F Writhingwood|QID|969780011|M|0.00,0.00|Z|Dread Wastes|ACH|6978;11|
-F Zan'vess|QID|969780012|M|0.00,0.00|Z|Dread Wastes|ACH|6978;12|
+F Heart of Fear|QID|969780001|M|40,34|Z|Dread Wastes|ACH|6978;1|
+F Klaxxi'vess|QID|969780002|M|55,35|Z|Dread Wastes|ACH|6978;2|
+F Kypari Vor|QID|969780003|M|57,17|Z|Dread Wastes|ACH|6978;3|
+F Kypari Zar|QID|969780004|M|59,57|Z|Dread Wastes|ACH|6978;4|
+F Rikkitun Village|QID|969780005|M|39,19|Z|Dread Wastes|ACH|6978;5|
+F Soggy's Gamble|QID|969780006|M|56,70|Z|Dread Wastes|ACH|6978;6|
+F Terrace of Gurthan|QID|969780007|M|70,25|Z|Dread Wastes|ACH|6978;7|
+F The Briny Muck|QID|969780008|M|39,65|Z|Dread Wastes|ACH|6978;8|
+F The Clutches of Shek'zeer|QID|969780009|M|47,35|Z|Dread Wastes|ACH|6978;9|
+F The Sunset Brewgarden|QID|969780010|M|50,11|Z|Dread Wastes|ACH|6978;10|
+F Writhingwood|QID|969780011|M|66,44|Z|Dread Wastes|ACH|6978;11|
+F Zan'vess|QID|969780012|M|30,75|Z|Dread Wastes|ACH|6978;12|
 
 N Congratulations on exploring Pandaria!
 ]]
