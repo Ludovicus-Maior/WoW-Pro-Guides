d63725e164eff59ec5284c1dbfaf57fbab359634
Ludovicus <ludovicus.maior@gmail.com>
2018-07-16 18:29:45 -0400

    Updated WoWPro_Leveling/Horde/110_120_Taron_Zuldazar.lua from WoW-Pro.COM

--- WoWPro_Leveling/Horde/110_120_Taron_Zuldazar.lua
+++ WoWPro_Leveling/Horde/110_120_Taron_Zuldazar.lua
@@ -1,3 +1,88 @@
+
+-- WoWPro Guides by "The WoW-Pro Community" are licensed under a Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License.
+-- Based on a work at github.com.
+-- Permissions beyond the scope of this license may be available at http://www.wow-pro.com/License.
+
+-- URL: http://wow-pro.com/wiki/bfa_zuldazar
+-- Date: 2018-07-03 02:45
+-- Who: Emmaleah
+-- Log: updated next guide
+
+-- URL: http://wow-pro.com/node/3762/revisions/29371/view
+-- Date: 2018-06-25 20:11
+-- Who: Emmaleah
+-- Log: adjust header and first few steps to match up with Intro Guide
+
+-- URL: http://wow-pro.com/node/3762/revisions/29317/view
+-- Date: 2018-05-25 18:58
+-- Who: Emmaleah
+-- Log: added source page header
+
+-- URL: http://wow-pro.com/node/3762/revisions/29313/view
+-- Date: 2018-05-14 23:56
+-- Who: Taronlock
+
+-- URL: http://wow-pro.com/node/3762/revisions/29307/view
+-- Date: 2018-05-13 05:53
+-- Who: Emmaleah
+-- Log: added standard text at top for Wiki page
+
+-- URL: http://wow-pro.com/node/3762/revisions/29299/view
+-- Date: 2018-05-12 16:33
+-- Who: Taronlock
+
+-- URL: http://wow-pro.com/node/3762/revisions/29298/view
+-- Date: 2018-05-12 16:24
+-- Who: Taronlock
+
+-- URL: http://wow-pro.com/node/3762/revisions/29297/view
+-- Date: 2018-05-12 16:21
+-- Who: Taronlock
+
+-- URL: http://wow-pro.com/node/3762/revisions/29296/view
+-- Date: 2018-05-12 16:18
+-- Who: Taronlock
+
+-- URL: http://wow-pro.com/node/3762/revisions/29294/view
+-- Date: 2018-05-11 16:28
+-- Who: Taronlock
+
+-- URL: http://wow-pro.com/node/3762/revisions/29288/view
+-- Date: 2018-05-10 19:34
+-- Who: Taronlock
+
+-- URL: http://wow-pro.com/node/3762/revisions/29286/view
+-- Date: 2018-05-10 18:43
+-- Who: Taronlock
+
+-- URL: http://wow-pro.com/node/3762/revisions/29285/view
+-- Date: 2018-05-10 18:19
+-- Who: Taronlock
+
+-- URL: http://wow-pro.com/node/3762/revisions/29284/view
+-- Date: 2018-05-10 18:06
+-- Who: Taronlock
+
+-- URL: http://wow-pro.com/node/3762/revisions/29283/view
+-- Date: 2018-05-10 17:54
+-- Who: Taronlock
+
+-- URL: http://wow-pro.com/node/3762/revisions/29282/view
+-- Date: 2018-05-10 17:47
+-- Who: Taronlock
+
+-- URL: http://wow-pro.com/node/3762/revisions/29281/view
+-- Date: 2018-05-10 16:16
+-- Who: Taronlock
+
+-- URL: http://wow-pro.com/node/3762/revisions/29280/view
+-- Date: 2018-05-10 15:24
+-- Who: Taronlock
+
+-- URL: http://wow-pro.com/node/3762/revisions/29278/view
+-- Date: 2018-05-10 14:51
+-- Who: Ludovicus_Maior
+
 local guide = WoWPro:RegisterGuide('TaronZuldazar', 'Leveling', 'Zuldazar', 'Taronlock', 'Horde')
 WoWPro:GuideLevels(guide,110, 120)
 WoWPro:GuideNextGuide(guide, 'EmmHWarCampaign')
@@ -8,7 +93,7 @@ t Zuldazar|QID|47514|M|57.80,44.31|Z|Zuldazar|N|To Princess Talanji.|
 A Trust of a King|QID|49615|M|57.80,44.31|Z|Zuldazar|N|From Princess Talanji.|PRE|47514|
 A The War Campaign|QID|52749|M|40.06,72.00|Z|Hall of Croniclers!DazarAlor_Upper|N|From Nathanos Blightcaller.|
 
-T Trust of a King|QID|49615|M|57.95,44.62|Z|Zuldazar|N|To King Rastakhan.| 
+T Trust of a King|QID|49615|M|57.95,44.62|Z|Zuldazar|N|To King Rastakhan.|
 A Tal'gurub|QID|49488|M|57.95,44.62|Z|Zuldazar|N|From King Rastakhan.|
 A The Zanchuli Council|QID|47445|M|57.88,44.57|Z|Zuldazar|N|From Yazma.|
 A The Port of Zandalar|QID|50835|M|57.91,44.59|Z|Zuldazar|N|From Zolani.|
