b49d35b9c81966c9faa1aa5a7b4cc7315ee7c0af
Ludovicus <ludovicus.maior@gmail.com>
2018-08-01 14:35:02 -0400

    Updated WoWPro_Achievements/Garrison_Horde/Ludo_Support.lua from WoW-Pro.COM

--- WoWPro_Achievements/Garrison_Horde/Ludo_Support.lua
+++ WoWPro_Achievements/Garrison_Horde/Ludo_Support.lua
@@ -4,6 +4,16 @@
 -- Permissions beyond the scope of this license may be available at http://www.wow-pro.com/License.
 
 -- URL: http://wow-pro.com/wiki/garrisson_support
+-- Date: 2018-08-01 20:15
+-- Who: Ludovicus_Maior
+-- Log: Add stub LudoGarrissonSupportH
+
+-- URL: http://wow-pro.com/node/3649/revisions/29467/view
+-- Date: 2018-08-01 20:10
+-- Who: Ludovicus_Maior
+-- Log: Add faction to guide name.
+
+-- URL: http://wow-pro.com/node/3649/revisions/28976/view
 -- Date: 2018-02-19 22:32
 -- Who: Ludovicus_Maior
 -- Log: Upload Grail corrections to PRE/LEAD tags
@@ -30,7 +40,6 @@ WoWPro:GuideName(guide,"On the Shadow's Trail")
 WoWPro:GuideAutoSwitch(guide)
 WoWPro:GuideSteps(guide, function()
 return [[
-
 
 ]]
 
