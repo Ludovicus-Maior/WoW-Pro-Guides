e2bcc9fefa63e526c49c23db33f03b99be54b197
Ludovicus <ludovicus.maior@gmail.com>
2018-08-01 13:45:23 -0400

    Updated WoWPro_Leveling/Horde/20_20_Ludovicus_Nightborne.lua from WoW-Pro.COM

--- WoWPro_Leveling/Horde/20_20_Ludovicus_Nightborne.lua
+++ WoWPro_Leveling/Horde/20_20_Ludovicus_Nightborne.lua
@@ -1,3 +1,8 @@
+
+-- WoWPro Guides by "The WoW-Pro Community" are licensed under a Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License.
+-- Based on a work at github.com.
+-- Permissions beyond the scope of this license may be available at http://www.wow-pro.com/License.
+
 local guide = WoWPro:RegisterGuide("LudoNightborne", "Leveling", "Suramar", "Ludovicus", "Horde")
 WoWPro:GuideLevels(guide,20,20)
 WoWPro:GuideRaceSpecific(guide,"Nightborne")
