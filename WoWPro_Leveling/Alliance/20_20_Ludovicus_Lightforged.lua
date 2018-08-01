eba12debba730ac891d456d0f0a0562723a59e7a
Ludovicus <ludovicus.maior@gmail.com>
2018-08-01 13:45:11 -0400

    Updated WoWPro_Leveling/Alliance/20_20_Ludovicus_Lightforged.lua from WoW-Pro.COM

--- WoWPro_Leveling/Alliance/20_20_Ludovicus_Lightforged.lua
+++ WoWPro_Leveling/Alliance/20_20_Ludovicus_Lightforged.lua
@@ -1,3 +1,8 @@
+
+-- WoWPro Guides by "The WoW-Pro Community" are licensed under a Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License.
+-- Based on a work at github.com.
+-- Permissions beyond the scope of this license may be available at http://www.wow-pro.com/License.
+
 local guide = WoWPro:RegisterGuide("LudoLightforged", "Leveling", "Lightforged Vindicaar", "Ludovicus", "Alliance")
 WoWPro:GuideLevels(guide,20,20)
 WoWPro:NewGuideLevels(guide,20,20)
