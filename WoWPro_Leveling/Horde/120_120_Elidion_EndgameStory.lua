5a7d876df6e9aa3a2625af06d39cdd8b7ef1df1d
Luis Ortiz <lortiz@cargometrics.com>
2018-10-20 18:51:13 +0000

    Updated WoWPro_Leveling/Horde/120_120_Elidion_EndgameStory.lua from WoW-Pro.COM

--- WoWPro_Leveling/Horde/120_120_Elidion_EndgameStory.lua
+++ WoWPro_Leveling/Horde/120_120_Elidion_EndgameStory.lua
@@ -3,6 +3,16 @@
 -- Based on a work at github.com.
 -- Permissions beyond the scope of this license may be available at http://www.wow-pro.com/License.
 
+-- URL: http://wow-pro.com/wiki/horde_endgame_storylines
+-- Date: 2018-10-20 09:14
+-- Who: elidion
+-- Log: Added the spirits be with you questline.
+
+-- URL: http://wow-pro.com/node/3783/revisions/29816/view
+-- Date: 2018-09-24 02:30
+-- Who: elidion
+-- Log: Initial file for patch 8.0 story content.
+
 local guide = WoWPro:RegisterGuide('EliEndgame', 'Leveling', 'Zandalar', 'Elidion', 'Horde')
 WoWPro:GuideLevels(guide,120, 120)
 WoWPro:GuideName(guide,'Endgame Storylines')
@@ -22,7 +32,7 @@ C They Want Us Alive|QID|47198|M|59.66,19.00|Z|Zuldazar|NC|N|Click on the Wounde
 C Ticks|QID|47200|M|59.58,19.46|Z|Zuldazar|US|N|Finish up your quota of Bloodbelly Flyers.|
 T Ticks|QID|47200|M|60.39,22.03|Z|Zuldazar|N|To King Rastakhan.|
 T They Want Us Alive|QID|47198|Z|Zuldazar|M|60.39,22.03|N|To King Rastakhan.|
-A Rokhan|QID|47201|M|60.39,22.03|Z|Zuldazar|N|From King Rastakhan.|PRE|47200;47198|
+A Rokhan|QID|47201|M|60.39,22.03|Z|Zuldazar|N|From King Rastakhan.|PRE|47200 & 47198|
 C Rokhan|QID|47201|M|60.99,20.53|Z|Zuldazar|QO|1|NC|N|Click on Old Rotana to go for a short ride.|
 T Rokhan|QID|47201|M|56.92,19.10|Z|Zuldazar|N|To Rokhan.|
 A Warmother|QID|47205|M|56.92,19.10|Z|Zuldazar|N|From Rokhan.|PRE|47201|
@@ -32,7 +42,7 @@ K Warmother Shazraka|QID|47205|M|58.02,18.19|Z|Zuldazar|QO|1|T|Warmother Shazrak
 C The New Frontline|QID|47204|M|57.08,20.68|Z|Zuldazar|US|N|Finish your share of the Blood Troll forces.|
 T Warmother|QID|47205|M|56.92,19.11|Z|Zuldazar|N|To Rokhan.|
 T The New Frontline|QID|47204|M|56.92,19.11|Z|Zuldazar|N|To Rokhan.|
-A Bulwark of Torcali|QID|47229|M|56.92,19.11|Z|Zuldazar|N|From Rokhan.|PRE|47204;47205|
+A Bulwark of Torcali|QID|47229|M|56.92,19.11|Z|Zuldazar|N|From Rokhan.|PRE|47204 & 47205|
 C Bulwark of Torcali|QID|47229|M|56.90,19.73|Z|Zuldazar|NC|QO|1|N|Hop back onto Old Rotana.|
 C Bulwark of Torcali|QID|47229|M|58.07,20.39|Z|Zuldazar|NC|QO|2|N|Destroy Blood Troll forces.\n1 is Cone AE\n2 is ligtning bolt, multi target\n3 is charge, must not be moving to use.|
 T Bulwark of Torcali|QID|47229|M|57.98,17.83|Z|Zuldazar|N|Make your way to Princess Talanji and eject from Old Rotana.|
@@ -54,8 +64,47 @@ C Reach Dazar'alor|ACTIVE|50954|M|54.75,35.98|Z|Zuldazar!Instance|SO|6;1|NC|N|Jo
 C Find King Rastakhan at the entrance to Dazar'alor.|ACTIVE|50954|M|55.27,38.09|Z|Zuldazar!Instance|SO|7;1|NC|N| Find King Rastakhan at the entrance to Dazar'alor.|
 C Reach Zul|ACTIVE|50954|M|56.40,39.67;56.43,41.52;57.03,41.48;57.90,40.60|Z|Zuldazar!Instance|SO|8;1|CS|N|Keep going up stairs, until you find one blocked by the bluish flames. Go left and Wait for King Rastakhan before you fight Ma'da Renkala.  The scenario ends with a cutscene as you finish off Ma'da.|
 T Zandalar Forever!|QID|50954|M|49.94,46.63|Z|Dazar'alor|N|To King Rastakhan.|
-
-N Not the end|AVAILABLE|-50954|N|That's it for patch 8.0. Come back in 8.1 when the story continues.|ACH|12479;;true|
+A Zalazane Returns|QID|51513|M|39.44,72.50|Z|Hall of Croniclers!Dazar'alor|N|From Rokhan.|PRE|50954|
+b Darkspear Isle|ACTIVE|51513|M|57.98,64.96|Z|Zuldazar|N|Get down to the docks and wait for the boat to arrive and step on once it does.|
+T Zalazane Returns|QID|51513|M|59.46,51.50|Z|Echo Isles|N|To Master Gadrin.|
+A Broken Bargain|QID|51514|M|59.46,51.50|Z|Echo Isles|N|From Master Gadrin.|PRE|51513|
+A The Glaive of Vol'jin|QID|51533|M|59.22,51.99|Z|Echo Isles|N|From Zen'tabra.|PRE|51513|
+C Broken Bargain|QID|51514|M|61.21,55.55|Z|Echo Isles|QO|1|S|N|Kill Mindless Trolls.|
+C The Glaive of Vol'jin|QID|51533|M|62.01,66.46|Z|Echo Isles|QO|1|NC|N|Pick up The Glaive of Vol'jin at the back of the hut.|
+C Broken Bargain|QID|51514|M|61.21,55.55|Z|Echo Isles|QO|1|US|N|Continue killing the rest of the Mindless Trolls.|
+T The Glaive of Vol'jin|QID|51533|M|59.22,51.98|Z|Echo Isles|N|To Zen'tabra.|
+T Broken Bargain|QID|51514|M|59.47,51.50|Z|Echo Isles|N|To Master Gadrin.|
+A Vengeance for Vol'jin|QID|51515|M|59.47,51.50|Z|Echo Isles|N|From Master Gadrin.|PRE|51514 & 51533|
+C Vengeance for Vol'jin|QID|51515|M|36.74,68.97|Z|Echo Isles|QO|1|N|Kill Zalazane.|T|Zalazane|
+C Vengeance for Vol'jin|QID|51515|M|36.74,68.97|Z|Echo Isles|QO|2|N|Target Zalazane's Spirit use the Glaive of Vol'jin.|U|160443|
+T Vengeance for Vol'jin|QID|51515|M|59.44,51.49|Z|Echo Isles|N|To Master Gadrin.|
+A Honoring a True Leader|QID|52114|M|59.44,51.49|Z|Echo Isles|N|From Master Gadrin.|PRE|51515|
+b Zuldazar|ACTIVE|52114|M|70.91,38.21|Z|Echo Isles|N|You can go back to the boat or use your hearthstone here if it's more convenient.|
+F Atal'dazar|ACTIVE|52114|Z|Zuldazar|TZ|The Golden Road|N|Goto your nearest flightmaster and fly to the Atal'Dazar flight point near the dungeon entrance.|
+T Honoring a True Leader|QID|52114|M|45.52,35.81|Z|Zuldazar|N|To Master Gadrin.|
+A Vol'jin, Son of Sen'jin|QID|52113|M|45.52,35.81|Z|Zuldazar|N|From Master Gadrin.||PRE|52114|
+C Vol'jin, Son of Sen'jin|QID|52113|M|45.78,36.17|Z|Zuldazar|QO|1|NC|N|Vision of the Bargain witnessed.|
+C Vol'jin, Son of Sen'jin|QID|52113|M|45.46,37.79|Z|Zuldazar|QO|2|NC|N|Vision of the Horde witnessed.|
+C Vol'jin, Son of Sen'jin|QID|52113|M|44.29,38.43|Z|Zuldazar|QO|3|NC|N|Vision of the Warchief witnessed. You will be attacked on your way to this final torch, don't expect any help.|
+T Vol'jin, Son of Sen'jin|QID|52113|M|43.77,39.54|Z|Zuldazar|N|To Princess Talanji.|
+A Atal'Dazar: Ashes of a Warchief|QID|51516|M|43.77,39.54|Z|Zuldazar|N|From Princess Talanji.|PRE|52113|
+C Atal'Dazar: Ashes of a Warchief|QID|51516|Z|Atal'Dazar|QO|1|N|Kill Yazma, the final boss in Atal'Dazar, can be completed on any difficulty but Mythic plus.|
+C Atal'Dazar: Ashes of a Warchief|QID|51516|Z|Atal'Dazar|QO|2|NC|N|Place the Urn of Vol'jin in the North-West Corner of the zone near a little pond.|
+T Atal'Dazar: Ashes of a Warchief|QID|51516|Z|Atal'Dazar|N|To Bwonsamdi.|
+A You Owe Me a Spirit|QID|51517|Z|Atal'Dazar|N|From Bwonsamdi.|PRE|51516|
+T You Owe Me a Spirit|QID|51517|M|39.57,24.54|Z|Nazmir|N|Bring the urn back to Bwonsamdi at his temple in Northern Nazmir.|
+A The Lost Spirit|QID|51518|M|45.12,19.98|Z|Dazar'alor|N|From Bwonsamdi a couple levels up the Zanchul.|PRE|51517|
+T The Lost Spirit|QID|51518|M|57.62,62.58|Z|Zuldazar|N|To Princess Talanji down by the docks.|
+A Spirit Call|QID|51519|M|57.62,62.58|Z|Zuldazar|N|From Princess Talanji.|
+C Spirit Call|QID|51519|M|57.59,62.91|Z|Zuldazar|QO|1|NC|N|Wait while Talanji performs her ritual.|
+T Spirit Call|QID|51519|M|57.59,62.97|Z|Zuldazar|N|To Spirit of Vol'jin.|
+A Justice for the Fallen|QID|51520|M|57.59,62.97|Z|Zuldazar|N|From Spirit of Vol'jin.|PRE|51519|
+C Justice for the Fallen|QID|51520|Z|Uldir|N|Now you need to kill the G'hun, the final boss in Uldir and throw the glaive at his corpse. This can be done on any difficulty though LFR is probabl fastest.|U|161452|
+T Justice for the Fallen|QID|51520|M|49.92,39.42|Z|Dazar'alor|N|To Spirit of Vol'jin outside of the Great Seal.|
+A The True Leader of Zandalar|QID|51521|M|49.92,39.42|Z|Dazar'alor|N|From Spirit of Vol'jin.|PRE|51520|
+C The True Leader of Zandalar|QID|51521|M|49.92,39.42|Z|Dazar'alor|QO|1|NC|N|Give Vol'jin's Glaive to Talanji and watch the cinematic.|
+T The True Leader of Zandalar|QID|51521|M|49.92,39.42|Z|Dazar'alor|N|To Spirit of Vol'jin.|
+N Not the end|AVAILABLE|-51521|N|That's it for patch 8.0. Come back in 8.1 when the story continues.|ACH|12719;;true|
 ]]
 end)
 
