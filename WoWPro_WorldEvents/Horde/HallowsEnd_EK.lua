d5cc163ce3391e7a8cd2ffbfcc6284dac7295bf9
Ludovicus <ludovicus.maior@gmail.com>
2018-10-31 05:31:48 -0400

    Updated WoWPro_WorldEvents/Horde/HallowsEnd_EK.lua from WoW-Pro.COM

--- WoWPro_WorldEvents/Horde/HallowsEnd_EK.lua
+++ WoWPro_WorldEvents/Horde/HallowsEnd_EK.lua
@@ -4,6 +4,21 @@
 -- Permissions beyond the scope of this license may be available at http://www.wow-pro.com/License.
 
 -- URL: http://wow-pro.com/wiki/hallows_end_horde
+-- Date: 2018-10-31 11:01
+-- Who: Emmaleah
+-- Log: corrections to MoP and Outlands 
+
+-- URL: http://wow-pro.com/node/3441/revisions/30103/view
+-- Date: 2018-10-30 20:13
+-- Who: Emmaleah
+-- Log: updated cata guide.
+
+-- URL: http://wow-pro.com/node/3441/revisions/30102/view
+-- Date: 2018-10-30 18:24
+-- Who: Emmaleah
+-- Log: Changes for BFA to Kalimdor and EK guides.
+
+-- URL: http://wow-pro.com/node/3441/revisions/28513/view
 -- Date: 2017-03-17 14:09
 -- Who: Ludovicus_Maior
 -- Log: Syntax and PRE for LudoHallowPanH
@@ -215,9 +230,10 @@ WoWPro:GuideSteps(guide, function()
 return [[
 
 N Wall of Text Part 1 of 2|QID|12369|N|This guide is geared toward actually completing all the Candy Buckets necessary for the Hallows End achievements "Tricks and Treats of...".|
-N Wall of Text Part 2 of 2|QID|12369|N|Also to note, you can get multiples of things like the Hallowed Helm (Unique) and Sinister Squashling(not unique). So be prepared do delete a lot treat bags, keep one Hallowed Helm in your inventory and that's it. It's easier to delete the bag, than it is to delete the helm.|
+N Wall of Text Part 2 of 2|QID|12369|N|Also to note, you can get multiples of things like the Hallowed Helm (Unique) and Sinister Squashling(not unique). So be prepared do delete a lot treat bags, keep one Hallowed Helm in your inventory and that's it. It's easier to delete the bag (after opening and removing other contents), than it is to delete the helm.|
 
 N This section starts...|QID|12369|N|In Silvermoon City, so head to Undercity and take the portal to Silvermoon City.|
+R Zidormi|QID|12382|M|69.46,62.81|Z|Tirisfal Glades|N|Must speak to Zidormi, just Southeast of Brill (basically where the teleport in Orgrimmar takes you), to revert to a pre-Battle for Lordaeron time. Return to Zidormi and tell her to return you to the present time when you are done in Tirisfal. Manually check this step off.|
 
 A Candy Bucket|QID|12369|M|79.45,57.71|Z|Silvermoon City|N|Innkeeper Velendra, The Royal Exchange|
 A Candy Bucket|QID|12370|M|67.55,72.90|Z|Silvermoon City|N|Innkeeper Jovia, The Bazaar|
@@ -237,6 +253,7 @@ A Candy Bucket|QID|28987|M|48.28,63.66|Z|Western Plaguelands|N|Roman Garner, And
 F The Bulwark|M|46.53,64.71|QID|28972|Z|Western Plaguelands|U|37586|N|Fly to The Bulwark, Tirisfal Glades.|
 A Candy Bucket|QID|28972|M|83.04,72.07|Z|Tirisfal Glades|N|Provisioner Elda, The Bulwark|
 F Brill|M|83.58,69.94|QID|12363|Z|Tirisfal Glades|U|37586|N|Fly to Brill, Tirisfal Glades.|
+
 A Candy Bucket|QID|12363|M|60.98,51.41|Z|Tirisfal Glades|N|Innkeeper Renee, Brill|
 F Undercity|M|58.84,51.94|QID|12368|Z|Tirisfal Glades|U|37586|N|Fly to Undercity.|
 A Candy Bucket|QID|12368|M|67.77,37.41|Z|Undercity|N|Innkeeper Norman, The Trade Quarter, on the upper level|
@@ -276,7 +293,7 @@ R Dreadmaul Hold|M|47.78,55.22|QID|28959|Z|Swamp of Sorrows|U|37586|N|Run to Dre
 A Candy Bucket|QID|28959|M|40.47,11.29|Z|Blasted Lands|N|Innkeeper Grak, Dreadmaul Hold|
 R Zidormi|QID|12382|M|48.16,7.29|N|Must speak to Zidormi, just SW of the entrance to the Blasted Lands from Swamp of Sorrows, to revert to a post-invasion time. Manually check this step off.|Z|Blasted Lands|
 R Stonard|M|47.70,55.06|QID|12382|Z|Swamp of Sorrows|U|37586|N|Run to Stonard, Swamp of Sorrows.|
-F Grom'gol Base Camp|M|72.02,12.04|QID|12382|Z|Swamp of Sorrows|U|37586|N|Fly to Grom'gol Base Camp, Northern Stranglethorn.|
+F Grom'gol|M|47.70,55.06|QID|12382|Z|Swamp of Sorrows|U|37586|N|Fly to Grom'gol Base Camp, Northern Stranglethorn.|
 A Candy Bucket|QID|12382|M|37.37,51.78|Z|Northern Stranglethorn|N|Innkeeper Thulbek, Grom'gol Base Camp|
 F Hardwrench Hideaway|M|39.01,51.25|QID|28969|Z|Northern Stranglethorn|U|37586|N|Fly to Hardwrench Hideaway, Northern Stranglethorn.|
 A Candy Bucket|QID|28969|M|35.05,27.21|Z|The Cape of Stranglethorn|N|Innkeeper Draxle, Hardwrench Hideaway|
@@ -284,6 +301,98 @@ F Booty Bay|M|35.14,29.39|QID|12397|Z|The Cape of Stranglethorn|U|37586|N|Fly to
 A Candy Bucket|QID|12397|M|40.91,73.73|Z|The Cape of Stranglethorn|N|Innkeeper Skindle, Booty Bay|
 
 N Breaktime|N|OK, Time to take a break and pick the next guide to do.|U|37586|
+
+Kalimdor
+
+local guide = WoWPro:RegisterGuide("LudoHallowKAL",'WorldEvents',"Kalimdor", "Ludovicus", "Horde")
+WoWPro:GuideLevels(guide,1,90,45.000000)
+WoWPro.WorldEvents:GuideHoliday(guide,"HallowsEnd", "Hallow's End - Kalimdor")
+WoWPro:GuideSteps(guide, function()
+return [[
+
+N Wall of Text Part 1 of 2|QID|12366|N|This guide is geared toward actually completing all the Candy Buckets necessary for the Hollows End achievments "Tricks and Treats of...".|
+N Wall of Text Part 2 of 2|QID|12366|N|Also to note, you can get multiples of things like the Hallowed Helm (Unique) and Sinister Squashling(not unique). So be prepared do delete a lot treat bags, keep one Hallowed Helm in your inventory and that's it. It's easier to delete the bag, than it is to delete the helm.|
+
+N This guide starts|QID|12366|N|in Orgrimmar, so if you are in the Eastern Kingdoms, take the blimp and transport to Orgrimmar.|
+A Candy Bucket|QID|12366|M|53.90,78.94|Z|Orgrimmar|N|Innkeeper Gryshka, The Valley of Strength|
+
+F Razor Hill|QID|12361|M|49.65,59.17|Z|Orgrimmar|U|37586|N|Fly to Razor Hill, Durotar.|
+A Candy Bucket|QID|12361|M|51.54,41.59|Z|Durotar|N|Innkeeper Grosk, Razor Hill|
+
+F Nozzlepot's Outpost|QID|29003|M|53.09,43.57|Z|Durotar|U|37586|N|Fly to Nozzlepot's Outpost, Northern Barrens.|
+A Candy Bucket|QID|29003|M|62.51,16.60|Z|Northern Barrens|N|Innkeeper Kritzle, Nozzlepot's Outpost|
+
+F Splintertree Post|QID|12377|M|62.31,17.11|Z|Northern Barrens|U|37586|N|Fly to Splintertree Post, Ashenvale.|
+A Candy Bucket|QID|12377|M|73.96,60.60|Z|Ashenvale|N|Innkeeper Kaylisk, Splintertree Post|
+
+F Bilgewater Harbor|QID|28992|M|73.18,61.59|Z|Ashenvale|U|37586|N|Fly to Bilgewater Harbor, Azshara.|
+A Candy Bucket|QID|28992|M|57.10,50.17|Z|Azshara|N|Mixi, Bilgewater Harbor|
+
+F Everlook|QID|12400|M|52.92,49.85|Z|Azshara|U|37586|N|Fly to Everlook, Winterspring.|
+A Candy Bucket|QID|12400|M|59.83,51.21|Z|Winterspring|N|Innkeeper Vizzie, Everlook|
+
+F Whisperwind Grove|QID|28994|N|Fly to Whisperwind Grove.|M|58.84,48.26|Z|Winterspring|U|37586|
+A Candy Bucket|QID|28994|M|44.58,28.99|Z|Felwood|N|Innkeeper Wylaria, Whisperwind Grove|
+
+F Zoram'gar Outpost|QID|28989|M|43.58,28.69|U|37586|Z|Felwood|N|Fly to Zoram'gar Outpost, Ashenvale.|
+A Candy Bucket|QID|28989|M|12.99,34.10|Z|Ashenvale|N|Innkeeper Duras, Zoram'gar Outpost|
+F Hellscream's Watch|QID|28958|M|11.16,34.43|U|37586|Z|Ashenvale|N|Fly to Hellscream's Watch, Ashenvale.|
+A Candy Bucket|QID|28958|M|38.65,42.35|Z|Ashenvale|N|Innkeeper Linkasa, Hellscream's Watch|
+F Silverwind Refuge|QID|28953|M|38.08,42.22|U|37586|Z|Ashenvale|N|Fly to Silverwind Refuge, Ashenvale.|
+A Candy Bucket|QID|28953|M|50.25,67.26|Z|Ashenvale|N|Innkeeper Chin'toka, Silverwind Refuge|
+
+F Ratchet|QID|12396|M|49.29,65.25|U|37586|Z|Ashenvale|N|Fly to Ratchet, Northern Barrens.|
+A Candy Bucket|QID|12396|M|67.33,74.66|Z|Northern Barrens|N|Innkeeper Wiley, Ratchet|
+F The Crossroads|QID|12374|M|69.13,70.70|U|37586|Z|Northern Barrens|N|Fly to The Crossroads, Northern Barrens.|
+A Candy Bucket|QID|12374|M|49.51,57.91|Z|Northern Barrens|N|Innkeeper Boorand Plainswind, The Crossroads|
+A Candy Bucket|QID|29002|M|56.21,40.03|Z|Northern Barrens|N|Innkeeper Kerntis, Grol'dom Farm|
+
+F Krom'gar Fortress|QID|29009|M|48.70,58.67|U|37586|Z|Northern Barrens|N|Fly to Krom'gar Fortress, Stonetalon Mountains.|
+A Candy Bucket|QID|29009|M|66.49,64.19|Z|Stonetalon Mountains|N|Felonius Stark, Krom'gar Fortress|
+F Sun Rock Retreat|QID|12378|M|66.53,62.79|U|37586|Z|Stonetalon Mountains|N|Fly to Sun Rock Retreat, Stonetalon Mountains.|
+A Candy Bucket|QID|12378|M|50.39,63.80|Z|Stonetalon Mountains|N|Innkeeper Jayka, Sun Rock Retreat|
+
+F Karnum's Glade|QID|28993|M|48.47,61.94|U|37586|Z|Stonetalon Mountains|N|Fly to Karnum's Glade, Desolace.|
+A Candy Bucket|QID|28993|M|56.73,50.12|Z|Desolace|N|Innkeeper Dessina, Karnum's Glade|
+F Shadowprey Village|QID|12381|M|57.71,49.75|U|37586|Z|Desolace|N|Fly to Shadowprey Village, Desolace.|
+A Candy Bucket|QID|12381|M|24.08,68.29|Z|Desolace|N|Innkeeper Sikewa, Shadowprey Village|
+
+F Camp Ataya|QID|28996|M|21.60,74.13|U|37586|Z|Desolace|N|Fly to Camp Ataya, Feralas.|
+A Candy Bucket|QID|28996|M|41.45,15.68|Z|Feralas|N|Adene Treetotem, Camp Ataya|
+F Stonemaul Hold|QID|28998|M|41.53,15.45|U|37586|Z|Feralas|N|Fly to Stonemaul Hold, Feralas.|
+A Candy Bucket|QID|28998|M|51.97,47.63|Z|Feralas|N|Chonk, Stonemaul Hold|
+F Camp Mojache|QID|12386|M|51.00,48.43|U|37586|Z|Feralas|N|Fly to Camp Mojache, Feralas.|
+A Candy Bucket|QID|12386|M|74.83,45.14|Z|Feralas|N|Innkeeper Greul, Camp Mojache|
+
+F Marshal's Stand|QID|29018|M|75.44,44.36|U|37586|Z|Feralas|N|Fly to Marshal's Stand, Un'Goro Crater.|
+A Candy Bucket|QID|29018|M|55.27,62.12|Z|Un'Goro Crater|N|Innkeeper Dreedle, Marshal's Stand|
+
+R Zidormi|QID|12401|M|78.93,21.93|Z|Silithus|U|37856|N|Fly to Zidormi and tell him to reset the zone to before Azeroth was wounded, (optional go back after you get the bucket if you want to put it back). This step must be manually checked off.|
+R Cenarion Hold|QID|12401|M|55.5,36.7|Z|Silithus|U|37586|N|Fly to Cenarion Hold, Silithus.|
+A Candy Bucket|QID|12401|M|55.5,36.7|Z|Silithus|N|Calandrath, Cenarion Hold|
+
+F Bootlegger Outpost|QID|29014|M|52.77,34.63|U|37586|Z|Silithus|N|Fly to Bootlegger Outpost, Tanaris.|
+A Candy Bucket|QID|29014|M|55.7,60.96|Z|Tanaris|N|Inkeeper Ibanozzel, Bootlegger Outpost.|
+
+F Gadgetzan|QID|12399|M|55.88,60.6|Z|Tanaris|U|37586|N|Fly to Gadgetzan.|
+A Candy Bucket|QID|12399|M|52.55,27.09|Z|Tanaris|N|Innkeeper Fizzgrimble, Gadgetzan|
+
+F Mudsprocket|QID|12398|M|52.04,27.61|U|37586|Z|Tanaris|N|Fly to Mudsprocket, Dustwallow Marsh|
+A Candy Bucket|QID|12398|M|41.86,74.09|Z|Dustwallow Marsh|N|Axle, Mudsprocket|
+F Brackenwall Village|QID|12383|M|42.83,72.43|U|37586|Z|Dustwallow Marsh|N|Fly to Brackenwall Village, Dustwallow Marsh|
+A Candy Bucket|QID|12383|M|36.78,32.43|Z|Dustwallow Marsh|N|“Little” Logok, Brackenwall Village|
+
+F Thunder Bluff|QID|12367|M|35.57,31.88|U|37586|Z|Dustwallow Marsh|N|Fly to Thunder Bluff.|
+A Candy Bucket|QID|12367|M|45.62,64.85|Z|Thunder Bluff|N|Innkeeper Pala, Lower Rise|
+F Bloodhoof Village|QID|12362|M|47.03,49.61|U|37586|Z|Thunder Bluff|N|Fly to Bloodhoof Village, Mulgore.|
+A Candy Bucket|QID|12362|M|46.79,60.41|Z|Mulgore|N|Innkeeper Kauth, Bloodhoof Village|
+
+F Hunter's Hill|QID|29004|M|47.44,58.64|U|37586|Z|Mulgore|N|Fly to Hunter's Hill, Southern Barrens.|
+A Candy Bucket|QID|29004|M|39.29,20.10|Z|Southern Barrens|N|Innkeeper Hurnahet, Hunter's Hill|
+F Desolation Hold|QID|29005|M|39.78,20.27|U|37586|Z|Southern Barrens|N|Fly to Desolation Hold, Southern Barrens.|
+A Candy Bucket|QID|29005|M|40.71,69.31|Z|Southern Barrens|N|Innkeeper Lhakadd, Desolation Hold|
+
+N BioBreak|N|Take a break and pick the next guide to do.|U|37586|
 
 ]]
 end)
