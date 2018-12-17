8eb9f5a087558a9bf30ec61da9e84be26153a674
elidion <elidion@yahoo.com>
2018-12-16 16:39:27 -0800

    Create Elidion_Hivemind.lua
    Hivemind chain.

---
+++ WoWPro_Achievements/General/Elidion_Hivemind.lua
@@ -0,0 +1,136 @@
+local guide = WoWPro:RegisterGuide('Eli_Hivemind', 'Achievements', "Dazar'alor", 'Elidion', 'Neutral')
+WoWPro.Achievements:GuideMisc(guide, "Hivemind", "Mount", "Battle for Azeroth")
+WoWPro:GuideIcon(guide,"ACH",12482)
+WoWPro:NewGuideLevels(guide,120, 120,120)
+WoWPro:GuideSteps(guide, function()
+
+return [[
+N Talisman of True Treasure Tracking|QID|40397|N|To start off this hunt you need pick up the Talisman of True Treasure Tracking in Shattrath. If you you already have it then you can skip the next few steps.|
+P Hellfire Peninsula|QID|40397|M|49.95,87.02|Z|Stormwind City|N|Take the Hellfire Peninsula portal in Stormwind if you have no other means.|FACTION|Alliance|
+P Hellfire Peninsula|QID|40397|M|30.16,58.21|Z|Cleft of Shadow@Orgrimmar!Dungeon|N|Take the Hellfire Peninsula portal in Orgrimmar if you have no other means.|FACTION|Horde|
+B Talisman of True Treasure Tracking|QID|40397|M|65.64,69.26|Z|Shattrath City|N|Buy from Griftah and then equip it.|L|27944|
+N Letter from Ms. Graham|QID|40397|M|65.48,69.55|Z|Shattrath City|N|Letter is glowing blue and is on the bottom right side of the table. "The key Factor is successful Wasp Ignition is a solid Ad campaign."|
+P Highmountain|QID|40314|M|57.40,27.91|Z|Highmountain|N|Get to highmountain by any means - Dalaran Hearthstone still works.|
+N The 2nd Letter|QID|40314|M|57.40,27.91|Z|Highmountain|N|Letter is the tent to the right on top of some crates. "Of all of Gai's cures for Nature, the most liberating is Death."|
+P Karazhan|QID|40404|M|32.07,71.67|Z|Aegwynn's Gallery@Dalaran|N|Get to highmountain by any means - Dalaran Hearthstone still works.|
+N The 3rd Letter|QID|40404|M|32.07,71.67|Z|Aegwynn's Gallery@Dalaran|N|In the original Karazhan dungeon, after the chess event get to Medivh's Chambers, there is a room with his bed in it, to the side will be a desk with a letter on the chair, click the letter. "I sat Dumbfounded, watching As the most Subtle Rat reached for the cheese a third time in under an hour."|
+P Thousand Needles|QID|40252|M|53.24,69.16|Z|Kalimdor|N|Now we need to head to Razorfen Downs in Thousand Needles. Took the Silithus portal there.|
+N The 4th Letter|QID|40252|M|53.24,69.16|Z|Kalimdor|N|The letter is located on a hay box behind the second to final boss, Death Speaker Blackthorn. "Ms. Sin will accompany you down The longest Streets Of the underworld."|
+P Mount Hyjal|QID|40293|M|44.30,47.30|Z|Mount Hyjal|N|Now we need to get to Mount Hyjal.|
+N The 5th Letter|QID|40293|M|44.30,47.30|Z|Mount Hyjal|N|The letter is on a table at the highest floor of the tree that serves as Aviana's shrine. "The Elite champions will rule the World with the mightiest F.C."|
+P Icecrown|QID|40288|M|30.82,84.25|Z|Aegwynn's Gallery@Dalaran|N|Get to Icrown by any means - Dalaran Hearthstone to Wyrmrest Temple works fast.|
+N The 6th Letter|QID|40288|M|70.70,73.30|Z|Icecrown|N|On top of the Ironwall Dam that separates the river that flows from Icecrown and Crystalsong Forest, you will find the next letter is floating on the tip of a spike. "Re: Codex of mastering Sine waves."|
+P Townlong Steppes|QID|50187|M|37.70,63.00|Z|Townlong Steppes|N|Get to Icrown by any means - Dalaran Hearthstone to Wyrmrest Temple works fast.|
+N The 7th Letter|QID|50187|M|37.81,63.00|Z|Townlong Steppes|N|The letter is located near a bell in the back part of the temple outside. "Mice look so sad when they have a Celft lip. Hoping you succeed, Ana"|
+P Borean Tundra|QID|50181|M|30.82,84.25|Z|Aegwynn's Gallery@Dalaran|N|Get to Borean Tundra by any means - Dalaran Hearthstone to Wyrmrest Temple works fast.|
+N The Blue Crystal Monocle|QID|50181|M|27.60,27.20|Z|Borean Tundra|N|The Blue Crystal Monocle is located in a box atop the highest Nexus ring in Coldarra labeled "Gift from Ms. Graham".|L|156724|
+P Spires of Arak|M|35.55,33.47|Z|Spires of Arak|N|Head to the Skyreach dungeon in Draenor.|
+N The Green Crystal Monocle|M|65.48,69.55|Z|Spires of Arak|N|After the final boss you will find orbs to interact with, to solve: Right -> Up -> Down -> Up -> Right -> Right -> Up -> Left -> Down -> Up -> Left -> Down|L|156727|
+P Uldum|M|69.09,52.83|Z|Uldum|N|Head to the Halls of Origination dungeon in Uldum.|
+N The Yellow Crystal Monocle|M|69.09,52.83|Z|Uldum|N|After the first boss there is a room with an elevator. Click on the Stellar Refraction Device below the elevator that looks like a giant glowing ball. Once you click it, you will see a bunch of colors under you. Head north and go down the path to access the puzzle. RED -> GREEN -> BLUE -> ORANGE|L|156726|
+P Kelp'thar Forest|M|60.00,59.00|Z|Kelp'thar Forest|N|Now to collect a TON of stuff in Vashj'ir.|
+B Sir Finley Mrrgglton - 23|M|44.54,20.23|Z|Shimmering Expanse|N|Before you start, be sure you can invest 2,380 gold and sawabout an hour into this, due to the timers, you will lose all progress if you try to return later. I have added a countdown in each title so you know how many steps are left. Buy 500 Seashells.|L|156791 500|
+B Volatile Violetscale - 22|M|40.14,77.21|Z|Abyssal Depths|N|Buy 100 Cavity-Free Great Shark Tooth. Near the bottom.|L|156780 100|
+B Manta Stargazer - 21|M|53.30,23.29|Z|Shimmering Expanse|N|Buy 50 Razoreel Larva. Near the top.|L|156786 50|
+B Lil' Whaley - 20|M|69.23,47.28|Z|Shimmering Expanse|N|Buy 250 Well-Fed Doctor Fish. Near the bottom.|L|156783 250|
+B Gloomy Bluefin - 19|M|64.34,41.00|Z|Abyssal Depths|N|Buy 10 Freshly Molted Crab Skin. All the way to the bottom.|L|156782 10|
+B Ol' Fishbreath - 18|M|60.48,58.45|Z|Kelp'thar Forest|N|Buy 50 Glittergill Glitter. Near the top.|L|156790 50|
+B Sir Finley Mrrgglton -17|M|44.54,20.23|Z|Shimmering Expanse|N|Buy 80 Seashells.|L|156791 80|
+B Gloomy Bluefin - 16|M|64.34,41.00|Z|Abyssal Depths|N|Buy 2 Giant Giant Toenail Clipping. All the way to the bottom.|L|156775 2|
+B Little Carp - 15|M|45.07,18.09|Z|Abyssal Depths|N|Buy 4 Makrura Eye. Don't swim back up, just ride in the deep.|L|156776 4|
+B Volatile Violetscale - 14|M|40.14,77.21|Z|Abyssal Depths|N|Buy 1 Accidentally Severed Seahorse Fin.|L|156773 1|
+B Crimson Angerfish - 13|M|53.00,88.00|Z|Shimmering Expanse|N|Buy 2 Shiny Sea Serpent Scale. Swims around the area.|L|156774 2|
+B Manta Stargazer - 12|M|53.30,23.29|Z|Shimmering Expanse|N|Buy 40 Symbiotic Plankton.|L|156789 40|
+B Sir Finley Mrrgglton - 11|M|44.54,20.23|Z|Shimmering Expanse|N|Buy 5 Scintillating Murloc Skin Lotion.|L|156793 5|
+B Sir Finley Mrrgglton - 10|M|44.54,20.23|Z|Shimmering Expanse|N|Buy 300 Seashells.|L|156791 300|
+B Ol' Fishbreath - 9|M|60.48,58.45|Z|Kelp'thar Forest|N|Buy 30 Vantus Black Squid Ink.|L|156771 30|
+B The Blackfish - 8|M|15.27,83.63|Z|Abyssal Depths|N|Buy 30 Super Slick Eel Slime. Near the bottom.|L|156770 30|
+B Volatile Violetscale - 7|M|40.14,77.21|Z|Abyssal Depths|N|Buy 3 Rock-Encrusted Whelk Shell.|L|156767 3|
+B Little Carp - 6|M|45.07,18.09|Z|Abyssal Depths|N|Buy 5 Potent Gastropod Gloop.|L|156788 5|
+B Sir Finley Mrrgglton - 5|M|44.54,20.23|Z|Shimmering Expanse|N|Buy 1500 Seashells.|L|156791 1500|
+B Lil' Whaley - 4|M|69.23,47.28|Z|Shimmering Expanse|N|Buy 300 Very Pretty Coral. Near the bottom.|L|156761 300|
+B Ol' Fishbreath - 3|M|60.48,58.45|Z|Kelp'thar Forest|N|Buy 100 Iridescent Shimmeray Skin.|L|156758 100|
+B Crimson Angerfish - 2|M|53.00,88.00|Z|Shimmering Expanse|N|Buy 20 Luxurious Luxscale Scale.|L|156764 20|
+B The Blackfish  - 1|M|15.27,83.63|Z|Abyssal Depths|N|Buy 5 Captured Bubbles. Near the bottom.|L|156787 5|
+B Sir Finley Mrrgglton - At last|M|44.54,20.23|Z|Shimmering Expanse|N|Buy the Red Crystal Monocle.|L|156725|
+P Suramar|M|41.0,69.5|Z|Suramar|N|Now you must make your way to Suramar with a group of other people that have the monacles.|
+N Rikei|M|71.80,62.50|Z|Suramar|N|Someone in group will need wear the Red Monocle. Click off if this isn't you|
+N Blom'an|M|46.80,28.60|Z|Suramar|N|Someone in group will need to wear the Blue Monocle. Click off if this isn't you|
+N Giluzui|M|19.90,46.30|Z|Suramar|N|Someone in group will need to wear the Green Monocle. Click off if this isn't you|
+N Yorilan|M|43.50,81.80|Z|Suramar|N|Someone in group will need to wear the Yellow Monocle. Click off if this isn't you|
+N Beams|M|41.0,69.5|Z|Suramar|N|Someone in group will need to wait here by the beams. Once everyone is ready, they attack the withered unil they start casting "Draw Energy". at that point the coresponding beam color will go away. This 5th person runs in and interacts with the "Lost Cat Toy". When you do, you will be teleported out of the room and take damage. Let the group how much damage was done as that will determine how many times you need to pet Lady Chatons Kittens in the Court of Stars.|
+P Court of Stars|M|50.84,65.54|Z|Suramar|N|Now that the beam player has the damage number, head to the court of stars and make your way near the 2nd boss.|
+N Manasaber Kittens|N|Near Lady Chaton, assign each player to a kitten to pet at the same time as the buff only lasts 15 seconds. The damage number should be 5 digits, if it is 4 then the first number is 0. 0 means do not pet. There is a coresponding buff on the kitten for each pet to help you keep track. With your 5 digit number, Mrs. Fluffy muffins is first number, and then sequentially, Shadow, Mew, Ash and the Bella. Click off once the cats run off and void orb appears.|
+N Jump Puzzle|N|Click on the void orb and it will port you to a jumping puzzle, you will need to coordinate well with your group to get across. Assign a player number to each player to help guide you through the puzzle. Skip to proceed.|
+N Player 1|N|Jump onto the platform from the center and forward once more. You will need to manually click off each step after it's been done.|
+N Player 2|N|Jump onto the platform from the center, then Left, then forward.|  
+N Player 1|N|Forward.|  
+N Player 3|N|Jump onto the platform from the original center, then forwars, then right, then right.|
+N Player 4|N|Jump onto the platform from the original center, then left.|    
+N Player 5|N|Jump onto the platform from the original center.|
+N Player 2|N|Left.|
+N Player 4|N|Forward.|
+N Player 2|N|Left.|
+N Player 4|N|Left, then forward.|
+N Player 2|N|Forward, then Left.|
+N Player 3|N|Back.|
+N Player 5|N|Forward, then Right, then Right, then Right.|
+N Player 3|N|Forward.|
+N Player 5|N|Forward.|
+N Player 1|N|Right, then Forward.|
+N Player 3|N|Back, then Forward, then Right.|
+N Player 1|N|Forward, then Left.|
+N Player 4|N|Forward.|
+N Player 2|N|Back, then Forward.|
+N Player 5|N|Forward.|
+N Player 2|N|Back, then Forward.|
+N Player 5|N|Right.|
+N Player 3|N|Forward.|
+N Player 1|N|Forward, then Right.|
+N Player 2|N|Forward, then Right.|
+N Player 4|N|Forward.|
+N Player 2|N|Left, then Forward.|
+N Player 4|N|Forward.|
+N Player 1|N|Right, then Forward.|
+N Player 3|N|Left.|
+N Player 4|N|Forward.|
+N Player 3|N|Forward, then Forward.|
+N Player 5|N|Forward.|
+N Player 1|N|Left.|
+N Player 4|N|Right|
+N Player 5|N|Left.|
+N Player 2|N|Forward.|
+N Player 4|N|Forward.|
+N Player 5|N|Forward. then Forward.|
+N Player 1|N|Forward.|
+N Player 3|N|Left.|
+N Player 5|N|Right, then Forward.|
+N Player 4|N|Forward.|
+N Player 1|N|Right.|
+N Player 5|N|Left.|
+N Player 1|N|Forward.|
+N Player 2|N|Forward.|
+N Player 3|N|Jump off (Leave vehicle). Goto far left platform and Jump on to it.|
+N Player 2|N|Forward.|
+N Player 3|N|Forward, then Forward, then Forward.|
+N Player 2|N|Back.|
+N Player 4|N|Right.|
+N Player 5|N|Forward.|
+N Player 2|N|Forward, then Right, then Forward.|
+N Player 3|N|Forward, then Forward, then Forward, then Forward.|
+N The door|N|Everyone exit vehicle and stack up on the door ahead of you to open it up and gain access to the final puzzle. Skip to next area.|
+N Arcane Lava|N|Each party member is randomly assigned a designation that you will have to determine with trial and error. Player A can ride with any other person single person except that Player A, B and C are the only group of 3 that can ride together. Players D and E are whatever is left and doesn't matter aside from giving a player a designation to follow. Determine your trio, skip when done.|
+N Arcane Lava|N|Person A, B, and C get on and go across.|
+N Arcane Lava|N|Person C gets off on other side, A and B go back across.|
+N Arcane Lava|N|Person B gets off at the start and A and D go back.|
+N Arcane Lava|N|Person D gets off on other side and person A and C come back to start.|
+N Arcane Lava|N|Person B gets on with person A and C and they travel back to the finish.|
+N Arcane Lava|N|Person C gets off at finish, person A and B go back to start.|
+N Arcane Lava|N|Person B gets off, person E gets on with person A. |
+N Arcane Lava|N|Person E gets off at finish, person A and C go back to the start.|
+N Arcane Lava|N|Person B gets on with A and C and go to finish.|
+N The Hivemind|N|Once again everyone must stack up on the door behind you to open it up. Run to the hivemind, each person stands in one of the 5 circles and clicks on the hivemind in the middle. Once all 5 of you have clicked, the mount will appear in your inventory. Congratulations!|
+]]
+end)
+
+
