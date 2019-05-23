4cb5488f0f4288030d452efe7db65f32e2e99c85
Ludovicus <ludovicus.maior@gmail.com>
2019-05-23 17:39:17 -0400

    Add a faux Orc starter (for warlocks).

---
+++ WoWPro_Leveling/Classic/Horde/01_05_OrcStarter_Ludo.lua
@@ -0,0 +1,48 @@
+local guide = WoWPro:RegisterGuide('Orc', 'Leveling', 'Durotar', 'Ludo', 'Horde', 1)
+WoWPro:GuideLevels(guide,1, 5)
+WoWPro:GuideNextGuide(guide, 'Next')
+WoWPro:GuideSteps(guide, function()
+return [[
+A Your Place In The World|QID|4641|M|43.29,68.54|N|From Kaltunk.|
+T Your Place In The World|QID|4641|M|42.06,68.33|N|To Gornek.|
+A Cutting Teeth|QID|788|M|42.06,68.33|N|From Gornek.|
+A Vile Familiars|QID|1485|M|42.59,68.99|N|From Ruzan.|
+L Level 2|QID|1485|LVL|2|N|You should be around level 2 by this point.|
+C Cutting Teeth|QID|788|M|43.70,71.28|QO|1|N|Mottled Boar slain: 10/10|
+C Vile Familiars|QID|1485|M|42.70,53.36|QO|1|N|Vile Familiar Head: 6/6|
+L Level 3|QID|1485|LVL|3|N|You should be around level 3 by this point.|
+T Vile Familiars|QID|1485|M|42.59,69.00|N|To Ruzan.|
+A Vile Familiars|QID|1499|M|42.59,69.00|N|From Ruzan.|
+T Cutting Teeth|QID|788|M|42.05,68.33|N|To Gornek.|
+A Tainted Parchment|QID|3090|M|42.05,68.33|N|From Gornek.|
+A Sting of the Scorpid|QID|789|M|42.05,68.33|N|From Gornek.|
+T Vile Familiars|QID|1499|M|42.84,69.14|N|To Zureetha Fargaze.|
+A Burning Blade Medallion|QID|794|M|42.84,69.14|N|From Zureetha Fargaze.|
+A Galgar's Cactus Apple Surprise|QID|4402|M|42.73,67.24|N|From Galgar.|
+A Lazy Peons|QID|5441|M|44.62,68.64|N|From Foreman Thazz'ril.|
+A Lazy Peons|QID|5441|M|40.63,67.84|
+T Tainted Parchment|QID|3090|M|40.65,68.52|N|To Nartok.|
+A Burning Blade Medallion|QID|794|M|40.65,68.52|
+C Lazy Peons|QID|5441|M|47.35,65.60|QO|1|N|Peons Awoken: 5/5|
+C Galgar's Cactus Apple Surprise|QID|4402|M|46.79,60.33|QO|1|N|Cactus Apple: 10/10|
+C Burning Blade Medallion|QID|794|M|42.56,53.00|QO|1|N|Burning Blade Medallion: 1/1|
+T Galgar's Cactus Apple Surprise|QID|4402|M|42.74,67.25|N|To Galgar.|
+L Level 4|QID|794|LVL|4|N|You should be around level 4 by this point.|
+T Burning Blade Medallion|QID|794|M|42.85,69.13|N|To Zureetha Fargaze.|
+A Report to Sen'jin Village|QID|805|M|42.85,69.13|N|From Zureetha Fargaze.|
+T Lazy Peons|QID|5441|M|44.61,68.64|N|To Foreman Thazz'ril.|
+A Thazz'ril's Pick|QID|6394|M|44.61,68.64|N|From Foreman Thazz'ril.|
+A Sarkoth|QID|790|M|40.64,62.60|N|From Hana'zua.|
+A Sarkoth|QID|790|M|44.61,68.64|
+C Sting of the Scorpid|QID|789|M|41.34,64.54|QO|1|N|Scorpid Worker Tail: 10/10|
+C Sarkoth|QID|790|M|40.43,66.68|QO|1|N|Sarkoth's Mangled Claw: 1/1|
+T Sting of the Scorpid|QID|789|M|42.06,68.33|N|To Gornek.|
+T Sarkoth|QID|790|M|40.61,62.60|N|To Hana'zua.|
+A Sarkoth|QID|804|M|40.61,62.60|N|From Hana'zua.|
+C Thazz'ril's Pick|QID|6394|M|43.73,53.78|QO|1|N|Thazz'ril's Pick: 1/1|
+L Level 5|QID|804|LVL|5|N|You should be around level 5 by this point.|
+T Thazz'ril's Pick|QID|6394|M|44.63,68.66|N|To Foreman Thazz'ril.|
+T Sarkoth|QID|804|M|42.05,68.33|N|To Gornek.|
+]]
+
+end)
