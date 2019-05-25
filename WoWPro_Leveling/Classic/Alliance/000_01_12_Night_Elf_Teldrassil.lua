dd2d0b4ed73547ad811f7045d11da1aa3c56179d
Shururu Ruun <shururuun@gmail.com>
2019-05-25 14:27:47 +0200

    Classic: cleanup

--- WoWPro_Leveling/Classic/Alliance/000_01_12_Night_Elf_Teldrassil.lua
+++ WoWPro_Leveling/Classic/Alliance/000_01_12_Night_Elf_Teldrassil.lua
@@ -36,7 +36,6 @@ T The Woodland Protector|QID|458|M|57.75,45.02|N|To Tarindrella.|
 A The Woodland Protector|QID|459|M|57.75,45.02|N|From Tarindrella.|
 C The Woodland Protector|QID|459|M|55.91,45.79|N|There is a second camp further north, however the Grellkin are up to level 4.|
 T The Woodland Protector|QID|459|M|57.75,45.02|N|To Tarindrella.|
-
 A Webwood Venom|QID|916|M|57.81,41.65|N|From Gilshalan Windwalker.|
 C Webwood Venom|QID|916|S|N|Kill Spiders along the way.|
 C The Balanche of Nature|QID|457|S|N|Kill Mangy Nightsabers and Thistle Boars along the way.|
@@ -45,14 +44,12 @@ A A Friend in Need|QID|3519|M|54.60,32.99|N|From Iverron.|
 T A Friend In Need|QID|3519|M|60.90,41.97|N|To Dirania Silvershine.|
 A Iverron's Antidote|QID|3521|M|60.90,41.96|N|From Dirania Silvershine.|
 N Sell junk while you are near.|QID|916|M|59.31,41.09|N|You will need the bag space for quest items next.|
-
 C Iverron's Antidote|QID|3521|S|L|10639 7|N|Pick up Hyacinth Mushrooms along the way. They are brown and grow next to the trees.|
 C The Balanche of Nature|QID|457|M|62.38,38.36|US|N|Kill Mangy Nightsabers and Thistle Boars.|
 C Webwood Venom|QID|916|US|M|57.91,33.36|N|Kill Spiders for the Webwood Venom Sacs and Webwood Ichor.|
 C Iverron's Antidote|QID|3521|M|57.91,33.36|N|Kill Spiders until you get the Webwood Ichor.|
 C Moonpetal Lilies|QID|3521|M|57.69,37.59|L|10641 4|N|You should find plenty of Moonpetal Lilies at the lake.|
 C Iverron's Antidote|QID|3521|US|L|10639 7|N|Finish picking up Hyacinth Mushrooms.|
-
 T Webwood Venom|QID|916|M|57.81,41.65|N|To Gilshalan Windwalker.|
 A Webwood Egg|QID|917|M|57.81,41.65|N|From Gilshalan Windwalker.|
 T The Balance of Nature|QID|457|M|58.69,44.27|N|To Conservator Ilthalaine.|
@@ -60,20 +57,17 @@ N Sell junk while you are near.|QID|3521|M|59.31,41.09|N|Also visit your class t
 T Iverron's Antidote|QID|3521|M|60.90,41.96|N|To Dirania Silvershine.|
 A Iverron's Antidote|QID|3522|M|60.90,41.96|N|This quest is on a 5 minite timer.|
 T Iverron's Antidite|QID|3522|M|54.60,32.99|N|To Iverron.|
-
 R Enter the spider cave|QID|917|M|56.77,31.72;56.66,29.33;56.87,27.24;55.72,25.12;56.43,25.14|CC|N|Take the middle path and then left.|
 C Webwood Egg|QID|917|M|56.750,26.46|N|Loot an egg from the ground.|
 H Shadowglen|QID|917|N|Hearthstone out of the cave.|
 T Webwood Egg|QID|917|M|57.81,41.65|N|To Gilshalan Windwalker.|
 A Tenaron's Summons|QID|920|M|57.81,41.65|N|From Gilshalan Windwalker.|
-
 T Tenaron's Summons|QID|920|M|59.10,39.44|N|Run up the ramp outside the tree.|
 A Crown of the Earth|QID|921|M|59.06,39.44|N|From Tenaron Stormgrip.|
 N Sell junk, repair, restock if needed.|QID|921|M|59.31,41.09|N|You can jump over the fence and drop down from the roof.|CC|
 C Crown of the Earth|QID|921|M|59.94,33.07|U|5185|N|Use the crystal phial at the moonwell.|
 T Crown of the Earth|QID|921|M|57.53,41.64;59.06,39.44|N|To Tenaron Stormgrip.|CC|
-A Crown of the Earth |QID|928|M|59.06,39.44|N|From Tenaron Stormgrip.|
-
+A Crown of the Earth|QID|928|M|59.06,39.44|N|From Tenaron Stormgrip.|
 A In Favor of Elune|QID|5622|C|Priest|M|59.96,41.95;59.18,40.45|N|Revisit your class trainer before you go.|CC|
 A Dolanaar Delivery|QID|2159|M|61.16,47.64|N|From Porthannius on the way out|
 
@@ -83,10 +77,9 @@ A Denalan's Earth|QID|997|M|56.09,57.72|N|From Sylal Bladeleaf.|
 A A Troubling Breeze|QID|475|M|55.96,57.28|N|From Athridas Bearmantle.|
 T In Favor of Elune|QID|5622|C|Priest|M|55.56,56.75|To Laurna Morninglight.|
 T Garments of the Moon|QID|5622|C|Priest|M|55.56,56.75|From Laurna Morninglight.|
-N First Aid|QID|997|M|55.29,56.82|N|You can learn first aid from the trainer Byancle.|RECIPE|3275|
+N First Aid|QID|928|M|55.29,56.82|N|You can learn first aid from the trainer Byancle.|RECIPE|3275|
 A The Emerald Dreamcatcher|QID|2438|M|55.58,56.95|N|At the top of the building from Tallonkai Swiftroot.|
 A Twisted Hatred|QID|932|M|55.58,56.95|N|From Tallonkai Swiftroot.|
-
 T Dolanaar Delivery|QID|2159|M|55.70,59.80|N|To the innkeeper.|
 h Dolanaar |QID|932|N|Set your Hearthstone to Dolanaar.|
 
