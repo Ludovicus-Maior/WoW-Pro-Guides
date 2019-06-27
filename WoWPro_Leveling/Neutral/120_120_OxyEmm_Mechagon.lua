b8ad60e68a89827556da8399442c1484ee4245d2
Ludovicus <ludovicus.maior@gmail.com>
2019-06-27 17:47:52 -0400

    Add Mechagon

---
+++ WoWPro_Leveling/Neutral/120_120_OxyEmm_Mechagon.lua
@@ -0,0 +1,113 @@
+
+local guide = WoWPro:RegisterGuide('Mechagon', 'Leveling', 'Mechagon', 'Oxycontin+Emmaleah', 'Neutral')
+WoWPro:GuideLevels(guide,120, 120)
+WoWPro:GuideNextGuide(guide, '')
+WoWPro:GuideSteps(guide, function()
+return [[
+
+N Draft!|ACTIVE|57009|N|This guide is under construction. We apologize for any missing quests, fires, or other hazards.|
+N Alliance|ACTIVE|57009|N|Currently this is an alliance guide, but It will be converted soon to a neutral guide. (i.e. it will show up for horde, but don't go to Boralas to get quests!!!)
+
+T Fame Waits for Gnome One|QID|57009|M|67.42,15.78|Z|Boralus|N|To Tinkmaster Overspark.|
+A The Legend of Mechagon|QID|54088|M|67.42,15.78|Z|Boralus|N|From Tinkmaster Overspark.|
+A Trogg Tromping|QID|53346|M|52.88,28.76|Z|Tiragarde Sound|
+T The Legend of Mechagon|QID|54088|M|65.63,65.04|Z|Tiragarde Sound|N|To Tinkmaster Overspark.|
+A Looking Inside|QID|55040|M|65.63,65.04|Z|Tiragarde Sound|N|From Tinkmaster Overspark.|
+C Looking Inside|QID|55040|M|65.91,66.20|Z|Tiragarde Sound|QO|1|N|1/1 Enter the vault|
+T Looking Inside|QID|55040|M|65.86,66.32|Z|Tiragarde Sound|N|To Tinkmaster Overspark.|
+A Let's Get It Started|QID|54945|M|65.86,66.32|Z|Tiragarde Sound|N|From Tinkmaster Overspark.|
+C Let's Get It Started|QID|54945|M|65.69,66.47|Z|Tiragarde Sound|QO|1|N|1/1 Gears repaired|
+C Let's Get It Started|QID|54945|M|65.99,65.88|Z|Tiragarde Sound|QO|2|N|1/1 Activate Electrode|
+C Let's Get It Started|QID|54945|M|66.11,66.23|Z|Tiragarde Sound|QO|3|N|1/1 Attach Alpha Wire to Alpha Pylon|
+C Let's Get It Started|QID|54945|M|65.94,66.38|Z|Tiragarde Sound|QO|4|N|1/1 Attach Beta Wire to Beta Pylon|
+T Let's Get It Started|QID|54945|M|65.84,66.37|Z|Tiragarde Sound|N|To Tinkmaster Overspark.|
+A You Must be This Height|QID|54087|M|65.84,66.37|Z|Tiragarde Sound|N|From Tinkmaster Overspark.|
+C You Must be This Height|QID|54087|M|65.82,66.32|Z|Tiragarde Sound|QO|1|N|1/1 Speak with Tinkmaster Overspark|
+C You Must be This Height|QID|54087|M|65.82,66.32|Z|Tiragarde Sound|QO|2|N|1/1 Purpose of device discovered|
+C You Must be This Height|QID|54087|M|65.88,66.39|Z|Tiragarde Sound|QO|3|N|1/1 Tri-dimensional Coordinates collected|
+T You Must be This Height|QID|54087|M|65.84,66.34|Z|Tiragarde Sound|N|To Tinkmaster Overspark.|
+A Report to Gila|QID|54946|M|65.84,66.34|Z|Tiragarde Sound|
+T Report to Gila|QID|54946|M|67.31,16.04|Z|Boralus|N|To Gila Crosswires.|
+A A Small Team|QID|54947|M|67.31,16.04|Z|Boralus|N|From Gila Crosswires.|
+C A Small Team|QID|54947|M|64.07,33.64|Z|Boralus|QO|1|N|1/1 Access Rope used (Optional)|
+C A Small Team|QID|54947|M|64.95,32.70|Z|Boralus|QO|2|N|1/1 Sapphronetta Flivvers recruited|
+C A Small Team|QID|54947|M|65.03,32.82|Z|Boralus|QO|3|N|1/1 Grizzek Fizzwrench recruited|
+C A Small Team|QID|54947|M|51.22,18.96|Z|Boralus|QO|4|N|1/1 Cog Captain Winklespring recruited|
+C A Small Team|QID|54947|M|43.16,31.31|Z|Boralus|QO|5|N|1/1 Reach the departure point|
+T A Small Team|QID|54947|M|43.24,31.97|Z|Boralus|N|To Gila Crosswires.|
+A The Start of Something Bigger|QID|54992|M|43.24,31.97|Z|Boralus|N|From Gila Crosswires.|
+C The Start of Something Bigger|QID|54992|M|42.77,31.84|Z|Boralus|QO|1|N|1/1 Expedition Gyrocopter boarded|
+C The Start of Something Bigger|QID|54992|M|78.37,40.61|Z|Mechagon Island|QO|2|N|1/1 Arrive at Mechagon|
+f Overspark Expedition Camp|QID|54992|M|77.93,40.84|Z|Mechagon Island|N|At Cog Captain Winklespring.|
+T The Start of Something Bigger|QID|54992|M|77.74,40.50|Z|Mechagon Island|N|To Tinkmaster Overspark.|
+A Princely Visit|QID|55645|M|77.74,40.50|Z|Mechagon Island|N|From Tinkmaster Overspark.|
+C Princely Visit|QID|55645|M|71.22,38.23|Z|Mechagon Island|QO|1|N|1/1 Rustbolt explored|
+A A Worthy Ally|QID|57006|M|71.22,38.23|Z|Mechagon Island|
+T Princely Visit|QID|55645|M|71.18,38.26|Z|Mechagon Island|N|To Prince Erazmin.|
+A The Resistance Needs YOU!|QID|55729|M|71.18,38.26|Z|Mechagon Island|N|From Prince Erazmin.|
+C The Resistance Needs YOU!|QID|55729|M|71.05,36.99|Z|Mechagon Island|QO|1|N|1/1 Ride with Prince Erazmin|
+T The Resistance Needs YOU!|QID|55729|M|59.13,55.00|Z|Mechagon Island|N|To Prince Erazmin.|
+A Rescuing the Resistance|QID|55730|M|59.13,55.00|Z|Mechagon Island|N|From Prince Erazmin.|
+A My Father's Armies|QID|55731|M|59.13,55.00|Z|Mechagon Island|N|From Prince Erazmin.|
+A We Can Fix It|QID|55995|M|56.83,60.00|Z|Mechagon Island|
+C Rescuing the Resistance|QID|55730|M|56.71,62.95|Z|Mechagon Island|QO|1|N|1/1 Scientists rescued|
+C My Father's Armies|QID|55731|M|56.22,59.40|Z|Mechagon Island|QO|1|N|12/12 Mechagon enemies slain|
+T My Father's Armies|QID|55731|M|55.45,59.11|Z|Mechagon Island|N|To Prince Erazmin.|
+C Rescuing the Resistance|QID|55730|M|53.49,55.70|Z|Mechagon Island|QO|3|N|1/1 Engineers rescued|
+C Rescuing the Resistance|QID|55730|M|52.20,63.26|Z|Mechagon Island|QO|2|N|1/1 Tinkerers rescued|
+T Rescuing the Resistance|QID|55730|M|52.11,62.55|Z|Mechagon Island|N|To Prince Erazmin.|
+C We Can Fix It|QID|55995|M|52.41,61.71|Z|Mechagon Island|QO|2|N|1/1 Collect Energy Cell from Mechanized Chest|
+C We Can Fix It|QID|55995|M|56.82,59.72|Z|Mechagon Island|QO|1|N|40/40 Spare Parts|
+T We Can Fix It|QID|55995|M|56.68,59.53|Z|Mechagon Island|N|To Prince Erazmin.|
+A Drill Rig Construction|QID|55734|M|56.68,59.53|Z|Mechagon Island|N|From Prince Erazmin.|
+C Drill Rig Construction|QID|55734|M|56.71,59.80|Z|Mechagon Island|QO|1|N|1/1 Construction project viewed|
+C Drill Rig Construction|QID|55734|M|56.71,59.80|Z|Mechagon Island|QO|2|N|40/40 Spare Parts contributed|
+C Drill Rig Construction|QID|55734|M|56.71,59.80|Z|Mechagon Island|QO|3|N|1/1 Energy Cell contributed|
+C Drill Rig Construction|QID|55734|M|56.71,59.80|Z|Mechagon Island|QO|4|N|1/1 Drill Rig construction complete|
+C Drill Rig Construction|QID|55734|M|56.73,60.05|Z|Mechagon Island|QO|5|N|1/1 Pascal-K1N6 rescued|
+T Drill Rig Construction|QID|55734|M|56.97,60.08|Z|Mechagon Island|N|To Prince Erazmin.|
+A Send My Father a Message|QID|55096|M|56.97,60.08|Z|Mechagon Island|N|From Prince Erazmin.|
+C Send My Father a Message|QID|55096|M|60.82,60.54|Z|Mechagon Island|QO|2|N|1/1 HK-8 Aerial Oppression Unit repelled|
+T Send My Father a Message|QID|55096|M|71.26,35.92|Z|Mechagon Island|N|To Prince Erazmin.|
+A Welcome to the Resistance|QID|55736|M|71.26,35.92|Z|Mechagon Island|N|From Prince Erazmin.|
+C Welcome to the Resistance|QID|55736|M|71.44,33.88|Z|Mechagon Island|QO|1|N|1/1 Tour Rustbolt with Prince Erazmin|
+T Welcome to the Resistance|QID|55736|M|74.09,36.92|Z|Mechagon Island|N|To Prince Erazmin.|
+A Security First|QID|56131|M|73.35,34.73|Z|Mechagon Island|N|From Prince Erazmin.|
+A Junkyard Tinkering and You|QID|55101|M|71.26,32.64|Z|Mechagon Island|N|From Pascal.|
+A Upgraded|QID|55708|M|69.92,32.37|Z|Mechagon Island|N|From Christy Punchcog.|
+C Junkyard Tinkering and You|QID|55101|M|71.41,32.35|Z|Mechagon Island|QO|1|N|1/1 Scrap Grenades crafted at Pascal-K1N6|
+T Junkyard Tinkering and You|QID|55101|M|71.41,32.35|Z|Mechagon Island|N|To Pascal.|
+A Shop Project|QID|55608|M|71.55,38.65|Z|Mechagon Island|N|From Recycler Kerchunk.|
+C Shop Project|QID|55608|M|63.44,42.99|Z|Mechagon Island|QO|1|N|1/1 Mechanobot Ignition|
+A Blueprint: Mechanocat Laser Pointer|QID|55056|M|63.44,42.99|Z|Mechagon Island|N|From Cork Stuttguard.|
+C Shop Project|QID|55608|M|80.04,55.67|Z|Mechagon Island|QO|2|N|1/1 Shellsteel Casing|
+C Shop Project|QID|55608|M|86.68,28.32|Z|Mechagon Island|QO|3|N|1/1 Multi-Leg Chaindrive|
+C Upgraded|QID|55708|M|87.13,16.60|Z|Mechagon Island|QO|1|N|20/20 Microcogs collected|
+T Shop Project|QID|55608|M|71.52,38.67|Z|Mechagon Island|N|To Recycler Kerchunk.|
+T Blueprint: Mechanocat Laser Pointer|QID|55056|M|71.30,32.38|Z|Mechagon Island|N|To Pascal-K1N6.|
+T Upgraded|QID|55708|M|69.81,32.44|Z|Mechagon Island|N|To Christy Punchcog.|
+A First One's Free|QID|55707|M|69.81,32.44|Z|Mechagon Island|N|From Christy Punchcog.|
+C First One's Free|QID|55707|M|51.20,28.67|Z|Mechagon Island|QO|1|N|1/1 Punchcard Satchel recovered|
+T First One's Free|QID|55707|M|69.89,32.46|Z|Mechagon Island|N|To Christy Punchcog.|
+A Collaborative Construction|QID|55153|M|73.10,33.56|Z|Mechagon Island|N|From Waren Gearhart.|
+A Batteries Not Included|QID|55210|M|70.95,38.96|Z|Mechagon Island|N|From Flip Quickcharge.|
+C Batteries Not Included|QID|55210|M|73.30,34.21|Z|Mechagon Island|QO|1|N|10/10 Damaged Energy Cell|
+T Batteries Not Included|QID|55210|M|70.93,38.84|Z|Mechagon Island|N|To Flip Quickcharge.|
+A Your First Charge is Free!|QID|56320|M|70.93,38.84|Z|Mechagon Island|N|From Flip Quickcharge.|
+C Collaborative Construction|QID|55153|M|66.54,35.26|Z|Mechagon Island|QO|1|N|1/1 Contribute to a construction project|
+C Your First Charge is Free!|QID|56320|M|61.17,37.82|Z|Mechagon Island|QO|1|N|1/1 Find Charging Station|
+T Collaborative Construction|QID|55153|M|73.08,33.51|Z|Mechagon Island|N|To Waren Gearhart.|
+C Your First Charge is Free!|QID|56320|M|61.30,37.41|Z|Mechagon Island|QO|2|N|1/1 Access bought from Flux|
+C Your First Charge is Free!|QID|56320|M|61.30,37.41|Z|Mechagon Island|QO|3|N|1/1 Energy Cell charged|
+T Your First Charge is Free!|QID|56320|M|70.62,38.47|Z|Mechagon Island|N|To Pristy Quickcharge.|
+A Fishing For Something Bigger|QID|55298|M|36.98,47.02|Z|Mechagon Island|N|From Danielle Anglers.|
+A Blueprint: Experimental Adventurer Augment|QID|56087|M|35.28,42.58|Z|Mechagon Island|
+C Fishing For Something Bigger|QID|55298|M|48.82,37.50|Z|Mechagon Island|QO|3|N|1/1 Suction Tube|
+A Ascending Alchemy|QID|48016|M|36.52,63.35|Z|Mechagon Island|
+T Blueprint: Experimental Adventurer Augment|QID|56087|M|71.33,32.28|Z|Mechagon Island|N|To Pascal-K1N6.|
+]]
+
+end)
+
+
+
