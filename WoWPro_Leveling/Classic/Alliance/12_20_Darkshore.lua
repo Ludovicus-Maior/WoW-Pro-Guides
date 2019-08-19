491e3d277e14789d2592fc256edb8dd1f01120eb
Emmaleah <emmaleah.wow@gmail.com>
2019-08-18 22:05:53 -0700

    Update 12_20_Darkshore.lua
    and remove the fixme from the header.

--- WoWPro_Leveling/Classic/Alliance/12_20_Darkshore.lua
+++ WoWPro_Leveling/Classic/Alliance/12_20_Darkshore.lua
@@ -1,9 +1,6 @@
 -- WoWPro Guides by "The WoW-Pro Community" are licensed under a Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License.
 -- Permissions beyond the scope of this license may be available at http://www.wow-pro.com/License.
 
--- FIXME:
--- Step C [Gathering the Cure:6123] in ClassicDarkshore1220 is missing a CS¦CC¦CN tag.
-
 local guide = WoWPro:RegisterGuide('ClassicDarkshore1220', 'Leveling', 'Darkshore', 'Hairbo', 'Alliance', 1)
 WoWPro:GuideName(guide, 'Darkshore 12-20')
 WoWPro:GuideLevels(guide, 12, 20, 13)
