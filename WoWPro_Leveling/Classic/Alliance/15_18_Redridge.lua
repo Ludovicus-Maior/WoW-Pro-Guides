93953cdef8fa0638f7471451abb123aadbc90237
Emmaleah <emmaleah.wow@gmail.com>
2019-09-02 16:39:38 -0700

    Update 15_18_Redridge.lua
    edits per comments on Discord and my own starting of the zone.

--- WoWPro_Leveling/Classic/Alliance/15_18_Redridge.lua
+++ WoWPro_Leveling/Classic/Alliance/15_18_Redridge.lua
@@ -6,23 +6,25 @@ WoWPro:GuideName(guide, 'Redridge 15-18')
 WoWPro:GuideLevels(guide, 15, 18, 16)
 WoWPro:GuideNextGuide(guide, 'ClassicWestfall1820')
 WoWPro:GuideSteps(guide, function () return [[
+;-------------------------- incorporated into Elywn guide-----------------------------------------
 R Elwynn Forest|QID|244|N|Go to Elwynn Forest|Z|Stormwind City|M|76.00,94.00|
 R Redridge Mountains|QID|244|N|Go to Redridge Mountains|Z|Elwynn Forest|M|93.00,72.00|
-A Encroaching Gnolls|QID|244|M|15.3,71.5|
-T Encroaching Gnolls|QID|244|M|30.7,60.0|
-A Assessing the Threat|QID|246|M|30.7,60.0|
-f Lakeshire|QID|65|N|Get the flight path.|M|30.60,59.40|
-A The Lost Tools|QID|125|M|32.1,48.6|
-A Elmore's Task|N||QID|1097|M|31.00,47.30|
-A The Price of Shoes|QID|118|M|31.0,47.3|
-A Messenger to Stormwind|QID|120|
-A A Free Lunch|QID|129|M|26.6,44.3|
-A Dry Times|QID|116|
-T The Defias Brotherhood |QID|65|M|26.5,45.3|
-A The Defias Brotherhood |QID|132|M|26.5,45.3|
-A Redridge Goulash|QID|92|M|22.7,43.8|
-A Hilary's Necklace|QID|3741|M|29.3,53.6|
-C Hilary's Necklace|N|Loot the necklace from the Glinting Mud on the ground in the water.  It may also be at 24, 54|QID|3741|M|31.00,54.30|
+A Encroaching Gnolls|QID|244|M|15.3,71.5|N|From Guard Parker.|
+T Encroaching Gnolls|QID|244|M|30.7,60.0|N|To Deputy Feldon.|
+A Assessing the Threat|QID|246|M|30.7,60.0|N|From Deputy Feldon.|
+f Lakeshire|QID|65|N|Get the flight path.|M|30.60,59.40|N|At Ariena Stormfeather.|
+;------------------end duplicated content (Westfall ends with flight to  Lakeshire )----------------
+A The Lost Tools|QID|125|M|32.1,48.6|N|From Foreman Oslo.|
+A Elmore's Task|QID|1097|M|31.00,47.30|N|From Verner Osgood.|
+A The Price of Shoes|QID|118|M|31.0,47.3|N|From Verner Osgood.|
+A Messenger to Stormwind|QID|120|M|29.98,44.46|N|From Magistrate Solomon, inside the town hall.|
+A A Free Lunch|QID|129|M|26.6,44.3|N|From Darcy, in the inn.|
+A Dry Times|QID|116|N|From Barkeep Daniels.|
+T The Defias Brotherhood |QID|65|M|26.5,45.3|N|To Wiley the Black, upstairs in the inn.|
+A The Defias Brotherhood |QID|132|M|26.5,45.3|N|From Wiley the Black.|
+A Redridge Goulash|QID|92|M|22.7,43.8|N|From Chef Breanna.|
+A Hilary's Necklace|QID|3741|M|29.3,53.6|N|From Shawn at the end of the dock.|
+C Hilary's Necklace|N|Loot the necklace from the Glinting Mud on the ground in the water.|QID|3741|M|31.00,54.30;24,54|CN|
 T A Free Lunch|QID|129|M|15.3,71.5|
 A Visit the Herbalist|QID|130|M|15.3,71.5|
 C Assessing the Threat|N|Kill mobs until you finish this quest.|QID|246|M|29.00,83.00|
@@ -31,7 +33,7 @@ C The Lost Tools|N|Loot the tools from the chest at the bottom of the lake.|QID|
 N Tough Condor Meat|QID|92|N|Kill Condors until you get 5 Tough Condor Meat|L|1080 5|M|46.00,76.00|
 C Redridge Goulash|N|Kill Goretusks and other mobs until you get the rest of the items for this quest.|QID|92|M|34.00,71.00|
 T Assessing the Threat|QID|246|M|30.7,60.0|
-T The Lost Tools|QID|125|M|32.1,48.6|
+T The Lost Tools|QID|125|M|32.11,48.61|N|To Forman Oslow.|
 T Hilary's Necklace|QID|3741|M|29.3,53.6|
 T Redridge Goulash|QID|92|M|22.7,43.8|
 T Visit the Herbalist|QID|130|M|21.9,46.3|
@@ -45,13 +47,14 @@ T The Price of Shoes|QID|118|M|41.7,65.5|
 A Return to Verner|QID|119|M|41.7,65.5|
 N Skin of Sweet Rum|QID|116|N|Buy a Skin of Sweet Rum from Barkeep Dobbins|L|1939 1|Z|Elwynn Forest|M|44.00,65.70|
 T Messenger to Stormwind |N||QID|120|Z|Stormwind City|M|69.15,82.70|
-A Messenger to Stormwind |QID|121|Z|Stormwind City|
-N Cask of Merlot|QID|116|N|Buy a Cask of Merlot from Roberto Pupellyverbos in the Southwest part of the Trade District|L|1941 1|Z|Stormwind City|M|60.00,76.90|
-A Oh Brother. . .|N||QID|167|M|70.30,40.85|
-A Collecting Memories|N||QID|168|M|70.30,40.85|
-A Underground Assault|N||QID|2040|M|62.65,34.10|
-T Elmore's Task|N||QID|1097|M|59.70,33.80|
-A Stormpike's Delivery|N||QID|353|M|59.70,33.80|
-F Westfall|N|Fly to Westfall|Z|Stormwind City|M|71.00,72.50|
+A Messenger to Stormwind |QID|121|Z|Stormwind City|M|69.15,82.70|
+N Cask of Merlot|QID|116|N|Buy a Cask of Merlot from Roberto Pupellyverbos in the Southwest part of the Trade District|L|1941 1|Z|Stormwind City|M|52.11,67.88|
+A Oh Brother. . .|N||QID|167|M|65.35,21.30|Z|Stormwind City|
+A Collecting Memories|N||QID|168|M|65.35,21.30|Z|Stormwind City|
+A Underground Assault|N||QID|2040|M|62.65,34.10|Z|Stormwind City|
+T Elmore's Task|N||QID|1097|M|59.70,33.80|Z|Stormwind City|
+A Stormpike's Delivery|N||QID|353|M|59.70,33.80|Z|Stormwind City|
+
+H Sentinel Hill|N|Hearth (or fly) to Westfall|Z|Stormwind City|M|71.00,72.50|
 ]]
 end)
