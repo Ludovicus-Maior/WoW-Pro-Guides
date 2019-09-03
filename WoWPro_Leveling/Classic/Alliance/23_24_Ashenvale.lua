867bfb41d972ea32fdc9763dc5a4e0c89f0f2ef3
elidion <elidion@yahoo.com>
2019-09-02 23:19:13 -0700

    Update 23_24_Ashenvale.lua
    Guide overhaul, still not complete but wanted to get my work out there for updates. Bottom half of guide is still original and unverified/tested.

--- WoWPro_Leveling/Classic/Alliance/23_24_Ashenvale.lua
+++ WoWPro_Leveling/Classic/Alliance/23_24_Ashenvale.lua
@@ -2,37 +2,56 @@
 -- Permissions beyond the scope of this license may be available at http://www.wow-pro.com/License.
 
 local guide = WoWPro:RegisterGuide('ClassicAshenvale2324', 'Leveling', 'Ashenvale', 'Boston', 'Alliance', 1)
-WoWPro:GuideName(guide, 'Ashenvale 23-24')
-WoWPro:GuideLevels(guide, 23, 24, 23)
+WoWPro:GuideName(guide, 'Ashenvale 21-24')
+WoWPro:GuideLevels(guide, 21, 24, 23)
 WoWPro:GuideNextGuide(guide, 'ClassicStonetalon2424')
 WoWPro:GuideSteps(guide, function() return [[
-R Ashenvale|QID|1133|N|Run south to Ashenvale.|Z|Darkshore|M|44.25,96.17|
-A Bathran's Hair|QID|1010|M|26.4,38.6|
-R Astranaar|QID|1133|M|34.39,48.03|
 f Astranaar|QID|1133|N|Get the flight path.|M|34.39,48.03|
-T Journey to Astranaar|QID|1133|
-A The Zoram Strand|QID|1008|
-A On Guard in Stonetalon|QID|1070|M|34.9,49.8|
-A Raene's Cleansing |QID|991|M|36.6,49.6|
-A Culling the Threat|QID|1054|M|36.6,49.6|
+A The Zoram Strand|QID|1008|M|34.68,48.84|Z|Ashenvale|N|From Shindrell Swiftfire. This is a pre-req for the stonetalon quests.|
+A Raene's Cleansing |QID|991|M|36.6,49.6|N|From Raene Wolfrunner|
 h Astranaar|QID|991|N|Make Astranaar your home location.|M|36.97,49.26|
+A The Tower of Althalaxx|QID|970|M|26.2,38.7|Z|Ashenvale|N|From Delgren the Purifier.|PRE|967|
+C The Tower of Althalaxx|QID|970|M|30.38,31.77|Z|Ashenvale|N|Kill the Dark Strand until you get the Flowing Soul Gem.|
+T The Tower of Althalaxx|QID|970|M|26.2,38.7|Z|Ashenvale|N|To Delgren the Purifier.|
+A The Tower of Althalaxx|QID|973|M|26.2,38.7|Z|Ashenvale|N|From Delgren the Purifier.|PRE|970|
+T Raene's Cleansing|QID|991|M|20.3,42.3|N|To Teronis' Corpse beware of the murlocs around.|
+A Raene's Cleansing|QID|1023|M|20.3,42.3|N|From Teronis' Corpse.|PRE|991|
+C Raene's Cleansing|QID|1023|M|20.30,44.40|N|Kill saltspittle murlocs until you get a Glowing Gem. They are all around the pool.|
+C The Zoram Strand|QID|1008|M|14.10,20.7|Z|Ashenvale|N|Kill Wrathtail Nagas until you get 20 Wrathtail Heads.|
+T The Zoram Strand|QID|1008|M|34.68,48.84|Z|Ashenvale|N|To Shindrell Swiftfire.|
+A Pridewings of Stonetalon|QID|1134|M|34.68,48.84|Z|Ashenvale|N|From Shindrell Swiftfire.|PRE|1008|
+A On Guard in Stonetalon|QID|1070|M|34.90,49.79|Z|Ashenvale|N|From Sentinel Thenysil.|
+A Journey to Stonetalon Peak|QID|1056|M|35.76,49.11|Z|Ashenvale|N|From Faldreas Goeth'Shael.|
+T Raene's Cleansing|QID|1023|M|36.6,49.6|N|To Raene Wolfrunner.|
+A Raene's Cleansing|QID|1024|M|36.6,49.6|N|From Raene Wolfrunner.|PRE|1023|
+R Stonetalon Mountains|QID|1070|CN|M|43,71.52;42.3,71.09|Z|Ashenvale|N|Head to the Stonetalon cave.|
+R Windshear Crag|QID|1070|M|76,46.21|Z|Stonetalon Mountains|N|Follow the path with lanterns throught the cave.|
+A Super Reaper 6000|QID|1093|M|59.0,62.6|Z|Stonetalon Mountains|N|From Ziz Fizziks.|
+T On Guard in Stonetalon |QID|1070|M|59.90,66.84|Z|Stonetalon Mountains|N|To Kaela Shadowspear.|
+A On Guard in Stonetalon |QID|1085|M|59.90,66.84|Z|Stonetalon Mountains|N|From Kaela Shadowspear.|
+T On Guard in Stonetalon |QID|1085|M|59.52,67.14|Z|Stonetalon Mountains|N|To Gaxim Rustfizzle.|
+A A Gnome's Respite|QID|1071|M|59.52,67.14|Z|Stonetalon Mountains|N|From Gaxim Rustfizzle.|
+C Pridewings of Stonetalon|QID|1134|S|M|55,63|Z|Stonetalon Mountains|N|Kill Pridewing Wyverns as you go.|
+C Super Reaper 6000|QID|1093|M|62.70,54.01|Z|Stonetalon Mountains|S|N|Kill Venture co. Operators until you get the blueprints needed for Super Reaper 6000.|
+C A Gnome's Respite|QID|1071|M|69.5,54.5|Z|Stonetalon Mountains|N|Kill Loggers and Deforesters around the area.|
+C Super Reaper 6000|QID|1093|M|62.70,54.01|Z|Stonetalon Mountains|US|N|If for some reason you still haven't found it, keep killing Venture Co. until drops.|
+T Super Reaper 6000|QID|1093|M|59.0,62.6|Z|Stonetalon Mountains|N|To Ziz Fizziks. Don't get the follow up quest.|
+T A Gnome's Respite|QID|1071|M|59.52,67.14|Z|Stonetalon Mountains|N|To Gaxim Rustfizzle. Don't get the follow up.|
+C Pridewings of Stonetalon|QID|1134|US|M|50.43,45.78|Z|Stonetalon Mountains|N|Kill Pridewing Wyverns to finish this up.|
+T Journey to Stonetalon Peak|QID|1056|M|37.11,8.10|Z|Stonetalon Mountains|N|To Keeper Albagorm. Kill Wyverns on the way.|
+f Stonetalon Peak|QID|1134|M|36.44,7.18|Z|Stonetalon Mountains|N|Grab the flightpath from Teloren|
+F Astranaar|QID|1134|M|36.44,7.18|Z|Stonetalon Mountains|N|Fly back to Astranaar.|
+T Pridewings of Stonetalon|QID|1134|M|34.68,48.84|Z|Ashenvale|N|To Shindrell Swiftfire.|
+A An Aggressive Defense|QID|1025|M|36.6,49.6|N|From Raene Wolfrunner.|PRE|1023|
+A Culling the Threat|QID|1054|M|36.6,49.6|N|From Raene Wolfrunner|
+
+A Bathran's Hair|QID|1010|M|26.4,38.6|
 C Culling the Threat|N|Kill Dal Bloodclaw and loot his skull. He wanders between the furbolg camps, follow the arrows and you should find him. (39.3,36.6)|QID|1054|M|35.3,32.4|
 R Follow the arrows 1|QID|991|N|Run to a small path through the trees. Close this step once you reach the final arrow. (35.83,27.81)|M|35.59,30.68|
 C Bathran's Hair|N|Loot five Bathran's Hair from Plant Bundles off the ground in this area. They are very small brown bags, and they don't gleam, so look for them carefully. (32.2,21.7)(32.5,21)|QID|1010|M|31.1,24.6|
-
-C The Zoram Strand|N|Kill Wrathtail Nagas until you get 20 Wrathtail Heads (14.5,23)(14.8,25.2)(11.85,26)|QID|1008|M|14.10,20.7|
-C Researching the Corruption|N|Go down to Blackfathom Deeps, explore the tunnels and kill Nagas and Satyrs here until you finish this quest. Do not go inside the instance portal.|QID|1275|M|14.24,14.64|
-T Raene's Cleansing|QID|991|M|20.3,42.3|
-A Raene's Cleansing |QID|1023|M|20.3,42.3|
-C Raene's Cleansing |N|Kill saltspittle murlocs until you get a Glowing Gem. They are all around the pool.|QID|1023|M|20.30,44.40|
 T Bathran's Hair|QID|1010|M|26.4,38.6|
 A Orendil's Cure|QID|1020|M|26.4,38.6|
-T The Zoram Strand|M|25.57,38.55;27,47;34.7,48.9|CS|
-A Pridewings of Stonetalon|QID|1134|M|34.7,48.8|
 A Kayneth Stillwind|QID|4581|M|34.7,48.8|
-T Raene's Cleansing |QID|1023|M|36.6,49.6|
-A Raene's Cleansing |QID|1024|M|36.6,49.6|
-A An Aggressive Defense|QID|1025|M|36.6,49.6|
 T Culling the Threat|QID|1054|M|36.6,49.6|
 T Orendil's Cure|QID|1020|M|37.4,51.8|
 A Elune's Tear|N|This quest becomes available about 20 seconds after turning in the previous one.|QID|1033|M|37.4,51.8|
