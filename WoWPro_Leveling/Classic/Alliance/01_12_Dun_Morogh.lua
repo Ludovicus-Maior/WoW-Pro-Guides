30ff1a4cf63695584b93e5346b769e97c4953753
Hendo72 <39675478+Hendo72@users.noreply.github.com>
2019-09-03 03:52:21 -0400

    Update 01_12_Dun_Morogh.lua
    Overhauled the guide from Coldridge Valley to Kharanos. Need to run a Priest to test class quest.

--- WoWPro_Leveling/Classic/Alliance/01_12_Dun_Morogh.lua
+++ WoWPro_Leveling/Classic/Alliance/01_12_Dun_Morogh.lua
@@ -12,10 +12,11 @@ WoWPro:GuideNextGuide(guide, 'ClassicLochModan')
 --        "Interface\\Icons\\Achievement_Character_Dwarf_Female"))
 WoWPro:GuideSteps(guide, function() return [[
 A Dwarven Outfitters|QID|179|M|29.92,71.21|N|From Sten Stoutarm|
-C Dwarven Outfitters|QID|179|M|29.80,74.30|N|Kill and loot the Ragged Young Wolves for the Tough Wolf Meat.|
-r Sell Junk|QID|179|ACTIVE|179|S|M|30.06,71.52|N|Take this opportunity to sell your junk with Adlin Pridedrift.|
+C Dwarven Outfitters|QID|179|M|29.80,74.30|QO|1||N|Kill Ragged Young Wolves to loot 8 Tough Wolf Meat.|T|Ragged Young Wolf|
+r Sell Junk|ACTIVE|179|M|30.06,71.52|N|Sell your junk to Adlin Pridedrift.|
 T Dwarven Outfitters|QID|179|M|29.92,71.21|N|To Sten Stoutarm|
 
+; Quest name changes depending upon your class
 A Encrypted Rune|QID|3109|C|Rogue|R|Dwarf|M|29.92,71.21|N|From Sten Stoutarm|
 A Etched Rune|QID|3108|C|Hunter|R|Dwarf|M|29.92,71.21|N|From Sten Stoutarm|
 A Hallowed Rune|QID|3110|C|Priest|R|Dwarf|M|29.92,71.21|N|From Sten Stoutarm|
@@ -28,37 +29,36 @@ A Simple Memorandum|QID|3112|C|Warrior|R|Gnome|M|29.92,71.21|N|From Sten Stoutar
 
 A Coldridge Valley Mail Delivery|QID|233|PRE|179|M|29.92,71.21|N|From Sten Stoutarm|
 A A New Threat|QID|170|PRE|179|M|29.71,71.25|N|From Balir Frosthammer|
-C A New Threat|QID|170|M|31.30,76.10|N|Kill the rockjaw troggs and Burly Rockjaw Troggs.|
+C A New Threat|QID|170|M|31.30,76.10|QO|1;2|N|Kill 6 Rockjaw Troggs and 6 Burly Rockjaw Troggs.|
 T A New Threat|QID|170|M|29.71,71.25|N|To Balir Frosthammer|
 
-T Encrypted Rune|QID|3109|C|Rogue|R|Dwarf|N|To Solm Hargrin. This is your class trainer, learn your Level 2 skill, then return here every couple of levels for your new skills.|
-T Etched Rune|QID|3108|C|Hunter|R|Dwarf|N|To Thorgas Grimson. This is your class trainer, learn your Level 2 skill, then return here every couple of levels for your new skills.|
-T Hallowed Rune|QID|3110|C|Priest|R|Dwarf|N|To Branstock Khalder. This is your class trainer, learn your Level 2 skill, then return here every couple of levels for your new skills.|
-T Simple Rune|QID|3106|C|Warrior|R|Dwarf|N|To Thran Khorman. This is your class trainer, learn your Level 2 skill, then return here every couple of levels for your new skills.|
-T Consecrated Rune|QID|3107|C|Paladin|R|Dwarf|N|To Bromos Grummner. This is your class trainer, learn your Level 2 skill, then return here every couple of levels for your new skills.|
-T Tainted Memorandum|QID|3115|C|Warlock|R|Gnome|M|28.65,66.14|N|To Alamar Grimm. This is your class trainer, learn your Level 2 skill, then return here every couple of levels for your new skills.|
-T Glyphic Memorandum|QID|3114|C|Mage|R|Gnome|N|To Marryk Nurribit. This is your class trainer, learn your Level 2 skill, then return here every couple of levels for your new skills.|
-T Encrypted Memorandum|QID|3113|C|Rogue|R|Gnome|N|To Solm Hargrin. This is your class trainer, learn your Level 2 skill, then return here every couple of levels for your new skills.|
-T Simple Memorandum|QID|3112|C|Warrior|R|Gnome|N|To Thran Khorman. This is your class trainer, learn your Level 2 skill, then return here every couple of levels for your new skills.|
+T Encrypted Rune|QID|3109|C|Rogue|R|Dwarf|M|28.37,67.51|N|To Solm Hargrin in Anvilmar.\nGrab your lv 2 training while you're here.|
+T Etched Rune|QID|3108|C|Hunter|R|Dwarf|M|29.17,67.45|N|To Thorgas Grimson in Anvilmar.\nGrab your lv 2 training while you're here.|
+T Hallowed Rune|QID|3110|C|Priest|R|Dwarf|M|28.60,66.38|N|To Branstock Khalder in Anvilmar.\nGrab your lv 2 training while you're here.|
+T Simple Rune|QID|3106|C|Warrior|R|Dwarf|M|28.83,67.24|N|To Thran Khorman in Anvilmar.\nGrab your lv 2 training while you're here.|
+T Consecrated Rune|QID|3107|C|Paladin|R|Dwarf|M|28.83,68.33|N|To Bromos Grummner.\nGrab your lv 2 training while you're here.|
+T Tainted Memorandum|QID|3115|C|Warlock|R|Gnome|M|28.65,66.14|N|To Alamar Grimm.\nGrab your lv 2 training while you're here.|
+T Glyphic Memorandum|QID|3114|C|Mage|R|Gnome|M|28.71,66.37|N|To Marryk Nurribit.\nGrab your lv 2 training while you're here.|
+T Encrypted Memorandum|QID|3113|C|Rogue|R|Dwarf|M|28.37,67.51|N|To Solm Hargrin in Anvilmar.\nGrab your lv 2 training while you're here.|
+T Simple Memorandum|QID|3112|C|Warrior|R|Gnome|M|28.83,67.24|N|To Thran Khorman in Anvilmar.\nGrab your lv 2 training while you're here.|
 
-L Level 3|QID|3361|LVL|3|N|You need to be level 3 to continue with this guide.|
+L Level 3|AVAILABLE|3361|LVL|3|N|You need to be level 3 to continue with this guide.|
 
 A A Refugee's Quandary|QID|3361|M|28.49,67.74|N|From Felix Whindlebolt|
+r Sell and Repair|ACTIVE|233|M|28.71,67.85|N|Repair and sell your junk to Rybrad Coldbank or Grundel Harkin.|
 
-T Coldridge Valley Mail Delivery|QID|233|M|22.60,71.43|N|To Talin Keeneye. Follow the road west.|
+T Coldridge Valley Mail Delivery|QID|233|M|28.66,70.45;22.60,71.43|CS|N|To Talin Keeneye. Exit Anvilmar and follow the road west.|
 A Coldridge Valley Mail Delivery|QID|234|PRE|233|M|22.60,71.43|N|From Talin Keeneye.|
 A The Boar Hunter|QID|183|M|22.60,71.43|N|From Talin Keeneye.|
-C The Boar Hunter|QID|183|M|22.0,72.0|S|N|Kill Small Crag Boar.|
+C The Boar Hunter|QID|183|M|22.0,72.0|S|N|Kill Small Crag Boar.|T|Small Crag Boar|
 T Coldridge Valley Mail Delivery|QID|234|M|25.08,75.71|N|To Grelin Whitebeard.|
 A The Troll Cave|QID|182|M|25.08,75.71|N|From Grelin Whitebeard.|
-C The Troll Cave|QID|182|M|26.3,78.8|S|N|Kill Frostmane Troll Whelp|
+C The Troll Cave|QID|182|M|26.3,78.8|S|N|Kill Frostmane Troll Whelp|T|Frostmane Troll Whelp|
 C Felix's Box|QID|3361|M|20.88,76.07|QO|1|NC|N|Loot Felix's Box from the ground in front of the tent.|
 C Felix's Chest|QID|3361|M|22.78,80.00|QO|2|NC|N|Loot Felix's Chest from the ground in front of the tent.|
 C Felix's Bucket of Bolts|QID|3361|M|26.33,79.28|QO|3|NC|N|Loot Felix's Bucket of Bolts from the ground outside the cave.|
+C The Troll Cave|QID|182|US|M|26.3,78.8|N|Kill Frostmane Troll Whelp|T|Frostmane Troll Whelp|
 
-C The Troll Cave|QID|182|US|M|26.3,78.8|N|Kill Frostmane Troll Whelp|
-C The Boar Hunter|QID|183|US|M|22.0,72.0|N|Kill Small Crag Boar.|
-T The Boar Hunter|QID|183|M|22.60,71.43|N|To Talin Keeneye.|
 T The Troll Cave|QID|182|M|25.08,75.71|N|To Grelin Whitebeard.|
 A The Stolen Journal|QID|218|PRE|182|M|25.08,75.71|N|From Grelin Whitebeard.|
 
@@ -67,27 +67,41 @@ L Level 4|QID|3364|LVL|4|N|You need to be level 4 to continue with this guide.|
 A Scalding Mornbrew Delivery|QID|3364|M|24.98,75.94|N|From Nori Pridedrift. On accepting the quest, a 5 minute timer will start.|
 T Scalding Mornbrew Delivery|QID|3364|M|28.78,66.66|N|To Durnan Furcutter, at the back of Anvilmar|
 A Bring Back the Mug|QID|3365|PRE|3364|M|28.78,66.66|N|From Durnan Furcutter|
-
-r Sell and Repair|QID|3361|S|M|28.78,66.66|ACTIVE|3361|N|Sell and Repair with Durnan furcutter.|
-
+r Sell and Repair|ACTIVE|3361|M|28.78,66.66|N|Repair and sell your junk to Durnan furcutter.|
 T Refugee's Quandary|QID|3361|M|28.52,67.68|N|To Felix Whindlebolt|
 
-T Bring Back the Mug|QID|3365|M|24.98,75.94|N|To Nori Pridedrift|
+N Level 4 class training|ACTIVE|183|C|Rogue|R|Dwarf|M|28.37,67.51|N|Go see Solm Hargrin to get your level 4 spell/skill.|
+N Level 4 class training|ACTIVE|183|C|Hunter|R|Dwarf|M|29.17,67.45|N|Go see Thorgas Grimson to get your level 4 spell/skill.| 
+N Level 4 class training|ACTIVE|183|C|Priest|R|Dwarf|M|28.60,66.38|N|Go see Branstock Khalder to get your level 4 spell/skill.|
+N Level 4 class training|ACTIVE|183|C|Warrior|R|Dwarf|M|28.83,67.24|N|Go see Thran Khorman to get your level 4 spell/skill.|
+N Level 4 class training|ACTIVE|183|C|Paladin|R|Dwarf|M|28.83,68.33|N|Go see Bromos Grummner to get your level 4 spell/skill.|
+N Level 4 class training|ACTIVE|183|C|Warlock|R|Gnome|M|28.65,66.14|N|Go see Alamar Grimm to get your level 4 spell/skill.|
+N Level 4 class training|ACTIVE|183|C|Mage|R|Gnome|M|28.71,66.37|N|Go see Marryk Nurribit to get your level 4 spell/skill.|
+N Level 4 class training|ACTIVE|183|C|Rogue|R|Dwarf|M|28.37,67.51|N|Go see Solm Hargrin to get your level 4 spell/skill.|
+N Level 4 class training|ACTIVE|183|C|Warrior|R|Dwarf|M|28.83,67.24|N|Go see Thran Khorman to get your level 4 spell/skill.|
+
 R Troll Cave|QID|218|ACTIVE|218|M|27.0,80.35|CC|N|Run back to the troll cave.|
 K Grik'nir The Cold|QID|218|QO|1|T|Grik'nir The Cold|M|30.5,80.2|N|Kill and loot Grik'nir The Cold for the journal. He can be found by turning left (taking the northern fork) in the cave and going all the way to the back.|
 
+T Bring Back the Mug|QID|3365|M|24.98,75.94|N|To Nori Pridedrift|
 T The Stolen Journal|QID|218|M|25.08,75.71|N|To Grelin Whitebeard.|
 A Senir's Observations|QID|282|PRE|218|M|25.08,75.71|N|From Grelin Whitebeard.|
 
+C The Boar Hunter|QID|183|US|M|22.0,72.0|N|Kill Small Crag Boar.|T|Small Crag Boar|
+T The Boar Hunter|QID|183|M|22.60,71.43|N|To Talin Keeneye.|
+
+r Sell and Repair|ACTIVE|282|M|28.71,67.85|N|Repair and sell your junk to Rybrad Coldbank or Grundel Harkin.|
+
 L Level 5|QID|3364|LVL|5|N|You need to be level 5 to continue with this guide.|C|Priest|R|Dwarf|
 
-A In Favor of the Light|QID|5626|C|Priest|R|Dwarf|
-T Senir's Observations|QID|282|M|33.48,71.84|N|To Mountaineer Thalos|
+A In Favor of the Light|QID|5626|C|Priest|R|Dwarf|M|47.3,52.2|N|Speak with Maxan Anvol.|S|
+
+T Senir's Observations|QID|282|M|28.86,70.49;33.48,71.84|CS|N|To Mountaineer Thalos|
 A Senir's Observations|QID|420|PRE|282|M|33.48,71.84|N|From Mountaineer Thalos|
 A Supplies to Tannok|QID|2160|M|33.85,72.23|N|From Hands Springsprocket|
 A In Favor of the Light|QID|5626|C|Priest|
 
-r Sell and Repair|QID|420|S|M|40.7,65.1|ACTIVE|420|N|Sell and Repair with Hegnar Rumbleshot, who is on the road to Kharanos.|
+r Sell and Repair|ACTIVE|420|M|40.7,65.1|N|Sell and Repair with Hegnar Rumbleshot, who is on the road to Kharanos.|
 R Kharanos|QID|420|ACTIVE|420|M|46.42,54.71|N|Go through the tunnel and follow the road to reach Kharanos.|
 T Senir's Observations|QID|420|M|46.73,53.82|N|To Senir Whitebeard|
 
