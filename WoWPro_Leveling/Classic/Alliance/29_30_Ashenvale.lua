02d6b62bd961f4290bf4a95955bc50f6838eede2
elidion <elidion@yahoo.com>
2019-09-13 21:41:38 -0700

    Update 29_30_Ashenvale.lua
    Updated the train note and C tag at the end because darnassus doesn't have a mage or warlock trainer either.

--- WoWPro_Leveling/Classic/Alliance/29_30_Ashenvale.lua
+++ WoWPro_Leveling/Classic/Alliance/29_30_Ashenvale.lua
@@ -118,8 +118,8 @@ A Worgen in the Woods|QID|223|Z|Duskwood|M|75.29,48.05|N|From Calor.|PRE|222|
 T The Scythe of Elune|QID|1043|M|75.32,49.03|Z|Duskwood|N|To Jonathan Carevin.|
 A Answered Questions|QID|1044|M|75.32,49.03|Z|Duskwood|N|From Jonathan Carevin.|PRE|1043|
 T Worgen in the Woods|QID|223|M|75.32,49.03|Z|Duskwood|N|To Jonathan Carevin.|
-F Trade District|QID|1044|M|77.50,44.30|N|Fly to Stormwind City.|C|Paladin|
-N Sorry Paladins|QID|1044M|37.6,32.6|Z|Stormwind City|N|Darnassus doesn't have Paladin trainers, so train while here.|C|Paladin|
+F Trade District|QID|1044|M|77.50,44.30|N|Fly to Stormwind City.|C|Paladin,Warlock,Mage|
+N Train Here|QID|1044|M|55.6,60.42|Z|Stormwind City|N|Darnassus doesn't have Paladin, Warlock or Mage trainers, so train while here.|C|Paladin,Warlock,Mage|
 H Darnassus|QID|1044|N|Hearth or fly/boat or otherwise make your way back to Darnassus.|
 T Answered Questions|QID|1044|M|61.77,39.18|Z|Darnassus|N|To Thyn'tel Bladeweaver.|
 R Auction House|QID|1135|M|56.38,51.82|CS|Z|Darnassus|N|Run to the Auction house, get ready for next guide with instructions..|
