291c757fc978633f7cd7d5d9916ea8e2f664cdcb
Emmaleah <emmaleah.wow@gmail.com>
2019-09-17 10:04:16 -0700

    Update 20_21_Redridge.lua
    moved h to stormwind, as there was no H step in guide and next guide opens with setting Hearth to Darkshire, so there was no need for it to be set in Lakeshire, so may as well have it somewhere useful. then, added H step to go turn in quest in Northshire.

--- WoWPro_Leveling/Classic/Alliance/20_21_Redridge.lua
+++ WoWPro_Leveling/Classic/Alliance/20_21_Redridge.lua
@@ -6,11 +6,14 @@ WoWPro:GuideName(guide, 'Redridge 20-21')
 WoWPro:GuideLevels(guide, 20, 21, 20)
 WoWPro:GuideNextGuide(guide, 'ClassicDuskwood2122')
 WoWPro:GuideSteps(guide, function() return [[
-A Speaking of Fortitude|QID|343|M|45.7,38.4|Z|Stormwind City|N|From Brother Kristoff.|
-T Speaking of Fortitude|QID|343|M|74.2,7.6|Z|Stormwind City|N|To Milton Sheaf.|
-A Brother Paxton|QID|344|M|74.2,7.5|Z|Stormwind City|PRE|343|
+
+F Stormwind|N|Guide begins with picking up a quest in Stormwind.|
+A Speaking of Fortitude|QID|343|M|45.7,38.4|Z|Stormwind City|N|From Brother Kristoff, in Cathedral Square.|
+T Speaking of Fortitude|QID|343|M|74.2,7.6|Z|Stormwind City|N|To Milton Sheaf, Royal Library in Stormwind Keep.|
+A Brother Paxton|QID|344|M|74.2,7.5|Z|Stormwind City|PRE|343|N|From Brother Paxton.|
 N Mathias and the Defias|QID|2360|M|68.4,70.2|Z|Westfall|N|You will need at least 70 in Lockpicking to complete this questchain.|C|Rogue|P|Lockpicking;633;0+70;1|
 A Mathias and the Defias|QID|2360|M|76,59.8|Z|Stormwind City|N|From Master Mathias Shaw. Rogue quest chain for poisons. You will need at least 70 in Lockpicking to complete this quest chain.|C|Rogue|P|Lockpicking;633;0+70;0|
+h Stormwind City|ACTIVE|344|M|52.61,65.71|N|At Innkeeper Allison.|
 F Sentinal Hill|QID|2360|N|Hearth (or fly )to Westfall|Z|Stormwind City|M|71.00,72.50|C|Rogue|P|Lockpicking;633;0+70;0|
 T Mathias and the Defias|QID|2360|M|68.4,70.2|Z|Westfall|N|To Agent Kearnen. You will need at least 70 in Lockpicking to complete this questchain.|C|Rogue|
 A Klaven's Tower|QID|2359|M|68.4,70.2|Z|Westfall|N|From Agent Kearnen. You will need at least 70 in Lockpicking to complete this quest.|C|Rogue|PRE|2360|
@@ -26,11 +29,12 @@ T The Touch of Zanzil|QID|2608|M|76,59.8|Z|Stormwind City|N|To Doc Mixilpixil.|C
 A The Touch of Zanzil|QID|2609|M|76,59.8|Z|Stormwind City|N|From Doc Mixilpixil.|C|Rogue|PRE|2608|
 C The Touch of Zanzil|QID|2609|M|40,46.4;64.4,60.6|CS|Z|Stormwind City|N|The thread is an object on the floor. The wildflowers can be bought from Felicia Gump. The vials can be found on any trade shop. The Bronze tube can be purchased sometimes from engineering suppliers, otherwise you need to buy them from the AH or have an Engineer make it for you. |C|Rogue|
 T The Touch of Zanzil|QID|2609|M|76,59.8|Z|Stormwind City|N|To Doc Mixilpixil.|C|Rogue|
-R Elwynn Forest|QID|344|N|Go to Elwynn Forest.|Z|Stormwind City|M|66.35,77.51;74.76,93.41|CS|
-T Brother Paxton|QID|344|M|49.6,40.3|Z|Elwynn Forest|
-A Ink Supplies|QID|345|M|49.6,40.3|Z|Elwynn Forest|PRE|344|
-A A Watchful Eye|QID|94|Z|Elwynn Forest|M|65.2,69.8|LVL|20|N|From Theocritus.|
-R Redridge Mountains|QID|126|N|Go to Redridge Mountains.|Z|Elwynn Forest|M|95.00,71.00|LVL|20|
+
+R Elwynn Forest|QID|344|N|Go to Elwynn Forest.|Z|Stormwind City|M|74.76,93.41|
+T Brother Paxton|QID|344|M|49.6,40.3|Z|Elwynn Forest|N|To Brother Paxton, in the Library of Northshire Abbey.|
+A Ink Supplies|QID|345|M|49.6,40.3|Z|Elwynn Forest|N|From Brother Paxton.|PRE|344|
+A A Watchful Eye|QID|94|Z|Elwynn Forest|M|65.2,69.8|LVL|20|N|From Theocritus, at the top of the Tower of Azora.|
+R Redridge Mountains|QID|126|N|Go to Redridge Mountains.|M|95.17,72.34|
 A The Everstill Bridge|QID|89|M|32.14,48.64|N|From Foreman Oslow.|PRE|125|
 T Ink Supplies|QID|345|M|32.2,48.6|N|To Foreman Oslow.|
 A Rethban Ore|QID|347|M|32.14,48.64|N|From Foreman Oslow.|PRE|345|
@@ -38,7 +42,6 @@ T Messenger to Westfall|QID|144|M|29.99,44.44|Z|Redridge Mountains|N|To Magistra
 A Messenger to Darkshire|QID|145|M|29.99,44.44|Z|Redridge Mountains|N|From Magistrate Solomon.|PRE|144|
 A Selling Fish|QID|127|M|27.8,47.3|N|From Dockmaster Baren.|
 A Murloc Poachers|QID|150|M|27.8,47.3|N|From Dockmaster Baren.|
-h Lakeshire|QID|126|N|Set your hearthstone to Lakeshire.|M|27.00,44.80|
 A Underbelly Scales|QID|122|M|30.96,47.27|N|From Verner Osgood.|
 A A Baying of Gnolls|QID|124|M|30.96,47.27|N|From Verner Osgood.|
 C Underbelly Scales|QID|122|M|30.96,47.27|S|N|Kill Black Dragon Whelps.|
@@ -55,11 +58,14 @@ T Underbelly Scales|QID|122|M|30.96,47.27|N|To Verner Osgood.|
 T A Baying of Gnolls|QID|124|M|30.96,47.27|N|To Verner Osgood.|
 T Selling Fish|QID|127|M|27.8,47.3|N|To Dockmaster Baren.|
 T Murloc Poachers|QID|150|M|27.8,47.3|N|To Dockmaster Baren.|
+
+H Stormwind City|ACTIVE|347|M|PLAYER|N|Hearth, or otherwise return to Stormwind.|
 T Rethban Ore|QID|347|M|49.6,40.3|Z|Elwynn Forest|N|To Brother Paxton in Northshire.|
 A Return to Kristoff|QID|346|M|49.6,40.3|Z|Elwynn Forest|N|From Brother Paxton in Northshire.|PRE|347|
-T Return to Kristoff|QID|346|M|45.8,38.6|Z|Stormwind City|N|To Brother Kristoff.|
-F Darkshire|N|Fly to Darkshire, Duskwood. If you followed Maw's guide, you should have this flight path. If not, close this step and follow the arrows to run there.|M|30.5,59.4|
-R Duskwood|M|6.39,91.51|N|Run to Duskwood.|
-R Darkshire|M|74.76,46.11|Z|Duskwood|N|Follow the road to Darkshire. WARNING: Avoid the level 20 mobs and keep running if they aggro you.|
+= Train|N|If needed, Don't forget to train while you are in town. (check step off manually)|
+T Return to Kristoff|QID|346|M|45.8,38.6|Z|Stormwind City|N|To Brother Kristoff in Cathedral Square.|
+F Darkshire|ACTIVE|145|M|62.25,62.26;66.28,62.13|CS|Z|Stormwind City|N|Fly to Darkshire, Duskwood. If you followed Maw's guide, you should have this flight path. If not, fly back to Lakeshire, close this step and follow the arrows to run there.|
+R Duskwood|ACTIVE|145|M|6.39,91.51|N|Run to Duskwood.|
+R Darkshire|ACTIVE|145|M|74.76,46.11|Z|Duskwood|N|Follow the road to Darkshire.|
 ]]
 end)
