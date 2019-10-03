5b6f135d632ea98cee6f6cf4b14af7114de243e4
Emmaleah <emmaleah.wow@gmail.com>
2019-10-03 00:53:10 -0700

    Update 16_18_LochModan.lua
    typos mostly

--- WoWPro_Leveling/Classic/Alliance/16_18_LochModan.lua
+++ WoWPro_Leveling/Classic/Alliance/16_18_LochModan.lua
@@ -10,9 +10,9 @@ A Report to Mountaineer Rockgar|QID|468|LEAD|455|M|37.33,45.30|N|From Mountainee
 A Ironband's Excavation|QID|436|M|37.24,47.39|N|From Jern Hornhelm, inside the house, down the stairs.|
 A WANTED: Chok'sul|QID|256|M|37.29,46.50|N|From Wanted Poster, you will probably want a group to kill this.|RANK|3|
 A Mercenaries|QID|255|M|34.69,43.18|N|From Magistrate Bluntnose, inside, down the stairs. This is an Elite Quest.|RANK|3|
-R Valley of Kings|AVAILABLE|224|M|25.33,67.67|N|Take the road south out of Thelsamar. **Grab the quests for here from 12-14 guide**|
-A The Trogg Threat|QID|267|M|23.24,73.67|N|From Captain Ruglefuss, inside the Bunker.|
-A In Defense of the King's Lands|QID|224|M|22.07,73.13|N|From Mountaineer Cobbleflint, outside, along the path.|
+R Valley of Kings|AVAILABLE|224|M|25.33,67.67|N|Take the road south out of Thelsamar.|LVL|-17|
+A The Trogg Threat|QID|267|M|23.24,73.67|N|From Captain Ruglefuss, inside the Bunker.|LVL|-17|
+A In Defense of the King's Lands|QID|224|M|22.07,73.13|N|From Mountaineer Cobbleflint, outside, along the path.|LVL|-17|
 C In Defense of the King's Lands|QID|224|M|33.00,73.00|N|Kill Troggs and Scouts until you finish this quest.|
 T In Defense of the King's Lands|QID|224|M|22.07,73.13|N|From Mountaineer Cobbleflint, outside|
 A In Defense of the King's Lands|QID|237|M|23.53,76.40|N|From Mountaineer Gravelgaw, just outside the entrance to the bunker.|PRE|224|
@@ -51,27 +51,27 @@ C A Hunter's Challenge|QID|258|M|60.80,38.47|N|This is a TIMED Quest. Kill Elder
 T A Hunter's Challenge|QID|258|M|83.51,65.53|N|To Daryl the Youngling.|
 A Vyrin's Revenge|QID|271|M|81.72,64.16|N|From Vyrin Swiftwind.|PRE|258|
 
-R Mor'grosh Stronghold|M|74,35|ACTIVE|255|N|I recomend a group for this even at level 20, you still will really benefit from at least 2 of you.|
-C Mercenaries|QID|255|M|73.06,27.18|QO|1;3|S|N|Mo'grosh Ogrea and Enforcers are found outside the cave.|
-C Mercenaries|QID|255|M|76.75,14.38|QO|2|S|N|Mo'grosh Brutea are found inside the cave.|
-K Chok'sul|QID|256|M|79.49,15.55|QO|1|T|Chok'sul|N|Chok'sul is found all the way to the back of the cave. Kill and take his head as proof.|
+R Mo'grosh Stronghold|M|74,35|ACTIVE|255|N|I recomend a group for this even at level 20, you still will really benefit from at least 2 of you.|
+C Mercenaries|QID|255|M|73.06,27.18|QO|1;3|S|N|Mo'grosh Ogres and Enforcers are found outside the cave.|
+C Mercenaries|QID|255|M|76.75,14.38|QO|2|S|N|Mo'grosh Brutes are found inside the cave.|
+K Chok'sul|ACTIVE|256|M|79.49,15.55|QO|1|T|Chok'sul|N|Chok'sul is found all the way to the back of the cave. Kill and take his head as proof.|
 C Mercenaries|QID|255|M|76.75,14.38|QO|2|US|N|Mo'grosh Brutea are found inside the cave.|
 C Mercenaries|QID|255|M|73.06,27.18|QO|1;3|US|N|Mo'grosh Ogrea and Enforcers are found outside the cave.|
 
 R Northern Island|ACTIVE|2038|M|56,28|CC|N|Run/Swim to the Northernmost Island in the middle of The Loch.|
-C Bingles' Missing Supplies|QID|2038|M|54.22,26.61|QO|4|N|Clear camp of trogs and Bingles' Blastencapper is here.|
-C Bingles' Missing Supplies|QID|2038|M|48.72,30.09|QO|1|N|Clear camp of trogs and his Wrench is here.|
-C Bingles' Missing Supplies|QID|2038|M|48.38,20.50|QO|2|N|Clear camp of trogs and his Screwdriver is here.|
-C Bingles' Missing Supplies|QID|2038|M|51.78,24.09|QO|3|N|Clear camp of trogs and his Hammer is here.|
+C Bingles' Missing Supplies|QID|2038|M|54.22,26.61|QO|4|N|Clear camp of troggs and Bingles' Blastencapper is here.|
+C Bingles' Missing Supplies|QID|2038|M|48.72,30.09|QO|1|N|Clear camp of troggs and his Wrench is here.|
+C Bingles' Missing Supplies|QID|2038|M|48.38,20.50|QO|2|N|Clear camp of troggs and his Screwdriver is here.|
+C Bingles' Missing Supplies|QID|2038|M|51.78,24.09|QO|3|N|Clear camp of troggs and his Hammer is here.|
 
-R Stonewroght Dam|ACTIVE|250|M|55.47,15.14|N|Run over to the ramp up on to the top of the dam.|
+R Stonewrought Dam|AVAILABLE|250|M|55.47,15.14|N|Run over to the ramp up on to the top of the dam.|
 A A Dark Threat Looms|QID|250|M|46.04,13.61|N|From Chief Engineer Hinderweir VII.|
 T A Dark Threat Looms|QID|250|M|56.07,13.24|N|To a Suspicious Barrel.|
 A A Dark Threat Looms|QID|199|M|56.07,13.24|N|From a Suspicious Barrel.|PRE|250|
 T A Dark Threat Looms|QID|199|M|46.04,13.62|N|To Chief Engineer Hinderweir VII.|
 A A Dark Threat Looms|QID|161|M|46.04,13.62|N|From Chief Engineer Hinderweir VII.|PRE|199|
 
-R Thelsamar|ACTIVE|467|M|33.82,48.41|N|Run back to Thelsamar.|
+R Thelsamar|ACTIVE|298|M|33.82,48.41|N|Run back to Thelsamar.|
 A Stonegear's Search|QID|467|M|33.82,48.41|N|From Mountaineer Kadrell.|
 T Excavation Progress Report|QID|298|M|37.24,47.38|N|To Jern Hornhelm.|
 A Report to Ironforge|QID|301|PRE|298|M|37.24,47.38|N|From Jern Hornhelm.|
@@ -86,7 +86,7 @@ F Thelsamar|ACTIVE|302|M|55.49,47.75|Z|Ironforge|N|Fly to Thelsamar at Gryth Thu
 T Powder to Ironband|QID|302|M|37.24,47.38|N|To Jern Hornhelm.|
 A Resupplying the Excavation|QID|273|PRE|302|M|37.24,47.38|N|From Jern Hornhelm.|
 R Grizzlepaw Ridge|QID|271|M|41.31,65.82|
-C Vyrin's Revenge|QID|271|M|40.21,66.95;36.81,61.23|CS|N|Go up this path to find Ol' Sooty's in a cave and take his head|
+K Ol' Sooty|ACTIVE|271|M|40.21,66.95;36.81,61.23|CS|N|T|Ol' Sooty|Go up this path to find Ol' Sooty in a cave and take his head.|
 C Resupplying the Excavation|QID|273|M|50.77,69.03|QO|1|N|Find Huldar, Miran, and Saean|
 T Resupplying the Excavation|QID|273|M|52.20,69.35|N|To Huldar.|
 A After the Ambush|QID|454|PRE|273|M|52.20,69.35|N|From Huldar.|
