f0f7397436245ebda617b0a456a651dcb8b9741c
Ludovicus <ludovicus.maior@gmail.com>
2020-04-09 16:49:50 -0400

    Silly Hendo, QO's use ; as the delimiter, not comma.

--- WoWPro_Leveling/Classic/Horde/12_15_Hendo_Silverpine_Forest.lua
+++ WoWPro_Leveling/Classic/Horde/12_15_Hendo_Silverpine_Forest.lua
@@ -124,9 +124,9 @@ A Gathering Materials|QID|1961|M|85.13,10.07|Z|Undercity|N|From Anastasia Hartwe
 F The Sepulcher|QID|479|M|63.27,48.55|Z|Undercity|N|Fly back to The Sepulcher.|R|Undead,Troll|C|Mage|
 ; ---
 
-C Gathering Materials|QID|1961|QO|1,2|N|Collect the Linen and Dalaran Gems you need.|R|Undead,Troll|C|Mage|S|
+C Gathering Materials|QID|1961|QO|1;2|N|Collect the Linen and Dalaran Gems you need.|R|Undead,Troll|C|Mage|S|
 C Ambermill Investigations|QID|479|M|55.71,64.46|CC|QO|1|N|Head to Ambermill and kill the residents to collect your Dalaran pendants.\n\n[color=FF0000]NOTE: [/color]The Dalaran Apprentice don't drop the pendants.\nStick to the hillside and it's a little easier to pull them.|
-C Gathering Materials|QID|1961|QO|1,2|N|Finish collecting the Linen and Dalaran Gems you need.|R|Undead,Troll|C|Mage|US|
+C Gathering Materials|QID|1961|QO|1;2|N|Finish collecting the Linen and Dalaran Gems you need.|R|Undead,Troll|C|Mage|US|
 
 L Level 14|QID|479|N|Make sure you are within 4 bubbles of level 14 before heading back to the Sepulcher. You'll be picking up your class quest when you get to Undercity shortly.|LVL|14;-2310|R|Tauren|C|Druid|
 
