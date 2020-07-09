43cbad0fe8489a090fbab2a015d511d56e44c3d4
Ludovicus <ludovicus.maior@gmail.com>
2020-07-09 18:21:02 -0400

    Cleanup in Westfall!

--- WoWPro_Leveling/Classic/Alliance/13_15_Westfall.lua
+++ WoWPro_Leveling/Classic/Alliance/13_15_Westfall.lua
@@ -24,16 +24,16 @@ A Goretusk Liver Pie|QID|22|M|56.4,30.5|N|From Salma Saldean.|
 T Return to Lewis|QID|6285|M|57.02,47.16|N|To Quartermaster Lewis.|
 A Red Leather Bandanas|QID|153|M|54.0,52.9|N|From Scout Galiaan.|
 
-C Goretusk Liver Pie|ACTIVE|22|N|From now on, kill Goretusks, Harvest Watchers, and Fleshrippers|S!US|
+C Goretusk Liver Pie|QID|22|N|From now on, kill Goretusks.|S!US|
 C The Killing Fields|S|QID|9|M|55.00,33.20|N|Kill Harvest Watchers until you finish this quest.|
 C The Forgotten Heirloom|QID|64|M|49.00,19.00|NC|N|Loot the watch from the cupboard in the house.|
 C Poor Old Blanchy|NC|N|Loot the rest of the oats for this quest. They may be far from this point in neighboring farms.|QID|151|M|51.00,22.00|
 T Poor Old Blanchy|QID|151|M|60.0,19.4|N|To Verna Furlbrow.|
 T The Forgotten Heirloom|QID|64|M|60.0,19.4|N|To Farmer Furlbrow.|
-C The Killing Fields|US|N|Kill Harvest Watchers until you finish this quest.|QID|9|M|55.00,33.20|
-l Flask of Oil|QID|103|M|38,51|S|N|Kill Harvest Watchers to collect Flask of Oil. Save the oil for later.|L|814 5|
+C Westfall Stew |QO|3|N|Kill Harvest Watchers for Okra.|QID|38|M|53.8,42.5|S!US|
+l Flask of Oil|QID|103|M|38,51|S!US|N|Kill Harvest Watchers to collect Flask of Oil. Save the oil for later.|L|814 5|
 l Hops|QID|117|M|38,51|N|Kill Harvest Watchers to collect Hops. Save the hops for later.|L|1274 5|
-l Flask of Oil|QID|103|M|38,51|US|N|Kill Harvest Watchers to collect Flask of Oil, save for later.|L|814 5|
+C The Killing Fields|US|N|Kill Harvest Watchers until you finish this quest.|QID|9|M|55.00,33.20|
 
 T Report to Gryan Stoutmantle|QID|109|M|56.3,47.5|N|To Gryan Stoutmantle.|
 A The People's Militia |QID|12|M|56.3,47.5|N|From Gryan Stoutmantle.|
@@ -43,11 +43,11 @@ A Patrolling Westfall|QID|102|M|56.4,47.6|N|From Captain Danuvin.|
 C Patrolling Westfall|QID|102|N|Add Gnolls to your kill list. |S!US|
 C The People's Militia |N|Kill defias mobs until you finish this quest.|QID|12|M|44.00,25.00;47,40;49,47|CN|
 C Red Leather Bandanas|N|Kill defias mobs until you get half the items for this quest.|QID|153|M|44.00,25.00|QO|1>8|
-C Murloc Eye|QID|38|N|Kill murlocs until you get 3 Murloc Eyes.|L|730 3|M|34.00,21.00|
+C Murloc Eye|QID|38|QO|2|N|Kill murlocs until you get 3 Murloc Eyes.|M|34.00,21.00|
 C Patrolling Westfall|N|Kill Gnolls until you get the items for this quest.|QID|102|M|36.00,29.00|
 
-C Westfall Stew |N|Kill mobs until you get the items for this quest.|QID|38|M|53.8,42.5|
-C Goretusk Liver Pie|N|Kill Goretusks until you get the items for this quest.|QID|22|M|53.8,42.5|
+C Westfall Stew |QO|1|N|Kill Vultures.|QID|38|M|53.8,42.5|S!US|
+C Westfall Stew |QO|2|N|Kill Goretusks.|QID|38|M|53.8,42.5|S!US|
 T The Killing Fields|QID|9|M|56.0,31.2|N|To Farmer Saldean.|
 T Westfall Stew |QID|38|M|56.4,30.5|N|To Salma Saldean.|
 T Goretusk Liver Pie|QID|22|M|56.4,30.5|N|To Salma Saldean.|
