5c477fce1cb630041d401a954bf3383314ae49ed
Hendo72 <39675478+Hendo72@users.noreply.github.com>
2020-07-28 00:43:36 -0400

    Update 12_15_Hendo_Silverpine_Forest.lua
    Completed run-through with my priest.
    - may still require more class quests to be added. (Rogue, warrior, etc.)

--- WoWPro_Leveling/Classic/Horde/12_15_Hendo_Silverpine_Forest.lua
+++ WoWPro_Leveling/Classic/Horde/12_15_Hendo_Silverpine_Forest.lua
@@ -54,16 +54,16 @@ T Ivar the Foul|QID|425|M|53.46,13.43|N|To Rane Yorick.|
 C The Dead Fields|QID|437|M|45,21|QO|1;2|N|Head SW to the Dead Fields. Nightlash might already be spawned, but if she isn't, kill all the Rot Hide Gnolls you see until she does.\n\n[color=FF0000]NOTE: [/color]Much like Ivar, you will want to clear the mobs as you go.|
 C A Recipe For Death|QID|447|M|36,15|QO|1;2|N|Head SW from here and finish killing the spiders and bears for their Skittering Bloods and Grizzled Bear Hearts.|US|
 H The Sepulcher|QID|422|N|Hearth back to The Sepulcher|
-
+N Cure Poison Class Quest|AVAILABLE|6126|N|We're not going to make a special trip back to Eastern Kingdoms. Instead, we're going to wait until we are finished with this area.|R|Tauren|C|Druid|
 T Arugal's Folly|QID|422|M|44.20,39.78|N|To Dalar Dawnweaver.|
 A Arugal's Folly|QID|423|M|44.20,39.78|N|From Dalar Dawnweaver.|PRE|422|
 ; lv 14
 T The Dead Fields|QID|437|M|43.14,41.37;43.43,40.86|CS|N|To High Executor Hadrec inside the Crypt.|
 A The Decrepit Ferry|QID|438|M|43.43,40.86|N|From High Executor Hadrec.|PRE|437|
 
-A Supplying the Sepulcher|QID|6321|R|Undead|M|43.43,41.69|N|From Deathguard Podrig at the Crypt entrance.|
-T Supplying the Sepulcher|QID|6321|R|Undead|M|45.62,42.60|N|To Karos Razok.|
-A Ride to the Undercity|QID|6323|R|Undead|M|45.62,42.60|N|From Karos Razok.|PRE|6321|
+A Supplying the Sepulcher|QID|6321|R|Undead|M|43.43,41.69|N|From Deathguard Podrig at the Crypt entrance.|R|Undead|
+T Supplying the Sepulcher|QID|6321|R|Undead|M|45.62,42.60|N|To Karos Razok.|R|Undead|
+A Ride to the Undercity|QID|6323|R|Undead|M|45.62,42.60|N|From Karos Razok.|PRE|6321|R|Undead|
 F Undercity|QID|447|M|45.62,42.60|N|Fly to Undercity.|
 
 T A Recipe For Death|QID|447|M|51.32,44.66;47.00,59.79;44.78,66.03;48.81,69.29|Z|Undercity|CC|N|To Master Apothecary Faranell in The Apothecarium area.|
@@ -71,13 +71,13 @@ A A Recipe For Death|QID|450|M|48.81,69.29|Z|Undercity|ELITE|N|[color=FF8000]Eli
 T Zinge's Delivery|QID|1359|M|50.15,67.93|Z|Undercity|N|To Apothecary Zinge.|
 A Sample for Helbrim|QID|1358|M|50.15,67.93|Z|Undercity|N|From Apothecary Zinge.|PRE|1359|
 
-T Ride to the Undercity|QID|6323|R|Undead|M|61.51,41.81|Z|Undercity|N|To Gordon Wendham.|
-A Michael Garrett|QID|6322|R|Undead|M|61.51,41.81|Z|Undercity|N|From Gordon Wendham.|PRE|6323|
-T Michael Garrett|QID|6322|R|Undead|M|63.27,48.55|Z|Undercity|N|To Michael Garrett.|
-A Return to Podrig|QID|6324|R|Undead|M|63.27,48.55|Z|Undercity|N|From Michael Garrett.|PRE|6322|
+T Ride to the Undercity|QID|6323|R|Undead|M|61.51,41.81|Z|Undercity|N|To Gordon Wendham.|R|Undead|
+A Michael Garrett|QID|6322|R|Undead|M|61.51,41.81|Z|Undercity|N|From Gordon Wendham.|PRE|6323|R|Undead|
+T Michael Garrett|QID|6322|R|Undead|M|63.27,48.55|Z|Undercity|N|To Michael Garrett.|R|Undead|
+A Return to Podrig|QID|6324|R|Undead|M|63.27,48.55|Z|Undercity|N|From Michael Garrett.|PRE|6322|R|Undead|
 F The Sepulcher|QID|438|M|63.27,48.55|Z|Undercity|N|Fly back to The Sepulcher.|
 
-T Return to Podrig|QID|6324|R|Undead|M|43.43,41.69|N|To Deathguard Podrig at the Crypt entrance.|
+T Return to Podrig|QID|6324|R|Undead|M|43.43,41.69|N|To Deathguard Podrig at the Crypt entrance.|R|Undead|
 R The Decrepit Ferry|ACTIVE|438|M|51.21,36.73;52.05,38.40;58.34,34.95|CC|N|Go to the Decrepit Ferry.\nBy sticking to the hillside around the edge, you can avoid some unnecessary fighting. The mobs in this area (Hand of Ravenclaw) can hit hard and they roam considerably.|
 T The Decrepit Ferry|QID|438|M|58.34,34.95|N|Click the Decrepit Ferry.|
 A Rot Hide Clues|QID|439|M|58.34,34.95|N|From the Decrepit Ferry.|PRE|438|
@@ -105,6 +105,7 @@ H The Sepulcher|QID|424|N|Hearth back to The Sepulcher.\n[color=FF0000]NOTE: [/c
 T Arugal's Folly|QID|424|M|44.20,39.78|N|To Dalar Dawnweaver.|
 
 ;-- Mage 'Spellfire Robe' Lv 15 class quest chain
+N Spellfire Robe|AVAILABLE|1959|N|Unlike the Druid quest, the Mage class quest is done in this area. It's your best interest to make the trip to start this class quest chain.|R|Undead,Troll|C|Mage|
 F Undercity|AVAILABLE|1959|M|45.62,42.60|N|Fly to Undercity.|R|Undead,Troll|C|Mage|
 R Leave Undercity|AVAILABLE|1959|M|65.99,36.85;66.22,0.90;66.22,1.27|Z|Undercity|CC|N|Take the elevator up and leave Undercity through the front gates.|R|Undead,Troll|C|Mage|
 b Durotar|AVAILABLE|1959|M|60.75,58.77|Z|Tirisfal Glades|N|Take the zeppelin to Orgrimmar.|R|Undead,Troll|C|Mage|
@@ -116,40 +117,39 @@ b Tirisfal Glades|ACTIVE|1959|M|50.88,13.83|Z|Durotar|N|Take the Zepplin to Tiri
 R Undercity|ACTIVE|1959|M|61.86,65.04|Z|Tirisfal Glades|N|Enter Undercity.|R|Undead,Troll|C|Mage|
 T Report to Anastasia|QID|1959|M|69.95,44.19;69.79,41.95;68.41,40.53;66.12,22.61;85.13,10.07|Z|Undercity|CC|N|To Anastasia Hartwell in the Magic Quarter.|R|Undead,Troll|C|Mage|
 A Investigate the Alchemist Shop|QID|1960|M|85.13,10.07|Z|Undercity|N|From Anastasia Hartwell.|PRE|1959|R|Undead,Troll|C|Mage|
-l Chest of Containment Coffers|ACTIVE|1960|M|85.13,10.07|Z|Undercity|L|7247|N|Obtain a Chest of Containment Coffers from behind Anastasia.|R|Undead,Troll|C|Mage|
-l Cantation of Manifestation|ACTIVE|1960|M|85.13,10.07|Z|Undercity|L|7308|N|Obtain a Cantation of Manifestation from behind Anastasia.|R|Undead,Troll|C|Mage|
+C Chest of Containment Coffers|ACTIVE|1960|M|85.13,10.07|Z|Undercity|L|7247|N|Obtain a Chest of Containment Coffers from behind Anastasia.|R|Undead,Troll|C|Mage|
+C Cantation of Manifestation|ACTIVE|1960|M|85.13,10.07|Z|Undercity|L|7308|N|Obtain a Cantation of Manifestation from behind Anastasia.|R|Undead,Troll|C|Mage|
 R Algernon's Alchemist Shop|ACTIVE|1960|M|79.46,23.49;56.68,67.73;51.47,74.48|Z|Undercity|CC|N|Follow the outer ring clockwise to Algernon's shop in the Apothecarium.|R|Undead,Troll|C|Mage|
 C Investigate the Alchemist Shop|QID|1960|M|51.47,74.48|Z|Undercity|U|7308|N|Use the Cantation of Manifestation at the shop to make the creatures visible.|R|Undead,Troll|C|Mage|
 C Investigate the Alchemist Shop|QID|1960|M|51.47,74.48|Z|Undercity|U|7247|QO|1|N|Subdue the creatures with your spells and use a coffer to capture them.|R|Undead,Troll|C|Mage|
 T Investigate the Alchemist Shop|QID|1960|M|85.13,10.07|Z|Undercity|N|To Anastasia Hartwell.|R|Undead,Troll|C|Mage|
 A Gathering Materials|QID|1961|M|85.13,10.07|Z|Undercity|N|From Anastasia Hartwell.|PRE|1960|R|Undead,Troll|C|Mage|
-F The Sepulcher|QID|479|M|63.27,48.55|Z|Undercity|N|Fly back to The Sepulcher.|R|Undead,Troll|C|Mage|
+F The Sepulcher|QID|1961|M|63.27,48.55|Z|Undercity|N|Fly back to The Sepulcher.|R|Undead,Troll|C|Mage|
 ; ---
 
 C Gathering Materials|QID|1961|QO|1;2|N|Collect the Linen and Dalaran Gems you need.|R|Undead,Troll|C|Mage|S|
-C Ambermill Investigations|QID|479|M|55.71,64.46|CC|QO|1|N|Head to Ambermill and kill the residents to collect your Dalaran pendants.\n\n[color=FF0000]NOTE: [/color]The Dalaran Apprentice don't drop the pendants.\nStick to the hillside and it's a little easier to pull them.|
+R Ambermill|ACTIVE|479|M|55.71,64.46|N|Make your way to Ambermill.|
+C Ambermill Investigations|QID|479|L|3354 8|N|Kill the residents to collect your Dalaran pendants.\n\n[color=FF0000]NOTE: [/color]The Dalaran Apprentice don't drop the pendants.\nStick to the hillside and it's a little easier to pull them.|
 C Gathering Materials|QID|1961|QO|1;2|N|Finish collecting the Linen and Dalaran Gems you need.|R|Undead,Troll|C|Mage|US|
-
-L Level 14|QID|479|N|Make sure you are within 4 bubbles of level 14 before heading back to the Sepulcher. You'll be picking up your class quest when you get to Undercity shortly.|LVL|14;-2310|R|Tauren|C|Druid|
-
+R The Sepulcher|QID|479|M|44.51,41.47|N|Return to The Sepulcher.|
 T Ambermill Investigations|QID|479|M|43.97,40.93|N|To Shadow Priest Allister.|
 F Undercity|QID|440|M|45.62,42.60|N|Fly to Undercity.|
 T Gathering Materials|QID|1961|M|70.6,30.6|Z|Undercity|N|To Josef Gregorian, Artisan Tailor.|R|Undead,Troll|C|Mage|
 R Leave Undercity|QID|440|M|65.99,36.85;66.22,0.90;66.22,1.27|Z|Undercity|CC|N|Take the elevator up and leave Undercity through the front gates.|
 T The Engraved Ring|QID|440|M|61.26,50.84|Z|Tirisfal Glades|N|To Magistrate Sevren in Brill.|
 A Raleigh and the Undercity|QID|441|M|61.2,50.8|Z|Tirisfal Glades|N|From Magistrate Sevren.|PRE|440|
-C Raleigh and the Undercity|QID|441|Z|Tirisfal Glades|N|Bring Deliah's Ring to Raleigh in the Trade Quarter of Undercity.|
-T Raleigh and the Undercity|QID|441|M|62,43|Z|Undercity|N|To Raleigh Andrean.|
+R Undercity|QID|441|M|61.86,65.04|Z|Tirisfal Glades|N|Return to Undercity.|
+T Raleigh and the Undercity|QID|441|M|62,43|Z|Undercity|N|To Raleigh Andrean in the Trade Quarter of Undercity.|
 
-; --- Making sure you have the Barrens breadcrumb.
+; --- Making sure they have the Barrens breadcrumb.
 A Sample for Helbrim|QID|1358|M|50,68.4|Z|Undercity|N|From Apothecary Zinge.|PRE|1359|
 R Leave Undercity|ACTIVE|1358|M|65.99,36.85;66.22,0.90;66.22,1.27|Z|Undercity|CC|N|Take the elevator up and leave Undercity through the front gates.|
 
 ; --- Travel to Durotar (Orgrimmar)
 b Durotar|ACTIVE|1358|M|60.75,58.77|Z|Tirisfal Glades|N|Take the zeppelin to Orgrimmar.|
-R Enter Orgrimmar|ACTIVE|1358|M|45.52,12.07|Z|Durotar|CC|
-R The Valley of Wisdom|AVAILABLE|1061||M|42.37,36.75|N|Go to The Valley of Wisdom.|
-A The Spirits of Stonetalon|QID|1061|M|38.93,38.37|N|From Zor Lonetree (just inside Grommash Hold).\n[color=FF0000]NOTE: [/color]Probably the only time you'll have to grab this quest. It leads to a quest we will be doing in the next guide.|LEAD|1062|
+R Orgrimmar|ACTIVE|1358|M|45.52,12.07|Z|Durotar|N|Enter Orgrimmar by the front gate.|
+R Valley of Wisdom|AVAILABLE|1061|M|42.37,36.75|Z|Orgrimmar|N|Go to The Valley of Wisdom.|
+A The Spirits of Stonetalon|QID|1061|M|38.93,38.37|Z|Orgrimmar|N|From Zor Lonetree (just inside Grommash Hold).\n[color=FF0000]NOTE: [/color]Probably the best time you'll get to grab this quest. It leads to a quest we will be doing in the next guide.|LEAD|1062|
 f Origrimmar|ACTIVE|1358|M|45.35,63.92|Z|Orgrimmar|N|Doras|TAXI|-Orgrimmar|
 
 ; --- Druid 'Cure Poison' lv 14 class quest
@@ -166,7 +166,7 @@ F Thunder Bluff|ACTIVE|6127|M|44.28,45.87|Z|Moonglade|N|Fly to Thunder Bluff.\nT
 F Crossroads|ACTIVE|1358|M|45.35,63.92|Z|Orgrimmar|N|Doras|TAXI|Crossroads|
 R Leave Orgrimmar|ACTIVE|1358|M|52.42,84.43|Z|Orgrimmar|CC|N|Exit Orgrimmar through the south gate.|TAXI|-Crossroads|
 R Far Watch Outpost|ACTIVE|1358|M|45.54,12.08;36.17,23.14;34.16,42.30|Z|Durotar|CC|N|As you exit the gate, turn right and follow the wall west to the river. Follow the shoreline south to the bridge and cross over to the Barrens.|TAXI|-Crossroads|
-R Crossroads|ACTIVE|1358|M|52.34,29.34|Z|The Barrens|CC|N|Take the road to the east and follow the signs. Sticking to the road is a safe bet. There may be mobs that are a few levels above you along your route.|TAXI|-Crossroads|
+R Crossroads|ACTIVE|1358|M|52.34,29.34|Z|The Barrens|N|Take the road west and follow the signs. Sticking to the road is a safe bet. There may be mobs that are a few levels above you along your route.|TAXI|-Crossroads|
 ; ---
 
 ]]
