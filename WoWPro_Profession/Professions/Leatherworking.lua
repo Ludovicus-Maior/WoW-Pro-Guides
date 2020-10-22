cbfe9c590acf29eb6bc93aa95a0b010454faf06b
Blanckaert <blanckaert@gmail.com>
2020-10-22 11:44:55 -0400

    Update Leatherworking.lua
    First run, BFA LW only

--- WoWPro_Profession/Professions/Leatherworking.lua
+++ WoWPro_Profession/Professions/Leatherworking.lua
@@ -1,34 +1,57 @@
-local guide = WoWPro:RegisterGuide("TwiLeather","Profession","Leatherworking", "Twists", "Neutral")
+local guide = WoWPro:RegisterGuide("BlancLeather","Profession","Leatherworking", "Blanckaert", "Neutral")
 WoWPro:GuideIcon(guide,"PRO",165)
 WoWPro:GuideLevels(guide)
 WoWPro:GuideName(guide, "Leatherworking")
 WoWPro:GuideSteps(guide, function()
 return [[
 
-N This Guide is a Work In Progress Guide.  Currently in Development, starting with BFA content.
+N This Guide is a Work In Progress Guide.|N|Currently in Development, starting with BFA content.\nAlso ASSUMES you have the appropriate Skill|
 
-N Learn Apprentice Leatherworking|QID|901640000|P|Leatherworking;165;*;0;75|N|Learn from a Trainer in any major city. You must be level 5.|
-; Items for 1 to 50 BFA
 
-; L Shimmerscale|QID|1657256757|L|153050 182|N|You will need 182 Shimmerscales for the next two parts.|
-; L Coarse Leather|QID|1657256756|L|152541 182|N|You will need 182 Coarse Leather for the next two parts.|
-; L Blood-Stained Bone|QID|1657256756|L|154164 256|N|You will need 256 Blood-Stained Dones for the next four parts.|
+; What to Craft 1 to 25 BFA
 
-; What to Craft 1 to 50 BFA
-N Shimmerscale Armguards|QID|1657256757|P|Leatherworking;165;7+13;1|N|1;Shimmerscale;78;182:1;Blood-Stained Bone;50;256|
-N Coarse Leather Armguards|QID|1657256756|P|Leatherworking;165;7+26;1|N|1;Coarse Leather;78;182:1;Blood-Stained Bone;50;206|
+N Shimmerscale Armguards|QID|1657256757|P|Leatherworking;165;7+13;1|N|1;Shimmerscale;78;78:1;Blood-Stained Bone;50;50|
+N Coarse Leather Armguards|QID|1657256756|P|Leatherworking;165;7+26;1|N|1;Coarse Leather;78;78:1;Blood-Stained Bone;50;50|
+
+; What to Craft 26 to 50 BFA
+
 B Shimmerscale Pauldrons|QID|1657256759|RECIPE|256759|P|Leatherworking;165;7+26;0|N|Learn the Pattern - Shimmerscale Pauldrons.|
 B Coarse Leather Pauldrons|QID|1657256754|RECIPE|256754|P|Leatherworking;165;7+26;0|N|Learn the Pattern - Coarse Leather Pauldrons.|
-N Shimmerscale Pauldrons|QID|1657256759|P|Leatherworking;165;7+39;1|N|1;Shimmerscale;104;104:1;Blood-Stained Bone;78;156|
+N Shimmerscale Pauldrons|QID|1657256759|P|Leatherworking;165;7+39;1|N|1;Shimmerscale;104;104:1;Blood-Stained Bone;78;78|
 N Coarse Leather Pauldrons|QID|1657256754|P|Leatherworking;165;7+52;1|N|1;Coarse Leather;104;104:1;Blood-Stained Bone;78;78|
 
+; What to Craft 51 to 70 BFA
+
 B Hardened Tempest Hide|QID|1657272278|RECIPE|272278|P|Leatherworking;165;7+50;0|N|Learn the Pattern - Hardened Tempest Hide.|
-B Amber Tanning Oil|QID|1657272278|L|160059 50|N|You will need these for the next step, Purchase from Trainer OR Trade Supplies Vendor.\nAlso available from Scrapping.|
+B Amber Tanning Oil|QID|1657272278|L|160059 50|P|Leatherworking;165;7+70;1|N|You will need about 50 of these for this step, Purchase from Trainer OR Trade Supplies Vendor.\nAlso available from Scrapping.|
 N Hardened Tempest Hide|QID|1657272278|P|Leatherworking;165;7+70;1|N|1;Tempest Hide;50;50:1;Amber Tanning Oil;50;50|
 
-N This guide has not yet been written. Should be available soon.|QID|901640001|
+; What to Craft 71 to 85 BFA
 
-N That completes Leatherworking.
+B Coarse Leather Barding|QID|1657256790|RECIPE|256790|P|Leatherworking;165;7+70;0|N|Learn the Pattern - Coarse Leather Barding.|
+N Coarse Leather Barding|QID|1657256790|P|Leatherworking;165;7+85;1|N|1;Coarse Leather;375;375|
+
+; What to Craft 85 to 120 BFA
+
+B Coarse Leather Barding|QID|1657286019|RECIPE|286019|P|Leatherworking;165;7+85;0|N|Learn the Pattern - Rank 2 Coarse Leather Barding.|
+B Drums of the Maelstrom|QID|1657256791|RECIPE|256791|P|Leatherworking;165;7+85;0|N|Learn the Pattern - Drums of the Maelstrom.|
+N Coarse Leather Barding|QID|1657286019|P|Leatherworking;165;7+120;1|N|1;Coarse Leather;900;900|
+U Coarse Leather Barding|QID|1657166313|RECIPE|166313|P|Leatherworking;165;7+115|N|Learn the Pattern - Rank 3 Coarse Leather Barding.\nRandom World Drop.|
+N Coarse Leather Barding|QID|1657286019|P|Leatherworking;165;7+125;1|N|1;Coarse Leather;75;75|
+
+; What to Craft 120 to 150 BFA
+
+N Drums of the Maelstrom|QID|1657256791|P|Leatherworking;165;7+120;1|N|1;Coarse Leather;750;750:1;Blood-Stained Bone;300;300|
+
+; What to Craft 150 to 175 BFA
+
+B Dredged Leather Bladder|QID|1657301405|PRE|55636|M|38.0,53.6|Z|Nazjatar|RECIPE|301405|P|Leatherworking;165;7+150;0|N|From Instructor Alikana - Learn the Pattern - Dredged Leather Bladder.|FACTION|ALLIANCE|
+B Dredged Leather Bladder|QID|1657301405|PRE|55636|M|49.0,61.6|Z|Nazjatar|RECIPE|301405|P|Leatherworking;165;7+150;0|N|From Jada - Learn the Pattern - Dredged Leather Bladder.|FACTION|HORDE|
+B Nylon Thread|QID|1657301405|L|159959 200|P|Leatherworking;165;7+175;1|N|You will need about 200 of these for this step, Purchase from Trainer OR Trade Supplies Vendor.\nAlso available from Scrapping.|
+
+N This guide has not yet been completed. The rest of the expansions will be covered, in reverse order.|QID|901640001|
+
+N That completes Leatherworking.|
 ]]
 end)
 
