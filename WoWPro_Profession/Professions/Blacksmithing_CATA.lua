591f97f0caea17e3e3072a5919b904d60e9d132c
Blanckaert <blanckaert@gmail.com>
2020-10-27 16:33:56 -0400

    Update Blacksmithing_CATA.lua
    Fixed some erros, but one is still alluding me.

--- WoWPro_Profession/Professions/Blacksmithing_CATA.lua
+++ WoWPro_Profession/Professions/Blacksmithing_CATA.lua
@@ -65,7 +65,7 @@ N Thorium Helm|QID|1640016653|P|Blacksmithing;164;0+300;1|N|12;Thorium Bar;120;1
 
 N Learn Outland Blacksmithing|QID|1641002018|M|70.2,41.4|Z|Shattrath City|P|Blacksmithing;164;1+0;0|T|Zula Slagfury|N|From Zula Slagfury, in Shattrath.|
 
-N Fel Weightstone|QID|16410034607|P|Blacksmithing;164;1+5;1|N|1;Fel Iron Bar;7;102:1;Netherweave Cloth;7;57|
+N Fel Weightstone|QID|1641034607|P|Blacksmithing;164;1+5;1|N|1;Fel Iron Bar;7;102:1;Netherweave Cloth;7;57|
 N Fel Iron Plate Belt|QID|1641029547|P|Blacksmithing;164;1+15;1|N|4;Fel Iron Bar;40;95|
 N Fel Iron Chain Gloves|QID|1641029552|P|Blacksmithing;164;1+20;1|N|5;Fel Iron Bar;25;55|
 N Fel Iron Plate Boots|QID|1641029548|P|Blacksmithing;164;1+25;1|N|6;Fel Iron Bar;30;30|
@@ -73,37 +73,37 @@ N Lesser Rune of Warding|QID|1641032284|P|Blacksmithing;164;1+35;1|N|1;Adamantit
 
 ; What to Craft 035 to 040 Outland
 
-B Plans: Adamantite Cleaver|QID|1641023591|M|64.0,71.8|Z|Shattrath City|P|Blacksmithing;164;1+30;0|RECIPE|23591|T|Aaron Hollman|N|Purchase from Aaron Hollman, Shattrath, Lower City|
+B Plans- Adamantite Cleaver|QID|1641023591|M|64.0,71.8|Z|Shattrath City|P|Blacksmithing;164;1+30;0|RECIPE|23591|T|Aaron Hollman|N|Purchase from Aaron Hollman, Shattrath, Lower City|
 N Adamantite Cleaver|QID|1641029568|P|Blacksmithing;164;1+40;1|N|8;Adamantite Bar;40;70|
 
 ; What to Craft 040 to 050 Outland
 
-B Plans: Lesser Ward of Shielding|QID|1641023638|M|53.2,38.2|Z|Hellfire Peninsula|P|Blacksmithing;164;1+40;0|RECIPE|23638|T|Rohok|N|Purchase from Rohok, Thrallmar|FACTION|Horde|
-B Plans: Lesser Ward of Shielding|QID|1641023638|M|36.8,55.0|Z|Shadowmoon Valley|P|Blacksmithing;164;1+40;0|RECIPE|23638|T|Mari Stonehand|N|Purchase from Mari Stonehand, Wildhammer Stronghold|FACTION|Alliance|
+B Plans- Lesser Ward of Shielding|QID|1641023638|M|53.2,38.2|Z|Hellfire Peninsula|P|Blacksmithing;164;1+40;0|RECIPE|23638|T|Rohok|N|Purchase from Rohok, Thrallmar|FACTION|Horde|
+B Plans- Lesser Ward of Shielding|QID|1641023638|M|36.8,55.0|Z|Shadowmoon Valley|P|Blacksmithing;164;1+40;0|RECIPE|23638|T|Mari Stonehand|N|Purchase from Mari Stonehand, Wildhammer Stronghold|FACTION|Alliance|
 N Lesser Ward of Shielding|QID|1641029728|P|Blacksmithing;164;1+50;1|N|1;Adamantite Bar;25;25:Might have to make more then 25|
 
 ; What to Craft 050 to 060 Outland
 
-B Plans: Adamantite Weightstone|QID|16410028632|M|79.2;63.8|Z|Zangarmarsh|RECIPE|28632|P|Blacksmithing;164;1+50;0|REP|Cenarion Expedition;1376;Honored-Exalted|T|Fedryen Swiftspear|N|Purchase from Fedryen Swiftspear, Cenarion Refuge\n(Needs at least Honoured with Cenarion Expedition).|
-N Adamantite Weightstone|QID|1641034608|P|Blacksmithing;164;1+60;1|N|1;Adamantite Bar;25;25|2;Netherweave Cloth;50;50|
+B Plans- Adamantite Weightstone|QID|1641028632|M|79.2,63.8|Z|Zangarmarsh|RECIPE|28632|P|Blacksmithing;164;1+50;0|REP|Cenarion Expedition;1376;Honored-Exalted|T|Fedryen Swiftspear|N|Purchase from Fedryen Swiftspear, Cenarion Refuge\n(Needs at least Honoured with Cenarion Expedition).|
+N Adamantite Weightstone|QID|1641034608|P|Blacksmithing;164;1+60;1|N|1;Adamantite Bar;25;25:2;Netherweave Cloth;50;50|
 
 ; What to Craft 061 to 075 Outland
 
 N Leveling from 60 to 75|N|There are three different ways to finish from 60 to 75, Reputation with The Scryers or The Aldor, OR running The Botanica AND Auchenai Crypts.  All three methods will be covered just RIGHT Click off which ever you want to skip.|
 
 N Leveling from 60 to 75|N|Reputation with The Scryers - for both plans you need at least Revered.|
-B Plans: Enchanted Adamantite Belt|QID|1641023597|M|60.6,64.2|Z|Shattrath City|P|Blacksmithing;164;1+55;0|RECIPE|23597|REP|The Scryers;934;Friendly-Exalted|T|Quartermaster Enuril|N|Purchase from Quartermaster Enuril, Scryer's Tier, Shattrath\nNeed at least Friendly Reputation|
-B Plans: Enchanted Adamantite Breastplate|QID|1641023599|M|60.6,64.2|Z|Shattrath City|P|Blacksmithing;164;1+60;0|RECIPE|23599|REP|The Scryers;934;Revered-Exalted|T|Quartermaster Enuril|N|Purchase from Quartermaster Enuril, Scryer's Tier, Shattrath\nNeed at least Revered Reputation|
+B Plans- Enchanted Adamantite Belt|QID|1641023597|M|60.6,64.2|Z|Shattrath City|P|Blacksmithing;164;1+55;0|RECIPE|23597|REP|The Scryers;934;Friendly-Exalted|T|Quartermaster Enuril|N|Purchase from Quartermaster Enuril, Scryer's Tier, Shattrath\nNeed at least Friendly Reputation|
+B Plans- Enchanted Adamantite Breastplate|QID|1641023599|M|60.6,64.2|Z|Shattrath City|P|Blacksmithing;164;1+60;0|RECIPE|23599|REP|The Scryers;934;Revered-Exalted|T|Quartermaster Enuril|N|Purchase from Quartermaster Enuril, Scryer's Tier, Shattrath\nNeed at least Revered Reputation|
 N Enchanted Adamantite Belt|QID|1641029608|P|Blacksmithing;164;1+67;1|N|2;Hardened Adamantite Bar;14;54:8;Arcane Dust;56;256:2;Large Prismatic Shard;14;54|REP|The Scryers;934;Friendly-Exalted|
 N Enchanted Adamantite Breastplace|QID|1641029610|P|Blacksmithing;164;1+75;1|N|4;Hardened Adamantite Bar;40;40:20;Arcane Dust;200;200:4;Large Prismatic Shard;40;40|REP|The Scryers;934;Revered-Exalted|
 
 N Leveling from 60 to 75|N|Reputation with The Aldor - for both plans you need at least Honoured.|
-B Plans: Flamebane Gloves|QID|1641023603|M|47.8,26.2|Z|Shattrath City|RECIPE|23603|P|Blacksmithing;164;1+60;0|REP|The Aldor;932;Honored-Exalted|T|Quartermaster Endarin|N|Purchase from Quartermaster Endarin, Aldor's Rise, Shattrath\nNeed at least Honored Reputation.|
+B Plans- Flamebane Gloves|QID|1641023603|M|47.8,26.2|Z|Shattrath City|RECIPE|23603|P|Blacksmithing;164;1+60;0|REP|The Aldor;932;Honored-Exalted|T|Quartermaster Endarin|N|Purchase from Quartermaster Endarin, Aldor's Rise, Shattrath\nNeed at least Honored Reputation.|
 N Flamebane Gloves|QID|1641029616|P|Blacksmithing;164;1+75;1|N|8;Fel Iron Bar;136;136:4;Primal Water;68;68:4;Primal Fire;68;68|REP|The Aldor;932;Honored-Exalted|
 
 N Leveling from 60 to 75|N|via Dungeons - The Botanica for Plans: Earthpeace Breastplate\nAuchenai Crypts for Plans: Felsteel Gloves.\n\nNOTE! Unknown if still drops.|
-U Plans: Felsteel Gloves|QID|1641023605|L|23605|P|Blacksmithing;164|RECIPE|29619|N|Run Auchenai Crypts until an Auchenai Monk drops these plans.|
-U Plans: Earthpeace Breastplate|QID|1641023617|L|23617|P|Blacksmithing;164|RECIPE|29649|N|Run The Botanica until High Botanist Freywinn drops these plans.\n\nYou can skip this, just need to make double of Felsteel Gloves instead.|
+U Plans- Felsteel Gloves|QID|1641023605|L|23605|P|Blacksmithing;164|RECIPE|29619|N|Run Auchenai Crypts until an Auchenai Monk drops these plans.|
+U Plans- Earthpeace Breastplate|QID|1641023617|L|23617|P|Blacksmithing;164|RECIPE|29649|N|Run The Botanica until High Botanist Freywinn drops these plans.\n\nYou can skip this, just need to make double of Felsteel Gloves instead.|
 N Felsteel Gloves|QID|1641029619|P|Blacksmithing;164;1+70;1|N|6;Felsteel Bar;60;60|
 N Felsteel Gloves|QID|1641029619|P|Blacksmithing;164;1+75;1|RECIPE|29649|N|6;Felsteel Bar;60;60|  ; checks to see if they have next plan, if not do this step
 N Earthpeace Breastplate|QID|1641029649|P|Blacksmithing;164;1+75;1|N|4;Hardended Adamantite Bar;40;40:6;Primal Life;60;60:4;Primal Earth;40;40|
