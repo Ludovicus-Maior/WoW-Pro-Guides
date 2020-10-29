cf2e9383c8dcc0062c6485d998f9bc78ace2d083
Hendo72 <ctkbkqu32f9z@opayq.com>
2020-10-29 11:48:00 -0400

    Update INTRO_Human.lua

--- WoWPro_Leveling/Alliance/INTRO_Human.lua
+++ WoWPro_Leveling/Alliance/INTRO_Human.lua
@@ -6,7 +6,6 @@ WoWPro:GuideName(guide,"Human: Intro")
 WoWPro:GuideNextGuide(guide, 'Chromie Time')
 WoWPro:GuideSteps(guide, function()
 return [[
-
 N It's Chromie Time!|QID|62567|M|62.25,29.93|Z|Stormwind City|JUMP|Chromie Time|N|Congratulations on hitting level 10.\n\nYou can now accept Chromie's Call at the Hero's Call board in Stormwind. This will allow you to choose which expansion you want to level in.\n\nYou're free to continue your current guide or you click the guide button next to this frame to direct you to Chromie!|LVL|10|S!US|
 A Beating Them Back!|QID|28762|M|33.55,53.00|N|From Marshal McBride.|R|Human|C|Paladin|
 A Beating Them Back!|QID|28766|M|33.55,53.00|N|From Marshal McBride.|R|Human|C|Warrior|
