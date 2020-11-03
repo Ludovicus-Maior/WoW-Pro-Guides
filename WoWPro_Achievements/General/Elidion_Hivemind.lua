b093982b99cc4511ba8e9868f434b56fb02e6f68
Emmaleah <emmaleah.wow@gmail.com>
2020-11-03 02:04:49 -0800

    Update Elidion_Hivemind.lua
    more portal descriptions, fixed one step on jumping game.

--- WoWPro_Achievements/General/Elidion_Hivemind.lua
+++ WoWPro_Achievements/General/Elidion_Hivemind.lua
@@ -5,61 +5,64 @@ WoWPro:GuideLevels(guide,50, 50)
 WoWPro:GuideSteps(guide, function()
 
 return [[
-N Talisman of True Treasure Tracking|QID|40397|N|To start off this hunt you need pick up the Talisman of True Treasure Tracking in Shattrath. If you you already have it then you can skip the next few steps.|
-P Shattrath City|QID|40397|M|49.95,87.02|Z|Stormwind City|N|Take the Shattrath portal in Stormwind if you have no other means.|FACTION|Alliance|
-P Shattrath City|QID|40397|M|57.38,91.47|Z|Orgrimmar|N|Take the Shattrath portal in the lower level of the Orgrimmar portal room if you have no other means.|FACTION|Horde|
-B Talisman of True Treasure Tracking|QID|40397|M|65.64,69.26|Z|Shattrath City|N|Buy from Griftah and then equip it.|L|27944|
-N Letter from Ms. Graham|QID|40397|M|65.48,69.55|Z|Shattrath City|N|Letter is glowing blue and is on the bottom right side of the table. "The key Factor is successful Wasp Ignition is a solid Ad campaign."|
-P Highmountain|QID|40314|M|57.40,27.91|Z|Highmountain|N|Get to highmountain by any means - Dalaran Hearthstone still works.|
-N The 2nd Letter|QID|40314|M|57.40,27.91|Z|Highmountain|N|Letter is the tent to the right on top of some crates. "Of all of Gai's cures for Nature, the most liberating is Death."|
-P Karazhan|QID|40404|N|Best way is your local mage friend and portal to Stonard and fly, Lacking that, Zep to Grom'gol.|FACTION|Horde|
-P Karazhan|QID|40404|N|Best way is to fly from Stormwind.|FACTION|Alliance|
-N The 3rd Letter|QID|40404|M|46.93,74.81|Z|Deadwind Pass|N|In the original Karazhan dungeon, after the chess event get to Medivh's Chambers, there is a room with his bed in it, to the side will be a desk with a letter on the chair, click the letter. "I sat Dumbfounded, watching As the most Subtle Rat reached for the cheese a third time in under an hour."|
-P Thousand Needles|QID|40252|M|53.24,69.16|Z|Kalimdor|N|Now we need to head to Razorfen Downs in Thousand Needles. Took the Silithus portal there.|
-N The 4th Letter|QID|40252|M|53.24,69.16|Z|Kalimdor|N|The letter is located on a hay box behind the second to final boss, Death Speaker Blackthorn. "Ms. Sin will accompany you down The longest Streets Of the underworld."|
-P Mount Hyjal|QID|40293|M|44.30,47.30|Z|Mount Hyjal|N|Now we need to get to Mount Hyjal.|
-N The 5th Letter|QID|40293|M|44.30,47.30|Z|Mount Hyjal|N|The letter is on a table at the highest floor of the tree that serves as Aviana's shrine. "The Elite champions will rule the World with the mightiest F.C."|
-P Icecrown|QID|40288|N|Get to Icrown by any means, zep to Borean Tundra for Horde - Boat to Borean Tundra for alliance.|
-N The 6th Letter|QID|40288|M|70.70,73.30|Z|Icecrown|N|On top of the Ironwall Dam that separates the river that flows from Icecrown and Crystalsong Forest, you will find the next letter is floating on the tip of a spike. "Re: Codex of mastering Sine waves."|
-P Townlong Steppes|QID|50187|M|37.70,63.00|Z|Townlong Steppes|N|Get to Townlong Steppes by any means.|
-N The 7th Letter|QID|50187|M|37.81,63.00|Z|Townlong Steppes|N|The letter is located near a bell in the back part of the temple outside. "Mice look so sad when they have a Celft lip. Hoping you succeed, Ana"|
-P Borean Tundra|QID|50181|M|30.82,84.25|Z|Aegwynn's Gallery@Dalaran|N|Get to Borean Tundra by any means - Dalaran Hearthstone to Wyrmrest Temple works fast.|
-N The Blue Crystal Monocle|QID|50181|M|27.60,27.20|Z|Borean Tundra|N|The [color=3399FF]Blue[/color] Crystal Monocle is located in a box atop the highest Nexus ring in Coldarra labeled "Gift from Ms. Graham".|L|156724|
-P Spires of Arak|M|35.55,33.47|Z|Spires of Arak|N|Head to the Skyreach dungeon in Draenor. If you have a garrison hearthstone, that makes it faster.|
-N The Green Crystal Monocle|M|65.48,69.55|Z|Spires of Arak|N|After the final boss you will find orbs to interact with, to solve: Right -> Up -> Down -> Up -> Right -> Right -> Up -> Left -> Down -> Up -> Left -> Down|L|156727|
-P Uldum|M|69.09,52.83|Z|Uldum|N|Head to the Halls of Origination dungeon in Uldum.|
-N The Yellow Crystal Monocle|M|69.09,52.83|Z|Uldum|N|After the first boss there is a room with an elevator. Click on the Stellar Refraction Device below the elevator that looks like a giant glowing ball. Once you click it, you will see a bunch of colors under you. Head north and go down the path to access the puzzle. There is an addon on twitch called Hivemind HoO Puzzle Helper that can help solve this puzzle.  RED -> GREEN -> BLUE -> ORANGE|L|156726|
-P Kelp'thar Forest|M|60.00,59.00|Z|Kelp'thar Forest|N|Now to collect a TON of stuff in Vashj'ir.|
-B Sir Finley Mrrgglton - 23|M|44.54,20.23|Z|Shimmering Expanse|N|Before you start, be sure you can invest 2,380 gold and sawabout an hour into this, due to the timers, you will lose all progress if you try to return later. I have added a countdown in each title so you know how many steps are left. Buy 500 Seashells.|L|156791 500|
-B Volatile Violetscale - 22|M|40.14,77.21|Z|Abyssal Depths|N|Buy 100 Cavity-Free Great Shark Tooth. Near the bottom.|L|156780 100|
-B Manta Stargazer - 21|M|53.30,23.29|Z|Shimmering Expanse|N|Buy 50 Razoreel Larva. Near the top.|L|156786 50|
-B Lil' Whaley - 20|M|69.23,47.28|Z|Shimmering Expanse|N|Buy 250 Well-Fed Doctor Fish. Near the bottom.|L|156783 250|
-B Gloomy Bluefin - 19|M|64.34,41.00|Z|Abyssal Depths|N|Buy 10 Freshly Molted Crab Skin. All the way to the bottom.|L|156782 10|
-B Ol' Fishbreath - 18|M|60.48,58.45|Z|Kelp'thar Forest|N|Buy 50 Glittergill Glitter. Near the top.|L|156790 50|
-B Sir Finley Mrrgglton -17|M|44.54,20.23|Z|Shimmering Expanse|N|Buy 80 Seashells.|L|156791 80|
-B Gloomy Bluefin - 16|M|64.34,41.00|Z|Abyssal Depths|N|Buy 2 Giant Giant Toenail Clipping. All the way to the bottom.|L|156775 2|
-B Little Carp - 15|M|45.07,18.09|Z|Abyssal Depths|N|Buy 4 Makrura Eye. Don't swim back up, just ride in the deep.|L|156776 4|
-B Volatile Violetscale - 14|M|40.14,77.21|Z|Abyssal Depths|N|Buy 1 Accidentally Severed Seahorse Fin.|L|156773 1|
-B Crimson Angerfish - 13|M|53.00,88.00|Z|Shimmering Expanse|N|Buy 2 Shiny Sea Serpent Scale. Swims around the area.|L|156774 2|
-B Manta Stargazer - 12|M|53.30,23.29|Z|Shimmering Expanse|N|Buy 40 Symbiotic Plankton.|L|156789 40|
-B Sir Finley Mrrgglton - 11|M|44.54,20.23|Z|Shimmering Expanse|N|Buy 5 Scintillating Murloc Skin Lotion.|L|156793 5|
-B Sir Finley Mrrgglton - 10|M|44.54,20.23|Z|Shimmering Expanse|N|Buy 300 Seashells.|L|156791 300|
-B Ol' Fishbreath - 9|M|60.48,58.45|Z|Kelp'thar Forest|N|Buy 30 Vantus Black Squid Ink.|L|156771 30|
-B The Blackfish - 8|M|15.27,83.63|Z|Abyssal Depths|N|Buy 30 Super Slick Eel Slime. Near the bottom.|L|156770 30|
-B Volatile Violetscale - 7|M|40.14,77.21|Z|Abyssal Depths|N|Buy 3 Rock-Encrusted Whelk Shell.|L|156767 3|
-B Little Carp - 6|M|45.07,18.09|Z|Abyssal Depths|N|Buy 5 Potent Gastropod Gloop.|L|156788 5|
-B Sir Finley Mrrgglton - 5|M|44.54,20.23|Z|Shimmering Expanse|N|Buy 1500 Seashells.|L|156791 1500|
-B Lil' Whaley - 4|M|69.23,47.28|Z|Shimmering Expanse|N|Buy 300 Very Pretty Coral. Near the bottom.|L|156761 300|
-B Ol' Fishbreath - 3|M|60.48,58.45|Z|Kelp'thar Forest|N|Buy 100 Iridescent Shimmeray Skin.|L|156758 100|
-B Crimson Angerfish - 2|M|53.00,88.00|Z|Shimmering Expanse|N|Buy 20 Luxurious Luxscale Scale.|L|156764 20|
-B The Blackfish  - 1|M|15.27,83.63|Z|Abyssal Depths|N|Buy 5 Captured Bubbles. Near the bottom.|L|156787 5|
-B Sir Finley Mrrgglton - At last|M|44.54,20.23|Z|Shimmering Expanse|N|Buy the [color=FF0000]Red[/color] Crystal Monocle.|L|156725|
+N Rank 1 for Suramar|N|Set guide to rank 1, to skip the steps on getting monocles. Remember if you know who is getting which one, you only need one monocle of each color in your group.|
+N Talisman of True Treasure Tracking|QID|40397|N|To start off this hunt you need pick up the Talisman of True Treasure Tracking in Shattrath. If you you already have it then you can skip the next few steps.|RANK|2|
+P Shattrath City|QID|40397|M|57.38,91.47|Z|Orgrimmar|N|Take the Shattrath portal in the lower level of the Orgrimmar portal room if you have no other means.|FACTION|Horde|RANK|2|
+B Talisman of True Treasure Tracking|QID|40397|M|65.64,69.26|Z|Shattrath City|N|Buy from Griftah and then equip it.|L|27944|RANK|2|
+N Letter from Ms. Graham|QID|40397|M|65.48,69.55|Z|Shattrath City|N|Letter is glowing blue and is on the bottom right side of the table. "The key Factor is successful Wasp Ignition is a solid Ad campaign."|RANK|2|
+P Highmountain|QID|40314|M|57.40,27.91|Z|Highmountain|N|Get to highmountain by any means - Dalaran Hearthstone still works.|RANK|2|
+N The 2nd Letter|QID|40314|M|57.40,27.91|Z|Highmountain|N|Letter is the tent to the right on top of some crates. "Of all of Gai's cures for Nature, the most liberating is Death."|RANK|2|
+P Karazhan|QID|40404|N|Best way is your local mage friend and portal to Stonard and fly, Lacking that, Zep to Grom'gol.|FACTION|Horde|RANK|2|
+P Karazhan|QID|40404|N|Best way is to fly from Stormwind.|FACTION|Alliance|RANK|2|
+N The 3rd Letter|QID|40404|M|46.93,74.81|Z|Deadwind Pass|N|In the original Karazhan dungeon, after the chess event get to Medivh's Chambers, there is a room with his bed in it, to the side will be a desk with a letter on the chair, click the letter. "I sat Dumbfounded, watching As the most Subtle Rat reached for the cheese a third time in under an hour."|RANK|2|
+P Thousand Needles|QID|40252|M|53.24,69.16|Z|Kalimdor|N|Now we need to head to Razorfen Downs in Thousand Needles. Took the Silithus portal there.|RANK|2|
+N The 4th Letter|QID|40252|M|53.24,69.16|Z|Kalimdor|N|The letter is located on a hay box behind the second to final boss, Death Speaker Blackthorn. "Ms. Sin will accompany you down The longest Streets Of the underworld."|RANK|2|
+P Mount Hyjal|QID|40293|M|44.30,47.30|Z|Mount Hyjal|N|Now we need to get to Mount Hyjal.|RANK|2|
+N The 5th Letter|QID|40293|M|44.30,47.30|Z|Mount Hyjal|N|The letter is on a table at the highest floor of the tree that serves as Aviana's shrine. "The Elite champions will rule the World with the mightiest F.C."|RANK|2|
+P Crystalsong Forest|M|56.22,91.76|Z|Orgrimmar|N|Travel to Ironwall Dam via means of your choosing, most likely via the Orgrimmar portal to Crystalsong Forest. Alternatively zep to Borean Tundra.|FACTION|Horde|RANK|2|
+P Crystalsong Forest|M|44.42,88.66|Z|Stormwind City|N|Travel to Ironwall Dam via means of your choosing, most likely via the Stormwind City portal to Crystalsong Forest, Alternatively boat to Borean Tundra.|FACTION|Alliance|RANK|2|
+N The 6th Letter|QID|40288|M|70.70,73.30|Z|Icecrown|N|On top of the Ironwall Dam that separates the river that flows from Icecrown and Crystalsong Forest, you will find the next letter is floating on the tip of a spike. "Re: Codex of mastering Sine waves."|RANK|2|
+P The Jade Forest||M|45.72,87.13|Z|Stormwind City|NC|N|Take the Portal in Dalaran back to Stormwind, then portal to The Jade Forest.|FACTION|Alliance|RANK|2|
+P The Jade Forest|M|57.45,92.29|Z|Orgrimmar|NC|N|Take the Portal in Dalaran back to Orgrimmar, then portal to The Jade Forest.|FACTION|Horde|RANK|2|
+N The 7th Letter|QID|50187|M|37.81,63.00|Z|Townlong Steppes|N|The letter is located near a bell in the back part of the temple outside. "Mice look so sad when they have a Celft lip. Hoping you succeed, Ana"|RANK|2|
+P Crystalsong Forest|M|56.22,91.76|Z|Orgrimmar|N|Travel to Nexus via means of your choosing, most likely via the Orgrimmar portal to Crystalsong Forest. Alternatively zep to Borean Tundra.|FACTION|Horde|RANK|2|
+P Crystalsong Forest|M|44.42,88.66|Z|Stormwind City|N|Travel to Nexus via means of your choosing, most likely via the Stormwind City portal to Crystalsong Forest, Alternatively boat to Borean Tundra.|FACTION|Alliance|RANK|2|
+N The Blue Crystal Monocle|QID|50181|M|27.60,27.20|Z|Borean Tundra|N|The [color=3399FF]Blue[/color] Crystal Monocle is located in a box atop the highest Nexus ring in Coldarra labeled "Gift from Ms. Graham".|L|156724|RANK|2|
+P Spires of Arak|M|35.55,33.47|Z|Spires of Arak|N|Head to the Skyreach dungeon in Draenor. If you have a garrison hearthstone, that makes it faster.|RANK|2|
+N The Green Crystal Monocle|M|65.48,69.55|Z|Spires of Arak|N|After the final boss you will find orbs to interact with, to solve: Right -> Up -> Down -> Up -> Right -> Right -> Up -> Left -> Down -> Up -> Left -> Down|L|156727|RANK|2|
+P Uldum|M|69.09,52.83|Z|Uldum|N|Head to the Halls of Origination dungeon in Uldum.|RANK|2|
+N The Yellow Crystal Monocle|M|69.09,52.83|Z|Uldum|N|After the first boss there is a room with an elevator. Click on the Stellar Refraction Device below the elevator that looks like a giant glowing ball. Once you click it, you will see a bunch of colors under you. Head north and go down the path to access the puzzle. There is an addon on twitch called Hivemind HoO Puzzle Helper that can help solve this puzzle.  RED -> GREEN -> BLUE -> ORANGE|L|156726|RANK|2|
+P Kelp'thar Forest|M|60.00,59.00|Z|Kelp'thar Forest|N|Now to collect a TON of stuff in Vashj'ir.|RANK|2|
+B Sir Finley Mrrgglton - 23|M|44.54,20.23|Z|Shimmering Expanse|N|Before you start, be sure you can invest 2,380 gold and about an hour into this, due to the timers, you will lose all progress if you try to return later. I have added a countdown in each title so you know how many steps are left. Buy 500 Seashells.|L|156791 500|RANK|2|
+B Volatile Violetscale - 22|M|40.14,77.21|Z|Abyssal Depths|N|Buy 100 Cavity-Free Great Shark Tooth. Near the bottom.|L|156780 100|RANK|2|
+B Manta Stargazer - 21|M|53.30,23.29|Z|Shimmering Expanse|N|Buy 50 Razoreel Larva. Near the top.|L|156786 50|RANK|2|
+B Lil' Whaley - 20|M|69.23,47.28|Z|Shimmering Expanse|N|Buy 250 Well-Fed Doctor Fish. Near the bottom.|L|156783 250|RANK|2|
+B Gloomy Bluefin - 19|M|64.34,41.00|Z|Abyssal Depths|N|Buy 10 Freshly Molted Crab Skin. All the way to the bottom.|L|156782 10|RANK|2|
+B Ol' Fishbreath - 18|M|60.48,58.45|Z|Kelp'thar Forest|N|Buy 50 Glittergill Glitter. Near the top.|L|156790 50|RANK|2|
+B Sir Finley Mrrgglton -17|M|44.54,20.23|Z|Shimmering Expanse|N|Buy 80 Seashells.|L|156791 80|RANK|2|
+B Gloomy Bluefin - 16|M|64.34,41.00|Z|Abyssal Depths|N|Buy 2 Giant Giant Toenail Clipping. All the way to the bottom.|L|156775 2|RANK|2|
+B Little Carp - 15|M|45.07,18.09|Z|Abyssal Depths|N|Buy 4 Makrura Eye. Don't swim back up, just ride in the deep.|L|156776 4|RANK|2|
+B Volatile Violetscale - 14|M|40.14,77.21|Z|Abyssal Depths|N|Buy 1 Accidentally Severed Seahorse Fin.|L|156773 1|RANK|2|
+B Crimson Angerfish - 13|M|53.00,88.00|Z|Shimmering Expanse|N|Buy 2 Shiny Sea Serpent Scale. Swims around the area.|L|156774 2|RANK|2|
+B Manta Stargazer - 12|M|53.30,23.29|Z|Shimmering Expanse|N|Buy 40 Symbiotic Plankton.|L|156789 40|RANK|2|
+B Sir Finley Mrrgglton - 11|M|44.54,20.23|Z|Shimmering Expanse|N|Buy 5 Scintillating Murloc Skin Lotion.|L|156793 5|RANK|2|
+B Sir Finley Mrrgglton - 10|M|44.54,20.23|Z|Shimmering Expanse|N|Buy 300 Seashells.|L|156791 300|RANK|2|
+B Ol' Fishbreath - 9|M|60.48,58.45|Z|Kelp'thar Forest|N|Buy 30 Vantus Black Squid Ink.|L|156771 30|RANK|2|
+B The Blackfish - 8|M|15.27,83.63|Z|Abyssal Depths|N|Buy 30 Super Slick Eel Slime. Near the bottom.|L|156770 30|RANK|2|
+B Volatile Violetscale - 7|M|40.14,77.21|Z|Abyssal Depths|N|Buy 3 Rock-Encrusted Whelk Shell.|L|156767 3|RANK|2|
+B Little Carp - 6|M|45.07,18.09|Z|Abyssal Depths|N|Buy 5 Potent Gastropod Gloop.|L|156788 5|RANK|2|
+B Sir Finley Mrrgglton - 5|M|44.54,20.23|Z|Shimmering Expanse|N|Buy 1500 Seashells.|L|156791 1500|RANK|2|
+B Lil' Whaley - 4|M|69.23,47.28|Z|Shimmering Expanse|N|Buy 300 Very Pretty Coral. Near the bottom.|L|156761 300|RANK|2|
+B Ol' Fishbreath - 3|M|60.48,58.45|Z|Kelp'thar Forest|N|Buy 100 Iridescent Shimmeray Skin.|L|156758 100|RANK|2|
+B Crimson Angerfish - 2|M|53.00,88.00|Z|Shimmering Expanse|N|Buy 20 Luxurious Luxscale Scale.|L|156764 20|RANK|2|
+B The Blackfish  - 1|M|15.27,83.63|Z|Abyssal Depths|N|Buy 5 Captured Bubbles. Near the bottom.|L|156787 5|RANK|2|
+B Sir Finley Mrrgglton - At last|M|44.54,20.23|Z|Shimmering Expanse|N|Buy the [color=FF0000]Red[/color] Crystal Monocle.|L|156725|RANK|2|
 P Suramar|M|41.0,69.5|Z|Suramar|N|Now you must make your way to Suramar with a group of other people that have the monacles.|
 N Rikei|M|71.80,62.50|Z|Suramar|N|Someone in group will need wear the [color=FF0000]Red[/color] Monocle. Click off if this isn't you|
 N Blom'an|M|46.80,28.60|Z|Suramar|N|Someone in group will need to wear the [color=3399FF]Blue[/color] Monocle. Click off if this isn't you|
 N Giluzui|M|19.90,46.30|Z|Suramar|N|Someone in group will need to wear the [color=00FF33]Green[/color] Monocle. Click off if this isn't you|
 N Yorilan|M|43.50,81.80|Z|Suramar|N|[color=919191]Someone in group will need to wear the [/color][color=FFF000]Yellow[/color][color=919191] Monocle. Click off if this isn't you.[/color]|
-N Beams|M|41.0,69.5|Z|Suramar|N|Someone in group will need to wait here by the beams. Once everyone is ready, they attack the withered unil they start casting "Draw Energy". at that point the coresponding beam color will go away. This 5th person runs in and interacts with the "Lost Cat Toy". When you do, you will be teleported out of the room and take damage. Let the group how much damage was done (include overkill damage) as that will determine how many times you need to pet Lady Chatons Kittens in the Court of Stars.|
+N Beams|M|41.0,69.5|Z|Suramar|N|Someone in group will need to wait here by the beams. Once everyone is ready, they attack the withered and DPS them down to very low health, (so you can kill with one spell/hit). When everyone has their withered at that point, Coordinate so you all do the last hit simultaineously so all are casthing "Draw Energy". At that point the coresponding beam color will go away. This 5th person runs in and interacts with the "Lost Cat Toy". When you do, you will be teleported out of the room and take damage. Let the group how much damage was done (include overkill damage) as that will determine how many times you need to pet Lady Chatons Kittens in the Court of Stars.|
 P Court of Stars|M|50.84,65.54|Z|Suramar|N|Now that the beam player has the damage number, head to the court of stars and make your way near the 2nd boss.|
 N Manasaber Kittens|N|Near Lady Chaton, assign each player to a kitten to pet at the same time as the buff only lasts 15 seconds. The damage number should be 5 digits, if it is 4 then the first number is 0. 0 means do not pet. There is a coresponding buff on the kitten for each pet to help you keep track. With your 5 digit number,\nPet Mrs. Fluffymuffins the number of times as first digit\nShadow-pet amount of second digit,\nMew-pet amount of third digit,\nAsh-pet amount of forth digit\nBella-pet amount of last digit. Click off once the cats run off and void orb appears.|
 N Jump Puzzle|N|Click on the void orb and it will port you to a jumping puzzle, you will need to coordinate well with your group to get across. Assign a target marker ([color=FF0000]Cross[/color], [color=3399FF]Square[/color], [color=00FF33]Triangle[/color], [color=FF00CC]Diamond[/color],[color=919191]Moon[/color]) to each player to help guide you through the puzzle. Skip to proceed.|
@@ -111,7 +114,7 @@ N Cross|N|[color=FF0000]Right[/color]|
 N Moon|N|[color=919191]Left[/color]|
 N Cross|N|[color=FF0000]Forward[/color]|
 N Square|N|[color=3399FF]Forward[/color]|
-N Triangle|N|[color=00FF33]Jump off (Leave vehicle). Goto far left platform and Jump on to it[/color]|
+N Triangle|N|[color=00FF33]Jump off (Leave vehicle). Goto far left platform and Jump on to it, then Forward.[/color]|
 N Square|N|[color=3399FF]Forward[/color]|
 N Triangle|N|[color=00FF33]Forward, then Forward, then Forward[/color]|
 N Square|N|[color=3399FF]Back[/color]|
