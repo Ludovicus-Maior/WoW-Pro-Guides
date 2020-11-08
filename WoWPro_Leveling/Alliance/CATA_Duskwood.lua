9275fece6ce73b7232c27a700443428099348d0f
Hendo72 <ctkbkqu32f9z@opayq.com>
2020-11-08 11:44:49 -0500

    Update CATA_Duskwood.lua

--- WoWPro_Leveling/Alliance/CATA_Duskwood.lua
+++ WoWPro_Leveling/Alliance/CATA_Duskwood.lua
@@ -64,77 +64,77 @@ A Classy Glass|QID|26685|M|79.53,47.41|N|From Viktori Prism'Antras.|PRE|26684|
 T Juice Delivery|QID|26676|M|87.43,35.21|N|To Abercrombie.|
 A Ogre Thieves|QID|26680|M|87.43,35.21|N|From Abercrombie.|PRE|26676|
 A The Legend of Stalvan|QID|26666|M|79.04,44.23|N|From Tobias Mistmantle.|
-A Worgen in the Woods|QID|26688|M|75.28,47.95|N|From Calor.|
-T The Legend of Stalvan|QID|26666|M|72.50,46.87|N|To Clerk Daltry.|
-A The Stolen Letters|QID|26667|M|72.50,46.87|N|From Clerk Daltry.|PRE|26666|
-C Worgen in the Woods|QID|26688|M|75.28,47.95|N|Kill Worgen as you go in Brightwood Grove.|S|
-C The Stolen Letters|QID|26667|M|61.24,40.39|N|Laying on the ground outside the tents.|NC|
-C Worgen in the Woods|QID|26688|M|75.28,47.95|N|Finish Killing Worgen.|US|
-T The Stolen Letters|QID|26667|M|72.50,46.87|N|To Clerk Daltry.|
-A In A Dark Corner|QID|26669|M|72.50,46.87|N|From Clerk Daltry.|PRE|26667|
-T Worgen in the Woods|QID|26688|M|75.28,47.95|N|To Calor across.|
-A The Rotting Orchard|QID|26689|M|75.28,47.95|N|From Calor.|PRE|26688|
+A Worgen in the Woods|QID|26688|M|75.33,48.01|N|From Calor.|
+T The Legend of Stalvan|QID|26666|M|72.45,46.91|N|To Clerk Daltry.|
+A The Stolen Letters|QID|26667|M|72.45,46.91|N|From Clerk Daltry.|PRE|26666|
+C Worgen in the Woods|QID|26688|M|75.33,48.01|N|Kill Worgen as you go in Brightwood Grove.|S|
+C The Stolen Letters|QID|26667|M|61.25,40.51|N|Laying on the ground outside the tents.|NC|
+C Worgen in the Woods|QID|26688|M|75.33,48.01|N|Finish Killing Worgen.|US|
+T The Stolen Letters|QID|26667|M|72.45,46.91|N|To Clerk Daltry.|
+A In A Dark Corner|QID|26669|M|72.45,46.91|N|From Clerk Daltry.|PRE|26667|
+T Worgen in the Woods|QID|26688|M|75.33,48.01|N|To Calor across.|
+A The Rotting Orchard|QID|26689|M|75.33,48.01|N|From Calor.|PRE|26688|
 C The Rotting Orchard|QID|26689|N|Kill Nightbane Shadow Weavers. Beware of the hidden stalkers in this area.|S|
-C In A Dark Corner|QID|26669|M|66.5,76.5|N|Inside the barn, at the back.|NC|
-C The Rotting Orchard|QID|26689|N|Finish Killing Nightbane Shadow Weavers. Beware of the hidden stalkers in this area.|US|
-A Vulgar Vul'Gol|QID|25235|M|45.18,66.95|N|From Watcher Dodds, along the road to the west.|
+C In A Dark Corner|QID|26669|M|66.59,76.45|N|Inside the barn, at the back.|NC|
+C The Rotting Orchard|QID|26689|M|65.27,74.51|N|Finish Killing Nightbane Shadow Weavers. Beware of the hidden stalkers in this area.|US|
+A Vulgar Vul'Gol|QID|25235|M|45.12,67.03|N|From Watcher Dodds, along the road to the west.|
 C Vulgar Vul'Gol|QID|25235|M|41.91,68.86|N|Kill any type Splinter Fist Ogres.|S|
-C Ogre Thieves|QID|26680|M|33.45,75.27|N|Just outside the cave, pickup Abercrombie's Crate.|NC|
+C Ogre Thieves|QID|26680|M|33.51,75.32|N|Just outside the cave, pickup Abercrombie's Crate.|NC|
 C Classy Glass|QID|26685|M|37.84,84.33|N|Head to the back of the cave, kill and loot Zzarc'Vul.|
 C Vulgar Vul'Gol|QID|25235|M|41.91,68.86|N|Finish killing Splinter Fist Ogres.|US|
-T Vulgar Vul'Gol|QID|25235|M|45.18,66.95|N|To Watcher Dodds.|
-A The Yorgen Worgen|QID|26717|M|44.81,67.32|N|From Apprentice Fess.|S|
-A A Deadly Vine|QID|26707|M|44.81,67.32|N|From Apprentice Fess.|
-A The Yorgen Worgen|QID|26717|M|44.81,67.32|N|From Apprentice Fess.|US|
+T Vulgar Vul'Gol|QID|25235|M|45.12,67.03|N|To Watcher Dodds.|
+A The Yorgen Worgen|QID|26717|M|44.92,67.42|N|From Apprentice Fess.|S|
+A A Deadly Vine|QID|26707|M|44.92,67.42|N|From Apprentice Fess.|
+A The Yorgen Worgen|QID|26717|M|44.92,67.42|N|From Apprentice Fess.|US|
 C A Deadly Vine|QID|26707|M|49.57,76.46|N|Kill and loot the Corpseweeds.|S|
 C The Yorgen Worgen|QID|26717|M|49.68,77.88|N|Between the two buildings, loot the Mound of Loose Dirt.  When you collect, a lurking worgen will pounce on you, knock you out, then run away.|NC|
 C A Deadly Vine|QID|26707|M|49.57,76.46|N|Kill and loot the Corpseweeds.|US|
-T The Yorgen Worgen|QID|26717|M|44.81,67.32|N|To Apprentice Fess.|S|
-T A Deadly Vine|QID|26707|M|44.81,67.32|N|To Apprentice Fess.|
-T The Yorgen Worgen|QID|26717|M|44.81,67.32|N|To Apprentice Fess.|US|
-A Delivery to Master Harris|QID|26719|M|44.81,67.32|N|From Apprentice Fess.|PRE|26717|
-A The Jitters-Bugs|QID|26721|M|18.6,58.21|N|From Jitters. Head to Raven hill to grab this quest and the flightpath to save time later.|
-f Raven Hill|QID|26777|M|21.03,56.63|N|Grab flightpath at Raven Hill. At John Shelby.|
-H Scarlet Raven Tavern |QID|26721|N|Fly or use your Hearthstone back to Darkshire.|
-T In A Dark Corner|QID|26669|M|72.50,46.87|N|To Clerk Daltry.|
-A Roland's Doom|QID|26670|M|72.50,46.87|N|From Clerk Daltry.|PRE|26669|
-T The Rotting Orchard|QID|26689|M|75.28,47.95|N|To Calor.|
-A Vile and Tainted|QID|26690|M|75.28,47.95|N|From Calor.|PRE|26689|
+T The Yorgen Worgen|QID|26717|M|44.92,67.42|N|To Apprentice Fess.|S|
+T A Deadly Vine|QID|26707|M|44.92,67.42|N|To Apprentice Fess.|
+T The Yorgen Worgen|QID|26717|M|44.92,67.42|N|To Apprentice Fess.|US|
+A Delivery to Master Harris|QID|26719|M|44.92,67.42|N|From Apprentice Fess.|PRE|26717|
+A The Jitters-Bugs|QID|26721|M|18.62,58.36|N|From Jitters. Head to Raven Hill to grab this quest and the flightpath to save time later.|
+f Raven Hill|QID|26669|M|21.08,56.45|N|Grab the Raven Hill flightpath from John Shelby.|TAXI|-Raven Hill|
+F Darkshire|ACTIVE|26669|M|21.08,56.45|N|Fly back to Darkshire.|
+T In A Dark Corner|QID|26669|M|72.45,46.91|N|To Clerk Daltry.|
+A Roland's Doom|QID|26670|M|72.45,46.91|N|From Clerk Daltry.|PRE|26669|
+T The Rotting Orchard|QID|26689|M|75.33,48.01|N|To Calor.|
+A Vile and Tainted|QID|26690|M|75.33,48.01|N|From Calor.|PRE|26689|
 T Classy Glass|QID|26685|M|79.53,47.41|N|To Viktori Prism'Antras.|
 T Ogre Thieves|QID|26680|M|87.43,35.21|N|To Abercrombie at Beggar's Haunt.|
 A Ghoulish Effigy|QID|26677|M|87.43,35.21|N|From Abercrombie.|PRE|26680|
 C Ghoulish Effigy|QID|26677|M|76.83,33.72|N|Kill and loot Fetid Corpses at Manor Mistmantle.|
 T Ghoulish Effigy|QID|26677|M|87.43,35.21|N|To Abercrombie.|
 A Note to the Mayor|QID|26681|M|87.43,35.21|N|From Abercrombie.|PRE|26677|
-T Note to the Mayor|QID|26681|M|71.85,46.45|N|To Lord Ello Ebonlocke.|
-A The Embalmer's Revenge|QID|26727|M|71.85,46.45|N|From Lord Ello Ebonlocke.|PRE|26681|
+T Note to the Mayor|QID|26681|M|71.93,46.44|N|To Lord Ello Ebonlocke.|
+A The Embalmer's Revenge|QID|26727|M|71.93,46.44|N|From Lord Ello Ebonlocke.|PRE|26681|
 C The Embalmer's Revenge|QID|26727|M|73.84,46.62|N|Kill Stitches, he spawns in the middle of town. If you die, come back quickly as you can still finish him.|
-T The Embalmer's Revenge|QID|26727|M|71.85,46.45|N|To Lord Ello Ebonlocke.|
-C The Jitters-Bugs|QID|26721|M|73.63,79.19|N|Kill and loot Black Widows for their Venom Sacks. Around the area as you go.|S|
+T The Embalmer's Revenge|QID|26727|M|71.93,46.44|N|To Lord Ello Ebonlocke.|
+C The Jitters-Bugs|QID|26721|M|73.63,79.19|N|Kill and loot Black Widows for their Venom Sacks.|S|
 C Vile and Tainted|QID|26690|M|73.63,79.19|N|Kill Nightbane Vile Fangs and Tainted Ones.|S|
-C Roland's Doom|QID|26670|M|73.63,79.19|N|In the back of the cave, pick up the Muddy Journal Page.|NC|
-C Vile and Tainted|QID|26690|N|Finish killing Worgen.|US|
-T Vile and Tainted|QID|26690|M|75.28,47.95|N|To Calor.|
-A Worgen in the Woods|QID|26691|M|75.28,47.95|N|From Calor.|PRE|26690|
+C Roland's Doom|QID|26670|M|73.63,79.19|N|Head to the back of the cave and pick up the Muddy Journal Page.|NC|
+C Vile and Tainted|QID|26690|M|72.76,73.26|N|Finish killing Worgen.|US|
+T Vile and Tainted|QID|26690|M|75.33,48.01|N|To Calor.|
+A Worgen in the Woods|QID|26691|M|75.33,48.01|N|From Calor.|PRE|26690|
 T Worgen in the Woods|QID|26691|M|75.35,48.93|N|To Jonathan Carevin.|
-T Roland's Doom|QID|26670|M|72.50,46.87|N|To Clerk Daltry.|
-A The Fate of Stalvan Mistmantle|QID|26671|M|72.50,46.87|N|From Clerk Daltry.|PRE|26670|
+T Roland's Doom|QID|26670|M|72.45,46.91|N|To Clerk Daltry.|
+A The Fate of Stalvan Mistmantle|QID|26671|M|72.45,46.91|N|From Clerk Daltry.|PRE|26670|
 T The Fate of Stalvan Mistmantle|QID|26671|M|78.95,44.25|N|To Tobias Mistmantle.|
 A Clawing at the Truth|QID|26672|M|78.95,44.25|N|From Tobias Mistmantle.|PRE|26671|
 T Clawing at the Truth|QID|26672|M|75.76,45.28|N|To Madame Eva.|
 A Mistmantle's Revenge|QID|26674|M|75.76,45.28|N|From Madame Eva.|PRE|26672|
 C Mistmantle's Revenge|QID|26674|M|77.39,36.32|N|Use the ring at the manor house. Then kill Stalvan Mistmantle after the talking.|U|59363|
-T Mistmantle's Revenge|QID|26674|M|78.98,44.21|N|To Tobia Mismantle.|
-F Raven Hill|ACTIVE|26760|M|77.45,44.25|N|Fly to Raven Hill.|
+T Mistmantle's Revenge|QID|26674|M|79.09,44.18|N|To Tobia Mismantle.|
+F Raven Hill|ACTIVE|26719|M|77.45,44.25|N|Fly to Raven Hill.|
 T Delivery to Master Harris|QID|26719|M|18.40,57.76|N|Deliver the message to Oliver Harris at Raven Hill.|
 A A Curse We Cannot Lift|QID|26720|M|18.40,57.76|N|From Oliver Harris.|PRE|26719|
 A Soothing Spirits|QID|26777|M|19.92,57.88|N|From Sister Eisington.|
-C The Jitters-Bugs|QID|26721|M|27.72,41.06|N|Kill and loot Black Widows for their Venom Sacks. They will 'disappear' on killing them, but they will return and be lootable a few seconds later.|US|
+C The Jitters-Bugs|QID|26721|M|27.72,41.06|N|Kill and loot Black Widows for their Venom Sacks.|US|
 T The Jitters-Bugs|QID|26721|M|18.6,58.21|N|To Jitters.|
 A Bear In Mind|QID|26787|M|18.6,58.21|N|From Jitters.|PRE|26721|
 C Bear In Mind|QID|26787|M|21.61,73.41|N|Kill and loot Black Bears for their brains.|S|
 C Soothing Spirits|QID|26777|M|21.32,71.48|N|Use the Holy Censer on Forlon Spirits.|U|60225|S|NC|
-C A Curse We Cannot Lift|QID|26720|M|21.61,73.41|N|Go into the barn at Addle's Stead.  The Lurking Worgen will jump out and this time attack.  Bring his health down until he stares and hesitates, then use Harris's Ampule.|U|60206|
+C A Curse We Cannot Lift|QID|26720|M|21.61,73.41|N|Go into the barn at Addle's Stead. The Lurking Worgen will jump out and this time attack. Bring his health down until he stares and hesitates, then use Harris's Ampule.|U|60206|
 C Soothing Spirits|QID|26777|M|21.32,71.48|N|Use the Holy Censer on Forlon Spirits.|U|60225|US|NC|
 T Soothing Spirits|QID|26777|M|19.92,57.88|N|To Sister Eisington.|
 T A Curse We Cannot Lift|QID|26720|M|18.40,57.76|N|To Oliver Harris.|
@@ -164,7 +164,7 @@ C The Cries of the Dead|QID|26778|M|16,48|N|Leave the catacombs by following the
 T Morbent's Bane|QID|26754|M|18.40,57.94|N|To Sven Yorgen.|O|
 T The Cries of the Dead|QID|26778|M|19.92,57.88|N|To Sister Eisington.|
 A Rebels Without a Clue|QID|26838|LEAD|26735|M|19.92,57.88|N|If you intend on going onto Northern Stranglethorn next, then accept this quest from Sister Eisington. This quest will not show up if you have the quest "Hero's Call: Northern Stranglethorn Vale!" or done the Hero's Call quest already.|
-F Darkshire|ACTIVE|26793|M|21.03,56.63|N|Fly back to, or hearthstone to Darkshire.|
+F Darkshire|ACTIVE|26793|M|21.08,56.45|N|Fly back to, or hearthstone to Darkshire.|
 T The Weathered Grave|QID|26793|M|72.62,47.64|N|To Sirra Von'lndi.|
 A Morgan Ladimore|QID|26794|M|72.62,47.64|N|From Sirra Von'lndi.|PRE|26793|
 T Morgan Ladimore|QID|26794|M|73.53,46.93|N|To Commander Althea Ebonlocke.|
