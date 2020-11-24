71b4c6793212c49e3d010c30379648943e234d77
Emmaleah <emmaleah.wow@gmail.com>
2020-11-23 17:20:51 -0800

    Update SL_Bastion.lua
    Move inspired moral inventory back...

--- WoWPro_Leveling/Neutral/SL_Bastion.lua
+++ WoWPro_Leveling/Neutral/SL_Bastion.lua
@@ -225,10 +225,8 @@ T A Gift for An Acolyte|QID|62714|M|53.87,73.95|Z|Bastion!The Shadowlands|N|To A
 A More Than A Gift|QID|62715|M|53.87,73.95|Z|Bastion!The Shadowlands|N|From Acolyte Amalthina.|PRE|62714|RANK|2|
 C The Temple of Purity|QID|57270|M|54.09,73.62|Z|Bastion!The Shadowlands|NC|N|Find Erida in the Temple of Purity.|MS|
 T The Temple of Purity|QID|57270|M|54.09,73.62|N|To Eridia.|MS|
-A An Inspired Moral Inventory|QID|57444|M|54.03,73.94|Z|Bastion!The Shadowlands|N|From Acolyte Galistos.|RANK|2|
 A Scour the Temple-Bonus Objective|QID|62705|M|55.29,73.57|Z|Bastion!The Shadowlands|N|Autoaccepted as you enter the temple grounds.|TOF|RANK|2|
 C Scour the Temple-Bonus Objective|QID|62705|M|55.29,73.57|Z|Bastion!The Shadowlands|S!US|N|Kill stuff, rescue terrified stewards, destroy scrolls, do quests.|TOF|
-C An Inspired Moral Inventory|QID|57444|M|58.00,75.85|Z|Bastion!The Shadowlands|N|Click the "[color=40C7EB]Introspection[/color]" Action Ability to throw a spear at the Elites to make them Non-Elite and kill them.|EAB|S|
 A A Temple in Need|QID|57977|M|54.09,73.62|Z|Bastion!The Shadowlands|N|From Acolyte Amalthina.|PRE|57270|MS|
 A On the Edge of a Revelation|QID|57264|M|56.68,74.41|Z|Bastion!The Shadowlands|N|From Desciple Kosmas.|PRE|57270|MS|
 C On the Edge of a Revelation|QID|57264|M|57.89,73.48|Z|Bastion!The Shadowlands|CHAT|N|Chat with the Vulnerable Aspirants to inspire them to action.|S|MS|
@@ -251,6 +249,8 @@ A A Once Sweet Sound|QID|57037|M|59.31,77.06|Z|Bastion!The Shadowlands|N|From Er
 A The Hand of Purification|QID|59147|M|59.31,77.06|Z|Bastion!The Shadowlands|N|From Eridia.|PRE|57717|MS|
 C The Hand of Purification|QID|59147|M|59.31,77.06|Z|Bastion!The Shadowlands|N|Cleanse the Temple by killing all the hostile mobs and rescuiing the terrified stewards.|S|MS|
 A Dangerous Discourse|QID|57719^60292|M|60.09,73.72|Z|Bastion!The Shadowlands|N|From The Scroll in front of the Vespar of Harmony.|PRE|57717|MS|
+A An Inspired Moral Inventory|QID|57444|M|54.03,73.94|Z|Bastion!The Shadowlands|N|From Acolyte Galistos.|RANK|2|
+C An Inspired Moral Inventory|QID|57444|M|57.98,75.81|Z|Bastion!The Shadowlands|N|Click the "[color=40C7EB]Introspection[/color]" Action Ability to throw a spear at the Elites to make them Non-Elite and kill them.|EAB|S|
 C Dangerous Discourse|QID|57719|M|60.09,73.72|Z|Bastion!The Shadowlands|NC|N|Find the scrolls around the area.|S|MS|
 C A Once Sweet Sound|QID|57037|M|60.08,73.49|Z|Bastion!The Shadowlands|QO|2|NC|N|Click the bell to clease the Vesper of Harmony.|MS|
 K Fallen Acolyte Erisne|QID|58222|QO|1|RARE|M|60.20,72.99|Z|Bastion!The Shadowlands|N|This silver elite paths around the Vesper of Harmony.|ITEM|180444|ACH|14307;16|
