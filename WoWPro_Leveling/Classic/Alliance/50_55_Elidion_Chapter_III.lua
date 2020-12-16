761b8e9c3fc0d9bdebc3723596b36af2b7ed2e58
Ludovicus <ludovicus.maior@gmail.com>
2020-12-16 14:06:27 -0500

    Added a | to [B Portal: DarnassusQID].

--- WoWPro_Leveling/Classic/Alliance/50_55_Elidion_Chapter_III.lua
+++ WoWPro_Leveling/Classic/Alliance/50_55_Elidion_Chapter_III.lua
@@ -17,7 +17,7 @@ A Rise of the Silithid|QID|162|M|50.21,27.48|Z|Tanaris|N|From Senior Surveyor Fi
 
 R Darnassus|QID|3449|M|55.92,89.66|Z|Teldrassil|N|Make your way to Darnassus by whatever means.|
 B Mithril Casing|QID|4244|M|PLAYER|Z|Darnassus|N|Made by engineers, buy or make a Mithril Casing before you leave.|L|10561|ITEM|10561|
-B Portal: DarnassusQID|3449|M|40.57,82.15|Z|Darnassus|N|[color=40C7EB]Mages:[/color]While you're here be sure to get your Portal:Darnassus. (Teleport as well if you haven't already)|LVL|50|C|Mage|SPELL|11419|
+B Portal: Darnassus|QID|3449|M|40.57,82.15|Z|Darnassus|N|[color=40C7EB]Mages:[/color]While you're here be sure to get your Portal:Darnassus. (Teleport as well if you haven't already)|LVL|51|C|Mage|SPELL|11419|
 T Rise of the Silithid|QID|162|M|41.82,85.63|Z|Darnassus|N|To Gracina Spiritmight.|
 A March of the Silithid|QID|4493|M|41.82,85.63|Z|Darnassus|N|From Gracina Spiritmight.|LVL|50|
 T Assisting Arch Druid Staghelm|QID|3790|M|35.31,8.94|Z|Darnassus|N|To Arch Druid Staghelm. Up on top of the tree.|
