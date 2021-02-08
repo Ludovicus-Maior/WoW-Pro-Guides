cb7f15dc559c0d5bfedb81fd29abb4dcd90fd6b7
Hendo72 <ctkbkqu32f9z@opayq.com>
2021-02-07 22:05:01 -0500

    Create BC_Dungeon_Blackfathom_Deeps.lua
    I'm assuming it would be BC. WH lists it under Classic 1-30
    Very much WIP

---
+++ WoWPro_Leveling/Horde/BC_Dungeon_Blackfathom_Deeps.lua
@@ -0,0 +1,14 @@
+local guide = WoWPro:RegisterGuide('Blackfathom Deeps', 'Leveling', 'Orgrimmar', 'Tester', 'Horde')
+WoWPro:GuideName(guide,"Blackfathom Deeps")
+WoWPro:GuideLevels(guide,1, 50)
+WoWPro:GuideNextGuide(guide, 'ChromieTime')
+WoWPro:GuideSteps(guide, function()
+return [[
+A The Rise of Aku'mai|QID|34673|Z|The Pool of Ask'Ar@Blackfathom Deeps!Dungeon|N|From Zeya.|
+C Blackfathom Deeps|Z|Blackfathom Deeps|SO|1|S|N|Defeat the bosses of Blackfathom Deeps|
+C Defeat Ghamoo-ra|Z|The Pool of Ask'Ar@Blackfathom Deeps!Dungeon|SO|1;1|N|Defeat Ghamoo-ra|
+C Defeat Thruk|Z|Moonshrine Sanctum@Blackfathom Deeps!Dungeon|SO|1;2|N|Defeat Thruk|
+C Defeat Twilight Lord Bathiel|Z|Moonshrine Sanctum@Blackfathom Deeps!Dungeon|SO|1;4|N|Defeat Twilight Lord Bathiel|
+]]
+
+end)
