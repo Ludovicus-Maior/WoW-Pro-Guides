77ccc4744d8476ec57eab14ccf955a9949b67d03
Ludovicus Maior <46032662+Ludovicus-Maior@users.noreply.github.com>
2021-03-11 18:26:35 -0500

    Update SL_Ardenweald.lua
    Night Fae First

--- WoWPro_Leveling/Neutral/SL_Ardenweald.lua
+++ WoWPro_Leveling/Neutral/SL_Ardenweald.lua
@@ -615,7 +615,7 @@ T Thread of Hope|QID|60065|M|38.97,29.93|Z|Ardenweald!The Shadowlands|N|To Caret
 A Silk for Ardenweald|QID|60066|PRE|60065&60061|M|38.97,29.93|Z|Ardenweald!The Shadowlands|N|From Caretaker Lillythistle.|RANK|2|
 T Anima-Laden Dreamcatcher|QID|62259|M|38.17,36.75|Z|Ardenweald!The Shadowlands|N|To Droman Dawnblossom in Dreamsong Fenn.|
 $ Dreamsong Heart|QID|61070|CS|M|38.01,36.29;37.6,37.1|Z|Ardenweald!The Shadowlands|N|Step on Bounding Shroom at first coordinate (behind bramble wall behind Dawnblossom), jump to treasure at top of the tree. (2nd coord). It may take more than one try. you need to WALK out the tiny branch to get it.|ITEM|179510|ACH|14313;11|
-T Silk for Ardenweald|QID|60066|M|48.48,50.40|Z|Ardenweald!The Shadowlands|N|Bring Gertie Jr. to Aithlyn at the Refugee.|
 T Silk for Ardenweald|QID|60066|M|59.46,31.85|Z|The Trunk@Heart of the Forest!Dungeon|N|Bring Gertie Jr. to Aithlyn at the Heart of the Forest.|COV|Night Fae|
+T Silk for Ardenweald|QID|60066|M|48.48,50.40|Z|Ardenweald!The Shadowlands|N|Bring Gertie Jr. to Aithlyn at the Refugee.|
 ]]
 end)
