f28749ff38545c21abe42bce7012174bace8cb17
Cagomei <55269647+Cagomei@users.noreply.github.com>
2021-07-29 19:52:34 -0500

    Update WOTLK_Howling_Fjord.lua

--- WoWPro_Leveling/Alliance/WOTLK_Howling_Fjord.lua
+++ WoWPro_Leveling/Alliance/WOTLK_Howling_Fjord.lua
@@ -7,7 +7,7 @@ WoWPro:GuideSteps(guide, function()
 return [[
 N It's Chromie Time!|AVAILABLE|62567|M|62.25,29.93|Z|Stormwind City|JUMP|Chromie Time|S!US|N|You can now accept Chromie's Call at the Hero's Call board in Stormwind. This will allow you to choose which expansion you want to level in and scale the content to your level.\n\nYou're free to continue your current guide by skipping this and continuing on, but it won't continue to scale. If you want to enable Chromie Time, click the guide button next to this frame to direct you to Chromie in Stormwind!|LVL|-50|CT|
 N Howling Fjord|QID|11228|N|Get to Howling Fjord with the boat from Menethil Harbor|
-T To Northrend!|QID|60096|M|PLAYER|N|Quest turn-in on quest tracker once you enter Howling Fjord.|
+T Wrath of the Lich King: To Northrend!|QID|60096|M|PLAYER|N|Quest turn-in on quest tracker once you enter Howling Fjord.|
 A Hell Has Frozen Over...|QID|11228|M|61.1,62.7|N|From Macalroy.|
 T Hell Has Frozen Over...|QID|11228|M|60.5,61.1|N|To Vice Admiral Keller.|
 A If Valgarde Falls...|QID|11243|PRE|11228|M|60.5,61.1|N|From Vice Admiral Keller.|
