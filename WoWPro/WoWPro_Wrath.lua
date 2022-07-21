777ca85c5f7ad877333983457f9a5db97a3263fa
Ludovicus <ludovicus.maior@gmail.com>
2022-07-21 16:43:31 -0400

    Force WoWPro.WRATH to be set if using WoWPro_Wrath.toc

---
+++ WoWPro/WoWPro_Wrath.lua
@@ -0,0 +1,9 @@
+--------------------------
+--    WoWPro_Wrath.lua  --
+--------------------------
+
+--- Force Wrath
+WoWPro.BC = false
+WoWPro.CLASSIC = false
+WoWPro.RETAIL = false
+WoWPro.WRATH = true
