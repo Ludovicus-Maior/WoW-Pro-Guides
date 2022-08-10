376f1739ced195d8e3924fc8d7bd0621cb73a7e7
Cagomei <sshort8503@hotmail.com>
2022-08-10 15:25:42 -0500

    Update VAN_BCC_54_55_Winterspring.lua

--- WoWPro_Leveling/Classic_BC/Horde/VAN_BCC_54_55_Winterspring.lua
+++ WoWPro_Leveling/Classic_BC/Horde/VAN_BCC_54_55_Winterspring.lua
@@ -128,7 +128,7 @@ A Testing for Corruption - Felwood|QID|-4561&4293|M|47.72,73.57|Z|Undercity|L|12
 T A Sample of Slime...|QID|4293|M|47.45,73.36|Z|1458|N|To Chemist Fuely beside you.|U|15102|O|
 U Un'Goro Tested Sample|ACTIVE|4294|N|Open the sample.|
 A Testing for Impurities - Un'Goro Crater|QID|-4561&4294|M|47.72,73.57|Z|Undercity|L|12236 5|N|From the Testing Equipment in the Apothecarium.|NOCACHE|
-A Testing for Impurities - Un'Goro Crater|QID|-4561&2494|M|47.72,73.57|Z|Undercity|L|12235 -1|N|From the Testing Equipment in the Apothecarium.\n[color=FF0000]NOTE: [/color]Finish testing the rest of your Samples before you turn this in '... and a Batch of Ooze'; as you will no longer be able to after that.|NOCACHE|
+A Testing for Impurities - Un'Goro Crater|QID|-4561&4294|M|47.72,73.57|Z|Undercity|L|12235 -1|N|From the Testing Equipment in the Apothecarium.\n[color=FF0000]NOTE: [/color]Finish testing the rest of your Samples before you turn this in '... and a Batch of Ooze'; as you will no longer be able to after that.|NOCACHE|
 T ... and a Batch of Ooze|QID|4294|M|47.45,73.36|Z|1458|N|To Chemist Fuely beside you.|
 A Melding of Influences|QID|4642|M|47.45,73.36|Z|1458|N|From Chemist Fuely.|PRE|4293&4294|
 * Felwood Slime Sample|ACTIVE|-4293|AVAILABLE|-4293|N|You can no longer use these. Free up some bag space.|U|12230|
