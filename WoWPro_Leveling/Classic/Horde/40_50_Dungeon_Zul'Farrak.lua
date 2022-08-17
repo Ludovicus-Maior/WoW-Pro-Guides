295beca13f112e2122397cf64709c8f8db3a0367
Cagomei <sshort8503@hotmail.com>
2022-08-16 19:38:12 -0500

    Update 40_50_Dungeon_Zul'Farrak.lua

--- WoWPro_Leveling/Classic/Horde/40_50_Dungeon_Zul'Farrak.lua
+++ WoWPro_Leveling/Classic/Horde/40_50_Dungeon_Zul'Farrak.lua
@@ -1,6 +1,6 @@
 local guide = WoWPro:RegisterGuide('Cag_HZF', 'Leveling', 'Orgrimmar', 'WowPro Team', 'Horde', 1)
 WoWPro:GuideLevels(guide,40, 50)
-WoWPro:GuideName(guide, 'Dungeon: Zul'Farrak')
+WoWPro:GuideName(guide, 'Dungeon: Zul_Farrak')
 WoWPro:GuideSteps(guide, function()
 return [[
 
