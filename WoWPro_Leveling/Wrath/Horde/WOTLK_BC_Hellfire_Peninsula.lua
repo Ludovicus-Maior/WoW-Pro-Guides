531c27ee8b8708344f3fb5999199fdd76eea8899
Hendo72 <ctkbkqu32f9z@opayq.com>
2022-09-06 10:23:44 -0400

    Update WOTLK_BC_Hellfire_Peninsula.lua
    I think this needed to be said.

--- WoWPro_Leveling/Wrath/Horde/WOTLK_BC_Hellfire_Peninsula.lua
+++ WoWPro_Leveling/Wrath/Horde/WOTLK_BC_Hellfire_Peninsula.lua
@@ -12,7 +12,7 @@ A The Dark Portal|QID|64217^64063|N|From your class trainer. I wouldn't advise g
 C The Dark Portal|QID|64217^64063|M|50.69,12.89|Z|Durotar|QO|1|N|Talk to Snurk at the top of the zep tower and then get on the zep when it arrives.|CHAT|
 F The Dark Portal|ACTIVE|64217^64063|M|32.54,29.36|Z|Stranglethorn Vale|QO|2|N|Talk to Thysta the flight master in Grom'gul for a free flight to Stonard.|CHAT|
 b Grom'gul Base Camp|ACTIVE|-64217^-64063|AVAILABLE|10120|M|50.69,12.89|Z|Durotar|N|Take the Zep to Grom'gul (or bug a mage for portal to Stonard)|IZ|1411;Durotar^1454;Orgrimmar|
-F Stonard|ACTIVE|-64217^-64063|AVAILABLE|10120|M|83.88,50.43|Z|1423|N|Fly to Stonard in the Swamp of Sorrows.|IZ|Acherus: The Ebon Hold|
+F Stonard|ACTIVE|-64217^-64063|AVAILABLE|10120|M|83.88,50.43|Z|1423|N|Fly to Stonard in the Swamp of Sorrows.\n[color=FF0000]NOTE: [/color]Grab a snack; this a 12 minute flight.|IZ|Acherus: The Ebon Hold|
 F Stonard|ACTIVE|-64217^-64063|AVAILABLE|10120|M|32.54,29.36|Z|Stranglethorn Vale|N|Fly to Stonard in the Swamp of Sorrows.|
 R Blasted Lands|AVAILABLE|10120|M|51.93,4.48|Z|1419; Blasted Lands|N|Follow the road south.|
 R The Dark Portal|AVAILABLE|10120|M|57.18,46.57|Z|1419; Blasted Lands|N|Continue south down the road.|
