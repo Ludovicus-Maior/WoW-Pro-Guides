b437ab11e619438ecc247777e4d719fad0ea5f4d
Ludovicus <ludovicus.maior@gmail.com>
2022-09-07 06:45:25 -0400

    Remove forced Wrath detection and use GetBuildInfo() instead.

--- WoWPro/WoWPro_Wrath.lua
+++
@@ -1,9 +0,0 @@
---------------------------
---    WoWPro_Wrath.lua  --
---------------------------
-
---- Force Wrath
-WoWPro.BC = false
-WoWPro.CLASSIC = false
-WoWPro.RETAIL = false
-WoWPro.WRATH = true
