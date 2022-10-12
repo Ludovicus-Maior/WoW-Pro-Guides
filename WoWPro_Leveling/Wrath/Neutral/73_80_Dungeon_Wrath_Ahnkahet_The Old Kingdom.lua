cf8744a91c26aedf4ac1b9dac218f5a75c829155
Hendo72 <ctkbkqu32f9z@opayq.com>
2022-10-12 12:50:57 -0400

    Wrath Errors found running Guide Checker
    These changes have been made blind, but look good on paper.

--- WoWPro_Leveling/Wrath/Neutral/73_80_Dungeon_Wrath_Ahnkahet_The Old Kingdom.lua
+++ WoWPro_Leveling/Wrath/Neutral/73_80_Dungeon_Wrath_Ahnkahet_The Old Kingdom.lua
@@ -5,7 +5,7 @@ WoWPro:GuideSteps(guide, function()
 return [[
 A The Faceless Ones|QID|13187|M|26.2,50.6|Z|Dragonblight|N|From Kilix the Unraveler.|
 K Savage Cave Beast|QID|13204|L|43512|N|Kill Savage Cave Beast until one drops a Ooze-covered Fungus.|
-A Puzzling...|QID|11941|U|35648|N|M|35.0,28.0|
+A Puzzling...|QID|11941|M|35.0,28.0|N|N|Right-click the Scintillating Fragment to accept this quest.|U|35648|
 A Funky Fungi|QID|13204|U|43512|N|Right-click the Ooze-covered Fungus to accept this quest.|O|
 l Grotesque Fungus|QID|13204|L|43511 6|N|Collect the Grotesque Fungus.|S|
 C The Faceless Ones|QID|13187|
