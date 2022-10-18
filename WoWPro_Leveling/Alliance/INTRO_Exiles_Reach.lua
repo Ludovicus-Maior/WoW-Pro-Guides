fdde080d39f779bccaef9db914ee2be5f9e19f65
Cagomei <sshort8503@hotmail.com>
2022-10-17 21:11:04 -0500

    DF run through

--- WoWPro_Leveling/Alliance/INTRO_Exiles_Reach.lua
+++ WoWPro_Leveling/Alliance/INTRO_Exiles_Reach.lua
@@ -1,4 +1,4 @@
-local guide = WoWPro:RegisterGuide('CagER0110', "Leveling", "Exile's Reach!Instance", 'Cagomei', 'Alliance')
+local guide = WoWPro:RegisterGuide('CagER0110', "Leveling", "Exile's Reach!Instance", 'WoWPro Team', 'Alliance')
 WoWPro:GuideLevels(guide, 1, 10)
 WoWPro:GuideSort(guide, 1)
 WoWPro:GuideNickname(guide, "Exile's Reach")
@@ -199,10 +199,10 @@ A An End to Beginnings|QID|55991|PRE|55992|M|39.66,31.99|N|From Captain Garrick.
 C An End to Beginnings|QID|55991|M|40.38,32.65|N|Click on the highlighted gryphon to ride back to Stormwind.|V|
 T An End to Beginnings|QID|55991|M|73.69,91.31|Z|Stormwind City|N|Turn in to Captain Garrick.|
 A Welcome to Stormwind|QID|59583|PRE|55991|M|73.69,91.31|Z|Stormwind City|N|From Captain Garrick.|
-T Welcome to Stormwind|QID|59583|M|66.38,78.23|Z|Stormwind City|N|Turn into Lindie Springsstock.|
-A Finding Your Way|QID|58908|PRE|59583|M|66.38,78.23|Z|Stormwind City|N|From Lindie Springsstock.|
-C Finding Your Way|QID|58908|M|66.06,77.64|Z|Stormwind City|QO|1|N|Ask a guard for directions to the Stable Master in Old Town (next to bottom on the list).|CHAT|
-C Finding Your Way|QID|58908|M|66.19,77.01|Z|Stormwind City|QO|2|N|Speak with Lindie Springstock to lead the way.|CHAT|
+T Welcome to Stormwind|QID|59583|M|70.21,85.39|Z|Stormwind City|N|Turn into Lindie Springsstock.|
+A Finding Your Way|QID|58908|PRE|59583|M|70.21,85.39|Z|Stormwind City|N|From Lindie Springsstock.|
+C Finding Your Way|QID|58908|M|70.28,83.72|Z|Stormwind City|QO|1|N|Ask a guard for directions to the Stable Master in Old Town (next to bottom on the list).|CHAT|
+C Finding Your Way|QID|58908|M|69.86,83.95|Z|Stormwind City|QO|2|N|Speak with Lindie Springstock to lead the way.|CHAT|
 C Finding Your Way|QID|58908|M|77.36,67.06|Z|Stormwind City|QO|3|N|Follow Lindie Springstock to the Stables. Stay right with him, he stops if you get too far ahead or behind.|NC|
 T Finding Your Way|QID|58908|M|77.28,66.95|Z|Stormwind City|N|Turn into Curly.|
 A License to Ride|QID|58909|PRE|58908|M|77.28,66.95|Z|Stormwind City|N|From Curly.|
@@ -258,10 +258,12 @@ T An Urgent Meeting|QID|58912|M|85.22,32.04|Z|Stormwind City|N|Turn into Captain
 A Battle for Azeroth: Tides of War|QID|58983|PRE|58912|M|85.22,32.04|Z|Stormwind City|N|From Captain Garrick|
 C Battle for Azeroth: Tides of War|QID|58983|M|85.27,32.30|Z|Stormwind City|N|Wait for their conversation to end to complete this step. You literally just stand there and do nothing.|NC|
 T Battle for Azeroth: Tides of War|QID|58983|M|85.91,31.56|Z|Stormwind City|N|Turn into Anduin Wrynn.|
-A The Nation of Kul Tiras|QID|59641|PRE|58983|M|85.91,31.56|Z|Stormwind City|N|From Anduin Wrynn|
+A The Nation of Kul Tiras|QID|59641^46728|PRE|58983|M|85.91,31.56|Z|Stormwind City|N|From Anduin Wrynn. Pick the bottom option if you would like a ride.|
 C The Nation of Kul Tiras|QID|59641|M|85.30,32.01|Z|Stormwind City|QO|1|N|Speak with Captain Garrick.|CHAT|
 C The Nation of Kul Tiras|QID|59641|M|77.68,39.26|Z|Stormwind City|QO|2|N|Run out of the keep and you will find Loretta Banks for a free ride to the Docks.|CHAT|
 C The Nation of Kul Tiras|QID|59641|M|22.78,24.87|Z|Stormwind City|QO|3|N|Speak with Jaina to set sail.|CHAT|
 T The Nation of Kul Tiras|QID|59641|Z|Boralus|M|65.59,50.72|N|Turn into Lady Jaina Proudmoore.|
+C The Nation of Kul Tiras|QID|46728|M|22.78,24.87|Z|Stormwind City|QO|1|N|Speak with Jaina to set sail.|CHAT|
+T The Nation of Kul Tiras|QID|46728|Z|Boralus|M|65.59,50.72|N|Turn into Lady Jaina Proudmoore.|
 ]]
 end)
