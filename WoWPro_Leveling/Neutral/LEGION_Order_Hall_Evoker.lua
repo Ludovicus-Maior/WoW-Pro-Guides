464803c4aa9b1aaa4e0fd4babf65014da7ebf10a
Hendo72 <ctkbkqu32f9z@opayq.com>
2023-02-24 11:58:56 -0500

    Evoker Order Hall (WIP)
    Working on making it work with Horde.

--- WoWPro_Leveling/Neutral/LEGION_Order_Hall_Evoker.lua
+++ WoWPro_Leveling/Neutral/LEGION_Order_Hall_Evoker.lua
@@ -1,4 +1,4 @@
-local guide = WoWPro:RegisterGuide('CagLegionEvoker', 'Leveling', 'EvokerOrderHallTerrain', 'WoWPro Team', 'Neutral')
+local guide = WoWPro:RegisterGuide('CagLegionEvoker', 'Leveling', 'Dalaran', 'WoWPro Team', 'Neutral')
 WoWPro:GuideLevels(guide, 10, 50, 10)
 WoWPro:GuideSort(guide, 3)
 WoWPro:GuideContent(guide, "Legion")
