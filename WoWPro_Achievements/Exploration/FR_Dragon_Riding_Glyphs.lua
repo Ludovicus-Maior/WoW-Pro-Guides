8c58ce9d9582ec3dae7eb58a76c2e74f5c505849
Cagomei <sshort8503@hotmail.com>
2023-03-11 17:58:39 -0600

    FR Glyphs

---
+++ WoWPro_Achievements/Exploration/FR_Dragon_Riding_Glyphs.lua
@@ -0,0 +1,16 @@
+local guide = WoWPro:RegisterGuide("FR_Dragonriding_Glyphs", "Achievements", "Dragonflight", "WowPro Team", "Neutral")
+WoWPro:GuideNextGuide(guide, "Dragonflight")
+WoWPro:GuideLevels(guide,60,70)
+WoWPro.Achievements:GuideMisc(guide, "The Forbidden Reach Flying Glyphs", "Exploration", "Dragonflight")
+WoWPro:GuideSteps(guide, function()
+return [[
+F Winglord's Perch Glyph |N|Inside the broken tower.|M|18.33,13.20|Z|2151;The Forbidden Reach|ACH|17398|
+F Caldera of the Menders Glyph |N|Inside the broken tower.|M|37.71,30.68|Z|2151;The Forbidden Reach|ACH|17405|
+F Froststone Peak Glyph |N|Inside the tower on the roof.|M|62.55,32.40|Z|2151;The Forbidden Reach|ACH|17400|
+F Dragonskull Island Glyph |N|Ontop of the mountain amongst the trees.|M|79.47,32.64|Z|2151;The Forbidden Reach|ACH|17401|
+F Stormsunder Mountain Glyph |N|Ontop of the mountain.|M|77.26,55.06|Z|2151;The Forbidden Reach|ACH|17402|
+F Talonlord's Perch Glyph |N|Inside the broken tower.|M|59.08,65.04|Z|2151;The Forbidden Reach|ACH|17404|
+F Frosted Spine Glyph |N|In the snow area.|M|48.52,68.99|Z|2151;The Forbidden Reach|ACH|17403|
+F Talon's Watch Glyph |N|Inside the broken tower.|M|20.56,91.42|Z|2151;The Forbidden Reach|ACH|17399|
+]]
+end)
