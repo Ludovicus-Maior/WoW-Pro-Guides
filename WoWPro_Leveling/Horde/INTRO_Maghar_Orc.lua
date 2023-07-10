067ea2825605b1b5be31d945816e40a25102a6f7
Ludovicus <ludovicus.maior@gmail.com>
2023-07-10 13:57:41 -0400

    Add some missing CT tags to the INTRO guides.

--- WoWPro_Leveling/Horde/INTRO_Maghar_Orc.lua
+++ WoWPro_Leveling/Horde/INTRO_Maghar_Orc.lua
@@ -14,6 +14,6 @@ T For the Horde|QID|51485|M|39.39,79.58|N|To Ambassador Blackguard near the Emba
 A Stranger in a Strange Land|QID|53502|M|39.39,79.58|N|From Ambassador Blackguard.|
 C Stranger in a Strange Land|QID|53502|M|49.67,76.45|N|Click on the Warchief's Command Board and select Chromie's Call.|
 T Stranger in a Strange Land|QID|53502|M|49.79,76.42|N|To Limbflayer Lasha.|
-N It's Chromie Time!|QID|62568|M|40.82,80.13|Z|Orgrimmar|JUMP|Chromie Time|LVL|10|N|You can now accept Chromie's Call at the Warchief's Command Board in Orgrimmar. This will allow you to choose which expansion you want to level in.\n\nClick the guide button next to this frame to direct you to Chromie!|
+N It's Chromie Time!|QID|62568|M|40.82,80.13|Z|Orgrimmar|JUMP|Chromie Time|LVL|10|N|You can now accept Chromie's Call at the Warchief's Command Board in Orgrimmar. This will allow you to choose which expansion you want to level in.\n\nClick the guide button next to this frame to direct you to Chromie!|CT|
 ]]
 end)
