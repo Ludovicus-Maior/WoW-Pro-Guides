185c41903383ddacfb8f89d6a1fb37fc5b8db977
capau <capautv@gmail.com>
2023-12-09 12:05:14 +0100

    Added GuideName

--- WoWPro_Achievements/General/Night_Elf_Heritage_Armor.lua
+++ WoWPro_Achievements/General/Night_Elf_Heritage_Armor.lua
@@ -1,4 +1,5 @@
 local guide = WoWPro:RegisterGuide('Night_Elf_Heritage_Armor', 'Achievements', 'Stormwind City', 'WoWPro Team', 'Alliance')
+WoWPro:GuideName(guide,"Night Elf Heritage Armor")
 WoWPro:GuideLevels(guide,50, 70)
 WoWPro.Achievements:GuideMisc(guide, "Night_Elf_Heritage_Armor", "Heritage Armor", "Dragonflight")
 WoWPro:GuideSteps(guide, function()
