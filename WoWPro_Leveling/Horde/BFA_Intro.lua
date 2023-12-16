3d84a5dd670c56654eaa1976381c984211f322dc
Hendo72 <39675478+Hendo72@users.noreply.github.com>
2023-12-16 15:25:26 -0500

    Update BFA_Intro.lua
    - Just a few tweaks I've found that needed to be added
    - added note to skip intro scenario.

--- WoWPro_Leveling/Horde/BFA_Intro.lua
+++ WoWPro_Leveling/Horde/BFA_Intro.lua
@@ -13,8 +13,8 @@ C Battle for Azeroth: Mission Statement|QID|60361^51443|M|54.62,78.34|Z|Orgrimma
 T Battle for Azeroth: Mission Statement|QID|60361^51443|M|54.45,78.42|Z|Orgrimmar|N|To Nathanos Blightcaller.|
 
 A The Stormwind Extraction|QID|50769|M|54.45,78.42|Z|Orgrimmar|N|From Nathanos Blightcaller.|PRE|51443^60361|
-C The Stormwind Extraction|QID|50769|M|54.55,78.37|Z|Orgrimmar|QO|1|N|Take a potion. Don't use it!|H|
-C The Stormwind Extraction|QID|50769|M|50.89,83.96|Z|Orgrimmar|QO|2|N|Fly up to the ramparts and hop on an eagle. This will enter you into the next scenario, (Stockades/Stormwind).|V|
+C The Stormwind Extraction|QID|50769|M|54.55,78.37|Z|Orgrimmar|QO|1|N|Pick up the potion.\n[color=FF0000]NOTE: [/color]If you've already done this scenario previously, talk to Nathanos without picking up the potion to get the option to skip it.|H|
+C The Stormwind Extraction|QID|50769|M|50.67,83.45|Z|Orgrimmar|QO|2|N|Mount the Skyhorn Eagle located by the front gate.\n[color=FF0000]NOTE: [/color]This will enter you into the next scenario (Stockades/Stormwind).|V|
 C In the Dead of Night|QID|50769|M|47.21,59.58|Z|Stormwind City!Stormwind City!Instance|SO|1;1|N|Infiltrate Stormwind City|NC|
 C Down the Drain|QID|50769|M|46.23,58.19|Z|Stormwind City!Stormwind City!Instance|SO|2;2|N|Open the sewer access gate|H|
 C Down the Drain|QID|50769|M|41.29,62.51|Z|Stormwind City!Stormwind City!Instance|SO|2;1|N|Go in the sewer access gate.|H|
@@ -39,41 +39,41 @@ C Rastakhan|QID|46930|M|57.97,56.43|Z|Zuldazar|QO|1|N|Ride Enforcer's Pterrordax
 C Rastakhan|QID|46930|M|57.59,43.96|Z|Zuldazar|QO|2|N|Speak with Princess Talanji. This starts a cut-scene you can end early with <ESC>.|CHAT|
 T Rastakhan|QID|46930|M|57.95,43.31|Z|Zuldazar|N|To King Rastakhan.|
 A Speaker of the Horde|QID|46931|M|57.95,43.31|Z|Zuldazar|N|From King Rastakhan.|PRE|46930|
-C Speaker of the Horde|QID|46931|M|57.55,44.05|Z|Zuldazar|QO|1|N|Head to Zolani, wait for her to move, then follow Zolani to the elevator. Take the elevator down to the lower level.|NC|
-C Speaker of the Horde|QID|46931|M|49.92,39.49|Z|Dazar'alor|QO|2|N|Head outside, then click the banner to summon your companions.|NC|
-C Speaker of the Horde|QID|46931|M|57.95,44.31|Z|Zuldazar|QO|3|N|Head back inside, and walk to the end, the location of The Great Seal.|NC|
-C Speaker of the Horde|QID|46931|M|67.00,71.83|Z|The Great Seal!Dazar'alor|QO|4|N|To the left, head down the stairs to the Hall of Ancient Paths. These will be your portals to the Horde Cities once this quest is complete.|NC|
-C Speaker of the Horde|QID|46931|M|32.22,70.00|Z|The Great Seal!Dazar'alor|CS|QO|5|N|Head back up the stairs, across the corridor, then down the stairs to the Vault of the King.|NC|
-T Speaker of the Horde|QID|46931|M|41.28,66.75|Z|The Great Seal!Dazar'alor|N|Back up the stairs, to Princess Talanji.|
+R Speaker of the Horde|ACTIVE|46931|M|57.55,44.05|Z|Zuldazar|QO|1|N|Head to Zolani and once she starts moving, take the elevator down to the lower level.\n[color=FF0000]NOTE: [/color]You don't need to wait for Zolani to complete these steps.|
+C Speaker of the Horde|QID|46931|M|49.92,39.49|Z|Dazar'alor|QO|2|N|Head outside and click the banner to summon your companions.|H|
+R Speaker of the Horde|ACTIVE|46931|M|57.95,44.31|Z|Zuldazar|QO|3|N|Head back inside and walk towards Brillin the Beauty (The Great Seal Innkeeper).|
+h The Great Seal|ACTIVE|46931|QO|6|N|Optionally, you can make The Great Seal your new home before moving on.\n[color=FF0000]NOTE: [/color]Manually check this step off to continue.|
+R Speaker of the Horde|ACTIVE|46931|M|67.00,71.83|Z|The Great Seal!Dazar'alor|QO|4|N|Head down the stairs to theleft into the Hall of Ancient Paths.\n[color=FF0000]NOTE: [/color]These will be your portals to the Horde Cities once this quest is complete.|
+R Speaker of the Horde|ACTIVE|46931|M|32.22,70.00|Z|The Great Seal!Dazar'alor|CS|QO|5|N|Head back up the stairs, across the corridor, then down the stairs to the Vault of the King.|
+T Speaker of the Horde|QID|46931|M|41.28,66.75|Z|The Great Seal!Dazar'alor|N|Back up the stairs, to Princess Talanji.\n[color=FF0000]NOTE: [/color]If the quest bugs and she's not there, log back in to fix it.|
 A Area to Explore|QID|47512^47513^47514|M|41.77,69.35|Z|1164|N|Pick Zuldazar from the map.|PRE|46931|
-t Nazmir|QID|47512|M|40.50,68.25|Z|1164|N|To Princess Talanji take the elevator behind you, she is on her throne.|
-t Vol'dun|QID|47513|M|40.50,68.25|Z|1164|N|To Princess Talanji take the elevator behind you, she is on her throne.|
-t Zuldazar|QID|47514|M|40.50,68.25|Z|1164|N|To Princess Talanji take the elevator behind you, she is on her throne.|
+t Nazmir|QID|47512|M|41.28,66.75|Z|1164|N|To Princess Talanji take the elevator behind you, she is on her throne.|
+t Vol'dun|QID|47513|M|41.28,66.75|Z|1164|N|To Princess Talanji take the elevator behind you, she is on her throne.|
+t Zuldazar|QID|47514|M|41.28,66.75|Z|1164|N|To Princess Talanji take the elevator behind you, she is on her throne.|
 A Journey to Nazmir|QID|47103|M|40.22,66.50|Z|Hall of Croniclers!Dazar'alor|N|From Princess Talanji.|PRE|47512|
 A Discreet Discussions|QID|47313|M|40.22,66.50|Z|Hall of Croniclers!Dazar'alor|N|From Princess Talanji.|PRE|47513|
 A Trust of a King|QID|49615|M|40.22,66.50|Z|Hall of Croniclers!Dazar'alor|N|From Princess Talanji.|PRE|47514|
 A The War Campaign|QID|52749|M|40.67,72.08|Z|Hall of Croniclers!Dazar'alor|N|From Nathanos Blightcaller.|LVL|35|PRE|46931|
-h The Great Seal|ACTIVE|47103^47313^49615|M|48.50,71.75|Z|The Great Seal!Dazar'alor|N|Set your hearth stone with Brillin the Beauty, with the portals nearby, you probably won't want to change it.|
 
 ; professions
-N Cooking|ACTIVE|49615|M|28.44,50.42|Z|1164|N|Train BfA Cooking at T'sarah the Royal Chef.|P|Cooking;185|RECIPE|259430|;
-N Archaeology|ACTIVE|49615|M|36.17,46.42|Z|1164|N|Train BfA Archaeology at Examiner Alerinda who wanders around in the Vault of Kings. Check off manually when done.|P|Archaeology;794|RECIPE|278910|;need to determine correct spell ID to autocomplete
-R Terrace of Crafters|AVAILABLE|51465|M|43.15,35.51|Z|Dazar'alor|N|Run down to Terrace of the Crafters to train your various professions.|
-N Skinning|ACTIVE|49615|M|43.76,34.67|Z|Dazar'alor|N|Train BfA skinning at Ranna the Cutta.|P|Skinning;393|RECIPE|257152|;
-N Leatherworking|ACTIVE|49615|M|44.07,34.65|Z|Dazar'alor|N|Train BfA Leatherworking at Xanjo.|P|Leatherworking;165|RECIPE|256756|;265813
-N Tailoring|ACTIVE|49615|M|44.49,33.90|Z|Dazar'alor|N|Train BfA Tailoring at Pin'jin the Patient.|P|Tailoring;197|RECIPE|257103|;265815
-N Herbalism|ACTIVE|49615|M|42.11,35.61|Z|Dazar'alor|N|Train BfA Herbalism at Jahden Fla.|P|Herbalism;182|RECIPE|252411|;267458
-N Alchemy|ACTIVE|49615|M|42.23,37.96|Z|Dazar'alor|N|Train BfA Alchemy at Clever Kumali.|P|Alchemy;171|RECIPE|252382|;
-N Inscription|ACTIVE|49615|M|42.33,39.72|Z|Dazar'alor|N|Train BfA Inscription at Chronicler Grazzul.|P|Inscription;773|RECIPE|264777|;265809
-N Engineering|ACTIVE|49615|M|45.13,40.58|Z|Dazar'alor|N|Train BfA Engineering at Shuga Blastcaps.|P|Engineering;202|RECIPE|255392|;265807
-N Mining|ACTIVE|49615|M|44.13,38.97|Z|Dazar'alor|N|Train BfA Mining at Secott the Goldsmith.|P|Mining;186|RECIPE|253333|;267482
-N Blacksmithing|ACTIVE|49615|M|43.65,38.30|Z|Dazar'alor|N|Train BfA Blacksmithing at Forgemaster Zak'aal.|P|Blacksmithing;164|RECIPE|253183|;
-N Jewelcrafting|ACTIVE|49615|M|47.05,37.94|Z|Dazar'alor|N|Train BfA Jewelcrafting at Seshuli.|P|Jewelcrafting;755|RECIPE|256689|;265811
-N Enchanting|ACTIVE|49615|M|47.08,35.68|Z|Dazar'alor|N|Train BfA Enchanting at Enchantress Quinni.|P|Enchanting;333|RECIPE|255074|;
+= Zandalari Cooking|ACTIVE|49615|M|28.44,50.42|Z|1164|N|Train BfA Cooking at T'sarah the Royal Chef.|P|Cooking;185|RECIPE|259430|;
+= Zandalari Archaeology|ACTIVE|49615|M|36.17,46.42|Z|1164|N|Train BfA Archaeology at Examiner Alerinda who wanders around in the Vault of Kings. Check off manually when done.|P|Archaeology;794|RECIPE|278910|;need to determine correct spell ID to autocomplete
+R Terrace of Crafters|AVAILABLE|51465|M|43.15,35.51|Z|Dazar'alor|N|Run down to Terrace of the Crafters to train your various professions.|TZ|Hall of Glimmers|
+= Zandalari Skinning|ACTIVE|49615|M|43.76,34.67|Z|Dazar'alor|N|Train BfA skinning at Ranna the Cutta.|P|Skinning;393|RECIPE|257152|;
+= Zandalari Leatherworking|ACTIVE|49615|M|44.07,34.65|Z|Dazar'alor|N|Train BfA Leatherworking at Xanjo.|P|Leatherworking;165|RECIPE|256756|;265813
+= Zandalari Tailoring|ACTIVE|49615|M|44.49,33.90|Z|Dazar'alor|N|Train BfA Tailoring at Pin'jin the Patient.|P|Tailoring;197|RECIPE|257103|;265815
+= Zandalari Herbalism|ACTIVE|49615|M|42.11,35.61|Z|Dazar'alor|N|Train BfA Herbalism at Jahden Fla.|P|Herbalism;182|RECIPE|252411|;267458
+= Zandalari Alchemy|ACTIVE|49615|M|42.23,37.96|Z|Dazar'alor|N|Train BfA Alchemy at Clever Kumali.|P|Alchemy;171|RECIPE|252382|;
+= Zandalari Inscription|ACTIVE|49615|M|42.33,39.72|Z|Dazar'alor|N|Train BfA Inscription at Chronicler Grazzul.|P|Inscription;773|RECIPE|264777|;265809
+= Zandalari Engineering|ACTIVE|49615|M|45.13,40.58|Z|Dazar'alor|N|Train BfA Engineering at Shuga Blastcaps.|P|Engineering;202|RECIPE|255392|;265807
+= Zandalari Mining|ACTIVE|49615|M|44.13,38.97|Z|Dazar'alor|N|Train BfA Mining at Secott the Goldsmith.|P|Mining;186|RECIPE|253333|;267482
+= Zandalari Blacksmithing|ACTIVE|49615|M|43.65,38.30|Z|Dazar'alor|N|Train BfA Blacksmithing at Forgemaster Zak'aal.|P|Blacksmithing;164|RECIPE|253183|;
+= Zandalari Jewelcrafting|ACTIVE|49615|M|47.05,37.94|Z|Dazar'alor|N|Train BfA Jewelcrafting at Seshuli in the Hall of Glimmers.\n[color=FF0000]NOTE: [/color]Go through the doorway to the right of the stairs.|P|Jewelcrafting;755|RECIPE|256689|;265811
+= Zandalari Enchanting|ACTIVE|49615|M|47.08,35.68|Z|Dazar'alor|N|Train BfA Enchanting at Enchantress Quinni in the Hall of Glimmers.\n[color=FF0000]NOTE: [/color]Go through the doorway to the right of the stairs.|P|Enchanting;333|RECIPE|255074|;
 
 ;to open up Jani's mysterious trashpiles
-N Jani's Mysterious Trashpiles|QID|47441|N|These are scattered around Zandalar. To open up interaction with them, you need to do the following two quests. Once you have completed them, when you are near one of the trashpiles, you will either see a quest "!" or see/hear a message saying "hey richmon…". Interact with the trashpile for your reward.|RANK|3|
-R The Zocalo|QID|47441|M|42.22,25.82|Z|Dazar'alor|N|Run down, down, down in the general direction of the waypoint.|RANK|3|;cant be ACTIVE as you don't have the quest yet
+N Jani's Mysterious Trashpiles|AVAILABLE|47441|N|These are scattered around Zandalar.\nUpon completion of the following 2 quests ('Pests' and 'Curse of Jani'), when you are near one of these Trashpiles, you will either see a quest "!", or see/hear a message saying "hey richmon…".\n[color=FF0000]NOTE: [/color]Manually check this step off to continue.|RANK|3|
+R The Zocalo|AVAILABLE|47441|M|42.22,25.82|Z|Dazar'alor|N|Run down, down, down in the general direction of the waypoint.|RANK|3|IZ|Dazar'alor|
 A Pests|QID|47441|M|40.24,19.06|Z|Dazar'alor|N|From Nokano.|RANK|3|
 C Pests|QID|47441|Z|Dazar'alor|M|41.80,25.00;38.90,20.30;36.30,11.10;37.35,16.10;36.45,13.70;39.25,18.32;40.90,22.90|CN|QO|1|N|Kill the little pests.|T|Thieving Snapper|
 C Pests|QID|47441|Z|Dazar'alor|M|35.35,7.67|QO|2|H|N|Follow the Big One. When you arrive at the trashpile, click on it to summon Jani.|
