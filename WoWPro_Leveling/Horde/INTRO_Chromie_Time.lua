0c60506bb32ff4fa8324d0db495bbc07f25e9328
Hendo72 <39675478+Hendo72@users.noreply.github.com>
2023-12-17 12:52:22 -0500

    Update INTRO_Chromie_Time.lua

--- WoWPro_Leveling/Horde/INTRO_Chromie_Time.lua
+++ WoWPro_Leveling/Horde/INTRO_Chromie_Time.lua
@@ -7,7 +7,7 @@ WoWPro:GuideName(guide,"Chromie Time")
 WoWPro:GuideSteps(guide, function()
 return [[
 
-A Adventurers Wanted: Chromie's Call|QID|62568|M|49.66,76.46|N|Make your way to the nearest  Warchief's Command Board and accept Chromie's Call. Onward to Adventure!|CT|LVL|-60|
+A Adventurers Wanted: Chromie's Call|QID|62568|M|49.66,76.46|N|Make your way to the nearest Warchief's Command Board and select 'Onward to Adventure'.|CT|LVL|-60|
 T Adventurers Wanted: Chromie's Call|QID|62568|M|40.82,80.13|N|To Chromie near the Orgrimmar Embassy.|CT|LVL|-60|
 
 N Choose The Cataclysm|QID|60887|M|40.82,80.13|JUMP|Cataclysm: Guide Hub;5|S|N|If you selected to go to the Cataclysm timeline, Vanilla is rolled into this.\n\nVanilla zones are level 10-30 and Cataclysm zones are 30-50.|CT|NOCACHE|LVL|-60|
@@ -25,14 +25,14 @@ N Make your choice|QID|99999|M|40.82,80.13|N|Speak with Chromie to transport you
 N Not Eligible|AVAILABLE|62568|M|62.25,29.93|N|You need to have leveled a character to level 60 before you are eligible to use Chromie.|LVL|-60|
 N Not Eligible|AVAILABLE|62567|M|62.25,29.93|N|You cannot use chromie time once you reach level 60.|LVL|60|
 
-;A Onward to Adventure: Kalimdor|QID|60887|M|40.82,80.15|N|From Chromie after activating Cataclysm timeline.|
-;A To Outland!|QID|60123|M|40.82,80.15|N|From Chromie after activating Burning Crusade timeline.|
-;A To Northrend!|QID|60097|M|40.82,80.15|N|From Chromie after activating Wrath of the Lich King timeline.|
-;A To Pandaria!|QID|60126|M|40.82,80.15|N|From Chromie after activating Mists of Pandaria timeline.|
-;A The Dark Portal|QID|34398|M|40.82,80.15|N|From Chromie after activating Warlords of Draenor timeline.|
-;A The Legion Returns|QID|43926|M|40.82,80.11|Z|Orgrimmar|N|From Chromie after activating Legion timeline.|C|-DemonHunter|
-;A Onward to Adventure: Broken Isles|QID|60970|M|40.82,80.15|N|From Chromie after activating Legion timeline.|C|DemonHunter|
-;A Mission Statement|QID|51443|M|49.40,76.58|N|From Chromie after activating Battle for Azeroth timeline.|
-;A A Chilling Summons|QID|61874|Z|Orgrimmar|N|From Highlord Darion Mograine. Auto Accepted.|FACTION|Horde|LVL|10|
+A Onward to Adventure: Kalimdor|QID|60887|M|40.82,80.15|N|From Chromie after activating Cataclysm timeline.|
+A To Outland!|QID|60123|M|40.82,80.15|N|From Chromie after activating Burning Crusade timeline.|
+A To Northrend!|QID|60097|M|40.82,80.15|N|From Chromie after activating Wrath of the Lich King timeline.|
+A To Pandaria!|QID|60126|M|40.82,80.15|N|From Chromie after activating Mists of Pandaria timeline.|
+A The Dark Portal|QID|34398|M|40.82,80.15|N|From Chromie after activating Warlords of Draenor timeline.|
+A The Legion Returns|QID|43926|M|40.82,80.11|Z|Orgrimmar|N|From Chromie after activating Legion timeline.|C|-DemonHunter|
+A Onward to Adventure: Broken Isles|QID|60970|M|40.82,80.15|N|From Chromie after activating Legion timeline.|C|DemonHunter|
+A Mission Statement|QID|51443|M|49.40,76.58|N|From Chromie after activating Battle for Azeroth timeline.|
+A A Chilling Summons|QID|61874|Z|Orgrimmar|N|From Highlord Darion Mograine. Auto Accepted.|FACTION|Horde|LVL|10|
 ]]
 end)
