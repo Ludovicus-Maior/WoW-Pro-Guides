8218facd08851d93db365e336576789b92c1f1bb
Hendo72 <39675478+Hendo72@users.noreply.github.com>
2023-12-16 23:51:09 -0500

    Update BFA_Intro.lua
    - shuffled coding syntax
    - rewrote some of the steps for better flow and clarity

--- WoWPro_Leveling/Horde/BFA_Intro.lua
+++ WoWPro_Leveling/Horde/BFA_Intro.lua
@@ -8,52 +8,52 @@ WoWPro:GuideNextGuide(guide, 'Zuldazar')
 WoWPro:GuideSteps(guide, function()
 return [[
 A Battle for Azeroth: Mission Statement|QID|60361^51443|M|49.40,76.58|Z|Orgrimmar|N|From Warchief's Herald next to the Warchief's Command Board.|
-C Battle for Azeroth: Mission Statement|QID|60361^51443|M|48.55,71.49|Z|Orgrimmar|QO|1|N|Speak to Warchief Sylvanas Windrunner|CHAT|
-C Battle for Azeroth: Mission Statement|QID|60361^51443|M|54.62,78.34|Z|Orgrimmar|QO|2|N|Go to the Broken Tusk Inn and meet your team|NC|
+C Battle for Azeroth: Mission Statement|QID|60361^51443|QO|1|M|48.55,71.49|Z|Orgrimmar|N|Speak to Warchief Sylvanas Windrunner|CHAT|
+R Battle for Azeroth: Mission Statement|ACTIVE|60361^51443|QO|2|M|54.62,78.34|Z|Orgrimmar|N|Go to the Broken Tusk Inn and meet your team|NC|
 T Battle for Azeroth: Mission Statement|QID|60361^51443|M|54.45,78.42|Z|Orgrimmar|N|To Nathanos Blightcaller.|
 
-A The Stormwind Extraction|QID|50769|M|54.45,78.42|Z|Orgrimmar|N|From Nathanos Blightcaller.|PRE|51443^60361|
-C The Stormwind Extraction|QID|50769|M|54.55,78.37|Z|Orgrimmar|QO|1|N|Pick up the potion.\n[color=FF0000]NOTE: [/color]If you've already done this scenario previously, talk to Nathanos without picking up the potion to get the option to skip it.|H|
-C The Stormwind Extraction|QID|50769|M|50.67,83.45|Z|Orgrimmar|QO|2|N|Mount the Skyhorn Eagle located by the front gate.\n[color=FF0000]NOTE: [/color]This will enter you into the next scenario (Stockades/Stormwind).|V|
-C In the Dead of Night|QID|50769|M|47.21,59.58|Z|Stormwind City!Stormwind City!Instance|SO|1;1|N|Infiltrate Stormwind City|NC|
-C Down the Drain|QID|50769|M|46.23,58.19|Z|Stormwind City!Stormwind City!Instance|SO|2;2|N|Open the sewer access gate|H|
-C Down the Drain|QID|50769|M|41.29,62.51|Z|Stormwind City!Stormwind City!Instance|SO|2;1|N|Go in the sewer access gate.|H|
-C The Stockades|QID|50769|M|51.30,39.62|Z|The Stockade!The Stockade!Instance|SO|3;1|N|Fight your way to meet up with Rokhan.|
-C Honor and Loyalty|QID|50769|M|48.05,33.68|Z|The Stockade!The Stockade!Instance|SO|4;2|N|Click on the Door to Saurfang's cell and go in.|H|
-C Honor and Loyalty|QID|50769|M|48.05,33.68|Z|The Stockade!The Stockade!Instance|SO|4;1|N|Talk to Saurfang.|CHAT|
-C The Primary Objective|QID|50769|M|56.00,64.16|Z|The Stockade!The Stockade!Instance|SO|5;1|N|Fight your way to the next waypoint and open the cell to release the Princess and the Prophet.|
-C The Primary Objective|QID|50769|M|56.67,40.64;87.01,23.28|Z|The Stockade!The Stockade!Instance|CS|SO|5;2|N|Fight your way down the hall and to the sewer grate, Click on the grade to go thru.|
-C The City Stirs|QID|50769|M|55.78,56.46|Z|Stormwind City!Stormwind City!Instance|SO|6;1|N|Talk to Rokhan and then follow closely so you stay in his invis-o sphere.|CHAT|
-C WAY Behind Enemy Lines|QID|50769|M|55.87,56.88|Z|Stormwind City!Stormwind City!Instance|SO|7;1|N|Defeat the worgen ambush.|
-C Escape Cathedral Square|QID|50769|M|45.42,43.19|Z|Stormwind City!Stormwind City!Instance|SO|7;2|N|Keep following your buddies and fighting your way thru Stormwind. Chat with Nathanos when you get to him, to end this stage.|
-C No Diplomatic Solution|QID|50769|M|39.67,41.04|Z|Stormwind City!Stormwind City!Instance|SO|8;1|N|Break down the ice wall.|
-C Reach Talanji's ship|QID|50769|M|20.59,25.79|Z|Stormwind City!Stormwind City!Instance|SO|9;1|N|Continue on with your buddies, killing those pesky Alliance trying to stop you from reaching Talanji's ship|
-C Escape Stormwind Harbor|QID|50769|M|20.63,28.92|Z|Stormwind City!Stormwind City!Instance|SO|10;1|N|Talk to Talanji on the bridge of her ship.|CHAT|
+A The Stormwind Extraction|QID|50769|PRE|51443^60361|M|54.45,78.42|Z|Orgrimmar|N|From Nathanos Blightcaller.|
+C The Stormwind Extraction|QID|50769|QO|1|M|54.55,78.37|Z|Orgrimmar|N|Pick up the potion.\n[color=FF0000]NOTE: [/color]If you've already done this scenario previously, talk to Nathanos without picking up the potion to get the option to skip it.|H|
+C The Stormwind Extraction|QID|50769|QO|2|M|50.67,83.45|Z|Orgrimmar|N|Mount the Skyhorn Eagle located by the front gate.\n[color=FF0000]NOTE: [/color]This will enter you into the next scenario (Stockades/Stormwind).|V|
+C In the Dead of Night|QID|50769|SO|1;1|M|47.21,59.58|Z|Stormwind City!Stormwind City!Instance|N|Infiltrate Stormwind City|NC|
+C Down the Drain|QID|50769|SO|2;2|M|46.23,58.19|Z|Stormwind City!Stormwind City!Instance|N|Open the sewer access gate|H|
+C Down the Drain|QID|50769|SO|2;1|M|41.29,62.51|Z|Stormwind City!Stormwind City!Instance|N|Go in the sewer access gate.|H|
+C The Stockades|QID|50769|SO|3;1|M|51.30,39.62|Z|The Stockade!The Stockade!Instance|N|Fight your way to meet up with Rokhan.|
+C Honor and Loyalty|QID|50769|SO|4;2|M|48.05,33.68|Z|The Stockade!The Stockade!Instance|N|Click on the Door to Saurfang's cell and go in.|H|
+C Honor and Loyalty|QID|50769|SO|4;1|M|48.05,33.68|Z|The Stockade!The Stockade!Instance|N|Talk to Saurfang.|CHAT|
+C The Primary Objective|QID|50769|SO|5;1|M|56.00,64.16|Z|The Stockade!The Stockade!Instance|N|Fight your way to the next waypoint and open the cell to release the Princess and the Prophet.|
+C The Primary Objective|QID|50769|SO|5;2|M|56.67,40.64;87.01,23.28|Z|The Stockade!The Stockade!Instance|CS|N|Fight your way down the hall and to the sewer grate, Click on the grade to go thru.|
+C The City Stirs|QID|50769|SO|6;1|M|55.78,56.46|Z|Stormwind City!Stormwind City!Instance|N|Talk to Rokhan and then follow closely so you stay in his invis-o sphere.|CHAT|
+C WAY Behind Enemy Lines|QID|50769|SO|7;1|M|55.87,56.88|Z|Stormwind City!Stormwind City!Instance|N|Defeat the worgen ambush.|
+C Escape Cathedral Square|QID|50769|SO|7;2|M|45.42,43.19|Z|Stormwind City!Stormwind City!Instance|N|Keep following your buddies and fighting your way thru Stormwind. Chat with Nathanos when you get to him, to end this stage.|
+C No Diplomatic Solution|QID|50769|SO|8;1|M|39.67,41.04|Z|Stormwind City!Stormwind City!Instance|N|Break down the ice wall.|
+C Reach Talanji's ship|QID|50769|SO|9;1|M|20.59,25.79|Z|Stormwind City!Stormwind City!Instance|N|Continue on with your buddies, killing those pesky Alliance trying to stop you from reaching Talanji's ship|
+C Escape Stormwind Harbor|QID|50769|SO|10;1|M|20.63,28.92|Z|Stormwind City!Stormwind City!Instance|N|Talk to Talanji on the bridge of her ship.|CHAT|
 T The Stormwind Extraction|QID|50769|M|39.50,70.83|Z|1164|N|To Nathanos Blightcaller.|
 
-A Welcome to Zuldazar|QID|46957|M|57.95,62.46|Z|Zuldazar|N|From Princess Talanji|PRE|50769|
+A Welcome to Zuldazar|QID|46957|PRE|50769|M|57.95,62.46|Z|Zuldazar|N|From Princess Talanji|
 C Welcome to Zuldazar|QID|46957|M|57.94,56.59|Z|Zuldazar|N|Follow Princess Talanji. You will need to stay close, otherwise she will stop running.|NC|
 T Welcome to Zuldazar|QID|46957|M|57.97,56.50|Z|Zuldazar|N|To General Jakra'zet.|
-A Rastakhan|QID|46930|M|57.97,56.50|Z|Zuldazar|N|From General Jakra'zet.|PRE|46957|
-C Rastakhan|QID|46930|M|57.97,56.43|Z|Zuldazar|QO|1|N|Ride Enforcer's Pterrordax to the Throne|V|
-C Rastakhan|QID|46930|M|57.59,43.96|Z|Zuldazar|QO|2|N|Speak with Princess Talanji. This starts a cut-scene you can end early with <ESC>.|CHAT|
+A Rastakhan|QID|46930|PRE|46957|M|57.97,56.50|Z|Zuldazar|N|From General Jakra'zet.|
+C Rastakhan|QID|46930|QO|1|M|57.97,56.43|Z|Zuldazar|N|Ride Enforcer's Pterrordax to the Throne|V|
+C Rastakhan|QID|46930|QO|2|M|57.59,43.96|Z|Zuldazar|N|Speak with Princess Talanji. This starts a cut-scene you can end early with <ESC>.|CHAT|
 T Rastakhan|QID|46930|M|57.95,43.31|Z|Zuldazar|N|To King Rastakhan.|
-A Speaker of the Horde|QID|46931|M|57.95,43.31|Z|Zuldazar|N|From King Rastakhan.|PRE|46930|
-R Speaker of the Horde|ACTIVE|46931|M|57.55,44.05|Z|Zuldazar|QO|1|N|Head to Zolani and once she starts moving, take the elevator down to the lower level.\n[color=FF0000]NOTE: [/color]You don't need to wait for Zolani to complete these steps.|
-C Speaker of the Horde|QID|46931|M|49.92,39.49|Z|Dazar'alor|QO|2|N|Head outside and click the banner to summon your companions.|H|
-R Speaker of the Horde|ACTIVE|46931|M|57.95,44.31|Z|Zuldazar|QO|3|N|Head back inside and walk towards Brillin the Beauty (The Great Seal Innkeeper).|
+A Speaker of the Horde|QID|46931|PRE|46930|M|57.95,43.31|Z|Zuldazar|N|From King Rastakhan.|
+R Speaker of the Horde|ACTIVE|46931|QO|1|M|57.55,44.05|Z|Zuldazar|N|Head to Zolani and once she starts moving, take the elevator down to the lower level.\n[color=FF0000]NOTE: [/color]You don't need to wait for Zolani to complete these steps.|
+C Speaker of the Horde|QID|46931|QO|2|M|49.92,39.49|Z|Dazar'alor|N|Head outside and click the banner to summon your companions.|H|
+R Speaker of the Horde|ACTIVE|46931|QO|3|M|48.78,71.83|Z|The Great Seal!Dazar'alor|N|Head back inside and walk towards Brillin the Beauty (The Great Seal Innkeeper).|
 h The Great Seal|ACTIVE|46931|QO|6|N|Optionally, you can make The Great Seal your new home before moving on.\n[color=FF0000]NOTE: [/color]Manually check this step off to continue.|
-R Speaker of the Horde|ACTIVE|46931|M|67.00,71.83|Z|The Great Seal!Dazar'alor|QO|4|N|Head down the stairs to theleft into the Hall of Ancient Paths.\n[color=FF0000]NOTE: [/color]These will be your portals to the Horde Cities once this quest is complete.|
-R Speaker of the Horde|ACTIVE|46931|M|32.22,70.00|Z|The Great Seal!Dazar'alor|CS|QO|5|N|Head back up the stairs, across the corridor, then down the stairs to the Vault of the King.|
+R Speaker of the Horde|ACTIVE|46931|QO|4|M|67.00,71.83|Z|The Great Seal!Dazar'alor|N|Head down the stairs to theleft into the Hall of Ancient Paths.\n[color=FF0000]NOTE: [/color]These will be your portals to the Horde Cities once this quest is complete.|
+R Speaker of the Horde|ACTIVE|46931|QO|5|M|32.22,70.00|Z|The Great Seal!Dazar'alor|CS|N|Head back up the stairs, across the corridor, then down the stairs to the Vault of the King.|
 T Speaker of the Horde|QID|46931|M|41.28,66.75|Z|The Great Seal!Dazar'alor|N|Back up the stairs, to Princess Talanji.\n[color=FF0000]NOTE: [/color]If the quest bugs and she's not there, log back in to fix it.|
-A Area to Explore|QID|47512^47513^47514|M|41.77,69.35|Z|1164|N|Pick Zuldazar from the map.|PRE|46931|
+A Area to Explore|QID|47512^47513^47514|PRE|46931|M|41.77,69.35|Z|1164|N|Pick Zuldazar from the map.|
 t Nazmir|QID|47512|M|41.28,66.75|Z|1164|N|To Princess Talanji take the elevator behind you, she is on her throne.|
 t Vol'dun|QID|47513|M|41.28,66.75|Z|1164|N|To Princess Talanji take the elevator behind you, she is on her throne.|
 t Zuldazar|QID|47514|M|41.28,66.75|Z|1164|N|To Princess Talanji take the elevator behind you, she is on her throne.|
-A Journey to Nazmir|QID|47103|M|40.22,66.50|Z|Hall of Croniclers!Dazar'alor|N|From Princess Talanji.|PRE|47512|
-A Discreet Discussions|QID|47313|M|40.22,66.50|Z|Hall of Croniclers!Dazar'alor|N|From Princess Talanji.|PRE|47513|
-A Trust of a King|QID|49615|M|40.22,66.50|Z|Hall of Croniclers!Dazar'alor|N|From Princess Talanji.|PRE|47514|
-A The War Campaign|QID|52749|M|40.67,72.08|Z|Hall of Croniclers!Dazar'alor|N|From Nathanos Blightcaller.|LVL|35|PRE|46931|
+A Journey to Nazmir|QID|47103|PRE|47512|M|40.22,66.50|Z|Hall of Croniclers!Dazar'alor|N|From Princess Talanji.|
+A Discreet Discussions|QID|47313|PRE|47513|M|40.22,66.50|Z|Hall of Croniclers!Dazar'alor|N|From Princess Talanji.|
+A Trust of a King|QID|49615|PRE|47514|M|40.22,66.50|Z|Hall of Croniclers!Dazar'alor|N|From Princess Talanji.|
+A The War Campaign|QID|52749|PRE|46931|M|40.67,72.08|Z|Hall of Croniclers!Dazar'alor|N|From Nathanos Blightcaller.|LVL|35|
 
 ; professions
 = Zandalari Cooking|ACTIVE|49615|M|28.44,50.42|Z|1164|N|Train BfA Cooking at T'sarah the Royal Chef.|P|Cooking;185|RECIPE|259430|;
@@ -75,16 +75,18 @@ R Terrace of Crafters|AVAILABLE|51465|M|43.15,35.51|Z|Dazar'alor|N|Run down to T
 N Jani's Mysterious Trashpiles|AVAILABLE|47441|N|These are scattered around Zandalar.\nUpon completion of the following 2 quests ('Pests' and 'Curse of Jani'), when you are near one of these Trashpiles, you will either see a quest "!", or see/hear a message saying "hey richmon…".\n[color=FF0000]NOTE: [/color]Manually check this step off to continue.|RANK|3|
 R The Zocalo|AVAILABLE|47441|M|42.22,25.82|Z|Dazar'alor|N|Run down, down, down in the general direction of the waypoint.|RANK|3|IZ|Dazar'alor|
 A Pests|QID|47441|M|40.24,19.06|Z|Dazar'alor|N|From Nokano.|RANK|3|
-C Pests|QID|47441|Z|Dazar'alor|M|41.80,25.00;38.90,20.30;36.30,11.10;37.35,16.10;36.45,13.70;39.25,18.32;40.90,22.90|CN|QO|1|N|Kill the little pests.|T|Thieving Snapper|
-C Pests|QID|47441|Z|Dazar'alor|M|35.35,7.67|QO|2|H|N|Follow the Big One. When you arrive at the trashpile, click on it to summon Jani.|
+C Pests|QID|47441|QO|1|M|41.80,25.00;38.90,20.30;36.30,11.10;37.35,16.10;36.45,13.70;39.25,18.32;40.90,22.90|CN|Z|Dazar'alor|N|Kill the little pests.|T|Thieving Snapper|
+R Mysterious Trashpile|ACTIVE|47441|QO|2|M|35.35,7.67|CC|Z|Dazar'alor|N|Follow the Big One.|
+C Pests|QID|47441|QO|2|M|35.35,7.67|Z|Dazar'alor|N|When you arrive at the trashpile, click on it to summon Jani.|H|
 T Pests|QID|47441|M|35.36,7.63|Z|Dazar'alor|N|To Jani.|
 A Curse of Jani|QID|47442|M|35.36,7.63|Z|Dazar'alor|N|From Jani.|RANK|3|
-C Curse of Jani|QID|47442|M|38.57,17.70|Z|Dazar'alor|QO|1|NC|N|Run back to Nokano. Save sprint for just before the guard in the middle of the road. If you get caught, ask Jani for new disguise.|
-C Curse of Jani|QID|47442|M|40.10,19.30|Z|Dazar'alor|QO|2|NC|N|Right click on Nokano to bite him.|
-T Curse of Jani|QID|47442|M|35.46,7.71|Z|Dazar'alor|N|To Jani. You have now opened up Jani's cursed treasure quests all over Zandalar.|
-R The Sliver|ACTIVE|49615|M|45.35,23.86;50.29,21.22|Z|Dazar'alor|CS|N|Run down several sets of stairs to the Sliver to find the fishing trainer.|P|Fishing;356|RECIPE|271677|
-f The Sliver|ACTIVE|49615|M|53.16,19.35|Z|Dazar'alor|N|As long as you are here, grab the flight path from Zena the Feeder.|P|Fishing;356|RECIPE|271677|
-N Fishing|ACTIVE|49615|M|50.52,23.35|Z|Dazar'alor|N|Train BfA Fishing at Silent Tali. Check off manually when done.|P|Fishing;356|RECIPE|271677|;need to find correct spell id to autocomplete
-H The Great Seal|ACTIVE|49615|N|Hearth, fly or run back to the flightmaster, just outside The Great Seal to continue in the guide of your choice.|RANK|3|
+R Nokano|QID|47442|QO|1|M|38.57,17.70|Z|Dazar'alor|N|Run back to Nokano; using 'Sprint' just before the guard in the middle of the road.\n[color=FF0000]NOTE: [/color]If you get caught, you'll be teleported back to the Trashpile to restart the run.|
+C Curse of Jani|QID|47442|QO|2|M|40.10,19.30|Z|Dazar'alor|N|Right-click on Nokano to bite him.\n[color=FF0000]NOTE: [/color]Do this quick before the guards catch up to you.|NC|
+T Curse of Jani|QID|47442|M|35.46,7.71|Z|Dazar'alor|N|To Jani.\n[color=FF0000]NOTE: [/color]You have now opened up Jani's cursed treasure quests all over Zandalar.|
+R Terrace of the Chosen|ACTIVE|49615|M|45.67,23.39|Z|Dazar'alor|N|Return to the (south)east end of Zocalo.|
+R The Sliver|ACTIVE|49615|M|45.35,23.86;50.29,21.22|CS|Z|Dazar'alor|N|Run down to the bottom of the stairs.|
+f The Sliver|ACTIVE|49615|M|53.16,19.35|Z|Dazar'alor|N|Grab the flight path from Zena the Feeder.|
+= Zandalari Fishing|ACTIVE|49615|M|50.52,23.35|Z|Dazar'alor|N|Learn BfA Fishing from Silent Tali.|P|Fishing;356|SPELL|Zandalari Fishing;271677|
+H The Great Seal|ACTIVE|49615|M|48.78,71.83|Z|The Great Seal!Dazar'alor|N|Hearth or use the flightmaster.|RANK|3|FLY|OLD|
 ]]
 end)
