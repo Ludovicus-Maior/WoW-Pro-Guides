e7847e31400b50d996e2398b11e394bdc9744747
Spoony36 <Spoony36@users.noreply.github.com>
2024-01-23 20:11:16 +0000

    Update WoWPro_Broker.lua

--- WoWPro/WoWPro_Broker.lua
+++ WoWPro/WoWPro_Broker.lua
@@ -1174,7 +1174,7 @@ if step then
         local questId = string.match(QID, "([^%^]*)")
 
         tinsert(dropdown,
-            {text = "wowhead Link", func = function()
+            {text = "Wowhead Link", func = function()
                 local link = "https://www.wowhead.com/quest=" .. questId
 
                 -- Create an EditBox to display the link
