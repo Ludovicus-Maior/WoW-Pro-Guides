002269f6f02b2736f55f42e50ef382107cb361aa
Ludovicus Maior <46032662+Ludovicus-Maior@users.noreply.github.com>
2024-03-08 13:44:42 -0500

    Update WoWPro_CurrentGuide.lua
    Use _G.UIParent

--- WoWPro/WoWPro_CurrentGuide.lua
+++ WoWPro/WoWPro_CurrentGuide.lua
@@ -1,4 +1,4 @@
--- luacheck: globals tostring ipairs max floor UIParent
+-- luacheck: globals tostring ipairs max floor
 
 -----------------------------------
 --      WoWPro_CurrentGuide      --
@@ -10,7 +10,7 @@ local NUMROWS, ROWHEIGHT, GAP, EDGEGAP = 12, 25, 8, 16
 local offset, rows, shownrows = 0, {}, NUMROWS
 WoWPro.CreateCurrentGuideTitle = true
 
-local frame = _G.CreateFrame("Frame", nil, UIParent, "BackdropTemplate")
+local frame = _G.CreateFrame("Frame", nil, _G.UIParent, "BackdropTemplate")
 frame.name = L["Current Guide"]
 frame.parent = "WoWPro"
 frame:SetSize(625, 430)
