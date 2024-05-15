6d759e5d56ea176a435d551ac78ee54f4ae74d5d
Hendo72 <39675478+Hendo72@users.noreply.github.com>
2024-05-15 11:00:26 -0400

    Update CATA_Bloodmyst.lua
    Working on a better flow ... may update current layout later.

--- WoWPro_Leveling/Classic_Cata/Alliance/CATA_Bloodmyst.lua
+++ WoWPro_Leveling/Classic_Cata/Alliance/CATA_Bloodmyst.lua
@@ -22,7 +22,7 @@ A Catch and Release|QID|9629|M|53.25,57.75|Z|1950;Bloodmyst Isle|N|From Morae.|
 r Repair/Restock|ACTIVE|9693|M|53.32,56.67|Z|1950;Bloodmyst Isle|N|Repair/Sell Junk at Beega.|
 = Training|ACTIVE|9693|M|PLAYER|CC|N|Check in with your trainer.\n[color=FF0000]NOTE: [/color]Manually check this step off to continue.|IZ|Blood Watch|
 A Learning from the Crystals|QID|9581|M|52.60,53.22|Z|1950;Bloodmyst Isle|N|From Harbinger Mikolaas.|
-A What Argus Means to Me|QID|9693|M|52.67,53.21|Z|1950;Bloodmyst Isle|N|From Exarch Admetius.|
+A What Argus Means to Me|QID|9693|M|52.68,53.22|Z|1950;Bloodmyst Isle|N|From Exarch Admetius.|
 T What Argus Means to Me|QID|9693|M|55.42,55.25|Z|1950;Bloodmyst Isle|N|To Vindicator Boros.|
 A Blood Watch|QID|9694|PRE|9693|M|55.42,55.25|Z|1950;Bloodmyst Isle|N|From Vindicator Boros.|
 f Blood Watch|ACTIVE|9634|M|57.69,53.92|Z|1950;Bloodmyst Isle|N|Get the flight path for Blood Watch.\n[color=FF0000]NOTE: [/color]Open the flight map to populate the FPs.|
@@ -155,7 +155,7 @@ C Culling the Flutterers|QID|9647|M|47.21,32.72|Z|1950;Bloodmyst Isle|N|Finish u
 T Galaen's Journal - The Fate of Vindicator Saruan|QID|9706|M|55.63,55.23|Z|1950;Bloodmyst Isle|N|To Vindicator Kuros.|
 A Matis the Cruel|QID|9711|PRE|9706|M|55.63,55.23|Z|1950;Bloodmyst Isle|N|From Vindicator Kuros.|
 T The Cryo-Core|QID|9703|M|55.63,55.23|Z|1950;Bloodmyst Isle|N|To Vindicator Kuros.|
-A Don't Drink the Water|QID|9748|PRE|9703|M|55.57,55.38|Z|1950;Bloodmyst Isle|N|From Vindicator Aesom.|
+A Don't Drink the Water|QID|9748|PRE|9703|M|55.55,55.40|Z|1950;Bloodmyst Isle|N|From Vindicator Aesom.|
 A What We Know...|QID|9753|M|55.6,55.4|Z|1950;Bloodmyst Isle|N|From Vindicator Aesom.|
 T Culling the Flutterers|QID|9647|M|55.86,56.99|Z|1950;Bloodmyst Isle|N|To Tracker Lyceon.|
 T Galaen's Fate|QID|9579|M|53.3,57.7|Z|1950;Bloodmyst Isle|N|To Morae.|
@@ -167,43 +167,45 @@ C Containing the Threat|QID|9569|QO|2;3;4|M|40.75,34.67|Z|1950;Bloodmyst Isle|N|
 C Don't Drink the Water|QID|9748|M|34.37,33.56|Z|1950;Bloodmyst Isle|N|Jump/climb down to the very bottom of the waterfall and use your flask.|U|24318|NC|
 H Blood Watch|ACTIVE|9569|M|55.84,59.80|Z|1950;Bloodmyst Isle|N|Hearth or run back to Blood Watch.|
 T Containing the Threat|QID|9569|M|55.09,58.00|Z|1950;Bloodmyst Isle|N|To Vindicator Aalesia.|
-T Don't Drink the Water|QID|9748|M|55.57,55.38|Z|1950;Bloodmyst Isle|N|To Vindicator Aesom.|
-A Limits of Physical Exhaustion|QID|9746|PRE|9748|M|55.57,55.38|Z|1950;Bloodmyst Isle|N|From Vindicator Aesom.|
+T Don't Drink the Water|QID|9748|M|55.55,55.40|Z|1950;Bloodmyst Isle|N|To Vindicator Aesom.|
+A Limits of Physical Exhaustion|QID|9746|PRE|9748|M|55.55,55.40|Z|1950;Bloodmyst Isle|N|From Vindicator Aesom.|
+T What We Know...|QID|9753|M|52.68,53.22|Z|1950;Bloodmyst Isle|N|To Exarch Admetius.|
+A What We Don't Know...|QID|9756|PRE|9753|M|52.68,53.22|Z|1950;Bloodmyst Isle|N|From Exarch Admetius.|
 T The Final Sample|QID|9585|M|52.60,53.22|Z|1950;Bloodmyst Isle|N|To Harbinger Mikolaas.|
-T What We Know...|QID|9753|M|52.66,53.23|Z|1950;Bloodmyst Isle|N|To Exarch Admetius.|
-A What We Don't Know...|QID|9756|PRE|9753|M|52.66,53.23|Z|1950;Bloodmyst Isle|N|From Exarch Admetius.|
 C What We Don't Know...|QID|9756|M|54.4,54.3|Z|1950;Bloodmyst Isle|N|Talk to the prisoner.|CHAT|
-T What We Don't Know...|QID|9756|M|52.66,53.23|Z|1950;Bloodmyst Isle|N|To Exarch Admetius.|
-A Vindicator's Rest|QID|9760|PRE|9756|M|52.66,53.23|Z|1950;Bloodmyst Isle|N|From Exarch Admetius.|
+T What We Don't Know...|QID|9756|M|52.68,53.22|Z|1950;Bloodmyst Isle|N|To Exarch Admetius.|
+A Vindicator's Rest|QID|9760|PRE|9756|M|52.68,53.22|Z|1950;Bloodmyst Isle|N|From Exarch Admetius.|
 A The Missing Expedition|QID|9669|M|53.3,57.0|Z|1950;Bloodmyst Isle|N|From Achelus.|
-C Matis the Cruel|QID|9711|Z|1950;Bloodmyst Isle|N|Use the flare gun! When you get into range, just use your flare gun, and the quest is easy. He pats along the road, you should see him somewhere near Vindicator's Rest.|U|24278|M|31.5,48|
-T Vindicator's Rest|QID|9760|M|30.6,46.6|Z|1950;Bloodmyst Isle|N|To Vindicator Corin.|
-A Fouled Water Spirits|QID|10067|M|30.6,46.6|Z|1950;Bloodmyst Isle|N|From Vindicator Corin.|
-A Oh, the Tangled Webs They Weave|QID|10066|M|30.6,46.6|Z|1950;Bloodmyst Isle|N|From Vindicator Corin.|
+C Limits of Physical Exhaustion|QID|9746|M|26,53|Z|1950;Bloodmyst Isle|N|Kill Sunhawk Pyromancers and Defenders.|S|
+C Matis the Cruel|QID|9711|M|47.59,40.18|Z|1950;Bloodmyst Isle|N|Use the flare gun when you get into range of him.|T|Matis the Cruel|U|24278|S|NC|
+T Vindicator's Rest|QID|9760|M|30.75,46.84|Z|1950;Bloodmyst Isle|N|To Vindicator Corin.|
+A Clearing the Way|QID|9761|PRE|9760|M|30.75,46.84|Z|1950;Bloodmyst Isle|N|From Vindicator Corin.|
+A Fouled Water Spirits|QID|10067|M|30.75,46.84|Z|1950;Bloodmyst Isle|N|From Vindicator Corin.|
+A Oh, the Tangled Webs They Weave|QID|10066|M|30.75,46.84|Z|1950;Bloodmyst Isle|N|From Vindicator Corin.|
 T Talk to the Hand|QID|10064|M|30.27,45.94|Z|1950;Bloodmyst Isle|N|To Scout Joril.|
 A Cutting a Path|QID|10065|M|30.28,45.88|Z|1950;Bloodmyst Isle|N|From Scout Joril.|
-A Critters of the Void|QID|9741|M|30.28,45.88|Z|1950;Bloodmyst Isle|N|From Scout Loryi.|
+A Critters of the Void|QID|9741|M|30.24,45.88|Z|1950;Bloodmyst Isle|N|From Scout Loryi.|
 C Cutting a Path|QID|10065|M|31.25,56.68|Z|1950;Bloodmyst Isle|N|Kill 10 Enraged Ravagers.|S|
-C Oh, the Tangled Webs They Weave|QID|10066|M|31.25,56.68|Z|1950;Bloodmyst Isle|N|Kill 8 Mutated Tanglers.|
-C Cutting a Path|QID|10065|M|31.25,56.68|Z|1950;Bloodmyst Isle|N|Kill 10 Enraged Ravagers.|US|
-C Limits of Physical Exhaustion|QID|9746|M|26,53|Z|1950;Bloodmyst Isle|N|Kill Sunhawk Pyromancers and Defenders.|S|
-C Critters of the Void|QID|9741|M|19,62|Z|1950;Bloodmyst Isle|N|Kill 12 Void Critters. They are small and can be hard to target sometimes.|
+C Oh, the Tangled Webs They Weave|QID|10066|M|31.25,56.68|Z|1950;Bloodmyst Isle|N|Kill 8 Mutated Tanglers.|T|Mutated Tangler|
+C Cutting a Path|QID|10065|M|31.25,56.68|Z|1950;Bloodmyst Isle|N|Kill 10 Enraged Ravagers.|T|Enraged Ravager|US|
+C Critters of the Void|QID|9741|M|19,62|Z|1950;Bloodmyst Isle|N|Kill 12 Void Critters. \n[color=FF0000]NOTE: [/color]Use the targetting button because they are small and can be hard to target.|T|Void Critter|
 C Limits of Physical Exhaustion|QID|9746|M|26,53|Z|1950;Bloodmyst Isle|N|Finish killing Sunhawk Pyromancers and Defenders.|US|
+C Matis the Cruel|QID|9711|M|25.73,51.94;47.59,40.18|CC|Z|1950;Bloodmyst Isle|N|Use the flare gun when you get into range of him.|T|Matis the Cruel|U|24278|US|NC|
 C Fouled Water Spirits|QID|10067|M|30,39|Z|1950;Bloodmyst Isle|N|Kill 6 Fouled Water Spirits.|S|
-A They're Alive! Maybe...|QID|9670|M|24.9,34.3|Z|1950;Bloodmyst Isle|N|From Researcher Cornelius.|
-C They're Alive! Maybe...|QID|9670|M|22,36|Z|1950;Bloodmyst Isle|N|Attack the Webbed Creatures. You may free a Researcher, or you may get a hostile mob.|S|
+A They're Alive! Maybe...|QID|9670|M|24.87,34.36|Z|1950;Bloodmyst Isle|N|From Researcher Cornelius.|
+C They're Alive! Maybe...|QID|9670|M|22,36|Z|1950;Bloodmyst Isle|N|Attack the Webbed Creatures. You may free a Researcher, or you may get a hostile mob.|T|Webbed Creature|S|
 C The Missing Expedition|QID|9669|M|22,36;18,37|CC|Z|1950;Bloodmyst Isle|N|Kill spiders on the way, then kill Zarakh.|
-C They're Alive! Maybe...|QID|9670|M|22,36|Z|1950;Bloodmyst Isle|N|Attack the Webbed Creatures. You may free a Researcher, or you may get a hostile mob.|US|
-T They're Alive! Maybe...|QID|9670|M|24.9,34.3|Z|1950;Bloodmyst Isle|N|To Researcher Cornelius.|
+C They're Alive! Maybe...|QID|9670|M|22,36|Z|1950;Bloodmyst Isle|N|Attack the Webbed Creatures. You may free a Researcher, or you may get a hostile mob.|T|Webbed Creature|US|
+T They're Alive! Maybe...|QID|9670|M|24.87,34.36|Z|1950;Bloodmyst Isle|N|To Researcher Cornelius.|
 C Fouled Water Spirits|QID|10067|M|30,39|Z|1950;Bloodmyst Isle|N|Kill 6 Fouled Water Spirits.|US|
 T Cutting a Path|QID|10065|M|30.28,45.88|Z|1950;Bloodmyst Isle|N|To Scout Joril.|
-T Fouled Water Spirits|QID|10067|M|30.6,46.6|Z|1950;Bloodmyst Isle|N|To Vindicator Corin.|
-T Oh, the Tangled Webs They Weave|QID|10066|M|30.6,46.6|Z|1950;Bloodmyst Isle|N|To Vindicator Corin.|
+T Fouled Water Spirits|QID|10067|M|30.75,46.84|Z|1950;Bloodmyst Isle|N|To Vindicator Corin.|
+T Oh, the Tangled Webs They Weave|QID|10066|M|30.75,46.84|Z|1950;Bloodmyst Isle|N|To Vindicator Corin.|
 H Blood Watch|ACTIVE|9711|M|55.84,59.80|Z|1950;Bloodmyst Isle|N|Hearth or run back to Blood Watch.|
 T Matis the Cruel|QID|9711|M|55.61,55.14|Z|1950;Bloodmyst Isle|N|To Vindicator Kuros.|
-T Limits of Physical Exhaustion|QID|9746|M|55.56,55.37|Z|1950;Bloodmyst Isle|N|To Vindicator Aesom.|
-A The Sun Gate|QID|9740|PRE|9746|M|55.56,55.37|Z|1950;Bloodmyst Isle|N|From Vindicator Aesom.|
-T Critters of the Void|QID|9741|M|55.56,55.37|Z|1950;Bloodmyst Isle|N|To Vindicator Aesom.|
+T Limits of Physical Exhaustion|QID|9746|M|55.55,55.40|Z|1950;Bloodmyst Isle|N|To Vindicator Aesom.|
+A The Sun Gate|QID|9740|PRE|9746|M|55.55,55.40|Z|1950;Bloodmyst Isle|N|From Vindicator Aesom.|
+T Critters of the Void|QID|9741|M|55.55,55.40|Z|1950;Bloodmyst Isle|N|To Vindicator Aesom.|
 T The Missing Expedition|QID|9669|M|53.25,57.02|Z|1950;Bloodmyst Isle|N|To Achelus.|
 A Urgent Delivery|QID|9671|PRE|9506|M|55.31,54.21;53.05,53.84;54.18,61.12|CN|Z|1950;Bloodmyst Isle|N|From Messenger Hermesius, who circles the road in Blood Watch.|
 N Check your mailbox|AVAILABLE|9672|PRE|9671|M|55.18,59.19|Z|1950;Bloodmyst Isle|L|24132|N|Urgent Delivery results in a mail sent to your mailbox.|NC|
@@ -241,15 +243,14 @@ N Non-Draenei races|ACTIVE|9740|Z|1950;Bloodmyst Isle|N|Non-Draenei have only on
 F The Exodar|ACTIVE|9740|M|57.68,53.89|Z|1950;Bloodmyst Isle|N|Fly to The Exodar to train your riding skill. Skip this step if you already have it.|R|Draenei|
 N Get your Mount|ACTIVE|9740|Z|1950;Bloodmyst Isle|N|Up the ramp and to the left is the riding trainer. You'll need 4 gold for training and 1 gold for the mount. Skip if you already have it. Click to close and continue.|R|Draenei|
 F Blood Watch|ACTIVE|9740|M|54.46,36.4|Z|1947; The Exodar|N|Fly back to Blood Watch. Skip if you're already there.|R|Draenei|
-A Clearing the Way|QID|9761|PRE|9760|M|30.6,46.6|Z|1950;Bloodmyst Isle|N|From Vindicator Corin.|
-A Ending Their World|QID|9759|PRE|9753|M|30.8,46.7|Z|1950;Bloodmyst Isle|N|From Demolitionist Legoso.|
+A Ending Their World|QID|9759|PRE|9753|M|30.82,46.66|Z|1950;Bloodmyst Isle|N|From Demolitionist Legoso.|
 C Clearing the Way|QID|9761|M|19.8,52.6|Z|1950;Bloodmyst Isle|N|Kill Sunhawk Agents and Saboteurs. Let the Legoso do most of the workMake sure you do 50%+ dmg to the quest mobs, so you get credit.|S|
 C Ending Their World|QID|9759|M|19,52|Z|1950;Bloodmyst Isle|N|Follow Legoso, he will stop in 2 places to plant explosives. Then you will have to kill Sironas.|
 C Clearing the Way|QID|9761|M|19.8,52.6|Z|1950;Bloodmyst Isle|N|Finish killing Sunhawk Agents and Saboteurs.|US|
 C The Sun Gate|QID|9740|M|18.95,63.5|Z|1950;Bloodmyst Isle|N|Go to the Sun Gate. Right click the 4 portal controllers, then right click the Sun Gate.|
-T Clearing the Way|QID|9761|M|30.6,46.6|Z|1950;Bloodmyst Isle|N|To Vindicator Corin.|
+T Clearing the Way|QID|9761|M|30.75,46.84|Z|1950;Bloodmyst Isle|N|To Vindicator Corin.|
 H Blood Watch|ACTIVE|9740|M|55.84,59.80|Z|1950;Bloodmyst Isle|N|Hearth or run back to Blood Watch.|
-T The Sun Gate|QID|9740|M|55.54,55.42|Z|1950;Bloodmyst Isle|N|To Vindicator Aesom.|
+T The Sun Gate|QID|9740|M|55.55,55.40|Z|1950;Bloodmyst Isle|N|To Vindicator Aesom.|
 T Ending Their World|QID|9759|M|52.60,53.22|Z|1950;Bloodmyst Isle|N|This is one of the best quest turn-ins in WoW. Get ready to enjoy a bit of a show. Turn the quest into Exarch Admetius.|
 A The Unwritten Prophecy|QID|9762|PRE|9759|M|54,55.4|Z|1950;Bloodmyst Isle|N|From Prophet Velen. If Prophet Velen despawns before you accept the quest, go to The Exodar to get the quest.|R|Draenei|
 A Newfound Allies|QID|9632|M|54.69,54.01|Z|1950;Bloodmyst Isle|N|From Anchorite Paetheus.|
