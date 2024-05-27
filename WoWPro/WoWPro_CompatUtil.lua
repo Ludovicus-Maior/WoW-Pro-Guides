f4db1e786623c1c7ca7c909b01e88b5d509ccdec
mdavidson37 <35685316+mdavidson37@users.noreply.github.com>
2024-05-27 23:16:56 +0100

    No longer define unused variables returend by legacy calls.

--- WoWPro/WoWPro_CompatUtil.lua
+++ WoWPro/WoWPro_CompatUtil.lua
@@ -210,7 +210,7 @@ function WoWPro.C_Reputation_GetFactionDataByID(factionID)
     if WoWPro.RETAIL then
         return _G.C_Reputation.GetFactionDataByID(factionID)
     else
-        local name, description, standingID, barMin, barMax, barValue, atWarWith, canToggleAtWar, isHeader, isCollapsed, hasRep, isWatched, isChild, factionID, hasBonusRepGain, canBeLFGBonus = _G.GetFactionInfoByID(factionID)
+        local name, description, _, _, _, _, atWarWith, canToggleAtWar, _, isCollapsed, _, isWatched, isChild, _, hasBonusRepGain, _ = _G.GetFactionInfoByID(factionID)
         return {
             hasBonusRepGain = hasBonusRepGain,
             description = description,
@@ -221,11 +221,11 @@ function WoWPro.C_Reputation_GetFactionDataByID(factionID)
             isWatched = isWatched,
             isCollapsed = isCollapsed,
             canToggleAtWar = canToggleAtWar,
-            nextReactionThreshold,
+            -- nextReactionThreshold,
             factionID = factionID,
             name = name,
-            currentStanding,
-            isAccountWide,
+            --currentStanding,
+            --isAccountWide,
             isChild = isChild,
             -- reaction
         }
@@ -237,7 +237,7 @@ function WoWPro.C_Spell_GetSpellInfo(spellID)
     if WoWPro.RETAIL then
         return _G.C_Spell.GetSpellInfo(spellID)
     else
-        local name, rank, icon, castTime, minRange, maxRange, spellID, originalIcon = _G.GetSpellInfo(spell)
+        local name, rank, icon, castTime, minRange, maxRange, _, originalIcon = _G.GetSpellInfo(spellID)
         return{
            castTime = castTime,
            name = name,
