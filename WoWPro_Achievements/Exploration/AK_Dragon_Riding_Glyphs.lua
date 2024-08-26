81bcdcb38434881397d9784682056e953cb6432a
Ludovicus <ludovicus.maior@gmail.com>
2024-08-26 17:56:05 -0400

    Azj-Kahet Glyph Hunter: missing commas

--- WoWPro_Achievements/Exploration/AK_Dragon_Riding_Glyphs.lua
+++ WoWPro_Achievements/Exploration/AK_Dragon_Riding_Glyphs.lua
@@ -6,17 +6,17 @@ WoWPro.Achievements:GuideMisc(guide, "Azj-Kahet Glyph Hunter", "Exploration", "T
 WoWPro:GuideSteps(guide, function()
 return [[
 
-F Arathi's End Glyph|M|63.43 16.98|Z|2255;The Reckoning|N|Behind the stone column|ACH|40691|
-F Siegehold Glyph|M|46.69 21.29|Z|2255;The Reckoning|N|High in the air, above the cliff|ACH|40692|
-F Ruptured Lake Glyph|M|25.14 40.58|Z|2255;The Reckoning|N|High in the air, between roots|ACH|40693|
-F Eye of Ansurek Glyph|M|43.12 57.02|Z|2255;The Reckoning|N|In the watchtower, top level|ACH|40694|
-F Old Sacrificial Pit Glyph|M|13.32 33.52|Z|2255;The Reckoning|N|High in the air|ACH|40695|
-F Deepwalker Pass Glyph|M|58.62 89.80|Z|2255;The Reckoning|N|Upper level of a floating tower|ACH|40696|
-F The Maddening Deep Glyph|M|66.29 85.05|Z|2255;The Reckoning|N|Under the web bridge|ACH|40697|
-F Rak-Ush Glyph|M|73.21 84.11|Z|2255;The Reckoning|N|Near stone structures|ACH|40698|
-F Silken Ward Glyph|M|57.61 57.55|Z|2255;The Reckoning|N|Under the floating structure|ACH|40699|
-F Trickling Abyss Glyph|M|70.58 25.19|Z|2255;The Reckoning|N|High in the air|ACH|40700|
-F Untamed Valley Glyph|M|65.43 51.76|Z|2255;The Reckoning|N|High in the air|ACH|40701|
+F Arathi's End Glyph|M|63.43,16.98|Z|2255;The Reckoning|N|Behind the stone column|ACH|40691|
+F Siegehold Glyph|M|46.69,21.29|Z|2255;The Reckoning|N|High in the air, above the cliff|ACH|40692|
+F Ruptured Lake Glyph|M|25.14,40.58|Z|2255;The Reckoning|N|High in the air, between roots|ACH|40693|
+F Eye of Ansurek Glyph|M|43.12,57.02|Z|2255;The Reckoning|N|In the watchtower, top level|ACH|40694|
+F Old Sacrificial Pit Glyph|M|13.32u,33.52|Z|2255;The Reckoning|N|High in the air|ACH|40695|
+F Deepwalker Pass Glyph|M|58.62,89.80|Z|2255;The Reckoning|N|Upper level of a floating tower|ACH|40696|
+F The Maddening Deep Glyph|M|66.29,85.05|Z|2255;The Reckoning|N|Under the web bridge|ACH|40697|
+F Rak-Ush Glyph|M|73.21,84.11|Z|2255;The Reckoning|N|Near stone structures|ACH|40698|
+F Silken Ward Glyph|M|57.61,57.55|Z|2255;The Reckoning|N|Under the floating structure|ACH|40699|
+F Trickling Abyss Glyph|M|70.58,25.19|Z|2255;The Reckoning|N|High in the air|ACH|40700|
+F Untamed Valley Glyph|M|65.43,51.76|Z|2255;The Reckoning|N|High in the air|ACH|40701|
 
 ]]
 end)
