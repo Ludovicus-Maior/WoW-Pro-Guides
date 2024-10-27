09821fdd6ad04c2453cce6c2a54ad1f64fcd8efd
mdavidson37 <35685316+mdavidson37@users.noreply.github.com>
2024-10-27 16:35:52 +0000

    Finally figrued out the correct flows for these breadcrumbs - I hope!

--- WoWPro_Leveling/Retail/Neutral/TWW_AzjKahet.lua
+++ WoWPro_Leveling/Retail/Neutral/TWW_AzjKahet.lua
@@ -83,8 +83,8 @@ C Of Pacts and Patrons|QID|84682|M|55.33,41.20|Z|2255;Azj-Kahet|QO|4|NC|N|Pick w
 T Of Pacts and Patrons|QID|84682|M|55.33,41.20|Z|2255;Azj-Kahet|N|To Y'tekhi.|
 T Making the Underworld Go Round|QID|84664|PRE|84682|M|55.78,43.74|Z|2255;Azj-Kahet|N|To Linny Leadfinger.|
 A Strange Bats|QID|83321|M|55.76,43.66|Z|2255;Azj-Kahet|N|From Linny Leadfinger.|RANK|2|
-A Advance to Faerin|QID|82248|M|55.86,43.80|Z|2255;Azj-Kahet|N|From Widow Arak'nai.|LVL|80|LEAD|81945|RANK|2|; This quest requires Level 80 - It's a breadcrub for "To Arathi's End(QID 81945)"
-A Advance to Faerin|QID|82248|M|55.86,43.80|Z|2255;Azj-Kahet|N|From Widow Arak'nai.|PRE|81929|LEAD|81945|RANK|2|; Also available at any level in Adventure mode
+A Advance to Faerin|QID|82248|M|55.86,43.80|Z|2255;Azj-Kahet|N|From Widow Arak'nai.|LVL|80|LEAD|81929|RANK|2|; This quest requires Level 80 - It's a breadcrub for "To Arathi's End(QID 81945)"
+A Advance to Faerin|QID|82248|M|55.86,43.80|Z|2255;Azj-Kahet|N|From Widow Arak'nai.|PRE|83543|LEAD|81929|RANK|2|; Also available at any level in Adventure mode
 C The Weaver's Gift|QID|78233|M|58.97,19.96|Z|2255;Azj-Kahet|QO|1|NC|N|Weaver's gift located.|
 T The Weaver's Gift|QID|78233|M|59.32,19.37|Z|2255;Azj-Kahet|N|To Faerin Lothar.|
 T Advance to Faerin|QID|82248|M|59.32,19.37|Z|2255;Azj-Kahet|N|To Faerin Lothar.|LEAD|81929|
