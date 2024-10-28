f60971762adbb9bef10a200de276cb0150837b03
Cagomei <sshort8503@hotmail.com>
2024-10-27 21:14:29 -0500

    Update HallowsEnd_DI.lua

--- WoWPro_WorldEvents/Retail/Neutral/HallowsEnd_DI.lua
+++ WoWPro_WorldEvents/Retail/Neutral/HallowsEnd_DI.lua
@@ -20,40 +20,40 @@ A Candy Bucket|QID|75675|M|65.22,57.93|Z|2022;The Waking Shore|N|Life Vault Ruin
 
 ;Ohn'ahran Plains
 A Candy Bucket|QID|75690|M|85.04,26.04|Z|2023;Ohn'ahran Plains|N|Timberstep Outpost|U|37586|
-A Candy Bucket|QID|75689|M|85.85,35.36|Z|2022;Ohn'ahran Plains|N|Rusza'thar Reach|U|37586|
-A Candy Bucket|QID|75688|M|81.28,59.21|Z|2022;Ohn'ahran Plains|N|Pinewood Post|U|37586|
-A Candy Bucket|QID|75692|M|72.13,80.39|Z|2022;Ohn'ahran Plains|N|Forkriver Crossing|U|37586|
-A Candy Bucket|QID|75691|M|41.91,60.44|Z|2022;Ohn'ahran Plains|N|Teerakaï|U|37586|
-A Candy Bucket|QID|75686|M|28.64,60.56|Z|2022;Ohn'ahran Plains|N|Shady Sanctuary|U|37586|
-A Candy Bucket|QID|75684|M|46.22,40.60|Z|2022;Ohn'ahran Plains|N|Broadhoof Outpost|U|37586|
-A Candy Bucket|QID|75687|M|57.13,76.70|Z|2022;Ohn'ahran Plains|N|Ohn'iri Springs|U|37586|
-A Candy Bucket|QID|75685|M|62.93,40.56|Z|2022;Ohn'ahran Plains|N|Maruukaï|U|37586|
-A Candy Bucket|QID|75693|M|66.25,24.53|Z|2022;Ohn'ahran Plains|N|Emberwatch|U|37586|
+A Candy Bucket|QID|75689|M|85.85,35.36|Z|2023;Ohn'ahran Plains|N|Rusza'thar Reach|U|37586|
+A Candy Bucket|QID|75688|M|81.28,59.21|Z|2023;Ohn'ahran Plains|N|Pinewood Post|U|37586|
+A Candy Bucket|QID|75692|M|72.13,80.39|Z|2023;Ohn'ahran Plains|N|Forkriver Crossing|U|37586|
+A Candy Bucket|QID|75691|M|41.91,60.44|Z|2023;Ohn'ahran Plains|N|Teerakaï|U|37586|
+A Candy Bucket|QID|75686|M|28.64,60.56|Z|2023;Ohn'ahran Plains|N|Shady Sanctuary|U|37586|
+A Candy Bucket|QID|75684|M|46.22,40.60|Z|2023;Ohn'ahran Plains|N|Broadhoof Outpost|U|37586|
+A Candy Bucket|QID|75687|M|57.13,76.70|Z|2023;Ohn'ahran Plains|N|Ohn'iri Springs|U|37586|
+A Candy Bucket|QID|75685|M|62.93,40.56|Z|2023;Ohn'ahran Plains|N|Maruukaï|U|37586|
+A Candy Bucket|QID|75693|M|66.25,24.53|Z|2023;Ohn'ahran Plains|N|Emberwatch|U|37586|
 
 ;Azure Span
-A Candy Bucket|QID|75671|M|18.81,24.55|Z|2022;The Waking Shore|N|Three-Falls Lookout|U|37586|
-A Candy Bucket|QID|75668|M|62.79,57.74|Z|2022;The Waking Shore|N|Camp Nowhere|U|37586|
-A Candy Bucket|QID|75669|M|12.38,49.36|Z|2022;The Waking Shore|N|Iskaara|U|37586|
-A Candy Bucket|QID|75670|M|65.50,16.25|Z|2022;The Waking Shore|N|Theron's Watch|U|37586|
-A Candy Bucket|QID|75667|M|47.03,40.26|Z|2022;The Waking Shore|N|Camp Antonidas|U|37586|
+A Candy Bucket|QID|75671|M|18.81,24.55|Z|2024;Azure Span|N|Three-Falls Lookout|U|37586|
+A Candy Bucket|QID|75668|M|62.79,57.74|Z|2024;Azure Span|N|Camp Nowhere|U|37586|
+A Candy Bucket|QID|75669|M|12.38,49.36|Z|2024;Azure Span|N|Iskaara|U|37586|
+A Candy Bucket|QID|75670|M|65.50,16.25|Z|2024;Azure Span|N|Theron's Watch|U|37586|
+A Candy Bucket|QID|75667|M|47.03,40.26|Z|2024;Azure Span|N|Camp Antonidas|U|37586|
 
 ;Thaldraszus
-A Candy Bucket|QID|75697|M|52.41,69.81|Z|2022;The Waking Shore|N|Gelikyr Post|U|37586|
-A Candy Bucket|QID|75695|M|59.85,82.69|Z|2022;The Waking Shore|N|Temporal Conflux|U|37586|
-A Candy Bucket|QID|75698|M|50.08,42.73|Z|2022;The Waking Shore|N|Algeth'era Court|U|37586|
-A Candy Bucket|QID|75696|M|35.08,79.20|Z|2022;The Waking Shore|N|Garden Shrine|U|37586|
+A Candy Bucket|QID|75697|M|52.41,69.81|Z|2025;Thaldraszus|N|Gelikyr Post|U|37586|
+A Candy Bucket|QID|75695|M|59.85,82.69|Z|2025;Thaldraszus|N|Temporal Conflux|U|37586|
+A Candy Bucket|QID|75698|M|50.08,42.73|Z|2025;Thaldraszus|N|Algeth'era Court|U|37586|
+A Candy Bucket|QID|75696|M|35.08,79.20|Z|2025;Thaldraszus|N|Garden Shrine|U|37586|
 
 ;Valdrakken
-A Candy Bucket|QID|75699|M|47.16,45.45|Z|2022;The Waking Shore|N|The roasted ram|U|37586|
-A Candy Bucket|QID|75701|M|22.41,30.85|Z|2022;The Waking Shore|N|Weyrnrest|U|37586|
-A Candy Bucket|QID|75700|M|72.37,46.67|Z|2022;The Waking Shore|N|The parting glass|U|37586|
+A Candy Bucket|QID|75699|M|47.16,45.45|Z|2112;Valdrakken|N|The roasted ram|U|37586|
+A Candy Bucket|QID|75701|M|22.41,30.85|Z|2112;Valdrakken|N|Weyrnrest|U|37586|
+A Candy Bucket|QID|75700|M|72.37,46.67|Z|2112;Valdrakken|N|The parting glass|U|37586|
 
 ;The Forbidden Reach
-A Candy Bucket|QID|75702|M|33.85,58.81|Z|2022;The Waking Shore|N|Morqut Village|U|37586|
+A Candy Bucket|QID|75702|M|33.85,58.81|Z|2151;The Forbidden Reach|N|Morqut Village|U|37586|
 
 ;Zaralek Caverns
-A Candy Bucket|QID|75704|M|56.37,56.35|Z|2022;The Waking Shore|N|Loamm|U|37586|
-A Candy Bucket|QID|75703|M|52.12,26.45|Z|2022;The Waking Shore|N|Obsidian Rest|U|37586|
+A Candy Bucket|QID|75704|M|56.37,56.35|Z|2133;Zaralek Caverns|N|Loamm|U|37586|
+A Candy Bucket|QID|75703|M|52.12,26.45|Z|2133;Zaralek Caverns|N|Obsidian Rest|U|37586|
 
 N This completes Hallow's End for The Dragon Isles|U|37586|
 
