77628eb75e735f1d5d6a28588385b40a3ec742ce
Cagomei <sshort8503@hotmail.com>
2024-10-27 23:32:00 -0500

    Part 2
    Khaz Algar Pumpkins

---
+++ WoWPro_WorldEvents/Retail/Neutral/HallowsEnd_TWW.lua
@@ -0,0 +1,41 @@
+local guide = WoWPro:RegisterGuide("CagHollTWW",'WorldEvents',"Khaz Algar", "WoWPro Team", "Neutral")
+WoWPro:GuideLevels(guide,10,80)
+WoWPro.WorldEvents:GuideHoliday(guide,"HallowsEnd", "Hallow's End - Khaz Algar")
+WoWPro:GuideSteps(guide, function()
+return [[
+
+;Dornogal
+A Candy Bucket|QID|84564|M|45.01,47.35|Z|2339;Dornogal|N|Dornogal|U|37586|
+
+;Isle of Dorn
+A Candy Bucket|QID|84567|M|58.17,27.13|Z|2248;Isle of Dorn|N|Rambleshire|U|37586|
+A Candy Bucket|QID|84566|M|42.00,74.39|Z|2248;Isle of Dorn|N|Freywold Village|U|37586|
+
+;The Ringing Deeps
+A Candy Bucket|QID|84569|M|47.88,32.11|Z|2214;The Ringing Deeps|N|Gundargaz|U|37586|
+A Candy Bucket|QID|84571|M|61.85,46.26|Z|2214;The Ringing Deeps|N|Shadowvein Point|U|37586|
+A Candy Bucket|QID|84568|M|59.45,64.09|Z|2214;The Ringing Deeps|N|Camp Murroch|U|37586|
+A Candy Bucket|QID|84570|M|63.40,78.97|Z|2214;The Ringing Deeps|N|Opportunity Point|U|37586|
+
+;Hallowfall
+A Candy Bucket|QID|84572|M|69.06,45.7|Z|2215;Hallowfall|N|Dunelle's Kindness|U|37586|
+A Candy Bucket|QID|84573|M|49.13,39.54|Z|2215;Hallowfall|N|Lorel's Crossing|U|37586|
+A Candy Bucket|QID|84575|M|42.76,55.71|Z|2215;Hallowfall|N|Mereldar|U|37586|
+A Candy Bucket|QID|84574|M|40.58,67.98|Z|2215;Hallowfall|N|Light's Redoubt|U|37586|
+
+;Azj-Kahet
+A Candy Bucket|QID|84579|M|58.96,19.65|Z|2255;Azj-Kahet|N|Faerin's Advance|U|37586|
+A Candy Bucket|QID|84582|M|56.85,38.99|Z|2255;Azj-Kahet|N|Weaver's Lair|U|37586|
+A Candy Bucket|QID|84581|M|44.84,66.27|Z|2255;Azj-Kahet|N|Wildcamp Ul'ar|U|37586|
+A Candy Bucket|QID|84580|M|77.96,62.80|Z|2255;Azj-Kahet|N|Mmarl|U|37586|
+
+;City of Threads - Lower
+A Candy Bucket|QID|84578|M|49.75,22.27|Z|2216;City of Threads - Lower|N|Umbral Bazzar|U|37586|
+A Candy Bucket|QID|84576|M|62.08,41.38|Z|2216;City of Threads - Lower|N|High Hollows|U|37586|
+A Candy Bucket|QID|84577|M|57.42,38.47|Z|2216;City of Threads - Lower|N|Lower, The Burrows|U|37586|
+
+
+N This completes Hallow's End for Khaz Algar
+
+]]
+end)
