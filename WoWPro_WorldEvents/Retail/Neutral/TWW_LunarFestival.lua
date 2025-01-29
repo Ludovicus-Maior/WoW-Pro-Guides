f33fdfa5e99c67a0b6f4cc3ceb0bb479dd17f1a3
Cagomei <sshort8503@hotmail.com>
2025-01-28 21:55:11 -0600

    OOps
    stuff sitting on PC since my suspension
    
    TWW LunarFest

---
+++ WoWPro_WorldEvents/Retail/Neutral/TWW_LunarFestival.lua
@@ -0,0 +1,19 @@
+local guide = WoWPro:RegisterGuide("TWW_LunFesEld",'WorldEvents',"Khaz-Algar Lunar Festival Elders", "WoWPro Team", "Neutral")
+WoWPro.WorldEvents:GuideHoliday(guide,"TWW Lunar Festival")
+WoWPro:GuideLevels(guide)
+WoWPro:GuideSteps(guide, function()
+return [[
+
+N Thanks Wowhead|N|Many thanks to the Wowhead Community for posting lovely summaries of how to do this.|
+
+A Archivist Rubbleglint|QID|85929|M|40.9,87.4|Z|2248; Isle of Dorn|N|From Archivist Rubbleglint.|
+A Archivist Coppermoss|QID|85930|M|48.6,14.0|Z|2248; Isle of Dorn|N|From Archivist Coppermoss.|
+A Archivist Farolt|QID|85931|M|48.9,673.|Z|2214|N|From Archivist Farolt.|
+A Archivist Silsigra|QID|85932|M|67.3,53.0|Z|2214|N|From Archivist Silsigra.|
+A Elder Knythall|QID|85933|M|25.9,51.9|Z|2215|N|From Elder Knythall.|
+A Elder Swornvow|QID|85934|M|66.3,46.3|Z|2215|N|From Elder Swornvow.|
+A Elder Ikk'zivan|QID|85935|M|66.9,85.5|Z|2255|N|From Elder Ikk'zivan.|
+A Elder Ikk'xataz|QID|85936|M|48.6,57.2|Z|2255|N|From Elder Ikk'xataz.|
+
+]]
+end)
