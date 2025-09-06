82e8b0d62c34c031b28022a9509ad18808d7dd0c
Road-block <dridzt.addons@hotmail.com>
2025-09-06 20:10:28 +0300

    avoid creating a new andhor frame every time we show a dropdown (or call a funcion that initialises one) centralize menu anchor management to a single place (compatutil) to limit code duplication and easier change management simplifly ShowGuideMenu

--- WoWPro/WoWPro_Frames.lua
+++ WoWPro/WoWPro_Frames.lua
@@ -48,14 +48,13 @@ function WoWPro:ResizeSet()
 end
 function WoWPro:DragSet()
     -- Drag Customization --
-    local menuFrame = _G.CreateFrame("Frame", "WoWProDropMenu", _G.UIParent)
     if WoWProDB.profile.drag then
         WoWPro.Titlebar:SetScript("OnMouseDown", function(this, button)
             if button == "LeftButton" and WoWProDB.profile.drag then
                 WoWPro.InhibitAnchorRestore = true
                 WoWPro.MainFrame:StartMoving()
             elseif button == "RightButton" then
-                WoWPro.EasyMenu(WoWPro.DropdownMenu, menuFrame, "cursor", 0 , 0, "MENU");
+                WoWPro.EasyMenu(WoWPro.DropdownMenu, this, "cursor", 0 , 0, "MENU");
             end
         end)
         WoWPro.Titlebar:SetScript("OnMouseUp", function(this, button)
@@ -69,7 +68,7 @@ function WoWPro:DragSet()
     else
         WoWPro.Titlebar:SetScript("OnMouseDown", function(this, button)
             if button == "RightButton" then
-                WoWPro.EasyMenu(WoWPro.DropdownMenu, menuFrame, "cursor", 0 , 0, "MENU")
+                WoWPro.EasyMenu(WoWPro.DropdownMenu, this, "cursor", 0 , 0, "MENU")
             end
         end)
         WoWPro.Titlebar:SetScript("OnMouseUp", function(this, button)
@@ -454,15 +453,14 @@ function WoWPro:CreateMainFrame()
     frame:SetPoint("TOPLEFT", _G.UIParent, "RIGHT", -210, 175)
     frame:EnableMouseWheel()
     WoWPro.MainFrame = frame
-    -- Menu --
-    local menuFrame = _G.CreateFrame("Frame", "WoWProDropMenu", _G.UIParent)
+
     -- Scripts --
     WoWPro.MainFrame:SetScript("OnMouseDown", function(this, button)
         if button == "LeftButton" and WoWProDB.profile.drag then
             WoWPro.InhibitAnchorRestore = true
             this:StartMoving()
         elseif button == "RightButton" then
-            WoWPro.EasyMenu(WoWPro.DropdownMenu, menuFrame, "cursor", 0 , 0, "MENU");
+            WoWPro.EasyMenu(WoWPro.DropdownMenu, this, "cursor", 0 , 0, "MENU");
         end
     end)
     WoWPro.MainFrame:SetScript("OnMouseUp", function(this, button)
@@ -573,10 +571,9 @@ end)
     WoWPro.TitleText = titletext
 
     -- Scripts --
-    local menuFrame = _G.CreateFrame("Frame", "WoWProDropMenu", _G.UIParent)
     titleicon:SetScript("OnMouseDown", function(this, button)
         if button == "RightButton" then
-            WoWPro.EasyMenu(WoWPro.DropdownMenu, menuFrame, "cursor", 0 , 0, "MENU");
+            WoWPro.EasyMenu(WoWPro.DropdownMenu, this, "cursor", 0 , 0, "MENU");
         end
     end)
     WoWPro.Titlebar:SetScript("OnMouseUp", function(this, button)
@@ -865,7 +862,7 @@ function WoWPro:CreateNextGuideDialog()
     button2text:SetText("Choose Guide From List")
     button2text:SetTextColor(1, 1, 1)
     button2:SetScript("OnClick", function(this, button)
-        WoWPro.ShowGuideMenu()
+        WoWPro.ShowGuideMenu(this)
         WoWPro.NextGuideDialog:Hide()
     end)
 
