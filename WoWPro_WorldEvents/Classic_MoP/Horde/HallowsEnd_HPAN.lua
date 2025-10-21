a9ba62b577d8e7b0d4951169f067859317ce1864
Hendo72 <hendo72@gmail.com>
2025-10-21 12:39:23 -0400

    Hallow's End
    These are the changes I've got so far.

--- WoWPro_WorldEvents/Classic_MoP/Horde/HallowsEnd_HPAN.lua
+++ WoWPro_WorldEvents/Classic_MoP/Horde/HallowsEnd_HPAN.lua
@@ -7,6 +7,7 @@ return [[
 
 N Wall of Text Part 1 of 2|QID|32050|N|This guide is geared toward actually completing all the Candy Buckets necessary for the Hollows End achievments "Tricks and Treats of...".|
 N Wall of Text Part 2 of 2|QID|32050|N|Also to note, you can get multiples of things like the Hallowed Helm (Unique) and Sinister Squashling(not unique). So be prepared do delete a lot treat bags, keep one Hallowed Helm in your inventory and that's it. It's easier to delete the bag, than it is to delete the helm.|
+U Handful of Treats|M|PLAYER|CC|N|Open your Handful of Candy.|U|37586|S!US|O| ; ** This will appear whenever they have one in their bag. - Hendo72
 
 R The Jade Forest|QID|32050|M|68.82,40.24|Z|Orgrimmar|N|Take your portal to The Jade Forest.|
 
@@ -14,72 +15,72 @@ A Candy Bucket|QID|32050|M|28.45,13.27|Z|The Jade Forest|N|Brewmother Kiki, Hone
 F Grookin Hill|QID|32028|M|28.1,15.6|Z|The Jade Forest|N|Ask Wing Hya to go to Grookin Hill|
 
 A Candy Bucket|QID|32028|M|28.00,47.40|Z|The Jade Forest|N|Grookin Bed-Haver, Grookin Hill|FACTION|Horde|
-F Jade Temple Grounds|QID|32032|M|28.00,47.40|Z|The Jade Forest|N|Ask Grookin Flapmaster to go to Jade Temple Grounds|U|37586|PRE|29936|
+F Jade Temple Grounds|QID|32032|M|28.00,47.40|Z|The Jade Forest|N|Ask Grookin Flapmaster to go to Jade Temple Grounds|PRE|29936|
 R Jade Temple Grounds|QID|32032|M|54.61,63.33|Z|The Jade Forest|N|Mount up and fly, no flightmaster for you since you haven't quested here.|AVAILABLE|29936|
 
 A Candy Bucket|QID|32032|M|54.61,63.33|Z|The Jade Forest|N|Fela Woodear, Jade Temple Grounds.|
-F Dawn's Blossom|QID|32027|M|54.57,61.76|Z|The Jade Forest|N|Ask Ginsa Arroweye to go to  Dawn's Blossom|U|37586|
+F Dawn's Blossom|QID|32027|M|54.57,61.76|Z|The Jade Forest|N|Ask Ginsa Arroweye to go to  Dawn's Blossom|
 
 A Candy Bucket|QID|32027|M|45.77,43.60|Z|The Jade Forest|N|Peiji Goldendraft, Dawn's Blossom|
-F Emperor's Omen|QID|32029|M|47.05,46.24|Z|The Jade Forest|N|Ask Keg Runner Lee to go to Emperor's Omen|U|37586|
+F Emperor's Omen|QID|32029|M|47.05,46.24|Z|The Jade Forest|N|Ask Keg Runner Lee to go to Emperor's Omen|
 
 A Candy Bucket|QID|32029|M|48.09,34.62|Z|The Jade Forest|N|Graceful Swan, Greenstone Village.|
-F Sri-La Village|QID|32031|M|50.82,26.80|Z|The Jade Forest|N|Ask Suppiler Towsa to send you to Sri-La Village|U|37586|
+F Sri-La Village|QID|32031|M|50.82,26.80|Z|The Jade Forest|N|Ask Suppiler Towsa to send you to Sri-La Village|
 
 A Candy Bucket|QID|32031|M|55.72,24.40|Z|The Jade Forest|N|Lana the Sea Breeze, Sri-La Village|
-F Tian Monastery|QID|32021|M|55.38,23.73|Z|The Jade Forest|N|Ask Gingo Alebottom to go to Tian Monastery|U|37586|
+F Tian Monastery|QID|32021|M|55.38,23.73|Z|The Jade Forest|N|Ask Gingo Alebottom to go to Tian Monastery|
 
 A Candy Bucket|QID|32021|M|41.68,23.14|Z|The Jade Forest|N|Bolo the Elder, Tian Monastery.|
-F Pang's Stead|QID|32048|M|43.58,24.53|Z|The Jade Forest|N|Ask Studious Chu to go to Pang's Stead|U|37586|
+F Pang's Stead|QID|32048|M|43.58,24.53|Z|The Jade Forest|N|Ask Studious Chu to go to Pang's Stead|
 
 A Candy Bucket|QID|32048|M|83.64,20.14|Z|Valley of the Four Winds|N|Nan Thunderfoot, Pang's Stead|
-F Stoneplow|QID|32046|M|84.50,21.06|Z|Valley of the Four Winds|N|Ask Princeton to go to Stoneplow|U|37586|
+F Stoneplow|QID|32046|M|84.50,21.06|Z|Valley of the Four Winds|N|Ask Princeton to go to Stoneplow|
 
 A Candy Bucket|QID|32046|M|19.87,55.79|Z|Valley of the Four Winds|N|Nan the Mason Mug, Stoneplow (Phasing Issues?)|
-F Marista|QID|32034|M|20.31,58.67|Z|Valley of the Four Winds|N|Ask "Dragonwing Dan" to go to Marista|U|37586|
+F Marista|QID|32034|M|20.31,58.67|Z|Valley of the Four Winds|N|Ask "Dragonwing Dan" to go to Marista|
 
 A Candy Bucket|QID|32034|M|51.42,77.25|Z|Krasarang Wilds|N|Cranfur the Noodler, Marista|
-F Dawnchaser Retreat|QID|32020|M|52.48,76.60|Z|Krasarang Wilds|N|Ask Nan-Po to go to Dawnchaser Retreat.|U|37586|
+F Dawnchaser Retreat|QID|32020|M|52.48,76.60|Z|Krasarang Wilds|N|Ask Nan-Po to go to Dawnchaser Retreat.|
 
 A Candy Bucket|QID|32020|M|28.25,50.74|Z|Krasarang Wilds|N|Aizra Dawnchaser, Dawnchaser Retreat|
-F Thunder Cleft|QID|32047|M|28.99,50.32|Z|Krasarang Wilds|N|Ask Munch Windhoof to go to Thunder Cleft.|U|37586|
+F Thunder Cleft|QID|32047|M|28.99,50.32|Z|Krasarang Wilds|N|Ask Munch Windhoof to go to Thunder Cleft.|
 
 A Candy Bucket|QID|32047|M|61.01,25.15|Z|Krasarang Wilds|N|Kosta Dawnchaser, Thunder Cleft|
-F Zhu's Watch|QID|32036|M|59.18,24.71|Z|Krasarang Wilds|N|Ask Lira Skysplitter to go to Zhu's Watch.|U|37586|
+F Zhu's Watch|QID|32036|M|59.18,24.71|Z|Krasarang Wilds|N|Ask Lira Skysplitter to go to Zhu's Watch.|
 
 A Candy Bucket|QID|32036|M|75.93,6.87|Z|Krasarang Wilds|N|Rude Sho, Zhu's Watch|
-F Tavern in the Mists|QID|32026|M|76.75,8.37|Z|Krasarang Wilds|N|Ask Gee Hung to go to Tavern in the Mists|U|37586|
+F Tavern in the Mists|QID|32026|M|76.75,8.37|Z|Krasarang Wilds|N|Ask Gee Hung to go to Tavern in the Mists|
 
 A Candy Bucket|QID|32026|M|55.09,72.25|Z|The Veiled Stair|N|Tong the Fixer, Tavern in the Mists|
-F Binan Village|QID|32039|M|56.74,75.75|Z|The Veiled Stair|N|Ask Shin the Weightless to go to Binan Village|U|37586|
+F Binan Village|QID|32039|M|56.74,75.75|Z|The Veiled Stair|N|Ask Shin the Weightless to go to Binan Village|
 
 A Candy Bucket|QID|32039|M|72.73,92.28|Z|Kun-Lai Summit|N|Puli the Even Handed, Binan Village|
-F Eastwind Rest|QID|32040|M|72.54,94.18|Z|Kun-Lai Summit|N|Ask Jo the Wind Watcher to go to Eastwind Rest|U|37586|PRE|30513|
+F Eastwind Rest|QID|32040|M|72.54,94.18|Z|Kun-Lai Summit|N|Ask Jo the Wind Watcher to go to Eastwind Rest|PRE|30513|
 
 A Candy Bucket|QID|32040|M|62.77,80.50|Z|Kun-Lai Summit|N|Mai the Sleepy, Eastwind Rest|FACTION|Horde|PRE|30513|
-F Temple of the White Tiger|QID|32041|M|62.4,80.6|Z|Kun-Lai Summit|N|Ask Soaring Paw to go to Temple of the White Tiger|U|37586|PRE|30513|
-F Temple of the White Tiger|QID|32041|M|72.54,94.18|Z|Kun-Lai Summit|N|Ask Jo the Wind Watcher to go to Temple of the White Tiger|U|37586|AVAILABLE|30513|
+F Temple of the White Tiger|QID|32041|M|62.4,80.6|Z|Kun-Lai Summit|N|Ask Soaring Paw to go to Temple of the White Tiger|PRE|30513|
+F Temple of the White Tiger|QID|32041|M|72.54,94.18|Z|Kun-Lai Summit|N|Ask Jo the Wind Watcher to go to Temple of the White Tiger|AVAILABLE|30513|
 
 A Candy Bucket|QID|32041|M|64.21,61.28|Z|Kun-Lai Summit|N|Li Goldendraft, The Grummle Bazaar|
-F One Keg|QID|32037|M|66.31,50.67|Z|Kun-Lai Summit|N|Ask Gig Greenfeather to go to One Keg|U|37586|
+F One Keg|QID|32037|M|66.31,50.67|Z|Kun-Lai Summit|N|Ask Gig Greenfeather to go to One Keg|
 
 A Candy Bucket|QID|32037|M|57.45,59.94|Z|Kun-Lai Summit|N|Chiyo Mistpaw, One Keg|
-F Zouchin Village|QID|32051|M|57.73,59.69|Z|Kun-Lai Summit|N|Ask Little Cleankite to go to Zouchin Village|U|37586|
+F Zouchin Village|QID|32051|M|57.73,59.69|Z|Kun-Lai Summit|N|Ask Little Cleankite to go to Zouchin Village|
 
 A Candy Bucket|QID|32051|M|62.50,28.91|Z|Kun-Lai Summit|N|Liu Ze, Zouchin Village |
-F Longying Outpost|QID|32043|M|62.42,30.12|Z|Kun-Lai Summit|N|Ask Bo the Wind Claimer to go to Longying Outpost in Townlong Steppes|U|37586|
+F Longying Outpost|QID|32043|M|62.42,30.12|Z|Kun-Lai Summit|N|Ask Bo the Wind Claimer to go to Longying Outpost in Townlong Steppes|
 
 A Candy Bucket|QID|32043|M|71.13,57.80|Z|Townlong Steppes|N|Saito the Sleeping Shadow, Longying Outpost|
-F Klaxxi'vess|QID|32024|M|71.08,57.31|Z|Townlong Steppes|N|Ask Kit Master Wong to go to Klaxxi'vess in Dread Wastes|U|37586|
+F Klaxxi'vess|QID|32024|M|71.08,57.31|Z|Townlong Steppes|N|Ask Kit Master Wong to go to Klaxxi'vess in Dread Wastes|
 
 A Candy Bucket|QID|32024|M|55.93,32.27|Z|Dread Wastes|N|Zit'tix, Klaxxi'vess|
-F Soggy's Gamble|QID|32023|M|55.83,34.87|Z|Dread Wastes|N|Ask Kik'Tik to go to Soggy's Gamble|U|37586|
+F Soggy's Gamble|QID|32023|M|55.83,34.87|Z|Dread Wastes|N|Ask Kik'Tik to go to Soggy's Gamble|
 
 A Candy Bucket|QID|32023|M|55.21,71.20|Z|Dread Wastes|N|San the Sea Calmer, Soggy's Gamble |
-F Shrine of Two Moons|QID|32022|M|56.10,70.18|Z|Dread Wastes|N|Ask Min the Breeze Rider to go to Shrine of Two Moons|U|37586|
+F Shrine of Two Moons|QID|32022|M|56.10,70.18|Z|Dread Wastes|N|Ask Min the Breeze Rider to go to Shrine of Two Moons|
 
 A Candy Bucket|QID|32022|M|58.22,77.68|Z|Vale of Eternal Blossoms|N|Brewmaster Skye, Shrine of Two Moons.|
-F Serpent's Spine|QID|32044|M|84.62,62.42|Z|Vale of Eternal Blossoms|N|Ask Sharinga Springrunner to go to Serpent's Spine|U|37586|
+F Serpent's Spine|QID|32044|M|84.62,62.42|Z|Vale of Eternal Blossoms|N|Ask Sharinga Springrunner to go to Serpent's Spine|
 
 A Candy Bucket|QID|32044|M|35.15,77.75|Z|Vale of Eternal Blossoms|N|Bartender Tomro, Mistfall Village|
 N You made it!|N|Done with MoP|
--- WoWPro_WorldEvents/Classic_MoP/Horde/HallowsEnd_KAL.lua
+++ WoWPro_WorldEvents/Classic_MoP/Horde/HallowsEnd_KAL.lua
@@ -5,86 +5,87 @@ WoWPro:GuideNickname(guide, "Hallow's End (Kalimdor)")
 WoWPro:GuideName(guide,"Hallow's End (Kalimdor)")
 WoWPro:GuideSteps(guide, function()
 return [[
-N Wall of Text Part 1 of 2|QID|12366|N|This guide is geared toward actually completing all the Candy Buckets necessary for the Hollows End achievments "Tricks and Treats of...".|
+N Wall of Text Part 1 of 2|QID|12366|N|This guide is geared toward completing all the Candy Buckets necessary for the Hallow's End achievements "Tricks and Treats of...".|
 N Wall of Text Part 2 of 2|QID|12366|N|Also to note, you can get multiples of things like the Hallowed Helm (Unique) and Sinister Squashling(not unique). So be prepared do delete a lot treat bags, keep one Hallowed Helm in your inventory and that's it. It's easier to delete the bag, than it is to delete the helm.|
+U Handful of Treats|M|PLAYER|CC|N|Open your Handful of Candy.|U|37586|S!US|O| ; ** This will appear whenever they have one in their bag. - Hendo72
 
-N This guide starts|QID|12366|N|in Orgrimmar, so if you are in the Eastern Kingdoms, take the blimp and transport to Orgrimmar.|
-A Candy Bucket|QID|12366|M|53.90,78.94|Z|Orgrimmar|N|Innkeeper Gryshka, The Valley of Strength|
-
-F Razor Hill|QID|12361|M|49.65,59.17|Z|Orgrimmar|U|37586|N|Fly to Razor Hill, Durotar.|
-A Candy Bucket|QID|12361|M|51.54,41.59|Z|Durotar|N|Innkeeper Grosk, Razor Hill|
-
-F Nozzlepot's Outpost|QID|29003|M|53.09,43.57|Z|Durotar|U|37586|N|Fly to Nozzlepot's Outpost, Northern Barrens.|
-A Candy Bucket|QID|29003|M|62.51,16.60|Z|Northern Barrens|N|Innkeeper Kritzle, Nozzlepot's Outpost|
-
-F Splintertree Post|QID|12377|M|62.31,17.11|Z|Northern Barrens|U|37586|N|Fly to Splintertree Post, Ashenvale.|
-A Candy Bucket|QID|12377|M|73.96,60.60|Ashenvale|N|Innkeeper Kaylisk, Splintertree Post|
-
-F Bilgewater Harbor|QID|28992|M|73.18,61.59|Z|Ashenvale|U|37586|N|Fly to Bilgewater Harbor, Azshara.|
-A Candy Bucket|QID|28992|M|57.10,50.17|Z|Azshara|N|Mixi, Bilgewater Harbor|
-
-F Everlook|QID|12400|M|52.92,49.85|Z|Azshara|U|37586|N|Fly to Everlook, Winterspring.|
-A Candy Bucket|QID|12400|M|59.83,51.21|Z|Winterspring|N|Innkeeper Vizzie, Everlook|
-
-F Whisperwind Grove|QID|28994|N|Fly to Whisperwind Grove.|M|58.84,48.26|Z|Winterspring|U|37586|
-A Candy Bucket|QID|28994|M|44.58,28.99|Z|Felwood|N|Innkeeper Wylaria, Whisperwind Grove|
-
-F Zoram'gar Outpost|QID|28989|M|43.58,28.69|U|37586|Z|Felwood|N|Fly to Zoram'gar Outpost, Ashenvale.|
-A Candy Bucket|QID|28989|M|12.99,34.10|Z|Ashenvale|N|Innkeeper Duras, Zoram'gar Outpost|
-F Hellscream's Watch|QID|28958|M|11.16,34.43|U|37586|Z|Ashenvale|N|Fly to Hellscream's Watch, Ashenvale.|
-A Candy Bucket|QID|28958|M|38.65,42.35|Z|Ashenvale|N|Innkeeper Linkasa, Hellscream's Watch|
-F Silverwind Refuge|QID|28953|M|38.08,42.22|U|37586|Z|Ashenvale|N|Fly to Silverwind Refuge, Ashenvale.|
-A Candy Bucket|QID|28953|M|50.25,67.26|Z|Ashenvale|N|Innkeeper Chin'toka, Silverwind Refuge|
-
-F Ratchet|QID|12396|M|49.29,65.25|U|37586|Z|Ashenvale|N|Fly to Ratchet, Northern Barrens.|
-A Candy Bucket|QID|12396|M|67.33,74.66|Z|Northern Barrens|N|Innkeeper Wiley, Ratchet|
-F The Crossroads|QID|12374|M|69.13,70.70|U|37586|Z|Northern Barrens|N|Fly to The Crossroads, Northern Barrens.|
-A Candy Bucket|QID|12374|M|49.51,57.91|Z|Northern Barrens|N|Innkeeper Boorand Plainswind, The Crossroads|
-A Candy Bucket|QID|29002|M|56.21,40.03|Z|Northern Barrens|N|Innkeeper Kerntis, Grol'dom Farm|
-
-F Krom'gar Fortress|QID|29009|M|48.70,58.67|U|37586|Z|Northern Barrens|N|Fly to Krom'gar Fortress, Stonetalon Mountains.|
-A Candy Bucket|QID|29009|M|66.49,64.19|Z|Stonetalon Mountains|N|Felonius Stark, Krom'gar Fortress|
-F Sun Rock Retreat|QID|12378|M|66.53,62.79|U|37586|Z|Stonetalon Mountains|N|Fly to Sun Rock Retreat, Stonetalon Mountains.|
-A Candy Bucket|QID|12378|M|50.39,63.80|Z|Stonetalon Mountains|N|Innkeeper Jayka, Sun Rock Retreat|
-
-F Cenarion Wildlands|QID|28993|M|48.47,61.94|U|37586|Z|Stonetalon Mountains|N|Fly to Karnum's Glade, Desolace.|
-A Candy Bucket|QID|28993|M|56.73,50.12|Z|Desolace|N|Innkeeper Dessina, Cenarion Wildlands|
-F Shadowprey Village|QID|12381|M|57.71,49.75|U|37586|Z|Desolace|N|Fly to Shadowprey Village, Desolace.|
-A Candy Bucket|QID|12381|M|24.08,68.29|Z|Desolace|N|Innkeeper Sikewa, Shadowprey Village|
-
-F Camp Ataya|QID|28996|M|21.60,74.13|U|37586|Z|Desolace|N|Fly to Camp Ataya, Feralas.|
-A Candy Bucket|QID|28996|M|41.45,15.68|Z|Feralas|N|Adene Treetotem, Camp Ataya|
-F Stonemaul Hold|QID|28998|M|41.53,15.45|U|37586|Z|Feralas|N|Fly to Stonemaul Hold, Feralas.|
-A Candy Bucket|QID|28998|M|51.97,47.63|Z|Feralas|N|Chonk, Stonemaul Hold|
-F Camp Mojache|QID|12386|M|51.00,48.43|U|37586|Z|Feralas|N|Fly to Camp Mojache, Feralas.|
-A Candy Bucket|QID|12386|M|74.83,45.14|Z|Feralas|N|Innkeeper Greul, Camp Mojache|
-
-F Cenarion Hold|QID|12401|M|75.44,44.36|U|37586|Z|Feralas|N|Fly to Cenarion Hold, Silithus.|
-A Candy Bucket|QID|12401|M|55.5,36.7|Z|Silithus|N|Calandrath, Cenarion Hold|
-
-F Marshal's Stand|QID|29018|M|52.77,34.63|U|37586|Z|Silithus|N|Fly to Marshal's Stand, Un'Goro Crater.|
-A Candy Bucket|QID|29018|M|55.27,62.12|Z|Un'Goro Crater|N|Innkeeper Dreedle, Marshal's Stand|
-
-F Bootlegger Outpost|QID|29014|M|55.98,64.17|Z|Un'Goro Crater|U|37586|N|Fly to Bootlegger Outpost, Tanaris.|
-A Candy Bucket|QID|29014|M|55.7,60.96|Z|Tanaris|N| YYY, Bootlegger Outpost.|
-
-F Gadgetzan|QID|12399|M|55.88,60.6|Z|Tanaris|U|37586|N|Fly to Gadgetzan.
-A Candy Bucket|QID|12399|M|52.55,27.09|Z|Tanaris|N|Innkeeper Fizzgrimble, Gadgetzan|
-
-F Mudsprocket|QID|12398|M|52.04,27.61|U|37586|Z|Tanaris|N|Fly to Mudsprocket, Dustwallow Marsh|
-A Candy Bucket|QID|12398|M|41.86,74.09|Z|Dustwallow Marsh|N|Axle, Mudsprocket|
-F Brackenwall Village|QID|12383|M|42.83,72.43|U|37586|Z|Dustwallow Marsh|N|Fly to Brackenwall Village, Dustwallow Marsh|
-A Candy Bucket|QID|12383|M|36.78,32.43|Z|Dustwallow Marsh|N|“Little” Logok, Brackenwall Village|
-
-F Thunder Bluff|QID|12367|M|35.57,31.88|U|37586|Z|Dustwallow Marsh|N|Fly to Thunder Bluff.|
-A Candy Bucket|QID|12367|M|45.62,64.85|Z|Thunder Bluff|N|Innkeeper Pala, Lower Rise|
-F Bloodhoof Village|QID|12362|M|47.03,49.61|U|37586|Z|Thunder Bluff|N|Fly to Bloodhoof Village, Mulgore.|
-A Candy Bucket|QID|12362|M|46.79,60.41|Z|Mulgore|N|Innkeeper Kauth, Bloodhoof Village|
-
-F Hunter's Hill|QID|29004|M|47.44,58.64|U|37586|Z|Mulgore|N|Fly to Hunter's Hill, Southern Barrens.|
-A Candy Bucket|QID|29004|M|39.29,20.10|Z|Southern Barrens|N|Innkeeper Hurnahet, Hunter's Hill|
-F Desolation Hold|QID|29005|M|39.78,20.27|U|37586|Z|Southern Barrens|N|Fly to Desolation Hold, Southern Barrens.|
-A Candy Bucket|QID|29005|M|40.71,69.31|Z|Southern Barrens|N|Innkeeper Lhakadd, Desolation Hold|
+; ** Orgrimmar
+R Orgrimmar|QID|12366|N|This guide starts in Orgrimmar, so if you are in the Eastern Kingdoms, take the blimp and transport to Orgrimmar.|IZ|-0085; Orgrimmar|
+A Candy Bucket|QID|12366|M|53.90,78.94|Z|0085; Orgrimmar|N|Innkeeper Gryshka, The Valley of Strength.|
+; ** Durotar
+F Razor Hill|QID|12361|M|49.65,59.17|Z|0085; Orgrimmar|N|Fly to Razor Hill, Durotar.|FLY|OLD|
+A Candy Bucket|QID|12361|M|51.54,41.59|Z|0001; Durotar|N|Innkeeper Grosk, Razor Hill.|
+; ** Northern Barrens
+F Nozzlepot's Outpost|QID|29003|M|53.09,43.57|Z|0001; Durotar|N|Fly to Nozzlepot's Outpost, Northern Barrens.|FLY|OLD|
+A Candy Bucket|QID|29003|M|62.51,16.60|Z|0010; Northern Barrens|N|Innkeeper Kritzle, Nozzlepot's Outpost.|
+; ** Ashenvale
+F Splintertree Post|QID|12377|M|62.31,17.11|Z|0010; Northern Barrens|N|Fly to Splintertree Post, Ashenvale.|FLY|OLD|
+A Candy Bucket|QID|12377|M|73.96,60.60|Z|0063; Ashenvale|N|Innkeeper Kaylisk, Splintertree Post.|
+; ** Azshara
+F Bilgewater Harbor|QID|28992|M|73.18,61.59|Z|0063; Ashenvale|N|Fly to Bilgewater Harbor, Azshara.|
+A Candy Bucket|QID|28992|M|57.10,50.17|Z|0076; Azshara|N|Mixi, Bilgewater Harbor.|
+; ** Winterspring
+F Everlook|QID|12400|M|52.92,49.85|Z|0076; Azshara|N|Fly to Everlook, Winterspring.|
+A Candy Bucket|QID|12400|M|59.83,51.21|Z|0083; Winterspring|N|Innkeeper Vizzie, Everlook.|
+F Whisperwind Grove|QID|28994|M|58.84,48.26|Z|0083; Winterspring|N|Fly to Whisperwind Grove.|
+A Candy Bucket|QID|28994|M|44.58,28.99|Z|0077; Felwood|N|Innkeeper Wylaria, Whisperwind Grove.|
+; ** Ashenvale
+F Zoram'gar Outpost|QID|28989|M|43.58,28.69|Z|0077; Felwood|N|Fly to Zoram'gar Outpost, Ashenvale.|
+A Candy Bucket|QID|28989|M|12.99,34.10|Z|0063; Ashenvale|N|Innkeeper Duras, Zoram'gar Outpost.|
+F Hellscream's Watch|QID|28958|M|11.16,34.43|Z|0063; Ashenvale|N|Fly to Hellscream's Watch, Ashenvale.|FLY|OLD|
+A Candy Bucket|QID|28958|M|38.65,42.35|Z|0063; Ashenvale|N|Innkeeper Linkasa, Hellscream's Watch.|
+F Silverwind Refuge|QID|28953|M|38.08,42.22|Z|0063; Ashenvale|N|Fly to Silverwind Refuge, Ashenvale.|FLY|OLD|
+A Candy Bucket|QID|28953|M|50.25,67.26|Z|0063; Ashenvale|N|Innkeeper Chin'toka, Silverwind Refuge.|
+; ** Northern Barrens
+F Ratchet|QID|12396|M|49.29,65.25|Z|0063; Ashenvale|N|Fly to Ratchet, Northern Barrens.|
+A Candy Bucket|QID|12396|M|67.33,74.66|Z|0010; Northern Barrens|N|Innkeeper Wiley, Ratchet.|
+F The Crossroads|QID|12374|M|69.13,70.70|Z|0010; Northern Barrens|N|Fly to The Crossroads, Northern Barrens.|FLY|OLD|
+A Candy Bucket|QID|12374|M|49.51,57.91|Z|0010; Northern Barrens|N|Innkeeper Boorand Plainswind, The Crossroads.|
+A Candy Bucket|QID|29002|M|56.21,40.03|Z|0010; Northern Barrens|N|Innkeeper Kerntis, Grol'dom Farm.|
+; ** Stonetalon Mountains
+F Krom'gar Fortress|QID|29009|M|48.70,58.67|Z|0010; Northern Barrens|N|Fly to Krom'gar Fortress, Stonetalon Mountains.|FLY|OLD|
+A Candy Bucket|QID|29009|M|66.49,64.19|Z|0065; Stonetalon Mountains|N|Felonius Stark, Krom'gar Fortress.|
+F Sun Rock Retreat|QID|12378|M|66.53,62.79|Z|0065; Stonetalon Mountains|N|Fly to Sun Rock Retreat, Stonetalon Mountains.|FLY|OLD|
+A Candy Bucket|QID|12378|M|50.39,63.80|Z|0065; Stonetalon Mountains|N|Innkeeper Jayka, Sun Rock Retreat.|
+; ** Desolace
+F Cenarion Wildlands|QID|28993|M|48.47,61.94|Z|0065; Stonetalon Mountains|N|Fly to Karnum's Glade, Desolace.|FLY|OLD|
+A Candy Bucket|QID|28993|M|56.73,50.12|Z|0066; Desolace|N|Innkeeper Dessina, Cenarion Wildlands.|
+F Shadowprey Village|QID|12381|M|57.71,49.75|Z|0066; Desolace|N|Fly to Shadowprey Village, Desolace.|FLY|OLD|
+A Candy Bucket|QID|12381|M|24.08,68.29|Z|0066; Desolace|N|Innkeeper Sikewa, Shadowprey Village.|
+; ** Feralas
+F Camp Ataya|QID|28996|M|21.60,74.13|Z|0066; Desolace|N|Fly to Camp Ataya, Feralas.|FLY|OLD|
+A Candy Bucket|QID|28996|M|41.45,15.68|Z|0069;Feralas|N|Adene Treetotem, Camp Ataya.|
+F Stonemaul Hold|QID|28998|M|41.53,15.45|Z|0069;Feralas|N|Fly to Stonemaul Hold, Feralas.|FLY|OLD|
+A Candy Bucket|QID|28998|M|51.97,47.63|Z|0069;Feralas|N|Chonk, Stonemaul Hold.|
+F Camp Mojache|QID|12386|M|51.00,48.43|Z|0069;Feralas|N|Fly to Camp Mojache, Feralas.|FLY|OLD|
+A Candy Bucket|QID|12386|M|74.83,45.14|Z|0069;Feralas|N|Innkeeper Greul, Camp Mojache.|
+; ** Silithus
+F Cenarion Hold|QID|12401|M|75.44,44.36|Z|0069;Feralas|N|Fly to Cenarion Hold, Silithus.|FLY|OLD|
+A Candy Bucket|QID|12401|M|55.5,36.7|Z|0081; Silithus|N|Calandrath, Cenarion Hold.|
+; ** Un'Goro Crater
+F Marshal's Stand|QID|29018|M|52.77,34.63|Z|0081; Silithus|N|Fly to Marshal's Stand, Un'Goro Crater.|FLY|OLD|
+A Candy Bucket|QID|29018|M|55.27,62.12|Z|0078; Un'Goro Crater|N|Innkeeper Dreedle, Marshal's Stand.|
+; ** Tanaris
+F Bootlegger Outpost|QID|29014|M|55.98,64.17|Z|0078; Un'Goro Crater|N|Fly to Bootlegger Outpost, Tanaris.|FLY|OLD|
+A Candy Bucket|QID|29014|M|55.7,60.96|Z|0071;Tanaris|N| YYY, Bootlegger Outpost.|
+; ** Gadgetzan
+F Gadgetzan|QID|12399|M|55.88,60.6|Z|0071;Tanaris|N|Fly to Gadgetzan.|FLY|OLD|
+A Candy Bucket|QID|12399|M|52.55,27.09|Z|0071;Tanaris|N|Innkeeper Fizzgrimble, Gadgetzan.|
+; ** Dustwallow Marsh
+F Mudsprocket|QID|12398|M|52.04,27.61|Z|0071;Tanaris|N|Fly to Mudsprocket, Dustwallow Marsh.|
+A Candy Bucket|QID|12398|M|41.86,74.09|Z|0070; Dustwallow Marsh|N|Axle, Mudsprocket.|
+F Brackenwall Village|QID|12383|M|42.83,72.43|Z|0070; Dustwallow Marsh|N|Fly to Brackenwall Village, Dustwallow Marsh.|FLY|OLD|
+A Candy Bucket|QID|12383|M|36.78,32.43|Z|0070; Dustwallow Marsh|N|“Little” Logok, Brackenwall Village.|
+; ** Thunder Bluff
+F Thunder Bluff|QID|12367|M|35.57,31.88|Z|0070; Dustwallow Marsh|N|Fly to Thunder Bluff.|FLY|OLD|
+A Candy Bucket|QID|12367|M|45.62,64.85|Z|0088; Thunder Bluff|N|Innkeeper Pala, Lower Rise.|
+F Bloodhoof Village|QID|12362|M|47.03,49.61|Z|0088; Thunder Bluff|N|Fly to Bloodhoof Village, Mulgore.|FLY|OLD|
+A Candy Bucket|QID|12362|M|46.79,60.41|Z|0007; Mulgore|N|Innkeeper Kauth, Bloodhoof Village.|
+; ** Southern Barrens
+F Hunter's Hill|QID|29004|M|47.44,58.64|Z|0007; Mulgore|N|Fly to Hunter's Hill, Southern Barrens.|FLY|OLD|
+A Candy Bucket|QID|29004|M|39.29,20.10|Z|0199; Southern Barrens|N|Innkeeper Hurnahet, Hunter's Hill.|
+F Desolation Hold|QID|29005|M|39.78,20.27|Z|0199; Southern Barrens|N|Fly to Desolation Hold, Southern Barrens.|FLY|OLD|
+A Candy Bucket|QID|29005|M|40.71,69.31|Z|0199; Southern Barrens|N|Innkeeper Lhakadd, Desolation Hold.|
 
 N BioBreak|N|Take a break and pick the next guide to do.|
 
