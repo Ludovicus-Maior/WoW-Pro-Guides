ad170de7a671a08a4b37ae5640c67fa31812b252
Furydin <35685316+Furydins@users.noreply.github.com>
2025-12-03 20:44:45 +0000

    Mindight guides only load on 12.0.0 clients!

--- WoWPro_Leveling/Retail/Neutral/MN_Harandar.lua
+++ WoWPro_Leveling/Retail/Neutral/MN_Harandar.lua
@@ -1,4 +1,4 @@
-local guide = WoWPro:RegisterGuide('Harandar', 'Leveling', 'Harandar', 'WoWPro Team', 'Neutral')
+local guide = WoWPro:RegisterGuide('Harandar', 'Leveling', 'Harandar', 'WoWPro Team', 'Neutral',12)
 WoWPro:GuideName(guide,"Harandar")
 WoWPro:GuideContent(guide, 11)
 WoWPro:GuideLevels(guide,80, 90)
