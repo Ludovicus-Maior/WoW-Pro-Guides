2fc44a80a1bc3d0d98c02ec68f22db368d42ee3b
Cagomei <sshort8503@hotmail.com>
2025-12-05 09:43:25 -0600

    Update TWW_Recap.lua

--- WoWPro_Leveling/Retail/Neutral/TWW_Recap.lua
+++ WoWPro_Leveling/Retail/Neutral/TWW_Recap.lua
@@ -11,7 +11,7 @@ return [[
 ;A The War Within Recap|QID|93929|M|46.51,60.13|Z|2112; Valdrakken|N|From UI|
 T The War Within Recap|QID|93929|M|46.51,60.13|Z|2112; Valdrakken|N|To Tome of History|
 
-N This guide is a work in progress. Keep your eyes open on discord for updates. 
+N This guide is a work in progress. Keep your eyes open on discord for updates.
 
 ]]
 end)
