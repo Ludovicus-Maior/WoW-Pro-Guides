ebd5a4fb6d631617ca0fd57e2751b1053e1ab33d
Furydin <35685316+Furydins@users.noreply.github.com>
2026-02-20 17:08:54 +0000

    But....

--- WoWPro/WoWPro_Command.lua
+++ WoWPro/WoWPro_Command.lua
@@ -180,7 +180,7 @@ local function handler(msg, editbox)
         local type = tokens[2] and tokens[2]:lower()
         local mapID = _G.C_Map.GetBestMapForUnit("player")
 
-        local flightNodes = {}
+        local flightNodes
         if "destination" == type or "dest" == type then
             flightNodes = _G.C_TaxiMap.GetAllTaxiNodes(mapID)
         else
