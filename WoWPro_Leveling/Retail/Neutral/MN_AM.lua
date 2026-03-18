f752b465b9408222f51d85da160919335a465df1
Cagomei <sshort8503@hotmail.com>
2026-03-18 00:26:40 -0500

    s1

--- WoWPro_Leveling/Retail/Neutral/MN_AM.lua
+++ WoWPro_Leveling/Retail/Neutral/MN_AM.lua
@@ -3,6 +3,7 @@ WoWPro:GuideName(guide,"MN_AM")
 WoWPro:GuideSecret(guide)
 WoWPro:GuideContent(guide, 11)
 WoWPro:GuideLevels(guide,80, 90)
+WoWPro:GuideQuestTriggers(guide, 94993)
 WoWPro:GuideSort(guide, 1)
 WoWPro:GuideSteps(guide, function()
 return [[
@@ -11,17 +12,17 @@ A Adventuring in Midnight|QID|94993|PRE|86852|AVAILABLE|94871^86708^89402^89388^
 C Adventuring in Midnight|QID|94993|M|45.49,70.45|Z|2393; Silvermoon City|N|Pick a zone from the Scouting Map.|SPELL|World Quests & Adventure Mode;1234841|NC|
 T Adventuring in Midnight|QID|94993|M|PLAYER|Z|2393; Silvermoon City|N|UI Turn in.|SPELL|World Quests & Adventure Mode;1234841|NC|
 
-A Eversong Woods|QID|94871|PRE|94993|M|45.49,70.45|Z|2393; Silvermoon City|N|Pick this up from the scounting map.|
-A Zul'Aman|QID|86708|PRE|94993|M|45.49,70.45|Z|2393; Silvermoon City|N|Pick this up from the scounting map.|
-A Harandar|QID|89402|PRE|94993|M|45.49,70.45|Z|2393; Silvermoon City|N|Pick this up from the scounting map.|
-A VoidStorm|QID|91854|PRE|94993|M|45.49,70.45|Z|2393; Silvermoon City|N|Pick this up from the scounting map.|
-A Arator's Journey|QID|89193|PRE|94993|M|45.49,70.45|Z|2393; Silvermoon City|N|Pick this up from the scounting map.|
+;A Eversong Woods|QID|94871|PRE|94993|M|45.49,70.45|Z|2393; Silvermoon City|N|Pick this up from the scounting map.|
+;A Zul'Aman|QID|86708|PRE|94993|M|45.49,70.45|Z|2393; Silvermoon City|N|Pick this up from the scounting map.|
+;A Harandar|QID|89402|PRE|94993|M|45.49,70.45|Z|2393; Silvermoon City|N|Pick this up from the scounting map.|
+;A VoidStorm|QID|91854|PRE|94993|M|45.49,70.45|Z|2393; Silvermoon City|N|Pick this up from the scounting map.|
+;A Arator's Journey|QID|89193|PRE|94993|M|45.49,70.45|Z|2393; Silvermoon City|N|Pick this up from the scounting map.|
 
-N Eversong Woods|ACTIVE|94871|M|PLAYER|N|Click the book to jump to the Eversong Woods guide.|JUMP|Eversong Woods|S|
-N Zul'Aman|ACTIVE|86708|M|PLAYER|N|Click the book to jump to Zul'Aman guide.|JUMP|Zul'Aman|S|
-N Harandar|ACTIVE|89402|M|PLAYER|N|Click the book to jump to the Harandar guide.|JUMP|Harandar|S|
-N Void Storm|ACTIVE|91854|M|PLAYER|N|Click the book to jump to the Void Storm guide.|JUMP|Void Storm|S|
-N Arators Journey|ACTIVE|83552|M|PLAYER|N|Click the book to jump to the Arators Journey guide.|JUMP|Arators Journey|S|
+N Eversong Woods|QID|94871|M|PLAYER|N|Click the book to jump to the Eversong Woods guide.|JUMP|Eversong Woods|S|
+N Zul'Aman|QID|86708|M|PLAYER|N|Click the book to jump to Zul'Aman guide.|JUMP|Zul'Aman|S|
+N Harandar|QID|89402|M|PLAYER|N|Click the book to jump to the Harandar guide.|JUMP|Harandar|S|
+N Void Storm|QID|91854|M|PLAYER|N|Click the book to jump to the Void Storm guide.|JUMP|Void Storm|S|
+N Arators Journey|QID|83552|M|PLAYER|N|Click the book to jump to the Arators Journey guide.|JUMP|Arators Journey|S|
 
 ]]
 end)
